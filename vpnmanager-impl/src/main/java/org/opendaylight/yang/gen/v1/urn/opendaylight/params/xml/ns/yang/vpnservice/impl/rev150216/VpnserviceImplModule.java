0a00e953f7ba69a8fce3aee9fc30e39a84999f62
Deepthi V V <deepthi.v.v@ericsson.com>
2015-02-16 12:04:39 +0530

    Initial checkin for vpnservice.
    1. Added .gitreview
    2. Support karaf features and distribution for vpnservice
    3. VPN Manager support: POM, config-subsytem yang, vpnservice yang.
    
    Signed-off-by: Deepthi V V <deepthi.v.v@ericsson.com>
    Change-Id: I8b3b8726c8646922f768f0fc5bf6d792f026c077

---
+++ vpnmanager-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/vpnservice/impl/rev150216/VpnserviceImplModule.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vpnservice.impl.rev150216;
+
+import org.opendaylight.vpnservice.VpnserviceProvider;
+
+public class VpnserviceImplModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vpnservice.impl.rev150216.AbstractVpnserviceImplModule {
+    public VpnserviceImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public VpnserviceImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vpnservice.impl.rev150216.VpnserviceImplModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        VpnserviceProvider provider = new VpnserviceProvider();
+        getBrokerDependency().registerProvider(provider);
+        return provider;
+    }
+
+}
