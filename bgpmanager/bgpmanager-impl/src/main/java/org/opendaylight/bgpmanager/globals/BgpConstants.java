99ab6f0692202e46d82c7b705d10419277143bf6
Manisha Malla <manisha.malla@ericsson.com>
2015-04-21 12:28:41 +0530

    BgpManager commit for: - Bgp Router/Neighbor Listener logic - Bgp FIB DS Writes - Bgp API Provider
    Signed-off-by: Manisha Malla <manisha.malla@ericsson.com>
    Change-Id: I9229f92e018b64c8175e15bbef1e7b476a35b66f

--- bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/bgpmanager/globals/BgpConstants.java
+++ bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/bgpmanager/globals/BgpConstants.java
@@ -3,8 +3,8 @@ package org.opendaylight.bgpmanager.globals;
 
 public class BgpConstants {
 
-    public static final String BGP_SPEAKER_HOST_NAME = "bpg.speaker.host.name";
-    public static final String BGP_SPEAKER_THRIFT_PORT = "bgp.speaker.thrift.port";
+    public static final String BGP_SPEAKER_HOST_NAME = "vpnservice.bgpspeaker.host.name";
+    public static final String BGP_SPEAKER_THRIFT_PORT = "vpnservice.bgpspeaker.thrift.port";
     public static final String DEFAULT_BGP_HOST_NAME = "localhost";
     public static final int DEFAULT_BGP_THRIFT_PORT = 7644;
 
--- bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/bgpmanager/thrift/server/implementation/BgpUpdateHandler.java
+++ bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/bgpmanager/thrift/server/implementation/BgpUpdateHandler.java
@@ -1,35 +1,49 @@
 package org.opendaylight.bgpmanager.thrift.server.implementation;
 
+import org.opendaylight.bgpmanager.BgpManager;
+import org.opendaylight.bgpmanager.FibDSWriter;
 import org.opendaylight.bgpmanager.thrift.gen.BgpUpdater;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
-
 class BgpUpdateHandler implements BgpUpdater.Iface {
 
     private static final Logger logger = LoggerFactory.getLogger(BgpUpdateHandler.class);
+    private BgpManager bgpManager;
+    private FibDSWriter fibDSWriter;
 
-    public BgpUpdateHandler() {}
+    public BgpUpdateHandler(BgpManager bgpMgr, FibDSWriter dsWriter) {
+        bgpManager = bgpMgr;
+        fibDSWriter = dsWriter;
+
+        //Test
+        onUpdatePushRoute("5", "10.1.1.2", 32, "1.2.3.4", 200);
+        onUpdatePushRoute("5", "10.1.1.3", 32, "1.2.3.5", 400);
+        onUpdatePushRoute("10", "10.10.0.10", 32, "5.4.3.2", 600);
+        onUpdateWithdrawRoute("5", "10.1.1.3", 32);
+
+
+    }
 
     public void onUpdatePushRoute(String rd, String prefix, int plen,
                                 String nexthop, int label) {
+
        logger.info("Route add ** " + rd + " ** " + prefix + "/" + plen
                + " ** " + nexthop + " ** " + label);
         //Write to FIB in Data Store
+        fibDSWriter.addFibEntryToDS(rd, prefix + "/" + plen, nexthop, label);
 
    }
 
    public void onUpdateWithdrawRoute(String rd, String prefix, int plen) {
        logger.info("Route del ** " + rd + " ** " + prefix + "/" + plen);
-       //Write to FIB in Data Store
+       fibDSWriter.removeFibEntryFromDS(rd, prefix + "/" + plen);
 
    }
 
    public void onStartConfigResyncNotification() {
        logger.info("BGP (re)started");
-
-        //Reconfigure BGP
+       bgpManager.reInitConn();
    }
 
 }
