37de449905ced7d55006497e7dd8f6dafe72edb2
Vishal Thapar <vishal.thapar@ericsson.com>
2015-05-14 12:51:34 +0530

    interfacemgr: Add copyright wherever missing
    Change-Id: I84e20abed5fd3c6d730e141360248bfb53476b2d
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

--- interfacemgr/interfacemgr-impl/src/main/java/org/opendaylight/vpnservice/interfacemgr/IfmUtil.java
+++ interfacemgr/interfacemgr-impl/src/main/java/org/opendaylight/vpnservice/interfacemgr/IfmUtil.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2015 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.vpnservice.interfacemgr;
 
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.Interfaces;
