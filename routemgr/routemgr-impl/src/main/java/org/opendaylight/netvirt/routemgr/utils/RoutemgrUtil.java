df63fa65597a26be8267c06b1b4bb8baf146c66f
kalaiselvik <Kalaiselvi_K@Dell.com>
2016-04-19 16:23:29 +0530

    Processing the received NS packets and sending NA response
    Change-Id: I2993e691c017c4463650da6f91b757201e3a67ca
    Signed-off-by: kalaiselvik <Kalaiselvi_K@Dell.com>

---
+++ routemgr/routemgr-impl/src/main/java/org/opendaylight/netvirt/routemgr/utils/RoutemgrUtil.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2016 Dell Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.routemgr.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IetfInetUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv6Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.routemgr.nd.packet.rev160302.EthernetHeader;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.routemgr.nd.packet.rev160302.Ipv6Header;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RoutemgrUtil {
+    public static final int ICMPv6_TYPE = 58;
+    public static final short ICMPv6_RS_CODE = 133;
+    public static final short ICMPv6_RA_CODE = 134;
+    public static final short ICMPv6_NS_CODE = 135;
+    public static final short ICMPv6_NA_CODE = 136;
+    public static final short ICMPv6_MAX_HOP_LIMIT = 255;
+    public static final int ICMPV6_OFFSET = 54;
+    public static final RoutemgrUtil instance = new RoutemgrUtil();
+
+    private RoutemgrUtil() {
+    }
+
+    public static RoutemgrUtil getInstance() {
+        return instance;
+    }
+
+    public String bytesToHexString(byte[] bytes) {
+        if(bytes == null) {
+            return "null";
+        }
+        StringBuffer buf = new StringBuffer();
+        for(int i = 0; i < bytes.length; i++) {
+            if(i > 0) {
+                buf.append(":");
+            }
+            short u8byte = (short) (bytes[i] & 0xff);
+            String tmp = Integer.toHexString(u8byte);
+            if(tmp.length() == 1) {
+                buf.append("0");
+            }
+            buf.append(tmp);
+        }
+        return buf.toString();
+    }
+
+    public byte[] bytesFromHexString(String values) {
+        String target = "";
+        if(values != null) {
+            target = values;
+        }
+        String[] octets = target.split(":");
+
+        byte[] ret = new byte[octets.length];
+        for(int i = 0; i < octets.length; i++) {
+            ret[i] = Integer.valueOf(octets[i], 16).byteValue();
+        }
+        return ret;
+    }
+
+    public int calcIcmpv6Checksum(byte[] packet, Ipv6Header ip6Hdr) {
+        long checksum = getSummation(ip6Hdr.getSourceIpv6());
+        checksum += getSummation(ip6Hdr.getDestinationIpv6());
+        checksum = normalizeChecksum(checksum);
+
+        checksum += ip6Hdr.getIpv6Length();
+        checksum += ip6Hdr.getNextHeader();
+
+        int icmp6Offset = ICMPV6_OFFSET;
+        long value = (((packet[icmp6Offset] & 0xff) << 8) | (packet[icmp6Offset + 1] & 0xff));
+        checksum += value;
+        checksum = normalizeChecksum(checksum);
+        icmp6Offset += 2;
+
+        //move to icmp6 payload skipping the checksum field
+        icmp6Offset += 2;
+        int length = packet.length - icmp6Offset;
+        while (length > 1) {
+            value = (((packet[icmp6Offset] & 0xff) << 8) | (packet[icmp6Offset + 1] & 0xff));
+            checksum += value;
+            checksum = normalizeChecksum(checksum);
+            icmp6Offset += 2;
+            length -= 2;
+        }
+
+        if (length > 0) {
+            checksum += packet[icmp6Offset];
+            checksum = normalizeChecksum(checksum);
+        }
+
+        int cSum = (int)(~checksum & 0xffff);
+        return cSum;
+    }
+
+    public boolean validateChecksum(byte[] packet, Ipv6Header ip6Hdr, int recvChecksum) {
+        int checksum = calcIcmpv6Checksum(packet, ip6Hdr);
+
+        if (checksum == recvChecksum) {
+            return true;
+        }
+        return false;
+    }
+
+    private long getSummation(Ipv6Address addr) {
+        byte[] bAddr = null;
+        try {
+            bAddr = InetAddress.getByName(addr.getValue()).getAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        long sum = 0;
+        int i = 0;
+        long value = 0;
+        while (i < bAddr.length) {
+            value = (((bAddr[i] & 0xff) << 8) | (bAddr[i+1] & 0xff));
+            sum += value;
+            sum = normalizeChecksum(sum);
+            i+=2;
+        }
+        return sum;
+    }
+
+    private long normalizeChecksum(long value) {
+        if((value & 0xffff0000) > 0) {
+            value = (value & 0xffff);
+            value += 1;
+        }
+        return value;
+    }
+
+    public byte[] convertEthernetHeaderToByte(EthernetHeader ethPdu) {
+        byte[] data = new byte[16];
+        Arrays.fill(data, (byte)0);
+
+        ByteBuffer buf = ByteBuffer.wrap(data);
+        buf.put(bytesFromHexString(ethPdu.getDestinationMac().getValue().toString()));
+        buf.put(bytesFromHexString(ethPdu.getSourceMac().getValue().toString()));
+        buf.putShort((short)ethPdu.getEthertype().intValue());
+        return data;
+    }
+
+    public byte[] convertIpv6HeaderToByte(Ipv6Header ip6Pdu) {
+        byte[] data = new byte[128];
+        Arrays.fill(data, (byte)0);
+
+        ByteBuffer buf = ByteBuffer.wrap(data);
+        long fLabel = (((long)(ip6Pdu.getVersion().shortValue() & 0x0f) << 28)
+                | (ip6Pdu.getFlowLabel().longValue() & 0x0fffffff));
+        buf.putInt((int)fLabel);
+        buf.putShort((short)ip6Pdu.getIpv6Length().intValue());
+        buf.put((byte)ip6Pdu.getNextHeader().shortValue());
+        buf.put((byte)ip6Pdu.getHopLimit().shortValue());
+        buf.put(IetfInetUtil.INSTANCE.ipv6AddressBytes(ip6Pdu.getSourceIpv6()));
+        buf.put(IetfInetUtil.INSTANCE.ipv6AddressBytes(ip6Pdu.getDestinationIpv6()));
+        return data;
+    }
+}
