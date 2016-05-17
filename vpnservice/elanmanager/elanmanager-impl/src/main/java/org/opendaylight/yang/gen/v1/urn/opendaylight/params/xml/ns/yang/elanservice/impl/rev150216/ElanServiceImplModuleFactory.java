d231020ec28b8aadf663bd2eb2fa87f000591e25
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-05-17 10:49:21 -0400

    Add blueprint wiring for elanmanager
    Change-Id: I1274ef299469602cd8fa4722bc037e4e2785e1ea
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>
    Signed-off by: Alexis de Talhouët <alexis.de.t@gmail.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/elanservice/impl/rev150216/ElanServiceImplModuleFactory.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/elanservice/impl/rev150216/ElanServiceImplModuleFactory.java
@@ -6,6 +6,30 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.elanservice.impl.rev150216;
-public class ElanServiceImplModuleFactory extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.elanservice.impl.rev150216.AbstractElanServiceImplModuleFactory {
 
+import org.opendaylight.controller.config.api.DependencyResolver;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @deprecated Replaced by blueprint wiring
+ */
+@Deprecated
+public class ElanServiceImplModuleFactory extends AbstractElanServiceImplModuleFactory {
+
+    @Override
+    public ElanServiceImplModule instantiateModule(final String instanceName, final DependencyResolver dependencyResolver,
+            final ElanServiceImplModule oldModule, final AutoCloseable oldInstance, final BundleContext bundleContext) {
+        final ElanServiceImplModule module = super.instantiateModule(instanceName, dependencyResolver, oldModule,
+                oldInstance, bundleContext);
+        module.setBundleContext(bundleContext);
+        return module;
+    }
+
+    @Override
+    public ElanServiceImplModule instantiateModule(final String instanceName, final DependencyResolver dependencyResolver,
+            final BundleContext bundleContext) {
+        final ElanServiceImplModule module = super.instantiateModule(instanceName, dependencyResolver, bundleContext);
+        module.setBundleContext(bundleContext);
+        return module;
+    }
 }
