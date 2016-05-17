77bb9a164ac0ecaa3506b2514c90cefe201fc8b5
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-05-17 11:52:01 -0400

    Add blueprint wiring for fcapsapplication
    Change-Id: I23ef46969a72eb73c92c4459109caabecf4c44b7
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- vpnservice/fcapsapplication/fcapsapplication-impl/src/main/java/org/opendaylight/netvirt/fcapsapp/portinfo/PortNameMapping.java
+++ vpnservice/fcapsapplication/fcapsapplication-impl/src/main/java/org/opendaylight/netvirt/fcapsapp/portinfo/PortNameMapping.java
@@ -7,19 +7,29 @@
  */
 package org.opendaylight.netvirt.fcapsapp.portinfo;
 
-import java.lang.String;
+import java.lang.management.ManagementFactory;
 import java.util.HashMap;
 import java.util.Map;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.management.*;
-import java.lang.management.ManagementFactory;
 
 public class PortNameMapping implements PortNameMappingMBean {
 
     private static final Logger LOG = LoggerFactory.getLogger(PortNameMapping.class);
 
-    private static Map<String,String> portNameToPortIdMap = new HashMap<String,String>();
+    private Map<String,String> portNameToPortIdMap = new HashMap<String,String>();
+
+    private final MBeanServer mbs;
+    private ObjectName mbeanName;
+
+    public PortNameMapping() {
+        this.mbs = ManagementFactory.getPlatformMBeanServer();
+    }
 
     @Override
     public Map<String,String> getPortIdtoPortNameMap() {
@@ -31,19 +41,17 @@ public class PortNameMapping implements PortNameMappingMBean {
         return portNameToPortIdMap.get(portId);
     }
 
-    public static void updatePortMap(String portName,String portId,String status) {
-        if (status.equals("ADD")){
+    public void updatePortMap(String portName,String portId,String status) {
+        if (status.equals("ADD")) {
             portNameToPortIdMap.put(portId,portName);
             LOG.debug("PortId {} : portName {} added",portId,portName);
-        } else if(status.equals("DELETE")){
+        } else if(status.equals("DELETE")) {
             portNameToPortIdMap.remove(portId);
             LOG.debug("PortId {} : portName {} removed",portId,portName);
         }
     }
 
-    public static void registerPortMappingBean() {
-        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-        ObjectName mbeanName = null;
+    public void registerMBean() {
         String BEANNAME = "Ports:type=PortNameMapping";
 
         try {
@@ -60,6 +68,16 @@ public class PortNameMapping implements PortNameMappingMBean {
 
         } catch (Exception e) {
             LOG.error("Registeration failed for Mbean {} :{}", mbeanName, e);
+        }
+    }
+
+    public void unregisterMBean() {
+        try {
+            if (!mbs.isRegistered(mbeanName)) {
+                mbs.unregisterMBean(mbeanName);
+            }
+        } catch (MBeanRegistrationException | InstanceNotFoundException e) {
+            LOG.error("Failed to unregister Mbean :{}", e);
         }
     }
 }
