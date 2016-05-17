7684c710297e44f19b96d915c90a1a8fa8a627f3
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-05-17 15:56:46 -0400

    Add blueprint wiring for openstack/netvirt/providers
    Change-Id: If0d6691d9cd77666c65c2c4a2751acfbae660572
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- openstack/net-virt-providers/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/netvirt/providers/impl/rev150513/NetvirtProvidersImplModuleFactory.java
+++ openstack/net-virt-providers/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/netvirt/providers/impl/rev150513/NetvirtProvidersImplModuleFactory.java
@@ -9,41 +9,9 @@
 */
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netvirt.providers.impl.rev150513;
 
-import org.opendaylight.controller.config.api.DependencyResolver;
-import org.opendaylight.controller.config.api.DynamicMBeanWithInstance;
-import org.opendaylight.controller.config.spi.Module;
-import org.osgi.framework.BundleContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class NetvirtProvidersImplModuleFactory extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netvirt.providers.impl.rev150513.AbstractNetvirtProvidersImplModuleFactory {
-    private static final Logger LOG = LoggerFactory.getLogger(NetvirtProvidersImplModuleFactory.class);
-
-    @Override
-    public Module createModule(String instanceName,
-                               DependencyResolver dependencyResolver,
-                               DynamicMBeanWithInstance old, BundleContext bundleContext)
-            throws Exception {
-        Module module =  super.createModule(instanceName, dependencyResolver, old, bundleContext);
-        setModuleBundleContext(bundleContext, module);
-        return module;
-    }
-
-    @Override
-    public Module createModule(String instanceName,
-                               DependencyResolver dependencyResolver, BundleContext bundleContext) {
-        Module module = super.createModule(instanceName, dependencyResolver, bundleContext);
-        setModuleBundleContext(bundleContext, module);
-        return module;
-    }
-
-    private void setModuleBundleContext(BundleContext bundleContext,
-                                        Module module) {
-        if (module instanceof NetvirtProvidersImplModule) {
-            ((NetvirtProvidersImplModule)module).setBundleContext(bundleContext);
-        } else {
-            LOG.warn("Module is of type {} expected type {}",
-                    module.getClass(), NetvirtProvidersImplModule.class);
-        }
-    }
+/**
+ * @deprecated Replaced by blueprint wiring
+ */
+@Deprecated
+public class NetvirtProvidersImplModuleFactory extends AbstractNetvirtProvidersImplModuleFactory {
 }
