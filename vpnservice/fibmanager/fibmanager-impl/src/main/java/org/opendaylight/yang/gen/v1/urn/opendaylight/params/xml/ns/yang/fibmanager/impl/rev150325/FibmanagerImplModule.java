327ea885c588deeb4492881a695af6894668fea1
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-05-17 12:25:16 -0400

    Add blueprint wiring for fibmanager
    Change-Id: I3487b2db833a8183d8fb05af37de716a6482b9f2
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- vpnservice/fibmanager/fibmanager-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/fibmanager/impl/rev150325/FibmanagerImplModule.java
+++ vpnservice/fibmanager/fibmanager-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/fibmanager/impl/rev150325/FibmanagerImplModule.java
@@ -6,12 +6,12 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.fibmanager.impl.rev150325;
-import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
-import org.opendaylight.netvirt.fibmanager.FibManagerProvider;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.OdlInterfaceRpcService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rpcs.rev160406.ItmRpcService;
+import org.opendaylight.controller.sal.common.util.NoopAutoCloseable;
 
+/**
+ * @deprecated Replaced by blueprint wiring
+ */
+@Deprecated
 public class FibmanagerImplModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.fibmanager.impl.rev150325.AbstractFibmanagerImplModule {
     public FibmanagerImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
@@ -28,16 +28,8 @@ public class FibmanagerImplModule extends org.opendaylight.yang.gen.v1.urn.opend
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        FibManagerProvider provider = new FibManagerProvider();
-        RpcProviderRegistry rpcProviderRegistry = getRpcregistryDependency();
-        provider.setMdsalManager(getMdsalutilDependency());
-        provider.setVpnmanager(getVpnmanagerDependency());
-        provider.setIdManager(rpcProviderRegistry.getRpcService(IdManagerService.class));
-        provider.setInterfaceManager(rpcProviderRegistry.getRpcService(OdlInterfaceRpcService.class));
-        provider.setITMProvider(rpcProviderRegistry.getRpcService(ItmRpcService.class));
-        provider.setRpcProviderRegistry(rpcProviderRegistry);
-        getBrokerDependency().registerProvider(provider);
-        return provider;
+        // FibManagerProvider instance is created via blueprint so this in a no-op.
+        return NoopAutoCloseable.INSTANCE;
     }
 
 }
