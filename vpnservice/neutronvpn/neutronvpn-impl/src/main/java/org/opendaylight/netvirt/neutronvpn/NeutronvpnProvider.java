c05b6bd196518f805d49bdd96cead474419eda54
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-05-17 14:12:48 -0400

    Add blueprint wiring for neutronvpn
    Change-Id: Iae7df2847fa53a998eca062c87286f22c44fe82b
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnProvider.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnProvider.java
@@ -7,97 +7,92 @@
  */
 package org.opendaylight.netvirt.neutronvpn;
 
+import java.util.List;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.NotificationPublishService;
 import org.opendaylight.controller.md.sal.binding.api.NotificationService;
 import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipService;
-import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
-import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
-import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
 import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
 import org.opendaylight.netvirt.neutronvpn.interfaces.INeutronVpnManager;
 import org.opendaylight.netvirt.neutronvpn.l2gw.L2GatewayProvider;
-import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.lockmanager.rev160413.LockManagerService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.NeutronvpnService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.ports.Port;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.subnets.rev150712.subnets.attributes.subnets.Subnet;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.NeutronvpnService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+public class NeutronvpnProvider implements INeutronVpnManager, AutoCloseable {
 
-public class NeutronvpnProvider implements BindingAwareProvider, INeutronVpnManager, AutoCloseable {
-
-    private static final Logger LOG = LoggerFactory.getLogger(NeutronvpnProvider.class);
+    private static final Logger LOG = LoggerFactory
+            .getLogger(NeutronvpnProvider.class);
     private NeutronvpnManager nvManager;
     private NeutronvpnNatManager nvNatManager;
-    private IMdsalApiManager mdsalManager;
-    private LockManagerService lockManager;
+    private final IMdsalApiManager mdsalManager;
+    private final LockManagerService lockManager;
+    private NeutronSecurityRuleListener securityRuleListener;
     private NeutronBgpvpnChangeListener bgpvpnListener;
     private NeutronNetworkChangeListener networkListener;
     private NeutronSubnetChangeListener subnetListener;
     private NeutronRouterChangeListener routerListener;
     private NeutronPortChangeListener portListener;
-    private NeutronSecurityRuleListener securityRuleListener;
     private NeutronFloatingToFixedIpMappingChangeListener floatingIpMapListener;
-    private RpcProviderRegistry rpcProviderRegistry;
-    private L2GatewayProvider l2GatewayProvider;
-    private NotificationPublishService notificationPublishService;
-    private NotificationService notificationService;
-    private EntityOwnershipService entityOwnershipService;
 
-    public NeutronvpnProvider(RpcProviderRegistry rpcRegistry,NotificationPublishService notificationPublishService,
-                              NotificationService notificationService) {
+    private final RpcProviderRegistry rpcProviderRegistry;
+    private L2GatewayProvider l2GatewayProvider;
+    private final NotificationPublishService notificationPublishService;
+    private final NotificationService notificationService;
+    private final EntityOwnershipService entityOwnershipService;
+    private RpcRegistration<NeutronvpnService> rpcRegistration;
+    private final DataBroker dbx;
+
+    public NeutronvpnProvider(final DataBroker dataBroker,
+            final RpcProviderRegistry rpcRegistry,
+            final NotificationPublishService notificationPublishService,
+            final NotificationService notificationService,
+            final EntityOwnershipService eos,
+            final IMdsalApiManager mdsalApiManager) {
+        this.dbx = dataBroker;
         this.rpcProviderRegistry = rpcRegistry;
         this.notificationPublishService = notificationPublishService;
         this.notificationService = notificationService;
+        this.entityOwnershipService = eos;
+        this.mdsalManager = mdsalApiManager;
+
+        this.lockManager = rpcProviderRegistry
+                .getRpcService(LockManagerService.class);
     }
 
-    public RpcProviderRegistry getRpcProviderRegistry() {
-        return rpcProviderRegistry;
-    }
-
-    public void setRpcProviderRegistry(RpcProviderRegistry rpcProviderRegistry) {
-        this.rpcProviderRegistry = rpcProviderRegistry;
-    }
-
-    public void setMdsalManager(IMdsalApiManager mdsalManager) {
-        this.mdsalManager = mdsalManager;
-    }
-
-    public void setLockManager(LockManagerService lockManager) {
-        this.lockManager = lockManager;
-    }
-
-    public void setEntityOwnershipService(EntityOwnershipService entityOwnershipService) {
-        this.entityOwnershipService = entityOwnershipService;
-    }
-
-    @Override
-    public void onSessionInitiated(ProviderContext session) {
+    public void start() {
         try {
-            final DataBroker dbx = session.getSALService(DataBroker.class);
             nvNatManager = new NeutronvpnNatManager(dbx, mdsalManager);
-            nvManager = new NeutronvpnManager(dbx, mdsalManager,notificationPublishService,notificationService,
+            nvManager = new NeutronvpnManager(dbx, mdsalManager,
+                    notificationPublishService, notificationService,
                     nvNatManager);
-            final BindingAwareBroker.RpcRegistration<NeutronvpnService> rpcRegistration =
-                    getRpcProviderRegistry().addRpcImplementation(NeutronvpnService.class, nvManager);
+            rpcRegistration = rpcProviderRegistry
+                    .addRpcImplementation(NeutronvpnService.class, nvManager);
             bgpvpnListener = new NeutronBgpvpnChangeListener(dbx, nvManager);
-            networkListener = new NeutronNetworkChangeListener(dbx, nvManager, nvNatManager);
+            networkListener = new NeutronNetworkChangeListener(dbx, nvManager,
+                    nvNatManager);
             subnetListener = new NeutronSubnetChangeListener(dbx, nvManager);
-            routerListener = new NeutronRouterChangeListener(dbx, nvManager, nvNatManager);
-            portListener = new NeutronPortChangeListener(dbx, nvManager, nvNatManager,
-                    notificationPublishService,notificationService, floatingIpMapListener);
+            routerListener = new NeutronRouterChangeListener(dbx, nvManager,
+                    nvNatManager);
+            portListener = new NeutronPortChangeListener(dbx, nvManager,
+                    nvNatManager, notificationPublishService,
+                    notificationService, floatingIpMapListener);
             portListener.setLockManager(lockManager);
             portListener.setLockManager(lockManager);
-            floatingIpMapListener = new NeutronFloatingToFixedIpMappingChangeListener(dbx);
+            floatingIpMapListener = new NeutronFloatingToFixedIpMappingChangeListener(
+                    dbx);
             nvManager.setLockManager(lockManager);
             portListener.setLockManager(lockManager);
             floatingIpMapListener.setLockManager(lockManager);
-            l2GatewayProvider = new L2GatewayProvider(dbx, rpcProviderRegistry, entityOwnershipService);
+            l2GatewayProvider = new L2GatewayProvider(dbx, rpcProviderRegistry,
+                    entityOwnershipService);
             securityRuleListener = new NeutronSecurityRuleListener(dbx);
             securityRuleListener.registerListener();
             LOG.info("NeutronvpnProvider Session Initiated");
@@ -108,15 +103,36 @@ public class NeutronvpnProvider implements BindingAwareProvider, INeutronVpnMana
 
     @Override
     public void close() throws Exception {
-        portListener.close();
-        subnetListener.close();
-        routerListener.close();
-        networkListener.close();
-        floatingIpMapListener.close();
-        bgpvpnListener.close();
-        nvManager.close();
-        l2GatewayProvider.close();
-        securityRuleListener.close();
+        if (portListener != null) {
+            portListener.close();
+        }
+        if (subnetListener != null) {
+            subnetListener.close();
+        }
+        if (routerListener != null) {
+            routerListener.close();
+        }
+        if (networkListener != null) {
+            networkListener.close();
+        }
+        if (bgpvpnListener != null) {
+            bgpvpnListener.close();
+        }
+        if (nvManager != null) {
+            nvManager.close();
+        }
+        if (l2GatewayProvider != null) {
+            l2GatewayProvider.close();
+        }
+        if (rpcRegistration != null) {
+            rpcRegistration.close();
+        }
+        if (securityRuleListener != null) {
+            securityRuleListener.close();
+        }
+        if (floatingIpMapListener != null) {
+            floatingIpMapListener.close();
+        }
         LOG.info("NeutronvpnProvider Closed");
     }
 
@@ -169,5 +185,4 @@ public class NeutronvpnProvider implements BindingAwareProvider, INeutronVpnMana
     public IpAddress getNeutronSubnetGateway(Uuid subnetId) {
         return nvManager.getNeutronSubnetGateway(subnetId);
     }
-
 }
