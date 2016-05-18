7514225123c4ea90f162cba19ff491b2729370e1
Sam Hague <shague@redhat.com>
2016-05-18 16:53:36 -0400

    updates for sfc models
    Change-Id: Id0d28f2bfacffff7bb7f691615eb772ed1e0215f
    Signed-off-by: Sam Hague <shague@redhat.com>
    (cherry picked from commit d101b7946e550c51843651b2597238913813b1a2)

--- openstack/net-virt-sfc/it/src/test/java/org/opendaylight/netvirt/openstack/netvirt/sfc/it/utils/AbstractUtils.java
+++ openstack/net-virt-sfc/it/src/test/java/org/opendaylight/netvirt/openstack/netvirt/sfc/it/utils/AbstractUtils.java
@@ -9,7 +9,7 @@
 package org.opendaylight.netvirt.openstack.netvirt.sfc.it.utils;
 
 import java.util.List;
-import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.service.function.forwarder.sff.data.plane.locator.DataPlaneLocatorBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarder.base.sff.data.plane.locator.DataPlaneLocatorBuilder;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sl.rev140701.VxlanGpe;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sl.rev140701.data.plane.locator.locator.type.IpBuilder;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
