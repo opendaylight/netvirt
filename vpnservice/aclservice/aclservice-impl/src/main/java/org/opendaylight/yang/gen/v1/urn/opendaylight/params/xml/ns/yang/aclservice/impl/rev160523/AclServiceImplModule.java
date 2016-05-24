67df4458951e8be3d90f333f114aab8a8e2695a5
Shashidhar R <shashidhar.raja@ericsson.com>
2016-05-24 17:49:14 +0530

    Initial changes for ACL Service
    Signed-off-by: Shashidhar R <shashidhar.raja@ericsson.com>

---
+++ vpnservice/aclservice/aclservice-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/aclservice/impl/rev160523/AclServiceImplModule.java
@@ -0,0 +1,25 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.aclservice.impl.rev160523;
+
+import org.opendaylight.netvirt.aclservice.AclServiceProvider;
+
+public class AclServiceImplModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.aclservice.impl.rev160523.AbstractAclServiceImplModule {
+    public AclServiceImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public AclServiceImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.aclservice.impl.rev160523.AclServiceImplModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        AclServiceProvider provider = new AclServiceProvider();
+        getBrokerDependency().registerProvider(provider);
+        return provider;
+    }
+}
