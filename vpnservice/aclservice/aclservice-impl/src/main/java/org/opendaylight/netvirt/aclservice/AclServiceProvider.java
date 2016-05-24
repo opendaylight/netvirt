ce8fbcfe3afc3f08197e6ae1b2b8bafe52fd037b
Shashidhar R <shashidhar.raja@ericsson.com>
2016-05-24 17:49:14 +0530

    Initial changes for ACL Service
    Signed-off-by: Shashidhar R <shashidhar.raja@ericsson.com>

---
+++ vpnservice/aclservice/aclservice-impl/src/main/java/org/opendaylight/netvirt/aclservice/AclServiceProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.aclservice;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.netvirt.aclservice.api.IAclService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AclServiceProvider implements BindingAwareProvider, IAclService, AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(AclServiceProvider.class);
+
+    private DataBroker broker;
+
+    public AclServiceProvider() {
+    }
+
+    @Override
+    public void onSessionInitiated(ProviderContext session) {
+        broker = session.getSALService(DataBroker.class);
+        LOG.info("ACL Service Initiated");
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOG.info("ACL Service closed");
+     }
+}
