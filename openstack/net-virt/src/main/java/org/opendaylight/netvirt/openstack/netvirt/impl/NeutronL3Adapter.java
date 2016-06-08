38af1195f6e0fb372b63f6e88165ed4e87fdcde8
Abbas Pareedkunju <abbas.pareedkunju@tcs.com>
2016-06-08 13:41:55 +0530

    Bug 5989: Thrown nullpointerexception while updating the port.
    This patch is added to catch the NullPointerExceptions which could occur while updating the port.
    
    Change-Id: Ia72959830b8b42f0d9c17f84cbddd30b96318d52
    Signed-off-by: Abbas Pareedkunju <abbas.pareedkunju@tcs.com>

--- openstack/net-virt/src/main/java/org/opendaylight/netvirt/openstack/netvirt/impl/NeutronL3Adapter.java
+++ openstack/net-virt/src/main/java/org/opendaylight/netvirt/openstack/netvirt/impl/NeutronL3Adapter.java
@@ -934,8 +934,12 @@ public class NeutronL3Adapter extends AbstractHandler implements GatewayMacResol
 
     private boolean isPortSecurityEnableUpdated(NeutronPort neutronPort) {
         LOG.trace("isPortSecuirtyEnableUpdated:" + neutronPort);
-        if (neutronPort.getOriginalPort().getPortSecurityEnabled()
-                != neutronPort.getPortSecurityEnabled()) {
+        if (neutronPort != null
+                && neutronPort.getOriginalPort() != null
+                && neutronPort.getOriginalPort().getPortSecurityEnabled() != null
+                && neutronPort.getPortSecurityEnabled() != null
+                && neutronPort.getOriginalPort().getPortSecurityEnabled() != neutronPort
+                        .getPortSecurityEnabled()) {
             return true;
         }
         return false;
@@ -1493,7 +1497,9 @@ public class NeutronL3Adapter extends AbstractHandler implements GatewayMacResol
         List<NeutronFloatingIP> neutronFloatingIps = neutronFloatingIpCache.getAllFloatingIPs();
         if (neutronFloatingIps != null) {
             for (NeutronFloatingIP neutronFloatingIP : neutronFloatingIps) {
-                if (neutronFloatingIP.getPortUUID().equals(neutronPort.getPortUUID())) {
+                if (neutronFloatingIP.getPortUUID() != null
+                        && neutronFloatingIP.getPortUUID().equals(
+                                neutronPort.getPortUUID())) {
                     handleNeutronFloatingIPEvent(neutronFloatingIP, UPDATE);
                 }
             }
