943be81f995a477cbcc3fa3642873e9d779825ca
Ritu Sood <ritu.sood@intel.com>
2016-06-09 06:01:38 -0700

    Enhance host config to add multiple configs per host
    Current implementation of host config allows one configuration
    per host. This patch adds support for multiple configurations
    per host. Also this patch removes the fixed host type values.
    Neutron patch: https://git.opendaylight.org/gerrit/#/c/36767/.
    Openstack doc: https://review.openstack.org/#/c/309630/
    
    Change-Id: I2cfe421611d66741970963dc80184a0e470d8697
    Signed-off-by: Ritu Sood <ritu.sood@intel.com>

--- openstack/net-virt/src/main/java/org/opendaylight/netvirt/openstack/netvirt/impl/HostConfigService.java
+++ openstack/net-virt/src/main/java/org/opendaylight/netvirt/openstack/netvirt/impl/HostConfigService.java
@@ -8,6 +8,7 @@
 
 package org.opendaylight.netvirt.openstack.netvirt.impl;
 
+import com.google.common.collect.Maps;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.netvirt.openstack.netvirt.ClusterAwareMdsalUtils;
@@ -22,6 +23,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev15071
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.hostconfigs.Hostconfig;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.OpenvswitchExternalIds;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.hostconfigs.HostconfigBuilder;
 import org.opendaylight.yangtools.yang.binding.DataObject;
@@ -31,14 +33,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Map;
 
 
 public class HostConfigService implements OvsdbInventoryListener, ConfigInterface {
     private static final Logger LOG = LoggerFactory.getLogger(HostConfigService.class);
 
     private static final String OS_HOST_CONFIG_HOST_ID_KEY = "odl_os_hostconfig_hostid";
-    private static final String OS_HOST_CONFIG_HOST_TYPE_KEY = "odl_os_hostconfig_hosttype";
-    private static final String OS_HOST_CONFIG_CONFIG_KEY = "odl_os_hostconfig_config";
+    private static final String OS_HOST_CONFIG_CONFIG_KEY_PREFIX = "odl_os_hostconfig_config_odl_";
+    private static int HOST_TYPE_STR_LEN = 8;
 
     private final DataBroker databroker;
     private final ClusterAwareMdsalUtils mdsalUtils;
@@ -52,18 +55,63 @@ public class HostConfigService implements OvsdbInventoryListener, ConfigInterfac
 
     @Override
     public void ovsdbUpdate(Node node, DataObject resourceAugmentationData, OvsdbType ovsdbType, Action action) {
-        boolean result;
-        Hostconfig hostConfig;
-        InstanceIdentifier<Hostconfig> hostConfigId;
-
         if (ovsdbType != OvsdbType.NODE) {
             return;
         }
-        hostConfig = buildHostConfigInfo(node);
-        if (hostConfig == null) {
-              return;
-        }
         LOG.trace("ovsdbUpdate: {} - {} - <<{}>> <<{}>>", ovsdbType, action, node, resourceAugmentationData);
+        String hostId = southbound.getExternalId(node, OvsdbTables.OPENVSWITCH, OS_HOST_CONFIG_HOST_ID_KEY);
+        if (hostId == null){
+            return;
+        }
+        for(Map.Entry<String,String> entry : extractHostConfig(node).entrySet()) {
+            ovsdbUpdateConfig(buildHostConfigInfo(hostId, entry.getKey(), entry.getValue()), action);
+        }
+    }
+
+    @Override
+    public void triggerUpdates() {
+        List<Node> ovsdbNodes = southbound.readOvsdbTopologyNodes();
+        for (Node node : ovsdbNodes) {
+            ovsdbUpdate(node, node.getAugmentation(OvsdbNodeAugmentation.class),
+                    OvsdbInventoryListener.OvsdbType.NODE, Action.ADD);
+        }
+    }
+
+    private Map<String, String> extractHostConfig(Node node) {
+        OvsdbNodeAugmentation ovsdbNode = southbound.extractNodeAugmentation(node);
+        Map<String, String> config = Maps.newHashMap();
+        if (ovsdbNode == null) {
+            Node nodeFromReadOvsdbNode = southbound.readOvsdbNode(node);
+            if (nodeFromReadOvsdbNode != null) {
+                ovsdbNode = southbound.extractNodeAugmentation(nodeFromReadOvsdbNode);
+            }
+        }
+        if (ovsdbNode != null && ovsdbNode.getOpenvswitchExternalIds() != null) {
+            for (OpenvswitchExternalIds openvswitchExternalIds : ovsdbNode.getOpenvswitchExternalIds()) {
+                if (openvswitchExternalIds.getExternalIdKey().startsWith(OS_HOST_CONFIG_CONFIG_KEY_PREFIX)) {
+                    // Extract the host type. Max 8 characters after suffix OS_HOST_CONFIG_CONFIG_KEY_PREFIX.length()
+                    String hostType = openvswitchExternalIds.getExternalIdKey().substring(
+                                                     OS_HOST_CONFIG_CONFIG_KEY_PREFIX.length());
+                    if (null != hostType && hostType.length() > 0) {
+                        if (hostType.length() > HOST_TYPE_STR_LEN) {
+                            hostType = hostType.substring(0, HOST_TYPE_STR_LEN);
+                        }
+                        hostType = "ODL " + hostType.toUpperCase();
+                        if (null != openvswitchExternalIds.getExternalIdValue())
+                            config.put(hostType, openvswitchExternalIds.getExternalIdValue());
+                    }
+                }
+            }
+        }
+        return config;
+    }
+
+    private void ovsdbUpdateConfig(Hostconfig hostConfig, Action action) {
+        boolean result;
+        InstanceIdentifier<Hostconfig> hostConfigId;
+        if (hostConfig == null) {
+            return;
+        }
         switch (action) {
             case ADD:
             case UPDATE:
@@ -79,34 +127,11 @@ public class HostConfigService implements OvsdbInventoryListener, ConfigInterfac
         }
     }
 
-    @Override
-    public void triggerUpdates() {
-        List<Node> ovsdbNodes = southbound.readOvsdbTopologyNodes();
-        for (Node node : ovsdbNodes) {
-            ovsdbUpdate(node, node.getAugmentation(OvsdbNodeAugmentation.class),
-                    OvsdbInventoryListener.OvsdbType.NODE, Action.ADD);
-        }
-    }
-
-    private Hostconfig buildHostConfigInfo(Node node) {
+    private Hostconfig buildHostConfigInfo(String hostId, String hostType, String hostConfig) {
         HostconfigBuilder hostconfigBuilder = new HostconfigBuilder();
-        String value;
-
-        value = southbound.getExternalId(node, OvsdbTables.OPENVSWITCH, OS_HOST_CONFIG_HOST_ID_KEY);
-        if (value == null){
-            return null;
-        }
-        hostconfigBuilder.setHostId(value);
-        value = southbound.getExternalId(node, OvsdbTables.OPENVSWITCH, OS_HOST_CONFIG_HOST_TYPE_KEY);
-        if (value == null) {
-            return null;
-        }
-        hostconfigBuilder.setHostType(value);
-        value = southbound.getExternalId(node, OvsdbTables.OPENVSWITCH, OS_HOST_CONFIG_CONFIG_KEY);
-        if (value == null) {
-            return null;
-        }
-        hostconfigBuilder.setConfig(value);
+        hostconfigBuilder.setHostId(hostId);
+        hostconfigBuilder.setHostType(hostType);
+        hostconfigBuilder.setConfig(hostConfig);
         return hostconfigBuilder.build();
     }
 
