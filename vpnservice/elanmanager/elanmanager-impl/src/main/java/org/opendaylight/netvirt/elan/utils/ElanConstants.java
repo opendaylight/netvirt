6646881dd48c1c5326cd7e284b13c186cabe673e
Guy Sela <guy.sela@hpe.com>
2016-06-19 13:26:59 +0300

    Complete ETree implementation in netvirt. Extended ElanService code to support Etree networks.
    Etree network adds another attribute for interfaces: Root or Leaf.
    In the same Etree network:
    Root interfaces can communicate with all interfaces.
    Leaf interfaces can communicate only with Root interfaces.
    Added import
    
    Change-Id: I7855015db9c34757249a7461ebf37924969d398c
    Signed-off-by: Guy Sela <guy.sela@hpe.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/utils/ElanConstants.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/utils/ElanConstants.java
@@ -11,6 +11,7 @@ import java.math.BigInteger;
 
 public class ElanConstants {
 
+    public static final String LEAVES_POSTFIX = "_leaves";
     public static final String ELAN_ID_POOL_NAME = "elan.ids.pool";
     public static final long ELAN_ID_LOW_VALUE = 5000L;
     public static final long ELAN_ID_HIGH_VALUE = 10000L;
