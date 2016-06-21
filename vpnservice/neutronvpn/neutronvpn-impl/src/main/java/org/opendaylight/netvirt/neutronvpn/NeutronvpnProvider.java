2aff824e41221e2103f6088ca780882354795b9e
Suraj Ranjan <suraj.ranjan@ericsson.com>
2016-06-21 10:16:54 +0530

    Following commit includes: 1> Fixing NPE in NeutronVpnUtils when no network-type is provided from northbound 2> NVPN: neutron northbound support for gateway mac for external networks 3> Use vif_type to determine portname prefix 4> NeutronVPN: Handling fixed-floating IP dissociation for floatingIP delete 5> NeutronVPN additional -ve validations via ECM for network/router assc/dissc
      a. Added validations for the following:
    
      aa) Associate network(s) to vpn:
        i) network already assc to another VPN
        ii) network's subnet added to some router as interface
    
      ab) Dissociate network(s) from vpn:
        i) network's subnet added to some router as interface
    
      ac) Associate router to vpn:
        i) router assc to another vpn
        ii) network corresponding to router's subnet interface already assc to VPN
    
      ad) Dissociate router from vpn:
        i) router assc to another vpn
    
      b. Updating VPNMaps once assc/dissc are done in assc/dissc network(s) from
         VPN
    
    6> NeutronVPN: adding yang model and notifications for router-dpn
    map
    
      a. Added a container to maintain mapping between neutron router and
      DPN(s) on which vpn-interfaces for router are present.
    
      b. Added notifications for router associated/dissociated to/from vpn
    
    Change-Id: I30931b4ce6013a4bdc0d58ccc86fa8c83d0cdff6
    Signed-off-by: Suraj Ranjan <suraj.ranjan@ericsson.com>

--- vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnProvider.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnProvider.java
@@ -160,8 +160,8 @@ public class NeutronvpnProvider implements BindingAwareProvider, INeutronVpnMana
     }
 
     @Override
-    public String uuidToTapPortName(Uuid id) {
-        return NeutronvpnUtils.uuidToTapPortName(id);
+    public String getVifPortName(Port port) {
+        return NeutronvpnUtils.getVifPortName(port);
     }
 
     @Override
