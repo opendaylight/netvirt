fe3379ada6bc73d53e3ae40b515dc3cb213dbfb5
Josh <jhershbe@redhat.com>
2016-06-28 11:24:14 +0200

    Auto-creation of bridges in vpnservice
    When a new OVSDB node is detected, the
    event is caught and we make sure that
    br-int and br-ex (if l3 is configured)
    exist. Note that br-ex is not technically
    required and once devstack is modified
    to not check for it, we should go back
    in to this code and no longer create it.
    
    This code add dependencies to the
    following ovsdb project utility bundles
    and jars:
    - southbound-api
    - utils.mdsal-utils
    - utils.southbound-utils
    - utils.config
    
    Change-Id: I852c2b619c99dc2748808efe27d8d24487e0cee7
    Signed-off-by: Josh <jhershbe@redhat.com>

---
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanOvsdbNodeListener.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.elan.internal;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.netvirt.elan.utils.BridgeUtils;
+import org.opendaylight.netvirt.elan.utils.ElanConstants;
+import org.opendaylight.genius.mdsalutil.AbstractDataChangeListener;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Listen for new OVSDB nodes and then make sure they have the necessary bridges configured
+ */
+public class ElanOvsdbNodeListener extends AbstractDataChangeListener<Node> {
+
+    private static final Logger logger = LoggerFactory.getLogger(ElanOvsdbNodeListener.class);
+
+    private ListenerRegistration<DataChangeListener> listenerRegistration;
+    private BridgeUtils bridgeUtils;
+    private boolean generateIntBridgeMac;
+
+    /**
+     * Constructor
+     * @param db the DataBroker
+     * @param generateIntBridgeMac true if the integration bridge should be given a fixed, random MAC address
+     */
+    public ElanOvsdbNodeListener(final DataBroker db, boolean generateIntBridgeMac) {
+        super(Node.class);
+        this.bridgeUtils = new BridgeUtils(db);
+        this.generateIntBridgeMac = generateIntBridgeMac;
+        registerListener(db);
+    }
+
+    private void registerListener(final DataBroker db) {
+        try {
+            listenerRegistration = db.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL,
+                    getWildCardPath(), ElanOvsdbNodeListener.this, AsyncDataBroker.DataChangeScope.SUBTREE);
+        } catch (final Exception e) {
+            logger.error("ElanOvsdbNodeListener: DataChange listener registration fail!", e);
+            throw new IllegalStateException("ElanOvsdbNodeListener: registration Listener failed.", e);
+        }
+    }
+
+    private InstanceIdentifier<Node> getWildCardPath() {
+        return InstanceIdentifier
+                .create(NetworkTopology.class)
+                .child(Topology.class, new TopologyKey(ElanConstants.OVSDB_TOPOLOGY_ID))
+                .child(Node.class);
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<Node> identifier, Node del) {
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<Node> identifier, Node original, Node update) {
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<Node> identifier, Node add) {
+        logger.info("ElanOvsdbNodeListener.add, new bridge detected{}", add);
+        bridgeUtils.prepareNode(add, generateIntBridgeMac);
+    }
+
+}
