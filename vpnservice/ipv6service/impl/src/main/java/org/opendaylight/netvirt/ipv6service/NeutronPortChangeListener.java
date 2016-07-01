9cb3e654490dd1a19bb7bf3c7e4b33718fab6084
Sridhar Gaddam <sgaddam@redhat.com>
2016-07-01 18:29:38 +0530

    ipv6service: Register listeners and configure handlers
    This patch includes the following changes.
    1. Registration api's for Neutron Router, Subnet and Port changes.
    2. Handlers for interface manager.
    
    Change-Id: I4570c95abcec2b61baa03d8aadcb1c8ebaa32f1c
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

---
+++ vpnservice/ipv6service/impl/src/main/java/org/opendaylight/netvirt/ipv6service/NeutronPortChangeListener.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.ipv6service;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.NotificationService;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.mdsalutil.AbstractDataChangeListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.Ports;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.ports.Port;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NeutronPortChangeListener extends AbstractDataChangeListener<Port> implements AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(NeutronPortChangeListener.class);
+
+    private ListenerRegistration<DataChangeListener> listenerRegistration;
+    private NotificationService notificationService;
+
+    public NeutronPortChangeListener(final DataBroker db) {
+        super(Port.class);
+        registerListener(db);
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (listenerRegistration != null) {
+            try {
+                listenerRegistration.close();
+            } catch (final Exception e) {
+                LOG.error("Error when cleaning up DataChangeListener.", e);
+            }
+            listenerRegistration = null;
+        }
+        LOG.info("Neutron Port listener Closed");
+    }
+
+
+    private void registerListener(final DataBroker db) {
+        try {
+            listenerRegistration = db.registerDataChangeListener(LogicalDatastoreType.CONFIGURATION,
+                    InstanceIdentifier.create(Neutron.class).child(Ports.class).child(Port.class),
+                    NeutronPortChangeListener.this, DataChangeScope.SUBTREE);
+        } catch (final Exception e) {
+            LOG.error("IPv6Service Neutron Port DataChange listener registration fail!", e);
+            throw new IllegalStateException("IPv6Service Neutron Port DataChange listener registration failed.", e);
+        }
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<Port> identifier, Port input) {
+        LOG.info("Add port notification handler is invoked...");
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<Port> identifier, Port input) {
+        LOG.info("remove port notification handler is invoked...");
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<Port> identifier, Port original, Port update) {
+        LOG.info("update port notification handler is invoked...");
+    }
+}
--- vpnservice/ipv6service/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/netvirt/ipv6service/impl/rev160620/Ipv6ServiceImplModule.java
+++ vpnservice/ipv6service/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/netvirt/ipv6service/impl/rev160620/Ipv6ServiceImplModule.java
@@ -1,6 +1,15 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.ipv6service.impl.rev160620;
 
 import org.opendaylight.netvirt.ipv6service.Ipv6ServiceProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.OdlInterfaceRpcService;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
 
 public class Ipv6ServiceImplModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.ipv6service.impl.rev160620.AbstractIpv6ServiceImplModule {
     public Ipv6ServiceImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
@@ -18,7 +27,10 @@ public class Ipv6ServiceImplModule extends org.opendaylight.yang.gen.v1.urn.open
 
     @Override
     public java.lang.AutoCloseable createInstance() {
+        RpcProviderRegistry rpcregistryDependency = getRpcregistryDependency();
         Ipv6ServiceProvider provider = new Ipv6ServiceProvider();
+        provider.setInterfaceManager(getOdlinterfaceDependency());
+        provider.setInterfaceManagerRpcService(rpcregistryDependency.getRpcService(OdlInterfaceRpcService.class));
         getBrokerDependency().registerProvider(provider);
         return provider;
     }
