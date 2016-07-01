12b2925d2b4990b152987ec675c32d5c97a97b4f
Sridhar Gaddam <sgaddam@redhat.com>
2016-07-01 18:29:38 +0530

    ipv6service: Register listeners and configure handlers
    This patch includes the following changes.
    1. Registration api's for Neutron Router, Subnet and Port changes.
    2. Handlers for interface manager.
    
    Change-Id: I4570c95abcec2b61baa03d8aadcb1c8ebaa32f1c
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- vpnservice/ipv6service/impl/src/main/java/org/opendaylight/netvirt/ipv6service/Ipv6ServiceProvider.java
+++ vpnservice/ipv6service/impl/src/main/java/org/opendaylight/netvirt/ipv6service/Ipv6ServiceProvider.java
@@ -9,14 +9,24 @@
 package org.opendaylight.netvirt.ipv6service;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
-
+import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.OdlInterfaceRpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Ipv6ServiceProvider implements BindingAwareProvider, AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(Ipv6ServiceProvider.class);
+    private IInterfaceManager interfaceManager;
+    private IMdsalApiManager mdsalManager;
+    private OdlInterfaceRpcService interfaceManagerRpc;
+    private NeutronPortChangeListener portListener;
+    private NeutronSubnetChangeListener subnetListener;
+    private NeutronRouterChangeListener routerListener;
+    private Ipv6ServiceInterfaceEventListener ipv6ServiceInterfaceEventListener;
 
     private DataBroker broker;
 
@@ -26,11 +36,32 @@ public class Ipv6ServiceProvider implements BindingAwareProvider, AutoCloseable 
     @Override
     public void onSessionInitiated(ProviderContext session) {
         broker = session.getSALService(DataBroker.class);
+        portListener = new NeutronPortChangeListener(broker);
+        subnetListener = new NeutronSubnetChangeListener(broker);
+        routerListener = new NeutronRouterChangeListener(broker);
+        ipv6ServiceInterfaceEventListener = new Ipv6ServiceInterfaceEventListener(broker);
+        ipv6ServiceInterfaceEventListener.registerListener(LogicalDatastoreType.OPERATIONAL, broker);
         LOG.info("IPv6 Service Initiated");
     }
 
     @Override
     public void close() throws Exception {
+        portListener.close();
+        subnetListener.close();
+        routerListener.close();
+        ipv6ServiceInterfaceEventListener.close();
         LOG.info("IPv6 Service closed");
+    }
+
+    public void setInterfaceManager(IInterfaceManager interfaceManager) {
+        this.interfaceManager = interfaceManager;
+    }
+
+    public void setInterfaceManagerRpcService(OdlInterfaceRpcService interfaceManager) {
+        this.interfaceManagerRpc = interfaceManagerRpc;
+    }
+
+    public void setMdsalManager(IMdsalApiManager mdsalManager) {
+        this.mdsalManager = mdsalManager;
     }
 }
---
+++ vpnservice/ipv6service/impl/src/main/java/org/opendaylight/netvirt/ipv6service/NeutronSubnetChangeListener.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.ipv6service;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.mdsalutil.AbstractDataChangeListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.subnets.rev150712.subnets.attributes.Subnets;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.subnets.rev150712.subnets.attributes.subnets.Subnet;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NeutronSubnetChangeListener extends AbstractDataChangeListener<Subnet> implements AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(NeutronSubnetChangeListener.class);
+
+    private ListenerRegistration<DataChangeListener> listenerRegistration;
+    private final DataBroker broker;
+
+    public NeutronSubnetChangeListener(final DataBroker db) {
+        super(Subnet.class);
+        broker = db;
+        registerListener(db);
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (listenerRegistration != null) {
+            listenerRegistration.close();
+            listenerRegistration = null;
+        }
+        LOG.info("Neutron Subnet listener Closed");
+    }
+
+    private void registerListener(final DataBroker db) {
+        listenerRegistration = db.registerDataChangeListener(LogicalDatastoreType.CONFIGURATION,
+                InstanceIdentifier.create(Neutron.class).child(Subnets.class).child(Subnet.class),
+                NeutronSubnetChangeListener.this, DataChangeScope.SUBTREE);
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<Subnet> identifier, Subnet input) {
+        LOG.info("Add Subnet notification handler is invoked...");
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<Subnet> identifier, Subnet input) {
+        LOG.info("Remove Subnet notification handler is invoked...");
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<Subnet> identifier, Subnet original, Subnet update) {
+        LOG.info("Update Subnet notification handler is invoked...");
+    }
+}
