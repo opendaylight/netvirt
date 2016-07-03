1650830536abf2c503d5448e3f7f3155aed5c6d6
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-07-03 09:47:20 -0400

    Add blueprint wiring for vpnmanager
    Change-Id: I935ea02c78e18787924d5dfb66c5805f1851f36d
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>
    Signed-off-by: Alexis de Talhouët <alexis.de.t@gmail.com>

---
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnManagerImpl.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2015 - 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.vpnmanager;
+
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import org.opendaylight.netvirt.fibmanager.api.IFibManager;
+import org.opendaylight.netvirt.vpnmanager.api.IVpnManager;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.CreateIdPoolInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.CreateIdPoolInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VpnManagerImpl implements IVpnManager {
+
+    private static final Logger LOG = LoggerFactory.getLogger(VpnManagerImpl.class);
+
+    private final VpnInterfaceManager vpnInterfaceManager;
+    private final VpnInstanceListener vpnInstanceListener;
+    private final IdManagerService idManager;
+
+    public VpnManagerImpl(final IdManagerService idManagerService,
+            final VpnInstanceListener vpnInstanceListener,
+            final VpnInterfaceManager vpnInterfaceManager) {
+        this.vpnInterfaceManager = vpnInterfaceManager;
+        this.vpnInstanceListener = vpnInstanceListener;
+        this.idManager = idManagerService;
+    }
+
+    public void start() {
+        LOG.info("VpnserviceProvider Session Initiated");
+        createIdPool();
+    }
+
+    private void createIdPool() {
+        CreateIdPoolInput createPool = new CreateIdPoolInputBuilder()
+            .setPoolName(VpnConstants.VPN_IDPOOL_NAME)
+            .setLow(VpnConstants.VPN_IDPOOL_START)
+            .setHigh(new BigInteger(VpnConstants.VPN_IDPOOL_SIZE).longValue())
+            .build();
+        try {
+           Future<RpcResult<Void>> result = idManager.createIdPool(createPool);
+           if ((result != null) && (result.get().isSuccessful())) {
+                LOG.debug("Created IdPool for VPN Service");
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            LOG.error("Failed to create idPool for VPN Service",e);
+        }
+    }
+
+    @Override
+    public void addExtraRoute(String destination, String nextHop, String rd, String routerID, int label) {
+        LOG.info("Adding extra route with destination {} and nexthop {}", destination, nextHop);
+        vpnInterfaceManager.addExtraRoute(destination, nextHop, rd, routerID, label, null);
+    }
+
+    @Override
+    public void delExtraRoute(String destination, String rd, String routerID) {
+        LOG.info("Deleting extra route with destination {}", destination);
+        vpnInterfaceManager.delExtraRoute(destination, rd, routerID);
+    }
+
+    @Override
+    public boolean isVPNConfigured() {
+        return vpnInstanceListener.isVPNConfigured();
+    }
+
+    @Override
+    public void setFibService(IFibManager fibManager) {
+        //no-op - will be deleted in follow up patch
+    }
+
+}
