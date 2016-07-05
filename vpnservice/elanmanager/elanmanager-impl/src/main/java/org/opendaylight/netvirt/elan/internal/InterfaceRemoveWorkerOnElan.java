3eba117b8edb46d1e3f217a6446e4d97ea51ca29
epgoraj <p.govinda.rajulu@ericsson.com>
2016-07-05 12:20:44 +0530

    ELAN Performance improvement changes.
    Change-Id: I8a965eed078355876a4ec35e0f326fa927d60cf3
    Signed-off-by: epgoraj <p.govinda.rajulu@ericsson.com>

---
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/InterfaceRemoveWorkerOnElan.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.elan.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.opendaylight.genius.interfacemanager.globals.InterfaceInfo;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.instances.ElanInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+public class InterfaceRemoveWorkerOnElan implements Callable<List<ListenableFuture<Void>>> {
+    private String key;
+    private ElanInstance elanInfo;
+    private String interfaceName;
+    private InterfaceInfo interfaceInfo;
+    private boolean isInterfaceStateRemoved;
+    private ElanInterfaceManager dataChangeListener;
+    private static final Logger logger = LoggerFactory.getLogger(InterfaceRemoveWorkerOnElan.class);
+
+    public InterfaceRemoveWorkerOnElan(String key, ElanInstance elanInfo, String interfaceName,
+                                        InterfaceInfo interfaceInfo, boolean isInterfaceStateRemoved, ElanInterfaceManager dataChangeListener) {
+        super();
+        this.key = key;
+        this.elanInfo = elanInfo;
+        this.interfaceName = interfaceName;
+        this.interfaceInfo = interfaceInfo;
+        this.isInterfaceStateRemoved = isInterfaceStateRemoved;
+        this.dataChangeListener = dataChangeListener;
+    }
+
+    @Override
+    public String toString() {
+        return "InterfaceRemoveWorkerOnElan [key=" + key + ", elanInfo=" + elanInfo +
+                ", interfaceName=" + interfaceName
+                + ", interfaceInfo=" + interfaceInfo + "]";
+    }
+
+    @Override
+    public List<ListenableFuture<Void>> call() throws Exception {
+        List<ListenableFuture<Void>> futures = new ArrayList<>();
+        try {
+            dataChangeListener.removeElanInterface(elanInfo, interfaceName, interfaceInfo, isInterfaceStateRemoved);
+        } catch (Exception e) {
+            logger.error("Error while processing {} for {}, error {}", key, interfaceName, e);
+        }
+        return futures;
+    }
+
+}
