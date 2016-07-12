c3d01658289e26279e1ceb88d41a437ba575f274
Ritu Sood <ritu.sood@intel.com>
2016-07-11 17:03:31 -0700

    Add Host Config functionality for Neutron
    Patch to read host configuration data from OVSDB
    external_ids and populates ODL neutron northbound
    mdsal data as per yang model defined in the patch
    https://git.opendaylight.org/gerrit/#/c/36767/.
    Complete details of the feature available at
    https://github.com/openstack/networking-odl/blob/master/doc/source/devref/hostconfig.rst
    Patch for original netvirt code
    https://git.opendaylight.org/gerrit/#/c/40143/
    
    Change-Id: I7da8c8e5cc18d661b21c70381990b7c2a7ebb40c
    Signed-off-by: Ritu Sood <ritu.sood@intel.com>

---
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronHostConfigChangeListener.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright (c) 2016 Intel Corporation.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.neutronvpn;
+
+import com.google.common.collect.Maps;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.mdsalutil.AbstractDataChangeListener;
+import org.opendaylight.ovsdb.utils.southbound.utils.SouthboundUtils;
+import org.opendaylight.ovsdb.utils.mdsal.utils.MdsalUtils;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.Hostconfigs;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.hostconfigs.Hostconfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.hostconfigs.HostconfigBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.OpenvswitchExternalIds;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Map;
+
+public class NeutronHostConfigChangeListener extends AbstractDataChangeListener<Node>{
+
+    private static final Logger logger= LoggerFactory.getLogger(NeutronHostConfigChangeListener.class);
+
+    private ListenerRegistration<DataChangeListener>listenerRegistration;
+    private final SouthboundUtils southboundUtils;
+    private final MdsalUtils mdsalUtils;
+
+    private static final String OS_HOST_CONFIG_HOST_ID_KEY = "odl_os_hostconfig_hostid";
+    private static final String OS_HOST_CONFIG_CONFIG_KEY_PREFIX = "odl_os_hostconfig_config_odl_";
+    private static int HOST_TYPE_STR_LEN = 8;
+
+    private enum Action {
+        ADD,
+        UPDATE,
+        DELETE
+    }
+
+    public NeutronHostConfigChangeListener(final DataBroker db){
+        super(Node.class);
+        registerListener(db);
+        this.mdsalUtils = new MdsalUtils(db);
+        this.southboundUtils = new SouthboundUtils(mdsalUtils);
+    }
+
+    private void registerListener(final DataBroker db){
+        try{
+            listenerRegistration=db.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL,
+                getWildCardPath(),NeutronHostConfigChangeListener.this,AsyncDataBroker.DataChangeScope.SUBTREE);
+        } catch(final Exception e){
+            logger.error("NeutronHostConfigChangeListener: DataChange listener registration fail!",e);
+            throw new IllegalStateException("NeutronHostConfigChangeListener: registration Listener failed.",e);
+        }
+    }
+
+    private InstanceIdentifier<Node> getWildCardPath(){
+        return InstanceIdentifier
+            .create(NetworkTopology.class)
+            .child(Topology.class,new TopologyKey(SouthboundUtils.OVSDB_TOPOLOGY_ID))
+            .child(Node.class);
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<Node>identifier, Node del){
+        logger.trace("NeutronHostConfigChangeListener.remove {}",del);
+        updateHostConfig(del, Action.DELETE);
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<Node>identifier, Node original, Node update){
+        logger.trace("NeutronHostConfigChangeListener.update {}",update);
+        updateHostConfig(update, Action.UPDATE);
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<Node>identifier, Node add){
+        logger.trace("NeutronHostConfigChangeListener.add {}",add);
+        updateHostConfig(add, Action.ADD);
+
+    }
+
+    private void updateHostConfig(Node node, Action action) {
+        String hostId = getExternalId(node, OS_HOST_CONFIG_HOST_ID_KEY);
+        if (hostId == null){
+            return;
+        }
+        for(Map.Entry<String,String> entry : extractHostConfig(node).entrySet()) {
+            updateMdsal(buildHostConfigInfo(hostId, entry.getKey(), entry.getValue()), action);
+        }
+    }
+
+    private Map<String, String> extractHostConfig(Node node) {
+        Map<String, String> config = Maps.newHashMap();
+        OvsdbNodeAugmentation ovsdbNode = getOvsdbNodeAugmentation(node);
+        if (ovsdbNode != null && ovsdbNode.getOpenvswitchExternalIds() != null) {
+            for (OpenvswitchExternalIds openvswitchExternalIds : ovsdbNode.getOpenvswitchExternalIds()) {
+                if (openvswitchExternalIds.getExternalIdKey().startsWith(OS_HOST_CONFIG_CONFIG_KEY_PREFIX)) {
+                    // Extract the host type. Max 8 characters after
+                    // suffix OS_HOST_CONFIG_CONFIG_KEY_PREFIX.length()
+                    String hostType = openvswitchExternalIds.getExternalIdKey().substring(
+                            OS_HOST_CONFIG_CONFIG_KEY_PREFIX.length());
+                    if (null != hostType && hostType.length() > 0) {
+                        if (hostType.length() > HOST_TYPE_STR_LEN) {
+                            hostType = hostType.substring(0, HOST_TYPE_STR_LEN);
+                        }
+                        hostType = "ODL " + hostType.toUpperCase();
+                        if (null != openvswitchExternalIds.getExternalIdValue())
+                            config.put(hostType, openvswitchExternalIds.getExternalIdValue());
+                    }
+                }
+            }
+        }
+        return config;
+    }
+
+    private void updateMdsal(Hostconfig hostConfig, Action action) {
+        boolean result;
+        InstanceIdentifier<Hostconfig> hostConfigId;
+        if (hostConfig == null) {
+            return;
+        }
+        switch (action) {
+            case ADD:
+            case UPDATE:
+                hostConfigId = createInstanceIdentifier(hostConfig);
+                result = mdsalUtils.put(LogicalDatastoreType.OPERATIONAL, hostConfigId, hostConfig);
+                logger.trace("Add Node: result: {}", result);
+                break;
+            case DELETE:
+                hostConfigId = createInstanceIdentifier(hostConfig);
+                result = mdsalUtils.delete(LogicalDatastoreType.OPERATIONAL, hostConfigId);
+                logger.trace("Delete Node: result: {}", result);
+                break;
+        }
+    }
+
+    private Hostconfig buildHostConfigInfo(String hostId, String hostType, String hostConfig) {
+        HostconfigBuilder hostconfigBuilder = new HostconfigBuilder();
+        hostconfigBuilder.setHostId(hostId);
+        hostconfigBuilder.setHostType(hostType);
+        hostconfigBuilder.setConfig(hostConfig);
+        return hostconfigBuilder.build();
+    }
+
+    private String getExternalId(Node node, String key) {
+        OvsdbNodeAugmentation ovsdbNode = getOvsdbNodeAugmentation(node);
+        if (ovsdbNode != null && ovsdbNode.getOpenvswitchExternalIds() != null) {
+            for (OpenvswitchExternalIds openvswitchExternalIds : ovsdbNode.getOpenvswitchExternalIds()) {
+                if (openvswitchExternalIds.getExternalIdKey().equals(key)) {
+                    return openvswitchExternalIds.getExternalIdValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    private OvsdbNodeAugmentation getOvsdbNodeAugmentation(Node node)
+    {
+        OvsdbNodeAugmentation ovsdbNode = southboundUtils.extractOvsdbNode(node);
+        if (ovsdbNode == null) {
+            Node nodeFromReadOvsdbNode = southboundUtils.readOvsdbNode(node);
+            if (nodeFromReadOvsdbNode != null) {
+                ovsdbNode = southboundUtils.extractOvsdbNode(nodeFromReadOvsdbNode);
+            }
+        }
+        return ovsdbNode;
+    }
+
+    private InstanceIdentifier<Hostconfig> createInstanceIdentifier() {
+        return InstanceIdentifier.create(Neutron.class)
+                .child(Hostconfigs.class)
+                .child(Hostconfig.class);
+    }
+
+    private InstanceIdentifier<Hostconfig> createInstanceIdentifier(Hostconfig hostconfig) {
+        return InstanceIdentifier.create(Neutron.class)
+                .child(Hostconfigs.class)
+                .child(Hostconfig.class, hostconfig.getKey());
+    }
+}
