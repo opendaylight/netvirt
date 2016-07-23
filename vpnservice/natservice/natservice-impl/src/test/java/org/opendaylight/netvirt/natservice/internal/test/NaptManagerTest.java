31e268e601f09d03d0692202dc58bdb698129f9b
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-07-22 23:04:25 -0400

    Add missing license headers
    Change-Id: I1c04ef692375fdc741858b77c1ae76b985da3721
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- vpnservice/natservice/natservice-impl/src/test/java/org/opendaylight/netvirt/natservice/internal/test/NaptManagerTest.java
+++ vpnservice/natservice/natservice-impl/src/test/java/org/opendaylight/netvirt/natservice/internal/test/NaptManagerTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.netvirt.natservice.internal.test;
 
 import static org.junit.Assert.assertEquals;
@@ -163,7 +170,7 @@ public class NaptManagerTest {
             assertEquals("true", "false");
         }
         naptManager.getExternalAddressMapping(5, internalIpPort);
-        PowerMockito.verifyStatic(); */  
+        PowerMockito.verifyStatic(); */
     }
 
     @Test
