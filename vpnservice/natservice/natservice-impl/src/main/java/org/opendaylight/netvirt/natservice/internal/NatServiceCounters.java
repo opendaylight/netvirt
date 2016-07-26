aca9059925bd79cb831bfe2a4f73cdefa5cae631
yair <yair.zinger@hpe.com>
2016-07-26 17:07:30 +0300

    First use of the infrautils counters in netvirt.
    Added install and remove flows counters for Dhcp and SNAT default flows.
    
    Change-Id: I2b4b6692675b4f22af600cb3edea6bf660462af6
    Signed-off-by: yair <yair.zinger@hpe.com>

---
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatServiceCounters.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016 Hewlett-Packard Enterprise and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.natservice.internal;
+
+import org.opendaylight.infrautils.counters.impl.OccurenceCounter;
+
+public enum NatServiceCounters {
+    install_default_nat_flow, //
+    remove_default_nat_flow;
+
+    private OccurenceCounter counter;
+
+    private NatServiceCounters() {
+        counter = new OccurenceCounter(getClass().getEnclosingClass().getSimpleName(), name(), name());
+    }
+
+    public void inc() {
+        counter.inc();
+    }
+}
