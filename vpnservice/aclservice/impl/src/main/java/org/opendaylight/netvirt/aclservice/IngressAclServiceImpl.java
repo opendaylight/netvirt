4604ab28a66c3162ad7cbe616ba070226fd1dd4e
Aswin Suryanarayanan <asuryana@redhat.com>
2016-07-27 15:37:32 +0530

    Fixed NPO and and some other changes
    1)Fixed NPO in case of empty access list
    
    2)Added code to support ether type.
    
    Change-Id: Ib62216d2a6b6cd10cbba3708a99bce57cbcae4a2
    Signed-off-by: Aswin Suryanarayanan <asuryana@redhat.com>

--- vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/IngressAclServiceImpl.java
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/IngressAclServiceImpl.java
@@ -130,6 +130,10 @@ public class IngressAclServiceImpl extends AbstractAclServiceImpl {
                                    IpPrefixOrAddress attachIp, int addOrRemove) {
         for (Uuid sgUuid :aclUuidList ) {
             Acl acl = AclServiceUtils.getAcl(dataBroker, sgUuid.getValue());
+            if (null == acl) {
+                LOG.warn("The ACL is empty");
+                continue;
+            }
             AccessListEntries accessListEntries = acl.getAccessListEntries();
             List<Ace> aceList = accessListEntries.getAce();
             for (Ace ace: aceList) {
@@ -160,6 +164,8 @@ public class IngressAclServiceImpl extends AbstractAclServiceImpl {
             flowName += "Ingress" + attachMac + String.valueOf(attachIp.getValue()) + ace.getKey().getRuleName();
             flows.add(new MatchInfo(MatchFieldType.eth_dst,
                 new String[] { attachMac }));
+            flows.add(new NxMatchInfo(NxMatchFieldType.ct_state,
+                new long[] { AclConstants.TRACKED_NEW_CT_STATE, AclConstants.TRACKED_NEW_CT_STATE_MASK}));
             flows.addAll(AclServiceUtils.getAllowedIpMatches(attachIp, MatchFieldType.ipv4_destination));
             List<InstructionInfo> instructions = new ArrayList<>();
             List<ActionInfo> actionsInfos = new ArrayList<>();
