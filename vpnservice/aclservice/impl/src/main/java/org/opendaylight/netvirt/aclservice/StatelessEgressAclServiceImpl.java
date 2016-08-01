e621ef9e8f130151b41a7e8823535a702e0cd352
Shlomi <shlomi.alfasi@hpe.com>
2016-08-01 19:12:10 +0300

    <WIP> stateless security groups
    1. refactor Egress/Ingress security group to allow inheritance of Stateless implementation
    2. TcpFlag implementation is depends on https://git.opendaylight.org/gerrit/#/c/42631/
    3. adding MethodInvocationParamSaver to allow asserts on parameters on invoked mocks.
    4. current implementation differ between stateless Vs statefull is by
       custom.properties flag (stateless.sg.enabled) and other suggestion is welcome.
    5. some check-style fixes
    
    Change-Id: I5a73d5087ada651162ee92a42beeea8d9aaf97ef
    Signed-off-by: Shlomi <shlomi.alfasi@hpe.com>

---
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/StatelessEgressAclServiceImpl.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.aclservice;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.InstructionInfo;
+import org.opendaylight.genius.mdsalutil.MatchFieldType;
+import org.opendaylight.genius.mdsalutil.MatchInfo;
+import org.opendaylight.genius.mdsalutil.MatchInfoBase;
+import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.genius.mdsalutil.NxMatchFieldType;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.netvirt.aclservice.utils.AclConstants;
+import org.opendaylight.netvirt.aclservice.utils.AclServiceOFFlowBuilder;
+import org.opendaylight.netvirt.aclservice.utils.AclServiceUtils;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.Ace;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.Matches;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.AceType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.AceIp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.OdlInterfaceRpcService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.DirectionEgress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.SecurityRuleAttr;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.interfaces._interface.AllowedAddressPairs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StatelessEgressAclServiceImpl extends EgressAclServiceImpl {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StatelessEgressAclServiceImpl.class);
+
+    public StatelessEgressAclServiceImpl(DataBroker dataBroker, OdlInterfaceRpcService interfaceManager,
+            IMdsalApiManager mdsalManager) {
+        super(dataBroker, interfaceManager, mdsalManager);
+    }
+
+    @Override
+    protected void programFixedRules(BigInteger dpid, String dhcpMacAddress, List<AllowedAddressPairs> allowedAddresses,
+            int lportTag, int addOrRemove) {
+        // do not create anti-spoof rules
+    }
+
+    @Override
+    protected void programAceRule(BigInteger dpId, int lportTag, int addOrRemove, Ace ace) {
+        SecurityRuleAttr aceAttr = AclServiceUtils.getAccesssListAttributes(ace);
+        if (!aceAttr.getDirection().equals(DirectionEgress.class)) {
+            return;
+        }
+        Matches matches = ace.getMatches();
+        AceType aceType = matches.getAceType();
+        Map<String, List<MatchInfoBase>> flowMap = null;
+        if (aceType instanceof AceIp) {
+            flowMap = AclServiceOFFlowBuilder.programIpFlow(matches);
+        }
+        if (null == flowMap) {
+            LOG.error("Failed to apply ACL {}", ace.getKey());
+            return;
+        }
+        for (Map.Entry<String, List<MatchInfoBase>> flow : flowMap.entrySet()) {
+            String flowName = flow.getKey();
+            List<MatchInfoBase> flowMatches = flow.getValue();
+            boolean hasTcpDstMatch = AclServiceUtils.containsMatchFieldType(flowMatches,
+                    NxMatchFieldType.nx_tcp_dst_with_mask);
+            if (hasTcpDstMatch || flowMatches.isEmpty()) {
+                flowName += "Egress" + lportTag + ace.getKey().getRuleName();
+                flowMatches.add(AclServiceUtils.buildLPortTagMatch(lportTag));
+
+                programAllowSynRules(dpId, flowName, flowMatches, addOrRemove);
+            }
+        }
+    }
+
+    private void programAllowSynRules(BigInteger dpId, String origFlowName,
+            List<MatchInfoBase> origFlowMatches, int addFlow) {
+        List<MatchInfoBase> flowMatches = new ArrayList<MatchInfoBase>();
+        flowMatches.addAll(origFlowMatches);
+        flowMatches.add(new MatchInfo(MatchFieldType.tcp_flags, new long[] { AclConstants.TCP_FLAG_SYN }));
+
+        List<ActionInfo> actionsInfos = new ArrayList<>();
+        List<InstructionInfo> instructions = getDispatcherTableResubmitInstructions(actionsInfos);
+
+        String flowName = "SYN_ACK_" + origFlowName;
+        syncFlow(dpId, NwConstants.EGRESS_ACL_TABLE_ID, flowName, AclConstants.PROTO_MATCH_SYN_ACK_PRIORITY,
+                "ACL_SYN_ACK", 0, 0, AclConstants.COOKIE_ACL_BASE, flowMatches, instructions, addFlow);
+        String oper = getOperAsString(addFlow);
+        LOG.debug("{} allow syn ack packet flow {}", oper, flowName);
+    }
+
+}
