18e7cfca718295d7615a786e0140d81255a337f1
Shlomi <shlomi.alfasi@hpe.com>
2016-08-01 19:12:10 +0300

    stateless security groups
    1. refactor Egress/Ingress security group to allow inheritance of Stateless implementation
    2. adding MethodInvocationParamSaver to allow asserts on parameters on invoked mocks.
    3. current implementation differ between stateless Vs statefull is by
       additional configuration in aclservice-config.yang.
    4. blueprint adaptation to support inheritence in security group
    4. some check-style fixes
    
    Change-Id: I5a73d5087ada651162ee92a42beeea8d9aaf97ef
    Signed-off-by: Shlomi <shlomi.alfasi@hpe.com>

---
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/AclServiceImplFactory.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.aclservice;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.config.rev160806.AclserviceConfig.SecurityGroupMode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.config.rev160806.AclserviceConfigBuilder;
+
+public class AclServiceImplFactory {
+
+    private DataBroker dataBroker;
+    private IMdsalApiManager mdsalManager;
+
+    public AclServiceImplFactory(DataBroker dataBroker, IMdsalApiManager mdsalManager) {
+        this.dataBroker = dataBroker;
+        this.mdsalManager = mdsalManager;
+    }
+
+    public IngressAclServiceImpl createIngressAclServiceImpl() {
+        AclserviceConfigBuilder acb = new AclserviceConfigBuilder();
+        SecurityGroupMode securityGroupMode = acb.build().getSecurityGroupMode();
+        if (securityGroupMode == null || securityGroupMode == SecurityGroupMode.Stateful) {
+            return new IngressAclServiceImpl(dataBroker, mdsalManager);
+        } else {
+            return new StatelessIngressAclServiceImpl(dataBroker, mdsalManager);
+        }
+    }
+
+    public EgressAclServiceImpl createEgressAclServiceImpl() {
+        AclserviceConfigBuilder acb = new AclserviceConfigBuilder();
+        SecurityGroupMode securityGroupMode = acb.build().getSecurityGroupMode();
+        if (securityGroupMode == null || securityGroupMode == SecurityGroupMode.Stateful) {
+            return new EgressAclServiceImpl(dataBroker, mdsalManager);
+        } else {
+            return new StatelessEgressAclServiceImpl(dataBroker, mdsalManager);
+        }
+    }
+}
