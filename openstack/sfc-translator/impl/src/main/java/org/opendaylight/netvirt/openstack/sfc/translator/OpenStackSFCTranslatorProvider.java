84e07409a67470ba67946bd8ca88610b235ee545
Anil Vishnoi <vishnoianil@gmail.com>
2016-08-02 03:05:49 -0700

    Port Pair/Group & Chain Listener registration and translator hookup
    Change-Id: I9bcb1e7906ded8cf19dbefb896e9cdd37359f1df
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>

--- openstack/sfc-translator/impl/src/main/java/org/opendaylight/netvirt/openstack/sfc/translator/OpenStackSFCTranslatorProvider.java
+++ openstack/sfc-translator/impl/src/main/java/org/opendaylight/netvirt/openstack/sfc/translator/OpenStackSFCTranslatorProvider.java
@@ -10,6 +10,9 @@ package org.opendaylight.netvirt.openstack.sfc.translator;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.netvirt.openstack.sfc.translator.flowclassifier.FlowClassifierTranslator;
+import org.opendaylight.netvirt.openstack.sfc.translator.portchain.PortChainTranslator;
+import org.opendaylight.netvirt.openstack.sfc.translator.portchain.PortPairGroupTranslator;
+import org.opendaylight.netvirt.openstack.sfc.translator.portchain.PortPairTranslator;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netvirt.openstack.sfc.translator.config.rev160720.OpenstackSfcTranslatorConfig;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
@@ -21,20 +24,28 @@ public class OpenStackSFCTranslatorProvider implements AutoCloseable {
     private final DataBroker dataBroker;
     private final BundleContext bundleContext;
     private final FlowClassifierTranslator flowClassifierTranslator;
+    private final PortPairTranslator portPairTranslator;
+    private final PortPairGroupTranslator portPairGroupTranslator;
+    private final PortChainTranslator portChainTranslator;
     public OpenStackSFCTranslatorProvider(
             final DataBroker dataBroker,
             final OpenstackSfcTranslatorConfig openstackSfcTranslatorConfig,
             final BundleContext bundleContext) {
-        LOG.info("OpenStack SFC Translator started");
         this.dataBroker = dataBroker;
         this.bundleContext = bundleContext;
         flowClassifierTranslator = new FlowClassifierTranslator(dataBroker);
+        portPairTranslator = new PortPairTranslator(dataBroker);
+        portPairGroupTranslator = new PortPairGroupTranslator(dataBroker);
+        portChainTranslator = new PortChainTranslator(dataBroker);
     }
 
     //This method will be called by blueprint, during bundle initialization.
     public void start() {
-        LOG.info("OpenStack SFC Translator Session Initiated");
+        LOG.info("OpenStack SFC Translator Session started");
         flowClassifierTranslator.start();
+        portPairTranslator.start();
+        portPairGroupTranslator.start();
+        portChainTranslator.start();
     }
 
     @Override
