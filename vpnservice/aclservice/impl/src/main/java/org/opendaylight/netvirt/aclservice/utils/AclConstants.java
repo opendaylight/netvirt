0ba1cdc5e141a535aa1bff5ffc764edb3c8d09b2
Shlomi <shlomi.alfasi@hpe.com>
2016-08-03 14:21:29 +0300

    learn security group
    this commit depends on:
    https://git.opendaylight.org/gerrit/#/c/43123/
    https://git.opendaylight.org/gerrit/#/c/42924/
    
    Change-Id: I2cf86f675d7439f073df7fd318c4e2e0655496cf
    Signed-off-by: Shlomi <shlomi.alfasi@hpe.com>

--- vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclConstants.java
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclConstants.java
@@ -9,6 +9,9 @@
 package org.opendaylight.netvirt.aclservice.utils;
 
 import java.math.BigInteger;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.opendaylight.ovsdb.utils.config.ConfigProperties;
 
 /**
@@ -61,11 +64,62 @@ public final class AclConstants {
     public static final long TCP_FLAG_ACK = 1 << 4;
     public static final long TCP_FLAG_SYN_ACK = TCP_FLAG_SYN + TCP_FLAG_ACK;
 
+    public static final short EGRESS_LEARN_TABLE = 42;
+    public static final short EGRESS_LEARN2_TABLE = 43;
+    public static final short INGRESS_LEARN_TABLE = 253;
+    public static final short INGRESS_LEARN2_TABLE = 254;
+
+    public static final BigInteger METADATA_MASK_LEARN_FLAG = new BigInteger("FFFFFFFFFFFFFFFE", 16);
+
+    private static final String SECURITY_GROUP_MODE_KEY = "security-group-mode";
+    public static final String SECURITY_GROUP_TCP_IDLE_TO_KEY = "security-group-tcp-idle-timeout";
+    public static final String SECURITY_GROUP_TCP_HARD_TO_KEY = "security-group-tcp-hard-timeout";
+    public static final String SECURITY_GROUP_TCP_FIN_IDLE_TO_KEY = "security-group-tcp-fin-idle-timeout";
+    public static final String SECURITY_GROUP_TCP_FIN_HARD_TO_KEY = "security-group-tcp-fin-hard-timeout";
+    public static final String SECURITY_GROUP_UDP_IDLE_TO_KEY = "security-group-udp-idle-timeout";
+    public static final String SECURITY_GROUP_UDP_HARD_TO_KEY = "security-group-udp-hard-timeout";
+
+    public static final String LEARN_MATCH_REG_VALUE = "1";
+
+    private static Map<String, Object> globalConf = Collections.synchronizedMap(new HashMap<>());
+
     private AclConstants() {
     }
 
-    public static boolean isStatelessAcl() {
-        final String enabledPropertyStr = ConfigProperties.getProperty(AclConstants.class, "stateless.sg.enabled");
-        return enabledPropertyStr != null && enabledPropertyStr.equalsIgnoreCase("yes");
+    public enum SecurtiyGroupMode {
+        STATELESS,
+        LEARN,
+        STATEFULL;
     }
+
+    public static SecurtiyGroupMode getSecurityGroupMode() {
+        SecurtiyGroupMode ret = SecurtiyGroupMode.STATEFULL;
+        SecurtiyGroupMode securityMode = (SecurtiyGroupMode)globalConf.get(SECURITY_GROUP_MODE_KEY);
+
+        if (securityMode == null) {
+            String modePropertyStr = ConfigProperties.getProperty(AclConstants.class, SECURITY_GROUP_MODE_KEY);
+            if (modePropertyStr != null) {
+                ret = SecurtiyGroupMode.valueOf(modePropertyStr.toUpperCase());
+            }
+            globalConf.put(SECURITY_GROUP_MODE_KEY, ret);
+        } else {
+            ret = securityMode;
+        }
+
+        return ret;
+    }
+
+    public static String getGlobalConf(String key, String defaultValue) {
+        String ret = defaultValue;
+        String value = (String)globalConf.get(key);
+        if (value == null) {
+            String propertyStr = ConfigProperties.getProperty(AclConstants.class, key);
+            if (propertyStr != null) {
+                ret = propertyStr;
+            }
+            globalConf.put(key, ret);
+        }
+        return ret;
+    }
+
 }
