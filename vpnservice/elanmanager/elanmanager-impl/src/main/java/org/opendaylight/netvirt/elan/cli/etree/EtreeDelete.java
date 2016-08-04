fe0c64b870aec37ef7cb59e559bb3a1ab64be5c6
Guy Sela <guy.sela@hpe.com>
2016-08-04 14:23:22 +0300

    Adding Etree:Remove CLI Command
    Change-Id: I7ff8401590ca544813bcfd7b15b97a5145993938
    Signed-off-by: Guy Sela <guy.sela@hpe.com>

---
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/cli/etree/EtreeDelete.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016 Hewlett Packard Enterprise, Co. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.elan.cli.etree;
+
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.opendaylight.netvirt.elanmanager.api.IElanService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Command(scope = "etree", name = "delete", description = "Deleting the Etree Instance")
+public class EtreeDelete extends OsgiCommandSupport {
+
+    @Argument(index = 0, name = "etreeName", description = "ETREE-NAME", required = true, multiValued = false)
+    private String etreeName;
+
+    private static final Logger LOG = LoggerFactory.getLogger(EtreeDelete.class);
+    private IElanService elanProvider;
+
+    public void setElanProvider(IElanService elanServiceProvider) {
+        this.elanProvider = elanServiceProvider;
+    }
+
+    @Override
+    protected Object doExecute() {
+        try {
+            LOG.debug("Executing the Deletion of EtreeInstance command" + "\t" + etreeName + "\t");
+            boolean isSuccess = elanProvider.deleteEtreeInstance(etreeName);
+            if (isSuccess) {
+                System.out.println("Etree Instance deleted successfully");
+            } else {
+                System.out.println("Etree Instance failed to delete");
+            }
+        } catch (Exception e) {
+            LOG.error("Failed to delete Etree Instance {}", e);
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
