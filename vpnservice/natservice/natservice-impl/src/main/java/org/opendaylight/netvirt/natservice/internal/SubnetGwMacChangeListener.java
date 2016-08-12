fbc22a04eb93e7d6ac243b057c2f9ac252759c29
Tali <tali.ben-meir@hpe.com>
2016-08-12 08:18:14 +0300

    <WIP> Listen to changes on subnet GW mac addresses
    - Update ext-net flow based on MAC changes
    
    Change-Id: Id107872a3c04d5d1a218ad87e020c1216adb6265
    Signed-off-by: Tali <tali.ben-meir@hpe.com>

---
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/SubnetGwMacChangeListener.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016 Hewlett Packard Enterprise, Co. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.natservice.internal;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
+import org.opendaylight.netvirt.neutronvpn.interfaces.INeutronVpnManager;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.NeutronVpnPortipPortData;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.neutron.vpn.portip.port.data.VpnPortipToPort;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SubnetGwMacChangeListener
+        extends AsyncDataTreeChangeListenerBase<VpnPortipToPort, SubnetGwMacChangeListener> {
+    private static final Logger LOG = LoggerFactory.getLogger(SubnetGwMacChangeListener.class);
+
+    private DataBroker broker;
+    private INeutronVpnManager nvpnManager;
+    private ExternalNetworkGroupInstaller extNetworkInstaller;
+
+    public SubnetGwMacChangeListener(DataBroker broker, INeutronVpnManager nvpnManager,
+            ExternalNetworkGroupInstaller extNetworkInstaller) {
+        super(VpnPortipToPort.class, SubnetGwMacChangeListener.class);
+        this.broker = broker;
+        this.nvpnManager = nvpnManager;
+        this.extNetworkInstaller = extNetworkInstaller;
+    }
+
+    public void start() {
+        registerListener(LogicalDatastoreType.OPERATIONAL, broker);
+    }
+
+    @Override
+    protected InstanceIdentifier<VpnPortipToPort> getWildCardPath() {
+        return InstanceIdentifier.builder(NeutronVpnPortipPortData.class).child(VpnPortipToPort.class).build();
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<VpnPortipToPort> key, VpnPortipToPort vpnPortipToPort) {
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<VpnPortipToPort> key, VpnPortipToPort origVpnPortipToPort,
+            VpnPortipToPort updatedVpnPortipToPort) {
+        handleSubnetGwIpChange(updatedVpnPortipToPort);
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<VpnPortipToPort> key, VpnPortipToPort vpnPortipToPort) {
+        handleSubnetGwIpChange(vpnPortipToPort);
+    }
+
+    @Override
+    protected SubnetGwMacChangeListener getDataTreeChangeListener() {
+        return this;
+    }
+
+    private void handleSubnetGwIpChange(VpnPortipToPort vpnPortipToPort) {
+        String macAddress = vpnPortipToPort.getMacAddress();
+        if (macAddress == null) {
+            LOG.trace("Mac address is null for VpnPortipToPort");
+            return;
+        }
+
+        String fixedIp = vpnPortipToPort.getPortFixedip();
+        if (fixedIp == null) {
+            LOG.trace("Fixed ip is null for VpnPortipToPort");
+            return;
+        }
+
+        for (Uuid subnetId : nvpnManager.getSubnetIdsForGatewayIp(new IpAddress(new Ipv4Address(fixedIp)))) {
+            LOG.trace("Updating MAC resolution for GW ip {} to {}", fixedIp, macAddress);
+            extNetworkInstaller.installExtNetGroupEntries(subnetId, macAddress);
+        }
+    }
+
+}
