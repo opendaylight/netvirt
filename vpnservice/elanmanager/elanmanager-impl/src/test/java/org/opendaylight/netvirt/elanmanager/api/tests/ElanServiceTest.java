21d37da01a70310ca607c71219822269f4f9cc8d
Michael Vorburger <vorburger@redhat.com>
2016-08-15 16:41:58 +0200

    elanmanager-impl component test v0.1; coverage from 0% to 5%
    incl. refactoring to make ElanStatusMonitor more pluggable for tests
    
    TODO ElanStatusMonitor stuff should disappear from this commit again..
    
    Change-Id: I3d07660ffc913844afc07201ef1854aba93c6aab
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ vpnservice/elanmanager/elanmanager-impl/src/test/java/org/opendaylight/netvirt/elanmanager/api/tests/ElanServiceTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.elanmanager.api.tests;
+
+import static org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType.CONFIGURATION;
+
+import com.google.common.base.Optional;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
+import org.opendaylight.controller.md.sal.binding.test.AbstractDataBrokerTest;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.mdsal.binding.testutils.AssertDataObjects;
+import org.opendaylight.netvirt.elanmanager.api.IElanService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.ElanInstances;
+import org.opendaylight.yangtools.yang.binding.ChildOf;
+import org.opendaylight.yangtools.yang.binding.DataRoot;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * End-to-end test of IElanService.
+ *
+ * @author Michael Vorburger
+ */
+public class ElanServiceTest extends AbstractDataBrokerTest {
+    // TODO remove "extends AbstractDataBrokerTest" when https://git.opendaylight.org/gerrit/#/c/43645/ is merged
+
+    DataBroker dataBroker;
+    IElanService elanService;
+    ElanServiceTestModule testModule;
+    IMdsalApiManager mdsalApiManager;
+
+    @Test
+    public void elanServiceTestModule() {
+        // Intentionally empty; the goal is just to test the ElanServiceTestModule
+    }
+
+    @Test
+    public void createElanInstance() throws Exception {
+        elanService.createElanInstance("TestELanName", 12345, "TestELan description");
+        ElanInstances actualElanInstance = read(CONFIGURATION, ElanInstances.class);
+        AssertDataObjects.assertEqualBeans(ExpectedObjects.createElanInstance(), actualElanInstance);
+    }
+
+    // TODO MOVE THIS CODE TO A HELPER; see idea in https://git.opendaylight.org/gerrit/#/c/44099/
+    <T extends ChildOf<? extends DataRoot>>
+        T read(LogicalDatastoreType store, final Class<T> container) throws ReadFailedException {
+
+        InstanceIdentifier<T> containerInstanceIdentifier = InstanceIdentifier.builder(container).build();
+        try ( ReadOnlyTransaction tx = dataBroker.newReadOnlyTransaction() ) {
+            Optional<T> optional = tx.read(store, containerInstanceIdentifier).checkedGet();
+            if (optional.isPresent()) {
+                return optional.get();
+            } else {
+                throw new ReadFailedException("Nothing in the " + store
+                        + " datastore at: " + containerInstanceIdentifier);
+            }
+        }
+    }
+
+
+    // TODO Replace this with new @Rule from infrautils...
+
+    @Before
+    public void setUp() throws Exception {
+        testModule = new ElanServiceTestModule(getDataBroker());
+        dataBroker = testModule.dataBroker();
+        mdsalApiManager = testModule.mdsalApiManager();
+        elanService = testModule.elanService();
+        testModule.start();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (testModule != null) {
+            testModule.close();
+        }
+    }
+}
