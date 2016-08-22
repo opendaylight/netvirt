f408ad1f50e729943e0fb1efc1d5355c33776def
Sam Hague <shague@redhat.com>
2016-08-21 23:50:35 -0400

    Add support for using kernel modules with docker for IT
    This enabled support for using the kernel modules with Docker containers. Also the default seucity-group-mode is set to transparent until the proper support for stateful and stateless are included in the IT.
    
    Use the following commands to test netvirt from the netvirt/vpnservice directory:
    
    1. Using DockerOvs in userspace
    mvn -nsu -f it/pom.xml verify -Pintegrationtest -Dovsdb.controller.address=172.17.0.1 -Dit.test=NetvirtIT#testNeutronNet -Dovsdb.userspace.enabled=yes -Dsgm=transparent
    
    2. Using DockerOvs with kernel modules.
    mvn -nsu -f it/pom.xml verify -Pintegrationtest -Dovsdb.controller.address=172.17.0.1 -Dit.test=NetvirtIT#testNeutronNet -Dsgm=transparent
    
    3. Using an externally started Docker container (or vm)
    mvn -nsu -f it/pom.xml verify -Pintegrationtest -Dovsdb.controller.address=172.17.0.1 -Dit.test=NetvirtIT#testNeutronNet -Dovsdb.userspace.enabled=yes -Dsgm=transparent -Dovsdbserver.ipaddress=127.0.0.1 -Dovsdbserver.port=6641
    
    Use the following commands to test legacy netvirt from the netvirt/openstack directory:
    
    mvn -nsu -f net-virt-it/pom.xml verify -Pintegrationtest -Dovsdb.controller.address=172.17.0.1 -Dit.test=NetvirtIT#testNeutronNet -Dovsdb.userspace.enabled=yes
    mvn -nsu -f net-virt-it/pom.xml verify -Pintegrationtest -Dovsdb.controller.address=172.17.0.1 -Dit.test=NetvirtIT#testNeutronNet
    mvn -nsu -f net-virt-it/pom.xml verify -Pintegrationtest -Dovsdb.controller.address=172.17.0.1 -Dit.test=NetvirtIT#testNeutronNet -Dovsdb.userspace.enabled=yes -Dovsdbserver.ipaddress=127.0.0.1 -Dovsdbserver.port=6641
    
    Change-Id: I9e8fac1fa25d9e55f763f66bc87984ebab8738d3
    Signed-off-by: Sam Hague <shague@redhat.com>

---
+++ vpnservice/it/src/test/java/org/opendaylight/netvirt/it/AbstractNetOvs.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.it;
+
+import com.google.common.collect.Maps;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.ovsdb.utils.mdsal.utils.MdsalUtils;
+import org.opendaylight.ovsdb.utils.ovsdb.it.utils.DockerOvs;
+import org.opendaylight.ovsdb.utils.southbound.utils.SouthboundUtils;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.Ports;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.ports.Port;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.ports.PortBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbstractNetOvs implements NetOvs {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractNetOvs.class);
+    protected final DockerOvs dockerOvs;
+    protected final Boolean isUserSpace;
+    protected final MdsalUtils mdsalUtils;
+    public final Neutron neutron;
+    private final SouthboundUtils southboundUtils;
+    protected static final int DEFAULT_WAIT = 30 * 1000;
+    protected Map<String, PortInfo> portInfoByName = new HashMap<>();
+
+    AbstractNetOvs(final DockerOvs dockerOvs, final Boolean isUserSpace, final MdsalUtils mdsalUtils,
+                   final Neutron neutron, SouthboundUtils southboundUtils) {
+        this.dockerOvs = dockerOvs;
+        this.isUserSpace = isUserSpace;
+        this.mdsalUtils = mdsalUtils;
+        this.neutron = neutron;
+        this.southboundUtils = southboundUtils;
+        LOG.info("{} isUserSpace: {}, usingExternalDocker: {}",
+                getClass().getSimpleName(), isUserSpace, dockerOvs.usingExternalDocker());
+    }
+
+    protected PortInfo buildPortInfo() {
+        long idx = portInfoByName.size() + 1;
+        return new PortInfo(idx);
+    }
+
+    @Override
+    public String createPort(Node bridgeNode) throws InterruptedException, IOException {
+        return null;
+    }
+
+    @Override
+    public void deletePort(String uuid) {
+        if (uuid == null) {
+            return;
+        }
+
+        Port port = new PortBuilder()
+                .setUuid(new Uuid(uuid))
+                .build();
+
+        mdsalUtils.delete(LogicalDatastoreType.CONFIGURATION, InstanceIdentifier
+                .create(org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron.class)
+                .child(Ports.class).child(Port.class, port.getKey()));
+    }
+
+    @Override
+    public void destroy() {
+        for (PortInfo portInfo : portInfoByName.values()) {
+            deletePort(portInfo.id);
+        }
+        portInfoByName.clear();
+
+        neutron.deleteSubnet();
+        neutron.deleteNetwork();
+    }
+
+    @Override
+    public void preparePortForPing(String portName) throws InterruptedException, IOException {
+    }
+
+    @Override
+    public int ping(String fromPort, String toPort) throws InterruptedException, IOException {
+        return 0;
+    }
+
+    protected void addTerminationPoint(PortInfo portInfo, Node bridge, String portType) {
+        Map<String, String> externalIds = Maps.newHashMap();
+        externalIds.put("attached-mac", portInfo.mac);
+        externalIds.put("iface-id", portInfo.id);
+        southboundUtils.addTerminationPoint(bridge, portInfo.name, portType, null, externalIds, portInfo.ofPort);
+    }
+
+    @Override
+    public void logState(int dockerInstance, String logText) throws IOException, InterruptedException {
+    }
+}
