7db4b7f4f60908963f0332051ffca42cf7bc4eb5
Anil Vishnoi <vishnoianil@gmail.com>
2016-08-29 12:14:11 -0700

    Bug 6555 - Translation layer skip the RSP creation API call
    Change-Id: Id02c7efed8d78960c7b74c813980b42a88335143
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>

--- openstack/sfc-translator/impl/src/main/java/org/opendaylight/netvirt/openstack/sfc/translator/portchain/NeutronPortPairListener.java
+++ openstack/sfc-translator/impl/src/main/java/org/opendaylight/netvirt/openstack/sfc/translator/portchain/NeutronPortPairListener.java
@@ -14,15 +14,28 @@ import org.opendaylight.netvirt.openstack.sfc.translator.DelegatingDataTreeListe
 import org.opendaylight.netvirt.openstack.sfc.translator.NeutronMdsalHelper;
 import org.opendaylight.netvirt.openstack.sfc.translator.OvsdbMdsalHelper;
 import org.opendaylight.netvirt.openstack.sfc.translator.SfcMdsalHelper;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.common.rev151017.SffName;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunction;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sf.rev140701.service.functions.ServiceFunctionKey;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.ServiceFunctionForwarder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.ServiceFunctionForwarderBuilder;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.ServiceFunctionForwarderKey;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sff.rev140701.service.function.forwarders.service.function.forwarder.ServiceFunctionDictionary;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.sfc.rev160511.sfc.attributes.PortPairs;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.sfc.rev160511.sfc.attributes.port.pairs.PortPair;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * OpenDaylight Neutron Port Pair yang models data change listener
  */
 public class NeutronPortPairListener extends DelegatingDataTreeListener<PortPair> {
+    private static final Logger LOG = LoggerFactory.getLogger(NeutronPortPairListener.class);
+
     private static final InstanceIdentifier<PortPair> portPairIid =
             InstanceIdentifier.create(Neutron.class).child(PortPairs.class).child(PortPair.class);
 
@@ -47,7 +60,48 @@ public class NeutronPortPairListener extends DelegatingDataTreeListener<PortPair
      */
     @Override
     public void remove(InstanceIdentifier<PortPair> path, PortPair deletedPortPair) {
-        sfcMdsalHelper.removeServiceFunction(PortPairTranslator.getSFKey(deletedPortPair));
+        ServiceFunctionKey sfKey = PortPairTranslator.getSFKey(deletedPortPair);
+        ServiceFunction serviceFunction = sfcMdsalHelper.readServiceFunction(sfKey);
+        if (serviceFunction != null ) {
+            //Current support is for one Service Function connected to one data plane locator
+            SffName sffName = serviceFunction.getSfDataPlaneLocator().get(0).getServiceFunctionForwarder();
+            if (sffName != null) {
+                ServiceFunctionForwarderKey sffKey = new ServiceFunctionForwarderKey(sffName);
+                ServiceFunctionForwarder sff = sfcMdsalHelper.readServiceFunctionForwarder(sffKey);
+                if (sff != null) {
+                    List<ServiceFunctionDictionary> sfDictionary =  sff.getServiceFunctionDictionary();
+                    if (sfDictionary != null) {
+                        ServiceFunctionDictionary matchingSfd = null;
+                        for (ServiceFunctionDictionary sfd : sfDictionary) {
+                            if (sfd.getName().equals(sfKey.getName())) {
+                                matchingSfd = sfd;
+                                break;
+                            }
+                        }
+                        if (matchingSfd != null) {
+                            sfDictionary.remove(matchingSfd);
+                            if (sfDictionary.isEmpty()) {
+                                //If the current SF is last SF attach to SFF, delete the SFF
+                                sfcMdsalHelper.deleteServiceFunctionForwarder(sffKey);
+                            } else {
+                                //If there are SF attached to the SFF, just update the SFF dictionary by removing
+                                // this deleted SF.
+                                ServiceFunctionForwarderBuilder sffBuilder = new ServiceFunctionForwarderBuilder(sff);
+                                LOG.info("Remove the SF {} from SFF {} dictionary and update in data store.", sfKey
+                                        .getName(), sffName);
+                                sfcMdsalHelper.addServiceFunctionForwarder(sffBuilder.build());
+                            }
+                        } else {
+                            LOG.info("Service Function {} not present in connected Service Function Forwarder {} " +
+                                    "dictionary", sfKey.getName(), sffName);
+                        }
+                    }
+                }
+            }
+        } else {
+            LOG.info("Service Function related to Port Pair {} don't exist.", deletedPortPair);
+        }
+        sfcMdsalHelper.removeServiceFunction(sfKey);
     }
 
     /**
