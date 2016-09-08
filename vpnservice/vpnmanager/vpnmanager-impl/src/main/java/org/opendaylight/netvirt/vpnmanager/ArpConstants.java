284775c67ca4db87bb4eccf3b919c069e3c905b9
Gobinath <gobinath@ericsson.com>
2016-09-08 20:01:48 +0530

    Arp cache feature changes
    Change the implementation of Arp Cache feature so that
    the Arp entries learnt for the VMs are removed only when
    those VMs are no longer accessible to the gateway.
    
    1) Remove the old implementation where as soon as a VM's mac
    is learnt, it is enqueued in a queue with a timer and this entry
    is removed after the timer expires. After the removal from queue,
    the mac and FIB entries of the VM were removed.
    
    2) When a VM's arp is learnt, use the Aliveness Monitor module to
    monitor the VM's by sending out ARP packets from gateway to the
    VM periodically. The aliveness monitor module should listen to the
    VpnPortIpData which would contain the ports present in the vpn.
    
    3) Unless the monitor event "down" is received,ie,the VM is no
    longer accessible from the gateway, the Mac entries for the VM are
    to be retained.
    
    4) When the monitor event "down" is received, the mac entries for
    that VM and their FIB entries are to be removed.
    
    Change-Id: I4c29e4390a1c5be0609e8ee1d682728ab0762588
    Signed-off-by: Gobinath <gobinath@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/ArpConstants.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/ArpConstants.java
@@ -9,22 +9,15 @@ package org.opendaylight.netvirt.vpnmanager;
 
 public class ArpConstants {
 
-        public static final int THREAD_POOL_SIZE = 5;
-        public static final int NO_DELAY = 0;
-        public static final int RETRY_COUNT = 5;
-        public static final short ARP_REQUEST_OP = (short) 1;
-        public static final short ETH_TYPE_ARP = 0x0806;
         public static final String PREFIX = "/32";
         public static final String NODE_CONNECTOR_NOT_FOUND_ERROR = "Node connector id not found for interface %s";
         public static final String FAILED_TO_GET_SRC_IP_FOR_INTERFACE = "Failed to get src ip for %s";
         public static final String FAILED_TO_GET_SRC_MAC_FOR_INTERFACE = "Failed to get src mac for interface %s iid %s ";
-        public static final int PERIOD = 10000;
         public static final String ARPJOB = "Arpcache";
-        public static final long DEFAULT_ARP_LEARNED_CACHE_TIMEOUT = 300000L; /* 300 seconds = 5 minutes */
-
+        public static final long DEFAULT_ARP_LEARNED_CACHE_TIMEOUT = 120000; /* 120 seconds = 2 minutes */
         public static final String ARP_CACHE_TIMEOUT_PROP = "arp.cache.timeout";
-
         public static long ARP_CACHE_TIMEOUT_MILLIS = Long.getLong(ArpConstants.ARP_CACHE_TIMEOUT_PROP,
                 ArpConstants.DEFAULT_ARP_LEARNED_CACHE_TIMEOUT);
-
+        public static final long FAILURE_THRESHOLD = 2;
+        public static final long MONITORING_WINDOW = 4;
 }
