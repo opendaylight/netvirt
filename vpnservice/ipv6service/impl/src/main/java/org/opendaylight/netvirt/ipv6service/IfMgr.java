a7637058d97e5bd3c6f8925db18bd34c56e5d6ba
Sridhar Gaddam <sgaddam@redhat.com>
2016-09-12 12:24:34 +0530

    Bug 6691: Fix exceptions in natservice for a dual-stack network
    This patch also updates some debug messages.
    
    Change-Id: I840b0782d3e6b5978df63d8885cdc7c3b6c8cc9e
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- vpnservice/ipv6service/impl/src/main/java/org/opendaylight/netvirt/ipv6service/IfMgr.java
+++ vpnservice/ipv6service/impl/src/main/java/org/opendaylight/netvirt/ipv6service/IfMgr.java
@@ -133,7 +133,7 @@ public class IfMgr {
             List<VirtualPort> intfList = unprocessedRouterIntfs.get(rtrUuid);
 
             if (intfList == null) {
-                LOG.info("intfList is null for {}", rtrUuid);
+                LOG.info("No unprocessed interfaces for the router {}", rtrUuid);
                 return;
             }
 
@@ -216,7 +216,7 @@ public class IfMgr {
 
             List<VirtualPort> intfList = unprocessedSubnetIntfs.get(snetId);
             if (intfList == null) {
-                LOG.info("interfaces are not available for the subnet {}", snetId);
+                LOG.info("No unprocessed interfaces for the subnet {}", snetId);
                 return;
             }
             for (VirtualPort intf : intfList) {
@@ -326,7 +326,7 @@ public class IfMgr {
         LOG.debug("updateRouterIntf portId {}, fixedIpsList {} ", portId, fixedIpsList);
         VirtualPort intf = vintfs.get(portId);
         if (intf == null) {
-            LOG.warn("Update Router interface failed. Could not get router interface details {}", portId);
+            LOG.info("Skip Router interface update for non-ipv6 port {}", portId);
             return;
         }
 
