4b9c651b3168b7844ab8903b0fc6dc3f570edef3
Akash <a.k.sahu@ericsson.com>
2016-09-13 16:25:51 +0530

    Bug 6589 adding support for hwvtep devices ha
    Added template code to copy the data between ha and child nodes.
    When a child node connects copy its operational data to its ha operational
    node.
    When operational update is received on child node , copy that operational
    update to its ha operational node.
    When some config update happens on ha node, copy that config update to
    child node.
    
    An add/delete logical switch update from child is immediately pushed to
    parent ha node.
    Duplicate updates from childs is properly filtered out while copying to
    the parent ha node.
    
    Change-Id: Iee182888d25fc45d1cddce5894272335b9650d56
    Signed-off-by: Akash <a.k.sahu@ericsson.com>

---
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/ha/HwvtepHAUtil.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.elan.l2gw.ha;
+
+import static org.opendaylight.genius.utils.hwvtep.HwvtepSouthboundConstants.HWVTEP_TOPOLOGY_ID;
+
+import java.util.List;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HwvtepHAUtil {
+
+    static Logger LOG = LoggerFactory.getLogger(HwvtepHAUtil.class);
+
+
+    public static Uuid getUUid(String key) {
+        return new Uuid(java.util.UUID.nameUUIDFromBytes(key.getBytes()).toString());
+    }
+
+    public static boolean isEmptyList(List list) {
+        if (list == null || list.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static InstanceIdentifier<Node> convertToInstanceIdentifier(String nodeIdString) {
+        NodeId nodeId = new NodeId(new Uri(nodeIdString));
+        NodeKey nodeKey = new NodeKey(nodeId);
+        TopologyKey topoKey = new TopologyKey(HWVTEP_TOPOLOGY_ID);
+        return InstanceIdentifier.builder(NetworkTopology.class)
+                .child(Topology.class, topoKey)
+                .child(Node.class, nodeKey)
+                .build();
+    }
+}
