a87e07e3707cb241f29f9d19bc524128d9bf7967
Akash <a.k.sahu@ericsson.com>
2016-09-13 16:25:51 +0530

    Bug 6589 adding support for hwvtep devices ha
    Added template code to copy the data between ha and child nodes.
    When a child node connects copy its operational data to its ha operational
    node.
    When operational update is received on child node , copy that operational
    update to its ha operational node.
    When some config update happens on ha node, copy that config update to
    child node.
    
    An add/delete logical switch update from child is immediately pushed to
    parent ha node.
    Duplicate updates from childs is properly filtered out while copying to
    the parent ha node.
    
    Change-Id: Iee182888d25fc45d1cddce5894272335b9650d56
    Signed-off-by: Akash <a.k.sahu@ericsson.com>

---
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/ha/commands/IMergeCommand.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.elan.l2gw.ha.commands;
+
+import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.concepts.Builder;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public interface IMergeCommand<T extends DataObject, BuilderType extends Builder, AugType extends DataObject> {
+
+    /**
+     * Abstract function to merge data from src to dst in Operational Topology
+     * while existing data helps in keeping track of data only updated
+     * @param dst Builder which will be used to build concrete object
+     * @param existingData dataObject which are already exisitng
+     * @param src
+     * @param nodePath nodePath of dest
+     */
+    public void mergeOperationalData(BuilderType dst,
+                                     AugType existingData,
+                                     AugType src,
+                                     InstanceIdentifier<Node> nodePath);
+
+    /**
+     * Abstract function to merge data from src to dst in Config Topology
+     * @param dst builder which will be used to build concrete object
+     * @param src builder which are to be merged in destination
+     * @param nodePath nodePath of dest
+     */
+    public void mergeConfigData(BuilderType dst,
+                                AugType src,
+                                InstanceIdentifier<Node> nodePath);
+
+    /**
+     * Abstract function to update data from src to dst in Config Topology
+     * while existing data helps in keeping track of data only updated
+     * @param existingData dataObject which are already exisitng
+     * @param updated updated data
+     * @param orig original data
+     * @param nodePath nodePath of dest
+     * @param tx ReadWriteTransaction
+     */
+    public void mergeConfigUpdate(AugType existingData,
+                                  AugType updated,
+                                  AugType orig,
+                                  InstanceIdentifier<Node> nodePath,
+                                  ReadWriteTransaction tx);
+
+    /**
+     * Abstract function to update data from src to dst in Operational Topology
+     * while existing data helps in keeping track of data only updated
+     * @param existingData dataObject which are already exisitng
+     * @param updatedSrc updated data source
+     * @param origSrc original data source
+     * @param nodePath nodePath of dest
+     * @param tx ReadWriteTransaction
+     */
+    public void mergeOpUpdate(AugType existingData,
+                              AugType updatedSrc,
+                              AugType origSrc,
+                              InstanceIdentifier<Node> nodePath,
+                              ReadWriteTransaction tx);
+
+}
