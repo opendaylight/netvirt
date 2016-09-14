e84c69eb35dc6b5ac70b6343f1a83474ea3cf1ab
Akash <a.k.sahu@ericsson.com>
2016-09-14 16:53:10 +0530

    Bug 6589 adding support for hwvtep devices ha
    Added template code to copy the data between ha and child nodes.
    When a child node connects copy its operational data to its ha operational
    node.
    When operational update is received on child node , copy that operational
    update to its ha operational node.
    When some config update happens on ha node, copy that config update to
    child node.
    
    An add/delete Macs.PhysicalLocator,TerminationPoint,Switch update from
    child is immediately pushed to parent ha node.
    Duplicate updates from childs is properly filtered out while copying to
    the parent ha node.
    
    Change-Id: I091ab1f1fa8b82445918e1030f20136421286299
    Signed-off-by: Akash <a.k.sahu@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/ha/HwvtepHAUtil.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/ha/HwvtepHAUtil.java
@@ -13,12 +13,21 @@ import java.util.List;
 
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepGlobalAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepLogicalSwitchRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepNodeName;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepPhysicalLocatorRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.LogicalSwitches;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.LogicalSwitchesKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TpId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPointKey;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,9 +36,24 @@ public class HwvtepHAUtil {
 
     static Logger LOG = LoggerFactory.getLogger(HwvtepHAUtil.class);
 
+    //TODO reuse HWvtepSouthboundConstants
+    public static final String TEP_PREFIX = "vxlan_over_ipv4:";
+
 
     public static Uuid getUUid(String key) {
         return new Uuid(java.util.UUID.nameUUIDFromBytes(key.getBytes()).toString());
+    }
+
+    public static HwvtepPhysicalLocatorRef buildLocatorRef(InstanceIdentifier<Node> nodeIid, String tepIp ) {
+        InstanceIdentifier<TerminationPoint> tepId = buildTpId(nodeIid, tepIp);
+        return new HwvtepPhysicalLocatorRef(tepId);
+    }
+
+    public static InstanceIdentifier<TerminationPoint> buildTpId(InstanceIdentifier<Node> nodeIid,String tepIp ) {
+        String tpKeyStr = TEP_PREFIX + tepIp;
+        TerminationPointKey tpKey = new TerminationPointKey(new TpId(tpKeyStr));
+        InstanceIdentifier<TerminationPoint> plIid = nodeIid.child(TerminationPoint.class, tpKey);
+        return plIid;
     }
 
     public static boolean isEmptyList(List list) {
@@ -47,5 +71,30 @@ public class HwvtepHAUtil {
                 .child(Topology.class, topoKey)
                 .child(Node.class, nodeKey)
                 .build();
+    }
+
+    public static HwvtepLogicalSwitchRef convertLogicalSwitchRef(HwvtepLogicalSwitchRef src,
+                                                                 InstanceIdentifier<Node> nodePath) {
+        InstanceIdentifier<LogicalSwitches> srcId = (InstanceIdentifier<LogicalSwitches>)src.getValue();
+        HwvtepNodeName switchName = srcId.firstKeyOf(LogicalSwitches.class).getHwvtepNodeName();
+        InstanceIdentifier<LogicalSwitches> iid = nodePath.augmentation(HwvtepGlobalAugmentation.class)
+                .child(LogicalSwitches.class, new LogicalSwitchesKey(switchName));
+        HwvtepLogicalSwitchRef ref = new HwvtepLogicalSwitchRef(iid);
+        return ref;
+    }
+
+    public static String getTepIpVal(HwvtepPhysicalLocatorRef locatorRef) {
+        InstanceIdentifier<TerminationPoint> tpId = (InstanceIdentifier<TerminationPoint>) locatorRef.getValue();
+        return tpId.firstKeyOf(TerminationPoint.class).getTpId().getValue().substring("vxlan_over_ipv4:".length());
+    }
+
+    public static HwvtepPhysicalLocatorRef convertLocatorRef(HwvtepPhysicalLocatorRef src,
+                                                             InstanceIdentifier<Node> nodePath) {
+        InstanceIdentifier<TerminationPoint> srcTepPath = (InstanceIdentifier<TerminationPoint>)src.getValue();
+        TpId tpId = srcTepPath.firstKeyOf(TerminationPoint.class).getTpId();
+        InstanceIdentifier<TerminationPoint> tpPath =
+                nodePath.child(TerminationPoint.class, new TerminationPointKey(tpId));
+        HwvtepPhysicalLocatorRef locatorRef = new HwvtepPhysicalLocatorRef(tpPath);
+        return locatorRef;
     }
 }
