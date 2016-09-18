53b548af99c9eb91985034a6e34baf3bb35a740c
Ravit Peretz <ravit.peretz@hpe.com>
2016-09-18 20:04:38 +0300

    Bug 6742 FloatingIPHanlder should use the external interface-name
    Change-Id: I9fb58b26139afec6d09fa7792b7e94570804244d
    Signed-off-by: Ravit Peretz <ravit.peretz@hpe.com>

--- vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatUtil.java
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatUtil.java
@@ -1086,30 +1086,37 @@ public class NatUtil {
         return listActionInfo;
     }
 
-    public static Port getNeutronPortForFloatingIp(DataBroker broker,
-        IpAddress targetIP) {
+    public static Port getNeutronPortForFloatingIp(DataBroker broker, IpAddress targetIP) {
     	return getNeutronPortForIp(broker, targetIP, NeutronConstants.DEVICE_OWNER_FLOATING_IP);
     }
 
-    public static Port getNeutronPortForRouterGetewayIp(DataBroker broker,
-        IpAddress targetIP) {
+    public static Port getNeutronPortForRouterGetewayIp(DataBroker broker, IpAddress targetIP) {
     	return getNeutronPortForIp(broker, targetIP, NeutronConstants.DEVICE_OWNER_GATEWAY_INF);
     }
 
-    public static Port getNeutronPortForIp(DataBroker broker,
-        IpAddress targetIP, String deviceType) {
+    public static List<Port> getNeutronPorts(DataBroker broker) {
         InstanceIdentifier<org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.Ports>
         portsIdentifier = InstanceIdentifier
                 .create(Neutron.class)
                 .child(org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.Ports.class);
         Optional<org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.Ports> portsOptional = read(
                 broker, LogicalDatastoreType.CONFIGURATION, portsIdentifier);
+
         if (!portsOptional.isPresent() || portsOptional.get().getPort() == null) {
             LOG.trace("No neutron ports found");
-            return null;
+            return Collections.EMPTY_LIST;
         }
 
-        for (Port port : portsOptional.get().getPort()) {
+        return portsOptional.get().getPort();
+
+    }
+
+    public static Port getNeutronPortForIp(DataBroker broker,
+        IpAddress targetIP, String deviceType) {
+        List<Port> ports = getNeutronPorts(
+                broker);
+
+        for (Port port : ports) {
             if (deviceType.equals(port.getDeviceOwner()) && port.getFixedIps() != null) {
                 for (FixedIps ip : port.getFixedIps()) {
                     if (Objects.equals(ip.getIpAddress(), targetIP)) {
