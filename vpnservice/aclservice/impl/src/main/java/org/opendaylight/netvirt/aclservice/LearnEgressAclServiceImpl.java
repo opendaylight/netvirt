d909858312360faefc5a079f1fee85f7354468d2
Tomer <tomer.pearl@hpe.com>
2016-09-19 17:53:06 +0300

    Bug 6748: ACL mechanism uses reg5 instead of reg6
    Change-Id: I7d686532abe60585c7f4294c27da044716127cb0
    Signed-off-by: Tomer <tomer.pearl@hpe.com>

--- vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/LearnEgressAclServiceImpl.java
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/LearnEgressAclServiceImpl.java
@@ -113,7 +113,7 @@ public class LearnEgressAclServiceImpl extends AbstractEgressAclServiceImpl {
                 NwConstants.NxmOfFieldType.NXM_OF_IP_PROTO.getFlowModHeaderLen() };
         flowMod[4] = new String[] {
                 NwConstants.LearnFlowModsType.COPY_FROM_VALUE.name(), AclConstants.LEARN_MATCH_REG_VALUE,
-                NwConstants.NxmOfFieldType.NXM_NX_REG6.getHexType(), "8" };
+                NwConstants.NxmOfFieldType.NXM_NX_REG5.getHexType(), "8" };
 
         String[] header = new String[] {
                 AclConstants.getGlobalConf(AclConstants.SECURITY_GROUP_UDP_IDLE_TO_KEY, "60"),
@@ -149,7 +149,7 @@ public class LearnEgressAclServiceImpl extends AbstractEgressAclServiceImpl {
                 NwConstants.NxmOfFieldType.NXM_OF_ETH_SRC.getFlowModHeaderLen() };
         flowMod[5] = new String[] {
                 NwConstants.LearnFlowModsType.COPY_FROM_VALUE.name(), AclConstants.LEARN_MATCH_REG_VALUE,
-                NwConstants.NxmOfFieldType.NXM_NX_REG6.getHexType(), "8" };
+                NwConstants.NxmOfFieldType.NXM_NX_REG5.getHexType(), "8" };
 
         String[] header = new String[] {
                 AclConstants.getGlobalConf(AclConstants.SECURITY_GROUP_UDP_IDLE_TO_KEY, "3600"),
@@ -185,7 +185,7 @@ public class LearnEgressAclServiceImpl extends AbstractEgressAclServiceImpl {
                 NwConstants.NxmOfFieldType.NXM_OF_ETH_SRC.getFlowModHeaderLen() };
         flowMod[5] = new String[] {
                 NwConstants.LearnFlowModsType.COPY_FROM_VALUE.name(), AclConstants.LEARN_MATCH_REG_VALUE,
-                NwConstants.NxmOfFieldType.NXM_NX_REG6.getHexType(), "8" };
+                NwConstants.NxmOfFieldType.NXM_NX_REG5.getHexType(), "8" };
 
         String[] header = new String[] {
                 AclConstants.getGlobalConf(AclConstants.SECURITY_GROUP_UDP_IDLE_TO_KEY, "60"),
--- vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/LearnIngressAclServiceImpl.java
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/LearnIngressAclServiceImpl.java
@@ -105,7 +105,7 @@ public class LearnIngressAclServiceImpl extends AbstractIngressAclServiceImpl {
                 NwConstants.NxmOfFieldType.NXM_OF_IP_PROTO.getFlowModHeaderLen() };
         flowMod[4] = new String[] {
                 NwConstants.LearnFlowModsType.COPY_FROM_VALUE.name(), AclConstants.LEARN_MATCH_REG_VALUE,
-                NwConstants.NxmOfFieldType.NXM_NX_REG6.getHexType(), "8" };
+                NwConstants.NxmOfFieldType.NXM_NX_REG5.getHexType(), "8" };
 
         String[] header = new String[] {
                 AclConstants.getGlobalConf(AclConstants.SECURITY_GROUP_UDP_IDLE_TO_KEY, "60"),
@@ -141,7 +141,7 @@ public class LearnIngressAclServiceImpl extends AbstractIngressAclServiceImpl {
                 NwConstants.NxmOfFieldType.NXM_OF_ETH_SRC.getFlowModHeaderLen() };
         flowMod[5] = new String[] {
                 NwConstants.LearnFlowModsType.COPY_FROM_VALUE.name(), AclConstants.LEARN_MATCH_REG_VALUE,
-                NwConstants.NxmOfFieldType.NXM_NX_REG6.getHexType(), "8" };
+                NwConstants.NxmOfFieldType.NXM_NX_REG5.getHexType(), "8" };
 
         String[] header = new String[] {
                 AclConstants.getGlobalConf(AclConstants.SECURITY_GROUP_UDP_IDLE_TO_KEY, "3600"),
@@ -177,7 +177,7 @@ public class LearnIngressAclServiceImpl extends AbstractIngressAclServiceImpl {
                 NwConstants.NxmOfFieldType.NXM_OF_ETH_SRC.getFlowModHeaderLen() };
         flowMod[5] = new String[] {
                 NwConstants.LearnFlowModsType.COPY_FROM_VALUE.name(), AclConstants.LEARN_MATCH_REG_VALUE,
-                NwConstants.NxmOfFieldType.NXM_NX_REG6.getHexType(), "8" };
+                NwConstants.NxmOfFieldType.NXM_NX_REG5.getHexType(), "8" };
 
         String[] header = new String[] {
                 AclConstants.getGlobalConf(AclConstants.SECURITY_GROUP_UDP_IDLE_TO_KEY, "60"),
