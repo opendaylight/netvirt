aa82462012880009a07e55143a73d8f877ab57ef
Vivekanandan Narasimhan <n.vivekanandan@ericsson.com>
2016-09-22 22:53:59 +0530

    Bug 6721: first few ping requests to a floating IP are receiving multiple responses
    This fix attempts to prevent multiple Datapaths replicating
    an IP response from external network to floating ip hosted
    inside the cloud.
    
    As part of this fix, we install floatingipportmac into
    MyMac Table (Table 19 at the time of this commit),
    only on those hypervisors where that floating-ip hosted
    VM is present.
    
    Please note that the duplicate IP response packets
    replication problem does not exists when external network
    uses tunnels (instead of VLANs) as the floatingip destined
    response packets from external network will be directed
    to only one hypervisor (ie., only one datapath).
    
    The fix also makes sure that when a floatingip is
    disassociated the corresponding floatingipportmac entry
    is removed from MyMac Table.
    
    There are still general bugs where floatingip rules in some
    tables (including MyMac table) are not removed when:
    
    a. Floating IP is deleted
    b. VM hosting the Floating IP is deleted (without disassociating).
    
    The workaround for both the above is to disassociate floatingip
    first before embarking on a (or) b . Issue a and b will
    be addressed with a different bug, as it needs a more deeper
    fix.
    
    Change-Id: Ic0eb7994a9829aa89aee5d8400e0d0be94dfac36
    Signed-off-by: Vivekanandan Narasimhan <n.vivekanandan@ericsson.com>

--- vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/VpnFloatingIpHandler.java
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/VpnFloatingIpHandler.java
@@ -150,8 +150,10 @@ public class VpnFloatingIpHandler implements FloatingIPHandler {
                     WriteTransaction writeTx = dataBroker.newWriteOnlyTransaction();
                     IpAddress externalIpAddress = new IpAddress(new Ipv4Address(externalIp));
                     Port neutronPort = NatUtil.getNeutronPortForFloatingIp(dataBroker, externalIpAddress);
+                    LOG.debug("Add Floating Ip {} , found associated to fixed port {}", externalIp, interfaceName);
                     if (neutronPort != null && neutronPort.getMacAddress() != null) {
-                        vpnManager.setupSubnetMacIntoVpnInstance(vpnName, neutronPort.getMacAddress().getValue(), writeTx, NwConstants.ADD_FLOW);
+                        vpnManager.setupSubnetMacIntoVpnInstance(vpnName, neutronPort.getMacAddress().getValue(),
+                                dpnId, writeTx, NwConstants.ADD_FLOW);
                     }
                     writeTx.submit();
                     return JdkFutureAdapters.listenInPoolThread(future);
@@ -199,8 +201,10 @@ public class VpnFloatingIpHandler implements FloatingIPHandler {
         WriteTransaction writeTx = dataBroker.newWriteOnlyTransaction();
         IpAddress externalIpAddress = new IpAddress(new Ipv4Address(externalIp));
         Port neutronPort = NatUtil.getNeutronPortForFloatingIp(dataBroker, externalIpAddress);
+        LOG.debug("Removing FloatingIp {}", externalIp);
         if (neutronPort != null && neutronPort.getMacAddress() != null) {
-            vpnManager.setupSubnetMacIntoVpnInstance(vpnName, neutronPort.getMacAddress().getValue(), writeTx, NwConstants.DEL_FLOW);
+            vpnManager.setupSubnetMacIntoVpnInstance(vpnName, neutronPort.getMacAddress().getValue(),
+                    dpnId, writeTx, NwConstants.DEL_FLOW);
         }
         writeTx.submit();
         //Remove Prefix from BGP
