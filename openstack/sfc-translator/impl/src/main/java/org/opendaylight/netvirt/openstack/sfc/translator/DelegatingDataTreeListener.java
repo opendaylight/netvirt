5c4c263a534f8672c0d9fd82480505d7cf273407
Stephen Kitt <skitt@redhat.com>
2016-10-25 16:50:50 +0200

    Clean up lambdas
    Use lambdas instead of functional interfaces, and use method
    references instead of lambdas where possible.
    
    Change-Id: I34a30b81ec5ceae94cbe10524cf8b147475de28c
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- openstack/sfc-translator/impl/src/main/java/org/opendaylight/netvirt/openstack/sfc/translator/DelegatingDataTreeListener.java
+++ openstack/sfc-translator/impl/src/main/java/org/opendaylight/netvirt/openstack/sfc/translator/DelegatingDataTreeListener.java
@@ -86,12 +86,7 @@ public abstract class DelegatingDataTreeListener<T extends DataObject> implement
     @Override
     public void onDataTreeChanged(@Nonnull final Collection<DataTreeModification<T>> changes) {
         Preconditions.checkNotNull(changes, "Changes may not be null!");
-        executorService.submit(new Runnable() {
-            @Override
-            public void run() {
-                processChanges(changes);
-            }
-        });
+        executorService.submit(() -> processChanges(changes));
     }
 
     @Override
