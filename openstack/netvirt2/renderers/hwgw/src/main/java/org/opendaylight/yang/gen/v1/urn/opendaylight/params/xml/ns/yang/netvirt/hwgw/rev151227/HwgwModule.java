520e393ad49db4cc05856beb7f39dae4e0bf0a73
Guo Ruijing <ruijing.guo@intel.com>
2016-10-30 18:20:49 -0400

    Add BP wiring for hwgwmodule
    Change-Id: I6121c2b0c7f702108cf6496782ed025dd7d14e0e
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- openstack/netvirt2/renderers/hwgw/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/netvirt/hwgw/rev151227/HwgwModule.java
+++ openstack/netvirt2/renderers/hwgw/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/netvirt/hwgw/rev151227/HwgwModule.java
@@ -1,14 +1,4 @@
-/*
- * Copyright (c) 2015 Red Hat, Inc. and others.  All rights reserved.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- */
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netvirt.hwgw.rev151227;
-
-import org.opendaylight.netvirt.netvirt.renderers.hwgw.HwgwProvider;
-
 public class HwgwModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netvirt.hwgw.rev151227.AbstractHwgwModule {
     public HwgwModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
@@ -25,9 +15,8 @@ public class HwgwModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.pa
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        HwgwProvider provider = new HwgwProvider();
-        getBrokerDependency().registerProvider(provider);
-        return provider;
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
     }
 
 }
