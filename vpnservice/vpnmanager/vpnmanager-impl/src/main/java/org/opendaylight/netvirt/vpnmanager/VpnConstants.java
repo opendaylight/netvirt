c39c5360fac6bc006b2c78ec1cf282deda33b864
Gobinath <gobinath@ericsson.com>
2016-11-04 08:01:35 +0530

    Decoupling of config and operational VpnPortip DS
    The following changes have been done to address the decoupling of the
    config and operational VpnPortipToPort DS:
    
    1. Create a new VPNVipToPort Datastore which will contain non-neutron IP
    addresses.  This Datastore will be of OPERATIONAL type.
    It will be constructed as part of ARP/GARP requests and ARP response
    discoveries by the controller.
    
    2. The existing NeutronVpnPortIpToPort will be changed as a pure Config
    only Datastore as it will store all Neutron Port (Floating IP including)
    and their IP and MAC Addresses.  This datastore will continue to be used
    for lookup during ARP resolution and SubnetRoute handling.
    
    3. Decoupling of the mymac changes(subnetMac) from Arp cache feature.
    Mymac changes would still be listening to the VpnPortipToPort  DS .
    
    4. The ArpNotificationHandler would now modify the VpnMipToPort DS which
    the ArpMonitoringHandler would listen to.
    
    The feature which initiates periodic ARP-based location discovery of
    non-neutron IPs from the controller brought in via the earlier review
    here: https://git.opendaylight.org/gerrit/#/c/45410/
    
    Now the above feature will be using only the new VPNVipToPort that carries
    only Non-Neutron IPs as in this current review.
    
    Change-Id: Ic96517f4a57fe547d42495274a74ec5aa32b7d9e
    Signed-off-by: Gobinath <gobinath@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnConstants.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnConstants.java
@@ -27,6 +27,8 @@ public class VpnConstants {
     public static final long PER_VPN_INSTANCE_OPDATA_MAX_WAIT_TIME_IN_MILLISECONDS = 180000;
     public static final int ELAN_GID_MIN = 200000;
     public static final int INVALID_LABEL = 0;
+    public static final String ARP_MONITORING_ENTITY = "arpmonitoring";
+
 
     // An IdPool for Pseudo LPort tags, that is, lportTags that are no related to an interface.
     // These lportTags must be higher than 170000 to avoid collision with interface LportTags and
