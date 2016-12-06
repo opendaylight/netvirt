b535446c65f99351a20020627855dc398d8ca9d1
Kency Kurian <kency.kurian@ericsson.com>
2016-12-06 11:07:15 +0530

    L3VPN ECMP forwarding through two DC-GW
    - Modified odl-fib.yang to support multiple route-paths for the same
      destination prefix.
    - Added code to program remote fib entry to load balancing group in case
      of more than one route-paths for BGP routes.
    
    Change-Id: I7bcd3b77ac452476b4190286c1f3e8815a1b8819
    Signed-off-by: Kency Kurian <kency.kurian@ericsson.com>

--- vpnservice/fibmanager/fibmanager-impl/src/main/java/org/opendaylight/netvirt/fibmanager/FibConstants.java
+++ vpnservice/fibmanager/fibmanager-impl/src/main/java/org/opendaylight/netvirt/fibmanager/FibConstants.java
@@ -8,6 +8,12 @@
 package org.opendaylight.netvirt.fibmanager;
 
 import java.math.BigInteger;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmNxReg;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmNxReg0;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmNxReg1;
 
 public class FibConstants {
     static final BigInteger COOKIE_TUNNEL = new BigInteger("9000000", 16);
@@ -16,4 +22,9 @@ public class FibConstants {
     static final String FLOWID_PREFIX = "L3.";
     static final String VPN_IDPOOL_NAME = "vpnservices";
     static final String SEPARATOR = ".";
+    public static final Map<Integer,  Class<? extends NxmNxReg>> nxmRegMapping = new ConcurrentHashMap<>();
+    static {
+        nxmRegMapping.put(0, NxmNxReg0.class);
+        nxmRegMapping.put(1, NxmNxReg1.class);
+    }
 }
