8998c522378dafd47d9cb29caec9df5b141c4225
Shashidhar Raja <shashidharr@altencalsoftlabs.com>
2016-12-15 19:28:10 +0530

    ACL: Operation Improvements
    Updated ACL to program "+trk+new" and "+trk+inv" DROP flows with lport tag
    as metadata for each of the VM (Neutron port) being added to support drop
    packet stats at port level.
    
    Spec reference: https://git.opendaylight.org/gerrit/#/c/49086/
    
    Change-Id: I9cf4934c7334d943c14c52d16a4b3d689c39e62d
    Signed-off-by: Shashidhar Raja <shashidharr@altencalsoftlabs.com>

--- vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclServiceOFFlowBuilder.java
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclServiceOFFlowBuilder.java
@@ -13,6 +13,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.InstructionInfo;
 import org.opendaylight.genius.mdsalutil.MatchInfoBase;
 import org.opendaylight.genius.mdsalutil.NwConstants;
 import org.opendaylight.genius.mdsalutil.NxMatchFieldType;
@@ -25,6 +27,8 @@ import org.opendaylight.genius.mdsalutil.matches.MatchIpv4Destination;
 import org.opendaylight.genius.mdsalutil.matches.MatchIpv4Source;
 import org.opendaylight.genius.mdsalutil.matches.MatchIpv6Destination;
 import org.opendaylight.genius.mdsalutil.matches.MatchIpv6Source;
+import org.opendaylight.genius.mdsalutil.actions.ActionDrop;
+import org.opendaylight.genius.mdsalutil.instructions.InstructionApplyActions;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.Matches;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.AceIp;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.ace.ip.ace.ip.version.AceIpv4;
@@ -292,6 +296,30 @@ public class AclServiceOFFlowBuilder {
             }
         }
         return flowMatches;
+    }
+
+    /** Adds LPort matches to the flow.
+     * @param lportTag lport tag
+     * @param conntrackState conntrack state to be used with matches
+     * @param conntrackMask conntrack mask to be used with matches
+     * @return list of matches
+     */
+    public static List<MatchInfoBase> addLPortTagMatches(int lportTag, int conntrackState, int conntrackMask) {
+        List<MatchInfoBase> matches = new ArrayList<>();
+        matches.add(AclServiceUtils.buildLPortTagMatch(lportTag));
+        matches.add(new NxMatchInfo(NxMatchFieldType.ct_state, new long[] {conntrackState, conntrackMask}));
+        return matches;
+    }
+
+    /** Returns drop instruction info.
+     * @return drop list of InstructionInfo objects
+     */
+    public static List<InstructionInfo> getDropInstructionInfo() {
+        List<InstructionInfo> instructions = new ArrayList<>();
+        List<ActionInfo> actionsInfos = new ArrayList<>();
+        actionsInfos.add(new ActionDrop());
+        instructions.add(new InstructionApplyActions(actionsInfos));
+        return instructions;
     }
 
     /**
