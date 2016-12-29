f0596a0a41d5bcf37ee7564254172c1b801274d1
HANAMANTAGOUD V Kandagal <hanamantagoud.v.kandagal@ericsson.com>
2016-12-29 17:16:56 +0530

    VrfEntryListener refactoring using chain of responsibility pattern
    This is a first review where BGP Route handling is separated into a new
    handler class BgpRouteVrfEntryHandler.
    
    As part of chain of responsibility pattern , VRF Entry will go through the
    chain of handlers which are interested in processing it.
    
    Subsequent dependent patches will contain handlers for subnetroute ,
    import-export route , inter-vpn link route etc
    
    Change-Id: Iea89b9b3c0576214623bb08590e4738c97c6b70f
    Signed-off-by: HANAMANTAGOUD V Kandagal <hanamantagoud.v.kandagal@ericsson.com>
    Signed-off-by: ehvkand <hanamantagoud.v.kandagal@ericsson.com>
    Signed-off-by: HANAMANTAGOUD V Kandagal <hanamantagoud.v.kandagal@ericsson.com>

---
+++ vpnservice/fibmanager/fibmanager-impl/src/main/java/org/opendaylight/netvirt/fibmanager/VrfEntryHandler.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2015 - 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.fibmanager;
+
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.vrfentries.VrfEntry;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public interface VrfEntryHandler {
+    /*Types of VrfEntry Handlers*/
+    short BGP_ROUTE_HANDLER = 0;
+    short SUBNET_ROUTE_HANDLER  = 1;
+    short IMP_EXP_ROUTE_HANDLER = 2;
+
+    /*VrfEntry processing status.
+    * VRF_PROCESSING_COMPLETED - means no more handlers are required to process the VRF entry.
+    * VRF_PROCESSING_CONTINUE - means next VRF Handler can process the VRF entry if required.
+    * */
+    short VRF_PROCESSING_COMPLETED = 1;
+    short VRF_PROCESSING_CONTINUE = 2;
+
+    void subscribeWithVrfListener();
+
+    void unSubscribeWithVrfListener();
+
+    boolean wantToProcessVrfEntry(VrfEntry vrfEntry);
+
+    String getHandlerType();
+
+    short createFlows(InstanceIdentifier<VrfEntry> identifier, VrfEntry vrfEntry, String rd);
+
+    short updateFlows(InstanceIdentifier<VrfEntry> identifier, VrfEntry original, VrfEntry update, String rd);
+
+    short removeFlows(InstanceIdentifier<VrfEntry> identifier, VrfEntry vrfEntry, String rd);
+}
