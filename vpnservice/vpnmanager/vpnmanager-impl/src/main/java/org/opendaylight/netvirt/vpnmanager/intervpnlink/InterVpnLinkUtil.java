785c26d9fe8234ad11fea6a044e0afa3c41e9dca
Miguel Perez <francisco.miguel.perez@ericsson.com>
2016-12-30 19:59:06 +0100

    Cleanup ivpnLink unused code + small modifications
     + This is the 1st change to cleanup/refactor InterVpnLink code
    
     + 3 new utility methods in InterVpnLinkDataComposite
     + add.getName() replaced by IvpnLinkName in InterVpnLinkListener
    
    Change-Id: Icf7e29d9f77f53cc3d06620f60e8fab7efd464da
    Signed-off-by: Miguel Perez <francisco.miguel.perez@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/intervpnlink/InterVpnLinkUtil.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/intervpnlink/InterVpnLinkUtil.java
@@ -33,7 +33,6 @@ import org.opendaylight.netvirt.vpnmanager.VpnUtil;
 import org.opendaylight.netvirt.vpnmanager.api.intervpnlink.InterVpnLinkCache;
 import org.opendaylight.netvirt.vpnmanager.api.intervpnlink.InterVpnLinkDataComposite;
 import org.opendaylight.netvirt.vpnmanager.utilities.InterfaceUtils;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.Instruction;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.FibEntries;
@@ -160,9 +159,9 @@ public class InterVpnLinkUtil {
         if (optOldVpnLinkState.isPresent()) {
             InterVpnLinkState newVpnLinkState =
                 new InterVpnLinkStateBuilder(optOldVpnLinkState.get()).setState(state)
-                    .setFirstEndpointState(newFirstEndpointState)
-                    .setSecondEndpointState(newSecondEndpointState)
-                    .build();
+                            .setFirstEndpointState(newFirstEndpointState)
+                            .setSecondEndpointState(newSecondEndpointState)
+                            .build();
             VpnUtil.syncUpdate(broker, LogicalDatastoreType.CONFIGURATION,
                 InterVpnLinkUtil.getInterVpnLinkStateIid(vpnLinkName), newVpnLinkState);
             InterVpnLinkCache.addInterVpnLinkStateToCaches(newVpnLinkState);
@@ -187,21 +186,24 @@ public class InterVpnLinkUtil {
      *
      * @param broker dataBroker service reference
      * @param mdsalManager MDSAL API accessor
-     * @param interVpnLink Object that holds the needed information about both endpoints of the InterVpnLink.
+     * @param interVpnLinkName Name of the InterVpnLink.
      * @param dpnList The list of DPNs where this flow must be installed
      * @param vpnUuidOtherEndpoint UUID of the other endpoint of the InterVpnLink
      * @param lportTagOfOtherEndpoint Dataplane identifier of the other endpoint of the InterVpnLink
      * @return the list of Futures for each and every flow that has been installed
      */
     public static List<ListenableFuture<Void>> installLPortDispatcherTableFlow(DataBroker broker,
-        IMdsalApiManager mdsalManager, InterVpnLink interVpnLink, List<BigInteger> dpnList, Uuid vpnUuidOtherEndpoint,
-        Long lportTagOfOtherEndpoint) {
+                                                                               IMdsalApiManager mdsalManager,
+                                                                               String interVpnLinkName,
+                                                                               List<BigInteger> dpnList,
+                                                                               String vpnUuidOtherEndpoint,
+                                                                               Long lportTagOfOtherEndpoint) {
         List<ListenableFuture<Void>> result = new ArrayList<>();
-        long vpnId = VpnUtil.getVpnId(broker, vpnUuidOtherEndpoint.getValue());
-        for (BigInteger dpnId : dpnList) {
+        long vpnId = VpnUtil.getVpnId(broker, vpnUuidOtherEndpoint);
+        for ( BigInteger dpnId : dpnList ) {
             // insert into LPortDispatcher table
-            Flow lportDispatcherFlow = buildLPortDispatcherFlow(interVpnLink.getName(), vpnId,
-                lportTagOfOtherEndpoint.intValue());
+            Flow lportDispatcherFlow = buildLPortDispatcherFlow(interVpnLinkName, vpnId,
+                                                                lportTagOfOtherEndpoint.intValue());
             result.add(mdsalManager.installFlow(dpnId, lportDispatcherFlow));
         }
 
@@ -529,8 +531,5 @@ public class InterVpnLinkUtil {
             vpnRd, destination, label, nexthopList);
         bgpManager.advertisePrefix(vpnRd, null /*macAddress*/, destination, nexthopList,
                 VrfEntry.EncapType.Mplsgre, label, 0 /*l3vni*/, null /*gatewayMacAddress*/);
-
-        // TODO: Leak if static-routes-leaking flag is active
-
     }
 }
