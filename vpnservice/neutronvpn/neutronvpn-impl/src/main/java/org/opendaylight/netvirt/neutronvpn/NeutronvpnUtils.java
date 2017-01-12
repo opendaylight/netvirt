faa3127af202f3a63883917383387d6f0168d068
Abhinav Gupta <abhinav.gupta@ericsson.com>
2017-01-12 16:40:02 +0530

    NeutronVPN: Error out if VxLAN network configured without seg-id
    This adds an error log based on the presence of segmentation-id when VxLAN
    based neutron networks are configured via Neutron.
    
    Additionally, some cleanup done:
    1. Moved some impl specific utils to NeutronVpnUtils
    2. Moved constants to NeutronConstants
    3. Renamed SubnetMapListener class to SubnetMapChangeListener
    4. Removed  unused xml dependency from QosListener
    
    Change-Id: I2d22773e4e7bdfa927c01d679542fa36146e625a
    Signed-off-by: Abhinav Gupta <abhinav.gupta@ericsson.com>

--- vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnUtils.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnUtils.java
@@ -37,6 +37,7 @@ import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.genius.mdsalutil.MDSALUtil;
+import org.opendaylight.netvirt.neutronvpn.api.utils.NeutronUtils;
 import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.VpnInstances;
 import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.vpn.instances.VpnInstance;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.FloatingIpPortInfo;
@@ -150,10 +151,10 @@ public class NeutronvpnUtils {
     private static TimeUnit secUnit = TimeUnit.SECONDS;
 
     static {
-        registerSuppoprtedNetworkType(NetworkTypeFlat.class);
-        registerSuppoprtedNetworkType(NetworkTypeVlan.class);
-        registerSuppoprtedNetworkType(NetworkTypeVxlan.class);
-        registerSuppoprtedNetworkType(NetworkTypeGre.class);
+        registerSupportedNetworkType(NetworkTypeFlat.class);
+        registerSupportedNetworkType(NetworkTypeVlan.class);
+        registerSupportedNetworkType(NetworkTypeVxlan.class);
+        registerSupportedNetworkType(NetworkTypeGre.class);
     }
 
     private NeutronvpnUtils() {
@@ -162,11 +163,11 @@ public class NeutronvpnUtils {
 
     static ConcurrentHashMap<String, ImmutablePair<ReadWriteLock,AtomicInteger>> locks = new ConcurrentHashMap<>();
 
-    public static void registerSuppoprtedNetworkType(Class<? extends NetworkTypeBase> netType) {
+    public static void registerSupportedNetworkType(Class<? extends NetworkTypeBase> netType) {
         supportedNetworkTypes.add(netType);
     }
 
-    public static void unregisterSuppoprtedNetworkType(Class<? extends NetworkTypeBase> netType) {
+    public static void unregisterSupportedNetworkType(Class<? extends NetworkTypeBase> netType) {
         supportedNetworkTypes.remove(netType);
     }
 
@@ -865,6 +866,16 @@ public class NeutronvpnUtils {
         }
     }
 
+    public static String getSegmentationIdFromNeutronNetwork(Network network) {
+        String segmentationId = null;
+        NetworkProviderExtension providerExtension = network.getAugmentation(NetworkProviderExtension.class);
+        if (providerExtension != null) {
+            Class<? extends NetworkTypeBase> networkType = providerExtension.getNetworkType();
+            segmentationId = NeutronUtils.getSegmentationIdFromNeutronNetwork(network, networkType);
+        }
+
+        return segmentationId;
+    }
 
     public static Class<? extends SegmentTypeBase> getSegmentTypeFromNeutronNetwork(Network network) {
         NetworkProviderExtension providerExtension = network.getAugmentation(NetworkProviderExtension.class);
