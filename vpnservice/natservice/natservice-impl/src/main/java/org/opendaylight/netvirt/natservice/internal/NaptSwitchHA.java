c13d9dbe303e4fad1c439c4c34ff9154673c1155
Abhinav Gupta <abhinav.gupta@ericsson.com>
2017-01-12 16:41:49 +0530

    ELAN: VNI support in datapath for VxLAN networks
    This commit adds support for VNI based datapath forwarding for VxLAN
    based provider networks.
    Spec: https://git.opendaylight.org/gerrit/#/c/48640/
    
    a. Unicast
      1. Table 51 now sets VNI in tun_id field for packets egressing on
         tunnel ports
      2. On egress, table 36 now matches on VNI in tun_id field, sets the
         ELAN tag in metadata, and forwards the traffic to table 51
    
    b. Broadcast
      1. Remote broadcast group now sets VNI in tun_id field for packets
         egressing on tunnel ports
      2. On egress, table 36 now matches on VNI in tun_id field, sets the
         ELAN tag in metadata, and forwards the traffic to table 51
    
    Also, integrates the openstack-vni-semantics-enforced flag to enable
    easier turning on/off of the feature
    
    Change-Id: I7a6b14897344a8d00cb19291602d92cf586f15f7
    Signed-off-by: Abhinav Gupta <abhinav.gupta@ericsson.com>

--- vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NaptSwitchHA.java
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NaptSwitchHA.java
@@ -38,9 +38,9 @@ import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
 import org.opendaylight.genius.mdsalutil.matches.MatchEthernetType;
 import org.opendaylight.genius.mdsalutil.matches.MatchMetadata;
 import org.opendaylight.netvirt.bgpmanager.api.IBgpManager;
+import org.opendaylight.netvirt.elanmanager.api.IElanService;
 import org.opendaylight.netvirt.fibmanager.api.IFibManager;
 import org.opendaylight.netvirt.natservice.api.SnatServiceManager;
-import org.opendaylight.netvirt.neutronvpn.interfaces.INeutronVpnManager;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeBase;
@@ -96,7 +96,7 @@ public class NaptSwitchHA {
     private final VpnRpcService vpnService;
     private final FibRpcService fibService;
     private final IFibManager fibManager;
-    private final INeutronVpnManager nvpnManager;
+    private final IElanService elanManager;
     private List<String> externalIpsCache;
     private HashMap<String, Long> externalIpsLabel;
     private final EvpnNaptSwitchHA evpnNaptSwitchHA;
@@ -115,7 +115,7 @@ public class NaptSwitchHA {
                         final FibRpcService fibService,
                         final IFibManager fibManager,
                         final EvpnNaptSwitchHA evpnNaptSwitchHA,
-                        final INeutronVpnManager nvpnManager,
+                        final IElanService elanManager,
                         final SnatServiceManager natServiceManager,
                         final NatserviceConfig config) {
         this.dataBroker = dataBroker;
@@ -130,7 +130,7 @@ public class NaptSwitchHA {
         this.fibService = fibService;
         this.fibManager = fibManager;
         this.evpnNaptSwitchHA = evpnNaptSwitchHA;
-        this.nvpnManager = nvpnManager;
+        this.elanManager = elanManager;
         this.natServiceManager = natServiceManager;
         if (config != null) {
             this.natMode = config.getNatMode();
@@ -784,8 +784,8 @@ public class NaptSwitchHA {
 
         if (addordel == NatConstants.ADD_FLOW) {
             List<ActionInfo> actionsInfo = new ArrayList<>();
-            long tunnelId = NatUtil.getTunnelIdForNonNaptToNaptFlow(dataBroker, nvpnManager, idManager,
-                                                                        routerVpnId, routerName);
+            long tunnelId = NatUtil.getTunnelIdForNonNaptToNaptFlow(dataBroker, elanManager, idManager, routerVpnId,
+                    routerName);
             actionsInfo.add(new ActionSetFieldTunnelId(BigInteger.valueOf(tunnelId)));
             LOG.debug("Setting the tunnel to the list of action infos {}", actionsInfo);
             actionsInfo.add(new ActionGroup(groupId));
