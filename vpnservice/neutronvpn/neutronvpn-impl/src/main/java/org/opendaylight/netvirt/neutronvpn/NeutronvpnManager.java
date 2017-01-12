cc2b82ed1bb56deb60f7946cadba13204eaeb5ce
Abhinav Gupta <abhinav.gupta@ericsson.com>
2017-01-12 16:40:02 +0530

    NeutronVPN: Error out if VxLAN/VLAN network configured without seg-id
    This adds an error log based on the presence of segmentation-id when VxLAN
    or VLAN based neutron networks are configured via Neutron.
    
    Additionally, some cleanup done:
    1. Moved some impl specific utils to NeutronVpnUtils
    2. Moved constants to NeutronConstants
    3. Renamed SubnetMapListener class to SubnetMapChangeListener
    4. Removed  unused xml dependency from QosListener
    
    Change-Id: I2d22773e4e7bdfa927c01d679542fa36146e625a
    Signed-off-by: Abhinav Gupta <abhinav.gupta@ericsson.com>

--- vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnManager.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnManager.java
@@ -27,7 +27,6 @@ import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.datastoreutils.DataStoreJobCoordinator;
 import org.opendaylight.genius.mdsalutil.MDSALUtil;
-import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
 import org.opendaylight.netvirt.elanmanager.api.IElanService;
 import org.opendaylight.netvirt.neutronvpn.api.utils.NeutronConstants;
 import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.VpnInstances;
@@ -129,17 +128,14 @@ public class NeutronvpnManager implements NeutronvpnService, AutoCloseable, Even
     private final VpnRpcService vpnRpcService;
     private final NeutronFloatingToFixedIpMappingChangeListener floatingIpMapListener;
     private final NeutronvpnConfig neutronvpnConfig;
-    private final IMdsalApiManager mdsalUtil;
     private final IElanService elanService;
 
     public NeutronvpnManager(
-            final DataBroker dataBroker, final IMdsalApiManager mdsalManager,
-            final NotificationPublishService notiPublishService, final NeutronvpnNatManager vpnNatMgr,
-            final VpnRpcService vpnRpcSrv, final IElanService elanService,
+            final DataBroker dataBroker, final NotificationPublishService notiPublishService,
+            final NeutronvpnNatManager vpnNatMgr, final VpnRpcService vpnRpcSrv, final IElanService elanService,
             final NeutronFloatingToFixedIpMappingChangeListener neutronFloatingToFixedIpMappingChangeListener,
             final NeutronvpnConfig neutronvpnConfig) {
         this.dataBroker = dataBroker;
-        mdsalUtil = mdsalManager;
         nvpnNatManager = vpnNatMgr;
         notificationPublishService = notiPublishService;
         vpnRpcService = vpnRpcSrv;
