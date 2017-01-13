701a9c20190de58d983192d7dc93607b3b264086
Stephen Kitt <skitt@redhat.com>
2017-01-13 10:20:23 +0100

    Boxing cleanup
    Change-Id: I669ea231b2cd2f23bd4a6b8ab3122b91172ca20f
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/SubnetRoutePacketInHandler.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/SubnetRoutePacketInHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ * Copyright © 2016, 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -188,6 +188,6 @@ public class SubnetRoutePacketInHandler implements PacketProcessingListener {
     getVpnIdToVpnInstanceIdentifier(long vpnId) {
         return InstanceIdentifier.builder(VpnIdToVpnInstance.class)
                 .child(VpnIds.class,
-                        new VpnIdsKey(Long.valueOf(vpnId))).build();
+                        new VpnIdsKey(vpnId)).build();
     }
 }
