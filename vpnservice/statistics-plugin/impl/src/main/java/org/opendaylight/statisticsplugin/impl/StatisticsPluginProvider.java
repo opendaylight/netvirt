e6e02f130d2bc6e2550d030c65a8ecdedd1cdef5
Guy Regev <guy.regev@hpe.com>
2017-01-16 15:53:16 +0200

    Added statistics bundle
    Change-Id: I2d10a9871c04373e944a07c1ff96bfd9a4f272fa
    Signed-off-by: Guy Regev <guy.regev@hpe.com>

---
+++ vpnservice/statistics-plugin/impl/src/main/java/org/opendaylight/statisticsplugin/impl/StatisticsPluginProvider.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2016 HPE, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.statisticsplugin.impl;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.statistics.plugin.rev170120.StatisticsPluginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class StatisticsPluginProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StatisticsPluginProvider.class);
+
+    private final DataBroker dataBroker;
+
+    private final RpcProviderRegistry rpcProviderRegistry;
+
+    private CounterRetriever counterRetriever;
+
+    @Inject
+    public StatisticsPluginProvider(final DataBroker dataBroker, final RpcProviderRegistry rpcProviderRegistry,
+            CounterRetriever counterRetriever) {
+        this.dataBroker = dataBroker;
+        this.rpcProviderRegistry = rpcProviderRegistry;
+        this.counterRetriever = counterRetriever;
+    }
+
+    /**
+     * Method called when the blueprint container is created.
+     */
+    @PostConstruct
+    public void init() {
+        LOG.info("StatisticsPluginProvider Session Initiated");
+        rpcProviderRegistry.addRpcImplementation(StatisticsPluginService.class,
+                new StatisticsPluginImpl(dataBroker, counterRetriever));
+    }
+
+    /**
+     * Method called when the blueprint container is destroyed.
+     */
+    @PreDestroy
+    public void destroy() {
+        LOG.info("StatisticsPluginProvider Closed");
+    }
+}
