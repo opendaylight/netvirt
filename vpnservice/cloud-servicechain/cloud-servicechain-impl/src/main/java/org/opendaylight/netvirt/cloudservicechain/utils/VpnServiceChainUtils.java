5f0b0fdaa52dd6accbb85df1ddbd0a8cd8fc16be
Miguel Perez <francisco.miguel.perez@ericsson.com>
2017-01-16 18:30:16 +0100

    Applying SC to response too
     + Problem: Ping from outside DC-GW to a VM located inside DC-GW
       works well in the Uplink, that is, packets going from outside
       DC-GW towards the VM are hijacked by SCF Pipeline and delivered
       to VM. However, when VM responds (Downlink), the packets are sent
       to DC-GW right away via L3VPN pipeline. Service Chaining is not
       applied for the return path.
    
     + Solution: try to divert to SCF Pipeline the packets that come from
       the VM too, not only the packets coming from the external Tunnel
       port.
    
     + How: by adding a listener on VpnInterface on CONFIG DS.
       When a VpnInterface is added, the listener checks if its VPN is
       involved in a ServiceChain or not. If so, the interface is bound
       to SCF too.
    
    Change-Id: I8e159e7e4084b63d93fe8218dae8caae2d8a8792
    Signed-off-by: Miguel Perez <francisco.miguel.perez@ericsson.com>

--- vpnservice/cloud-servicechain/cloud-servicechain-impl/src/main/java/org/opendaylight/netvirt/cloudservicechain/utils/VpnServiceChainUtils.java
+++ vpnservice/cloud-servicechain/cloud-servicechain-impl/src/main/java/org/opendaylight/netvirt/cloudservicechain/utils/VpnServiceChainUtils.java
@@ -12,8 +12,11 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
 import org.opendaylight.genius.mdsalutil.ActionInfo;
 import org.opendaylight.genius.mdsalutil.FlowEntity;
 import org.opendaylight.genius.mdsalutil.InstructionInfo;
@@ -33,6 +36,7 @@ import org.opendaylight.genius.mdsalutil.matches.MatchMetadata;
 import org.opendaylight.genius.mdsalutil.matches.MatchMplsLabel;
 import org.opendaylight.genius.utils.ServiceIndex;
 import org.opendaylight.netvirt.cloudservicechain.CloudServiceChainConstants;
+import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.vpn.interfaces.VpnInterface;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowBuilder;
@@ -50,6 +54,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.op.data.VpnInstanceOpDataEntryKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.op.data.vpn.instance.op.data.entry.VpnToDpnList;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.op.data.vpn.instance.op.data.entry.VpnToDpnListKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.op.data.vpn.instance.op.data.entry.vpn.to.dpn.list.VpnInterfaces;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.to.vpn.id.VpnInstance;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.to.vpn.id.VpnInstanceKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowjava.nx.match.rev140421.NxmNxReg2;
@@ -466,4 +471,32 @@ public class VpnServiceChainUtils {
                                  .append(CloudServiceChainConstants.DEFAULT_SCF_FLOW_PRIORITY).toString();
     }
 
+    public static List<String> getAllVpnIfaceNames(DataBroker dataBroker, String vpnName) {
+
+        String vpnRd = getVpnRd(dataBroker, vpnName);
+        InstanceIdentifier<VpnInstanceOpDataEntry> vpnOpDataIid =
+            InstanceIdentifier.builder(VpnInstanceOpData.class)
+                              .child(VpnInstanceOpDataEntry.class, new VpnInstanceOpDataEntryKey(vpnRd)).build();
+
+        try {
+            VpnInstanceOpDataEntry vpnOpData =
+                SingleTransactionDataBroker.syncRead(dataBroker, LogicalDatastoreType.OPERATIONAL, vpnOpDataIid);
+
+            if (vpnOpData == null) {
+                return Collections.emptyList();
+            }
+            List<VpnToDpnList> dpnToVpns = vpnOpData.getVpnToDpnList();
+            if (dpnToVpns == null) {
+                return Collections.emptyList();
+            }
+
+            return dpnToVpns.stream()
+                            .flatMap(dpn -> dpn.getVpnInterfaces().stream())
+                            .map(vpnIf -> vpnIf.getInterfaceName())
+                            .collect(Collectors.toList());
+        } catch (ReadFailedException e) {
+            LOG.warn("getAllVpnInterfaces for vpn {}: Failure on read operation", vpnName, e);
+            return Collections.emptyList();
+        }
+    }
 }
