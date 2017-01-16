d468b82f7f7ecaee8e457c05093101c41e063d34
Vishal Thapar <vishal.thapar@ericsson.com>
2017-01-16 21:25:29 +0530

    Add Trunks support
    This adds hooks to bring Neutron Trunk configuration into netvirt.
    Genius/IFM already has required parameters to support this.
    
    Change-Id: Ic087c0b9a7d1c27d880533331164ae3af0a1fabb
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

---
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronTrunkChangeListener.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.neutronvpn;
+
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
+import org.opendaylight.genius.datastoreutils.DataStoreJobCoordinator;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana._if.type.rev140508.L2vlan;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.Interface;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.InterfaceBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.PhysAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.IfL2vlan;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.IfL2vlanBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.l2.types.rev130827.VlanId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.ElanInterfaces;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.interfaces.ElanInterface;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.interfaces.ElanInterfaceBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.interfaces.ElanInterfaceKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.NetworkTypeVlan;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.ports.Port;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.trunks.rev170118.trunk.attributes.SubPorts;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.trunks.rev170118.trunks.attributes.Trunks;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.trunks.rev170118.trunks.attributes.trunks.Trunk;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NeutronTrunkChangeListener extends AsyncDataTreeChangeListenerBase<Trunk, NeutronTrunkChangeListener>
+        implements AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(NeutronTrunkChangeListener.class);
+    private final DataBroker dataBroker;
+    public NeutronTrunkChangeListener(final DataBroker dataBroker) {
+        super();
+        this.dataBroker = dataBroker;
+    }
+
+    public void start() {
+        LOG.info("{} start", getClass().getSimpleName());
+        registerListener(LogicalDatastoreType.CONFIGURATION, dataBroker);
+    }
+
+    @Override
+    protected InstanceIdentifier<Trunk> getWildCardPath() {
+        return InstanceIdentifier.create(Neutron.class).child(Trunks.class).child(Trunk.class);
+    }
+
+    @Override
+    protected NeutronTrunkChangeListener getDataTreeChangeListener() {
+        return NeutronTrunkChangeListener.this;
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<Trunk> identifier, Trunk input) {
+        Preconditions.checkNotNull(input.getPortId());
+        if (input.getSubPorts() != null) {
+            for (SubPorts subPort : input.getSubPorts()) {
+                createSubPortInterface(input, subPort);
+            }
+        }
+        LOG.trace("Adding Trunk : key: {}, value={}", identifier, input);
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<Trunk> identifier, Trunk input) {
+        Preconditions.checkNotNull(input.getPortId());
+        LOG.trace("Removing Trunk : key: {}, value={}", identifier, input);
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<Trunk> identifier, Trunk original, Trunk update) {
+        List<SubPorts> added = new ArrayList<>(update.getSubPorts());
+        List<SubPorts> deleted = new ArrayList<>(original.getSubPorts());
+        added.retainAll(original.getSubPorts());
+        deleted.retainAll(update.getSubPorts());
+        for (SubPorts subPort:deleted) {
+            deleteSubPortInterface(subPort);
+        }
+        for (SubPorts subPort:added) {
+            createSubPortInterface(update,  subPort);
+        }
+        LOG.trace("Updated Trunk : key: {}. subPortsAdded={}, subPortsDeleted={}", identifier, added, deleted);
+    }
+
+    private void createSubPortInterface(Trunk trunk, SubPorts subPort) {
+        if(!NetworkTypeVlan.class.equals(subPort.getSegmentationType())) {
+            LOG.warn("SegmentationType {} not supported for Trunks", subPort.getSegmentationType().getSimpleName());
+            return;
+        }
+        String portName = subPort.getPortId().getValue();
+        InstanceIdentifier<Interface> interfaceIdentifier = NeutronvpnUtils.buildVlanInterfaceIdentifier(portName);
+        final DataStoreJobCoordinator dataStoreCoordinator = DataStoreJobCoordinator.getInstance();
+
+        // Should we use parentName?
+        dataStoreCoordinator.enqueueJob("PORT- " + portName, () -> {
+            WriteTransaction txn = dataBroker.newWriteOnlyTransaction();
+            List<ListenableFuture<Void>> futures = new ArrayList<>();
+            InterfaceBuilder interfaceBuilder = new InterfaceBuilder();
+            IfL2vlanBuilder ifL2vlanBuilder = new IfL2vlanBuilder();
+            ifL2vlanBuilder.setL2vlanMode(IfL2vlan.L2vlanMode.TrunkMember)
+                .setVlanId(new VlanId(subPort.getSegmentationId().intValue()));
+            interfaceBuilder.setEnabled(true).setName(portName).setType(L2vlan.class)
+                .addAugmentation(IfL2vlan.class, ifL2vlanBuilder.build());
+            Interface iface = interfaceBuilder.build();
+            txn.put(LogicalDatastoreType.CONFIGURATION, interfaceIdentifier, iface);
+            LOG.trace("Creating trunk member interface {}", interfaceBuilder.build());
+            createElanInterface(subPort, portName, txn);
+            futures.add(txn.submit());
+            return futures;
+        });
+    }
+
+    private void createElanInterface(SubPorts subPort, String name, WriteTransaction txn) {
+        Port port = NeutronvpnUtils.getNeutronPort(dataBroker, subPort.getPortId());
+        String elanInstanceName = port.getNetworkId().getValue();
+        List<PhysAddress> physAddresses = new ArrayList<>();
+        physAddresses.add(new PhysAddress(port.getMacAddress().getValue()));
+
+        InstanceIdentifier<ElanInterface> id = InstanceIdentifier.builder(ElanInterfaces.class).child(ElanInterface
+                .class, new ElanInterfaceKey(name)).build();
+        ElanInterface elanInterface = new ElanInterfaceBuilder().setElanInstanceName(elanInstanceName)
+                .setName(name).setStaticMacEntries(physAddresses).setKey(new ElanInterfaceKey(name)).build();
+        txn.put(LogicalDatastoreType.CONFIGURATION, id, elanInterface);
+        LOG.trace("Creating new Elan Interface {}", elanInterface);
+    }
+
+    private void deleteSubPortInterface(SubPorts subPort) {
+        String portName = subPort.getPortId().getValue();
+        InstanceIdentifier<Interface> interfaceIdentifier =
+                        NeutronvpnUtils.buildVlanInterfaceIdentifier(subPort.getPortId().getValue());
+        final DataStoreJobCoordinator dataStoreCoordinator = DataStoreJobCoordinator.getInstance();
+        dataStoreCoordinator.enqueueJob("PORT- " + portName, () -> {
+            WriteTransaction txn = dataBroker.newWriteOnlyTransaction();
+            List<ListenableFuture<Void>> futures = new ArrayList<>();
+            txn.delete(LogicalDatastoreType.CONFIGURATION, interfaceIdentifier);
+            futures.add(txn.submit());
+            return futures;
+        });
+
+    }
+}
