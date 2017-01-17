54eae3ee047a4a23d740fe62403a86196e17953c
Arun Sharma <arun.e.sharma@ericsson.com>
2017-01-17 14:43:13 +0530

    Netvirt: Neutron Qos Alert support
    Adds support to monitor the drop counts and log alert message when
    Qos rate limit rule if applied.
    
    Supports:
    - Configuration to start or stop monitoring the port statistics
    - Configuration of rx drop count threshold value in %
    - Configuration of location and name of log file
    
    Description:
    - As a result of Qos rate limit rule, ingress packets may get dropped
      by the switch.
    - If monitoring is enabled, port statistics are queried every 5 minutes
      and alert message is logged in a log file if packet drop % is more
      than configured threshold value.
    - Threshold value is specified in % of drop counts
    - All configurations are applied at controller level.
    - Default or initial configuration available in netvirt-qosalert-config.xml
        * qos-alert-enabled - false
        * qos-drop-packet-threshold - 25
        * qos-alert-log-file - qosalert/qos-alert.log
    
    Spec Review Link:
    https://git.opendaylight.org/gerrit/50689
    
    Testing procedure:
    1. Create neutron network,subnet and instance.
    2. Create neutron qos policy.
    3. Associate policy to the port/network
    4. Send traffic more than the limit specified in policy.
    5. Configure the threshold value and enable port statistics monitoring
    6. Check for alert log messages in the file if packet drop % cross the
       threshold value.
    
    Result:
    - Port statistics polling is started when qos-alert-enabled is set to true
    - Alerts messages are logged when packet drop % cross the threshold value
    
    Change-Id: I5b2675a2c036835d4ac2e5bd585c3da8e9b268e8
    Signed-off-by: Arun Sharma <arun.e.sharma@ericsson.com>

---
+++ vpnservice/qosalert/qosalert-impl/src/main/java/org/opendaylight/netvirt/qosalert/QosAlertConfigListener.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.qosalert;
+
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netvirt.qosalert.config.rev161205.QosalertConfig;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class QosAlertConfigListener  extends
+        AsyncClusteredDataTreeChangeListenerBase<QosalertConfig, QosAlertConfigListener>  implements AutoCloseable   {
+
+    private static final Logger LOG = LoggerFactory.getLogger(QosAlertConfigListener.class);
+    private final DataBroker broker;
+    private final QosAlertManager manager;
+    private static QosAlertConfigListener instance;
+
+    private QosAlertConfigListener(final DataBroker broker, final QosAlertManager manager) {
+        super(QosalertConfig.class, QosAlertConfigListener.class);
+        this.broker = broker;
+        this.manager = manager;
+        LOG.info("QosConfig Listener created");
+    }
+
+    public static QosAlertConfigListener getInstance(final DataBroker broker, final QosAlertManager manager) {
+        LOG.info("QosConfig Listener getInstance");
+        if (instance == null) {
+            instance = new QosAlertConfigListener(broker, manager);
+        }
+        return (instance);
+    }
+
+    public void init() {
+        registerListener(LogicalDatastoreType.CONFIGURATION, broker);
+        LOG.info("Registration success");
+    }
+
+
+    @Override
+    protected InstanceIdentifier<QosalertConfig> getWildCardPath() {
+        return InstanceIdentifier.create(QosalertConfig.class);
+    }
+
+    @Override
+    public void close() throws Exception {
+        super.close();
+        LOG.info("QosConfig Listener Closed");
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<QosalertConfig> identifier, QosalertConfig del) {
+        LOG.info("QosalertConfig removed: {}", del);
+        manager.restoreDefaultConfig();
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<QosalertConfig> identifier, QosalertConfig original,
+                                                                                QosalertConfig update) {
+        LOG.info("QosalertConfig changed to {}", update);
+        manager.setQosalertConfig(update);
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<QosalertConfig> identifier, QosalertConfig add) {
+        LOG.info("QosalertConfig added {}", add);
+        manager.setQosalertConfig(add);
+    }
+
+    @Override
+    protected QosAlertConfigListener getDataTreeChangeListener() {
+        return this;
+    }
+
+}
