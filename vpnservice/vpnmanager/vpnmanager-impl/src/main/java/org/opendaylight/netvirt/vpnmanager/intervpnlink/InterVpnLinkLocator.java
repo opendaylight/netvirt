f415c4a2f3da2227c80177237569ed91175e6772
Miguel Perez <francisco.miguel.perez@ericsson.com>
2017-01-24 18:01:29 +0100

    Bug 7680: leaked routes not advertised to DC-GW
     + It's been found that the routes that are leaked to DC-GW
       at InterVpnLink creation time (well, at InterVpnLink added
       callback) are correctly advertised to DC-GW. However, the
       routes that are leaked after the InterVpnLink has been
       created are not being advertised to DC-GW.
    
     + These second type of leaked routes are handled on fibmanager,
       but fibmanager does not have access to BgpManager. So the
       solution consists in moving part of the logic from fibmanager
       to vpnmanager where the BgpManager is accessible.
    
     + InterVpnLinkService class, which was just an internal class in
       vpnmanager-impl, is renamed to InterVpnLinkLocator whose only
       responsibility is to search for the most suitable DPNs to
       place new InterVpnLinks. The name change is to avoid confusion
       with IVpnLinkServiceImpl which is the implementation of the
       public interface 'IVpnLinkServce'.
    
     + Besides the renaming of InterVpnLinkService, most of its code
       is moved to IVpnLinkServiceImpl, and that's why this change
       may llok big.
    
     + Note: there is a <wip> [1] for taking the route leaking out
       of the fibmanager completely.
    
     [1] https://git.opendaylight.org/gerrit/#/c/48599/
    
    Change-Id: I1a7311345307716ef223c707b22c6ecec66d97a2
    Signed-off-by: Miguel Perez <francisco.miguel.perez@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/intervpnlink/InterVpnLinkLocator.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/intervpnlink/InterVpnLinkLocator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -43,22 +43,15 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class InterVpnLinkService {
+public class InterVpnLinkLocator {
 
-    private static final Logger LOG = LoggerFactory.getLogger(InterVpnLinkService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(InterVpnLinkLocator.class);
     private static final String NBR_OF_DPNS_PROPERTY_NAME = "vpnservice.intervpnlink.number.dpns";
 
     private final DataBroker dataBroker;
-    private final IdManagerService idManager;
-    private final IBgpManager bgpManager;
-    private final IFibManager fibManager;
 
-    public InterVpnLinkService(final DataBroker dataBroker, final IdManagerService idManager,
-                               final IBgpManager bgpManager, final IFibManager fibManager) {
+    public InterVpnLinkLocator(final DataBroker dataBroker) {
         this.dataBroker = dataBroker;
-        this.idManager = idManager;
-        this.bgpManager = bgpManager;
-        this.fibManager = fibManager;
     }
 
     /**
@@ -216,100 +209,3 @@ public class InterVpnLinkService {
 
         return interVpnLinks.stream().filter(areSameGroup).collect(Collectors.toList());
     }
-
-    private Map<String, String> buildRouterXL3VPNMap() {
-        InstanceIdentifier<VpnMaps> vpnMapsIdentifier = InstanceIdentifier.builder(VpnMaps.class).build();
-        Optional<VpnMaps> optVpnMaps =
-            MDSALUtil.read(dataBroker, LogicalDatastoreType.CONFIGURATION, vpnMapsIdentifier);
-        if (!optVpnMaps.isPresent()) {
-            LOG.info("Could not retrieve VpnMaps object from Configurational DS");
-            return new HashMap<>();
-        }
-        Predicate<VpnMap> isExternalVpn =
-            (vpnMap) -> vpnMap.getRouterId() != null
-                        && ! vpnMap.getVpnId().getValue().equalsIgnoreCase(vpnMap.getRouterId().getValue());
-
-        return optVpnMaps.get().getVpnMap()
-                .stream()
-                .filter(isExternalVpn)
-                .collect(Collectors.toMap(v -> v.getRouterId().getValue(), v -> v.getVpnId().getValue()));
-    }
-
-    /*
-     * Checks if there are any static routes pointing to any of both
-     * InterVpnLink's endpoints. Goes through all routers checking if they have
-     * a route whose nexthop is an InterVpnLink endpoint
-     */
-    public void handleStaticRoutes(InterVpnLinkDataComposite interVpnLink) {
-        // Map that corresponds a routerId with the L3VPN that it's been assigned to.
-        Map<String, String> routerXL3VpnMap = buildRouterXL3VPNMap();
-
-        // Retrieving all Routers
-        InstanceIdentifier<Routers> routersIid = InstanceIdentifier.builder(Neutron.class).child(Routers.class).build();
-        Optional<Routers> routerOpData = MDSALUtil.read(dataBroker, LogicalDatastoreType.CONFIGURATION, routersIid);
-        if ( !routerOpData.isPresent() ) {
-
-            return;
-        }
-        List<Router> routers = routerOpData.get().getRouter();
-        for ( Router router : routers ) {
-            String vpnId = routerXL3VpnMap.get(router.getUuid().getValue());
-            if ( vpnId == null ) {
-                LOG.warn("Could not find suitable VPN for router {}", router.getUuid());
-                continue; // with next router
-            }
-            List<Routes> routerRoutes = router.getRoutes();
-            if ( routerRoutes != null ) {
-                for ( Routes route : routerRoutes ) {
-                    handleStaticRoute(vpnId, route, interVpnLink);
-                }
-            }
-        }
-    }
-
-    /**
-     * Takes care of an static route to see if flows related to interVpnLink
-     * must be installed in tables 20 and 17.
-     *
-     * @param vpnId Vpn to which the route belongs
-     * @param route Route to handle. Will only be considered if its nexthop is the VPN's endpoint IpAddress
-     *              at the other side of the InterVpnLink
-     */
-    // TODO Clean up exception handling
-    @SuppressWarnings("checkstyle:IllegalCatch")
-    private void handleStaticRoute(String vpnId, Routes route, InterVpnLinkDataComposite interVpnLink) {
-
-        IpAddress nhIpAddr = route.getNexthop();
-        String routeNextHop = (nhIpAddr.getIpv4Address() != null) ? nhIpAddr.getIpv4Address().getValue()
-                                                                  : nhIpAddr.getIpv6Address().getValue();
-        String destination = String.valueOf(route.getDestination().getValue());
-
-        // is nexthop the other endpoint's IP
-        String otherEndpoint = interVpnLink.getOtherEndpoint(vpnId);
-        if ( !routeNextHop.equals(otherEndpoint) ) {
-            LOG.debug("VPN {}: Route to {} nexthop={} points to an InterVpnLink endpoint, but its not "
-                      + "the other endpoint. Other endpoint is {}",
-                      vpnId, destination, routeNextHop, otherEndpoint);
-            return;
-        }
-
-        // Lets work: 1) write Fibentry, 2) advertise to BGP and 3) check if it must be leaked
-        String vpnRd = VpnUtil.getVpnRd(dataBroker, vpnId);
-        if ( vpnRd == null ) {
-            LOG.warn("Could not find Route-Distinguisher for VpnName {}", vpnId);
-            return;
-        }
-
-        int label = VpnUtil.getUniqueId(idManager, VpnConstants.VPN_IDPOOL_NAME,
-                                        VpnUtil.getNextHopLabelKey(vpnId, destination));
-
-        try {
-            InterVpnLinkUtil.handleStaticRoute(interVpnLink, vpnId, destination, routeNextHop, label,
-                                               dataBroker, fibManager, bgpManager);
-        } catch (Exception e) {
-            LOG.warn("InterVpnLink [{}]: Could not handle static route [vpn={} prefix={} nexthop={} label={}]",
-                     interVpnLink.getInterVpnLinkName(), vpnId, destination, routeNextHop, label, e);
-        }
-    }
-
-}
