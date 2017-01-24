1f228d4402e82a5f144f8ff6047254a755b1f3d9
Miguel Perez <francisco.miguel.perez@ericsson.com>
2017-01-24 18:01:29 +0100

    Bug 7680: leaked routes not advertised to DC-GW
     + It's been found that the routes that are leaked to DC-GW
       at InterVpnLink creation time (well, at InterVpnLink added
       callback) are correctly advertised to DC-GW. However, the
       routes that are leaked after the InterVpnLink has been
       created are not being advertised to DC-GW.
    
     + These second type of leaked routes are handled on fibmanager,
       but fibmanager does not have access to BgpManager. So the
       solution consists in moving part of the logic from fibmanager
       to vpnmanager where the BgpManager is accessible.
    
     + InterVpnLinkService class, which was just an internal class in
       vpnmanager-impl, is renamed to InterVpnLinkLocator whose only
       responsibility is to search for the most suitable DPNs to
       place new InterVpnLinks. The name change is to avoid confusion
       with IVpnLinkServiceImpl which is the implementation of the
       public interface 'IVpnLinkServce'.
    
     + Besides the renaming of InterVpnLinkService, most of its code
       is moved to IVpnLinkServiceImpl, and that's why this change
       may llok big.
    
     + Note: there is a <wip> [1] for taking the route leaking out
       of the fibmanager completely.
    
     [1] https://git.opendaylight.org/gerrit/#/c/48599/
    
    Change-Id: I1a7311345307716ef223c707b22c6ecec66d97a2
    Signed-off-by: Miguel Perez <francisco.miguel.perez@ericsson.com>

---
+++ vpnservice/vpnmanager/vpnmanager-api/src/main/java/org/opendaylight/netvirt/vpnmanager/api/intervpnlink/IVpnLinkService.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.vpnmanager.api.intervpnlink;
+
+import java.util.List;
+import org.opendaylight.netvirt.fibmanager.api.RouteOrigin;
+
+public interface IVpnLinkService {
+
+    /**
+     * Leaks a route belonging to a L3VPN to other L3VPN if the neccessary
+     * circumstances are met, like there is an InterVpnLink linking both L3VPNs
+     * and the corresponding leaking flag is active (bgp/static/connected).
+     *
+     * @param vpnName Vpn name of the L3VPN that holds the original route
+     * @param prefix Prefix/destination of the route
+     * @param nextHopList List of nexthops (ECMP) of the route
+     * @param label Label of the route to be leaked
+     * @param origin Origin of the route (BGP|STATIC|CONNECTED)
+     * @param addOrRemove states if the routes must be leaked or withdrawn
+     */
+    void leakRouteIfNeeded(String vpnName, String prefix, List<String> nextHopList, int label, RouteOrigin origin,
+                           int addOrRemove);
+
+    /**
+     * Requests IVpnLinkService to take care of those static routes that point
+     * to the specified InterVpnLink and that may be configured in any Neutron
+     * Router
+     *
+     * @param interVpnLink InterVpnLink to be considered
+     */
+    void handleStaticRoutes(InterVpnLinkDataComposite interVpnLink);
+
+}
