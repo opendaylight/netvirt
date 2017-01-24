24c78a9bbffdcde315655d971e29702db42daca7
Miguel Perez <francisco.miguel.perez@ericsson.com>
2017-01-24 18:01:29 +0100

    Bug 7680: leaked routes not advertised to DC-GW
     + It's been found that the routes that are leaked to DC-GW
       at InterVpnLink creation time (well, at InterVpnLink added
       callback) are correctly advertised to DC-GW. However, the
       routes that are leaked after the InterVpnLink has been
       created are not being advertised to DC-GW.
    
     + These second type of leaked routes are handled on fibmanager,
       but fibmanager does not have access to BgpManager. So the
       solution consists in moving part of the logic from fibmanager
       to vpnmanager where the BgpManager is accessible.
    
     + InterVpnLinkService class, which was just an internal class in
       vpnmanager-impl, is renamed to InterVpnLinkLocator whose only
       responsibility is to search for the most suitable DPNs to
       place new InterVpnLinks. The name change is to avoid confusion
       with IVpnLinkServiceImpl which is the implementation of the
       public interface 'IVpnLinkServce'.
    
     + Besides the renaming of InterVpnLinkService, most of its code
       is moved to IVpnLinkServiceImpl, and that's why this change
       may llok big.
    
     + Note: there is a <wip> [1] for taking the route leaking out
       of the fibmanager completely.
    
     [1] https://git.opendaylight.org/gerrit/#/c/48599/
    
    Change-Id: I1a7311345307716ef223c707b22c6ecec66d97a2
    Signed-off-by: Miguel Perez <francisco.miguel.perez@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/intervpnlink/InterVpnLinkListener.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/intervpnlink/InterVpnLinkListener.java
@@ -35,6 +35,7 @@ import org.opendaylight.netvirt.vpnmanager.VpnConstants;
 import org.opendaylight.netvirt.vpnmanager.VpnFootprintService;
 import org.opendaylight.netvirt.vpnmanager.VpnOpDataSyncer;
 import org.opendaylight.netvirt.vpnmanager.VpnUtil;
+import org.opendaylight.netvirt.vpnmanager.api.intervpnlink.IVpnLinkService;
 import org.opendaylight.netvirt.vpnmanager.api.intervpnlink.InterVpnLinkCache;
 import org.opendaylight.netvirt.vpnmanager.api.intervpnlink.InterVpnLinkDataComposite;
 import org.opendaylight.netvirt.vpnmanager.intervpnlink.tasks.InterVpnLinkCleanedCheckerTask;
@@ -77,26 +78,23 @@ public class InterVpnLinkListener extends AsyncDataTreeChangeListenerBase<InterV
 
     private static final long INVALID_ID = 0;
 
-    private final InterVpnLinkService ivpnLinkService;
     private final DataBroker dataBroker;
     private final IMdsalApiManager mdsalManager;
     private final IdManagerService idManager;
     private final IBgpManager bgpManager;
     private final IFibManager fibManager;
     private final NotificationPublishService notificationsService;
+    private final IVpnLinkService ivpnLinkService;
+    private final InterVpnLinkLocator ivpnLinkLocator;
     private final VpnFootprintService vpnFootprintService;
     private final VpnOpDataSyncer vpnOpDataSyncer;
-
-
-    // A couple of listener in order to maintain the InterVpnLink cache
-    private InterVpnLinkCacheFeeder interVpnLinkCacheFeeder;
-    private InterVpnLinkStateCacheFeeder interVpnLinkStateCacheFeeder;
 
 
     public InterVpnLinkListener(final DataBroker dataBroker, final IdManagerService idManager,
                                 final IMdsalApiManager mdsalManager, final IBgpManager bgpManager,
                                 final IFibManager fibManager, final NotificationPublishService notifService,
-                                final InterVpnLinkService interVpnLinkService,
+                                final IVpnLinkService interVpnLinkService,
+                                final InterVpnLinkLocator interVpnLinkLocator,
                                 final VpnFootprintService vpnFootprintService,
                                 final VpnOpDataSyncer vpnOpDataSyncer) {
         super(InterVpnLink.class, InterVpnLinkListener.class);
@@ -107,6 +105,7 @@ public class InterVpnLinkListener extends AsyncDataTreeChangeListenerBase<InterV
         this.fibManager = fibManager;
         this.notificationsService = notifService;
         this.ivpnLinkService = interVpnLinkService;
+        this.ivpnLinkLocator = interVpnLinkLocator;
         this.vpnFootprintService = vpnFootprintService;
         this.vpnOpDataSyncer = vpnOpDataSyncer;
     }
@@ -116,8 +115,6 @@ public class InterVpnLinkListener extends AsyncDataTreeChangeListenerBase<InterV
         LOG.info("{} start", getClass().getSimpleName());
         registerListener(LogicalDatastoreType.CONFIGURATION, dataBroker);
         InterVpnLinkCache.createInterVpnLinkCaches(dataBroker);
-        interVpnLinkCacheFeeder = new InterVpnLinkCacheFeeder(dataBroker);
-        interVpnLinkStateCacheFeeder = new InterVpnLinkStateCacheFeeder(dataBroker);
     }
 
     @Override
@@ -208,7 +205,7 @@ public class InterVpnLinkListener extends AsyncDataTreeChangeListenerBase<InterV
             }
         }
 
-        List<BigInteger> firstDpnList = ivpnLinkService.selectSuitableDpns(add);
+        List<BigInteger> firstDpnList = ivpnLinkLocator.selectSuitableDpns(add);
         if (firstDpnList != null && !firstDpnList.isEmpty()) {
             List<BigInteger> secondDpnList = firstDpnList;
 
