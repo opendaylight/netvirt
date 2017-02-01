67645fc9bcab70d0be3d95722e2a0065df4566c8
Stephen Kitt <skitt@redhat.com>
2017-02-01 17:48:04 +0100

    Checkstyle: fix vpnmanager-api and enforce
    Change-Id: I72a0dac453115aa6498c8d62cbd534b57c45c714
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- vpnservice/vpnmanager/vpnmanager-api/src/main/java/org/opendaylight/netvirt/vpnmanager/api/ICentralizedSwitchProvider.java
+++ vpnservice/vpnmanager/vpnmanager-api/src/main/java/org/opendaylight/netvirt/vpnmanager/api/ICentralizedSwitchProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Hewlett Packard Enterprise, Co. and others. All rights reserved.
+ * Copyright © 2016, 2017 Hewlett Packard Enterprise, Co. and others. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -16,8 +16,7 @@ import java.math.BigInteger;
  * The centralized switch is currently implemented using NAPT switch models
  * residing in natservice bundle. As the roles of centralized switch will grow
  * beyond NAT use cases, the associated models and logic need to be renamed
- * and moved to either vpnmanager or new bundle as part of Carbon model changes
- *
+ * and moved to either vpnmanager or new bundle as part of Carbon model changes.
  */
 public interface ICentralizedSwitchProvider {
 
@@ -25,8 +24,8 @@ public interface ICentralizedSwitchProvider {
      * Get the primary switch selected for the router if it has previously been
      * allocated.
      *
-     * @param routerName
-     * @return primary switch id
+     * @param routerName The router's name.
+     * @return The primary switch id.
      */
     BigInteger getPrimarySwitchForRouter(String routerName);
 
