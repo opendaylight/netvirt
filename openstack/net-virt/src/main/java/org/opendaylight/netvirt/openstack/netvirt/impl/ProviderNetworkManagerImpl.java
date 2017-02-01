65b93ffe0953f28992b24e70bf55994dc3dc6001
Vinh Nguyen <vinh.nguyen@hcl.com>
2017-01-31 17:06:50 -0800

    BUG 7717 Fix OOM when defining large number of networks
    Use NodeId instead of Node as key for the cache
    ProviderNetworkManagerImpl::nodeToProviderMapping
    The size bridge Node object is huge when there are
    large number of network/subnets on the bridge.
    
    Change-Id: I6956318477dfa524ce20680d12bba3c7818540ec
    Signed-off-by: Vinh Nguyen <vinh.nguyen@hcl.com>

--- openstack/net-virt/src/main/java/org/opendaylight/netvirt/openstack/netvirt/impl/ProviderNetworkManagerImpl.java
+++ openstack/net-virt/src/main/java/org/opendaylight/netvirt/openstack/netvirt/impl/ProviderNetworkManagerImpl.java
@@ -19,6 +19,7 @@ import org.opendaylight.netvirt.openstack.netvirt.api.NetworkingProvider;
 import org.opendaylight.netvirt.openstack.netvirt.api.NetworkingProviderManager;
 import org.opendaylight.netvirt.openstack.netvirt.api.OvsdbInventoryService;
 import org.opendaylight.netvirt.utils.servicehelper.ServiceHelper;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
@@ -27,13 +28,14 @@ import org.slf4j.LoggerFactory;
 public class ProviderNetworkManagerImpl implements ConfigInterface, NetworkingProviderManager {
     private static final Logger LOG = LoggerFactory.getLogger(ProviderNetworkManagerImpl.class);
     private Map<Long, ProviderEntry> providers = Maps.newHashMap();
-    private Map<Node, NetworkingProvider> nodeToProviderMapping = Maps.newHashMap();
+    private Map<NodeId, NetworkingProvider> nodeToProviderMapping = Maps.newHashMap();
     private volatile OvsdbInventoryService ovsdbInventoryService;
 
     @Override
     public NetworkingProvider getProvider(Node node) {
-        if (nodeToProviderMapping.get(node) != null) {
-            return nodeToProviderMapping.get(node);
+        NodeId nodeId = node.getNodeId();
+        if (nodeToProviderMapping.get(nodeId) != null) {
+            return nodeToProviderMapping.get(nodeId);
         }
 
         final String targetVersion = Constants.OPENFLOW13;
@@ -51,7 +53,7 @@ public class ProviderNetworkManagerImpl implements ConfigInterface, NetworkingPr
         // Return the first match as only have one matching provider today
         // ToDo: Tie-breaking logic
         NetworkingProvider provider = matchingProviders.get(0).getProvider();
-        nodeToProviderMapping.put(node, provider);
+        nodeToProviderMapping.put(nodeId, provider);
         return provider;
     }
 
