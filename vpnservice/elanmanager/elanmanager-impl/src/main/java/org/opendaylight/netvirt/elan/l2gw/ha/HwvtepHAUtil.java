b8f01751ef727793f76bf2fe4e3bf997860e871d
K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>
2017-02-05 14:44:56 +0530

    bug 7599 added l2gw validate cli
    Added a helper cli to validate various config and operational
    data models.
    Checks if the config and operational datastore differ
    Checks if the ha parent node and child nodes differ
    Checks if the logical switches/ mcast macs and vlan bindings
    are created for the configured l2gw connection
    
    Change-Id: I96924a520d30985b98508afdaccbe379ef54d76e
    Signed-off-by: K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/ha/HwvtepHAUtil.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/ha/HwvtepHAUtil.java
@@ -14,6 +14,7 @@ import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -214,6 +215,13 @@ public class HwvtepHAUtil {
 
     public static boolean isEmptyList(List list) {
         if (list == null || list.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isEmpty(Collection collection) {
+        if (collection == null || collection.isEmpty()) {
             return true;
         }
         return false;
