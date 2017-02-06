e7acb7b50c3aa52180f0944fea7fd5f65f8184ed
Michael Vorburger <vorburger@redhat.com>
2017-02-07 00:56:15 +0100

    Fixed NodeConnectedHandlerTest with AbstractConcurrentDataBrokerTest
    use new AbstractConcurrentDataBrokerTest instead of @deprecated
    AbstractDataBrokerTest, and manage the impacts this had in
    NodeConnectedHandlerTest (which was a bit of a mess; e.g. we typically
    shouldn't mix a Mockito mocked transaction with a real one... and we
    need to wait for DataBroker submit() to process via a get() before
    asserting something)
    
    Bug: 7538
    Change-Id: I15ed9908b03d56bd84c4f637fc83fc62328f6f11
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- vpnservice/elanmanager/elanmanager-impl/src/test/java/org/opendaylight/netvirt/elan/l2gw/nodehandlertest/NodeConnectedHandlerTest.java
+++ vpnservice/elanmanager/elanmanager-impl/src/test/java/org/opendaylight/netvirt/elan/l2gw/nodehandlertest/NodeConnectedHandlerTest.java
@@ -15,11 +15,10 @@ import java.util.UUID;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
-import org.opendaylight.controller.md.sal.binding.test.AbstractDataBrokerTest;
+import org.opendaylight.controller.md.sal.binding.test.AbstractConcurrentDataBrokerTest;
 import org.opendaylight.netvirt.elan.l2gw.ha.handlers.NodeConnectedHandler;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
@@ -36,11 +35,17 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by eaksahu on 10/14/2016.
  */
-public class NodeConnectedHandlerTest extends AbstractDataBrokerTest {
+public class NodeConnectedHandlerTest extends AbstractConcurrentDataBrokerTest {
+
+    // Uncomment this to keep running this test indefinitely
+    // This is very useful to detect concurrency issues, respectively prove
+    // that the use of AbstractConcurrentDataBrokerTest instead of AbstractDataBrokerTest
+    // does NOT cause any concurrency issues and make this test flaky...
+    // public static @ClassRule RunUntilFailureClassRule classRepeater = new RunUntilFailureClassRule();
+    // public @Rule RunUntilFailureRule repeater = new RunUntilFailureRule(classRepeater);
 
     static Logger LOG = LoggerFactory.getLogger(NodeConnectedHandlerTest.class);
     DataBroker dataBroker;
-    ReadWriteTransaction tx;
 
     NodeConnectedHandler nodeConnectedHandler = new NodeConnectedHandler(this.dataBroker);
     NodeConnectedHandlerUtils handlerUtils = new NodeConnectedHandlerUtils();
@@ -80,7 +85,6 @@ public class NodeConnectedHandlerTest extends AbstractDataBrokerTest {
     @Before
     public void setupForHANode() {
         dataBroker = getDataBroker();
-        tx = Mockito.spy(dataBroker.newReadWriteTransaction());
         d1UUID = java.util.UUID.nameUUIDFromBytes("d1uuid".getBytes()).toString();
         d2UUID = java.util.UUID.nameUUIDFromBytes("d2uuid".getBytes()).toString();
         d1NodePath = getInstanceIdentifier(d1UUID);
@@ -102,12 +106,14 @@ public class NodeConnectedHandlerTest extends AbstractDataBrokerTest {
 
     @Test
     public void testD1Connect() throws Exception {
-        handlerUtils.addPsNode(d1PsNodePath, d1NodePath, DataProvider.getPortNameListD1(), tx);
+        ReadWriteTransaction tx = this.dataBroker.newReadWriteTransaction();
+        handlerUtils.addPsNode(d1PsNodePath, d1NodePath, DataProvider.getPortNameListD1(), tx).checkedGet();
+
         tx = this.dataBroker.newReadWriteTransaction();
         handlerUtils.addNode(d1NodePath, d1PsNodePath, DataProvider.getLogicalSwitchDataD1(),
                 DataProvider.getLocalUcasMacDataD1(), DataProvider.getLocalMcastDataD1(),
                 DataProvider.getRemoteMcastDataD1(), DataProvider.getRemoteUcasteMacDataD1(),
-                DataProvider.getGlobalTerminationPointIpD1(), tx);
+                DataProvider.getGlobalTerminationPointIpD1(), tx).checkedGet();
 
         readNodes();
         tx = this.dataBroker.newReadWriteTransaction();
@@ -123,8 +129,6 @@ public class NodeConnectedHandlerTest extends AbstractDataBrokerTest {
                 && haPsOpNode.isPresent());
         TestUtil.verifyHAOpNode(d1GlobalOpNode.get(), haGlobalOpNode.get(),
                 d1PsOpNode.get(), haPsOpNode.get(), haNodePath, d1PsNodePath, haPsNodePath, haNodeId, this.dataBroker);
-
-
     }
 
     public static InstanceIdentifier<Node> createInstanceIdentifier(String nodeIdString) {
@@ -170,6 +174,7 @@ public class NodeConnectedHandlerTest extends AbstractDataBrokerTest {
         d1PsConfigNode = TestUtil.readNode(CONFIGURATION, d1PsNodePath, tx);
         d2PsConfigNode = TestUtil.readNode(CONFIGURATION, d2PsNodePath, tx);
 
+        tx.close();
     }
 
 }
