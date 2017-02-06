d07a1b328c9a006e91790d7cfa34f7156dc496a0
Michael Vorburger <vorburger@redhat.com>
2017-02-07 00:56:15 +0100

    Fixed NodeConnectedHandlerTest with AbstractConcurrentDataBrokerTest
    use new AbstractConcurrentDataBrokerTest instead of @deprecated
    AbstractDataBrokerTest, and manage the impacts this had in
    NodeConnectedHandlerTest (which was a bit of a mess; e.g. we typically
    shouldn't mix a Mockito mocked transaction with a real one... and we
    need to wait for DataBroker submit() to process via a get() before
    asserting something)
    
    Bug: 7538
    Change-Id: I15ed9908b03d56bd84c4f637fc83fc62328f6f11
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- vpnservice/elanmanager/elanmanager-impl/src/test/java/org/opendaylight/netvirt/elan/l2gw/LogicalSwitchesCmdTest.java
+++ vpnservice/elanmanager/elanmanager-impl/src/test/java/org/opendaylight/netvirt/elan/l2gw/LogicalSwitchesCmdTest.java
@@ -17,7 +17,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
-import org.opendaylight.controller.md.sal.binding.test.AbstractDataBrokerTest;
+import org.opendaylight.controller.md.sal.binding.test.AbstractConcurrentDataBrokerTest;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.netvirt.elan.l2gw.ha.HwvtepHAUtil;
 import org.opendaylight.netvirt.elan.l2gw.ha.commands.LogicalSwitchesCmd;
@@ -30,7 +30,14 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hw
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
-public class LogicalSwitchesCmdTest  extends AbstractDataBrokerTest {
+public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
+
+    // Uncomment this to keep running this test indefinitely
+    // This is very useful to detect concurrency issues, respectively prove
+    // that the use of AbstractConcurrentDataBrokerTest instead of AbstractDataBrokerTest
+    // does NOT cause any concurrency issues and make this test flaky...
+    // public static @ClassRule RunUntilFailureClassRule classRepeater = new RunUntilFailureClassRule();
+    // public @Rule RunUntilFailureRule repeater = new RunUntilFailureRule(classRepeater);
 
     DataBroker dataBroker;
     ReadWriteTransaction tx;
