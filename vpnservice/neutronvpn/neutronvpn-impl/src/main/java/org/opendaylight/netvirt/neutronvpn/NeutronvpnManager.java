c5a3a287c7ca0bb978bfe3ad41f84391684453e3
eceghkl <manu.b@ericsson.com>
2017-02-07 15:35:57 +0530

    Enhancement to support list of rds for l3vpn
    	* This review contains enhancement to support list of rds while
     	  creating l3vpn instances.
    	* Primary rd is obtained from the list of rds which will be used
    	  as route distinguisher for primary adjacencies.
    
    Change-Id: Ib8d0dd41712c734b5868c5dc87ee0e54e547ff34
    Signed-off-by: eceghkl <manu.b@ericsson.com>

--- vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnManager.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnManager.java
@@ -11,7 +11,6 @@ import com.google.common.base.Optional;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EventListener;
@@ -439,7 +438,7 @@ public class NeutronvpnManager implements NeutronvpnService, AutoCloseable, Even
             Ipv4FamilyBuilder ipv4vpnBuilder = new Ipv4FamilyBuilder().setVpnTargets(vpnTargets);
 
             if (rd != null && !rd.isEmpty()) {
-                ipv4vpnBuilder.setRouteDistinguisher(rd.get(0));
+                ipv4vpnBuilder.setRouteDistinguisher(rd);
             }
 
             VpnInstance newVpn = builder.setIpv4Family(ipv4vpnBuilder.build()).build();
@@ -1012,7 +1011,7 @@ public class NeutronvpnManager implements NeutronvpnService, AutoCloseable, Even
                 // create VpnMaps id
                 L3vpnInstancesBuilder l3vpn = new L3vpnInstancesBuilder();
 
-                List<String> rd = Arrays.asList(vpnInstance.getIpv4Family().getRouteDistinguisher().split(","));
+                List<String> rd = vpnInstance.getIpv4Family().getRouteDistinguisher();
                 List<VpnTarget> vpnTargetList = vpnInstance.getIpv4Family().getVpnTargets().getVpnTarget();
 
                 List<String> ertList = new ArrayList<>();
