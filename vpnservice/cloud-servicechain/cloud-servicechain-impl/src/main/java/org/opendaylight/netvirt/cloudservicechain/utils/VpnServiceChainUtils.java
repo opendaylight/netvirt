69deddb6e3334198af84ae74a1d44aa307a6134f
Kency Kurian <kency.kurian@ericsson.com>
2017-02-08 11:33:02 +0530

    Support for multiple route paths
    - Modified odl-fib.yang to support multiple route-paths for the same
      destination prefix.
    - Added a FibHelper which provides util methods to construct VrfEntry.
    
    Change-Id: I7bcd3b77ac452476b4190286c1f3e8815a1b8819
    Signed-off-by: Kency Kurian <kency.kurian@ericsson.com>

--- vpnservice/cloud-servicechain/cloud-servicechain-impl/src/main/java/org/opendaylight/netvirt/cloudservicechain/utils/VpnServiceChainUtils.java
+++ vpnservice/cloud-servicechain/cloud-servicechain-impl/src/main/java/org/opendaylight/netvirt/cloudservicechain/utils/VpnServiceChainUtils.java
@@ -327,21 +327,28 @@ public class VpnServiceChainUtils {
      *
      */
     public static void programLFibEntriesForSCF(IMdsalApiManager mdsalMgr, BigInteger dpId, List<VrfEntry> vrfEntries,
-                                                int lportTag, int addOrRemove) {
-        for (VrfEntry vrfEntry : vrfEntries) {
-            Long label = vrfEntry.getLabel();
-            for (String nexthop : vrfEntry.getNextHopAddressList()) {
-                FlowEntity flowEntity = buildLFibVpnPseudoPortFlow(dpId, label, nexthop, lportTag);
-                if (addOrRemove == NwConstants.ADD_FLOW) {
-                    mdsalMgr.installFlow(flowEntity);
-                } else {
-                    mdsalMgr.removeFlow(flowEntity);
-                }
-                LOG.debug("LFIB Entry for label={}, destination={}, nexthop={} {} successfully in dpn={}",
-                          label, vrfEntry.getDestPrefix(), vrfEntry.getNextHopAddressList(),
-                          addOrRemove == NwConstants.DEL_FLOW ? "removed" : "installed", dpId);
-            }
-        }
+            int lportTag, int addOrRemove) {
+        java.util.Optional.ofNullable(vrfEntries).ifPresent(entries -> {
+            entries.stream()
+                    .forEach(vrfEntry -> vrfEntry.getRoutePaths()
+                            .stream()
+                            .forEach(routePath -> {
+                                Long label = routePath.getLabel();
+                                String nextHop = routePath.getNexthopAddress();
+                                FlowEntity flowEntity =
+                                        buildLFibVpnPseudoPortFlow(dpId, label, nextHop, lportTag);
+                                if (addOrRemove == NwConstants.ADD_FLOW) {
+                                    mdsalMgr.installFlow(flowEntity);
+                                } else {
+                                    mdsalMgr.removeFlow(flowEntity);
+                                }
+                                LOG.debug(
+                                        "LFIBEntry for label={}, destination={}, nexthop={} {} successfully in dpn={}",
+                                        label, vrfEntry.getDestPrefix(), nextHop,
+                                        addOrRemove == NwConstants.DEL_FLOW ? "removed"
+                                                : "installed", dpId);
+                            }));
+        });
     }
 
     /**
