e9b04d7d54c1b2f478d03f40a3f89d306ca50f52
Alon Kochba <alonko@hpe.com>
2017-02-12 18:29:05 +0200

    Fix several NPEs showing up in CSIT
    1. ElanUtils - an ELAN instance can have no segments
    2. TEPChangeListener - TEP could be missing in interfaces-state
    3. VpnInterfaceManager - gateway IP/MAC could be missing when creating
    ARP responder flows.
    4. BgpConfigurationManager - NextHop in list could be null
    5. TransportZoneNoticationUtil - not all interfaces have a neutron port
    
    Change-Id: Ibbf6e5834cd01389e29e36736073927cdd3dbb1a
    Signed-off-by: Alon Kochba <alonko@hpe.com>
    Signed-off-by: Sam Hague <shague@redhat.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnInterfaceManager.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnInterfaceManager.java
@@ -588,9 +588,20 @@ public class VpnInterfaceManager extends AsyncDataTreeChangeListenerBase<VpnInte
                 setupGwMacIfRequired(dpnId, vpnName, interfaceName, vpnId, subnetId,
                         writeInvTxn, NwConstants.ADD_FLOW);
                 final Optional<String> gatewayIp = VpnUtil.getVpnSubnetGatewayIp(dataBroker, subnetId);
-                gwMac = getGatewayMacAddressForInterface(vpnName, interfaceName, gatewayIp.get());
-                addArpResponderFlow(dpnId, lPortTag, vpnName, vpnId, interfaceName, subnetId,
-                        gwMac.get(), gatewayIp.get(), writeInvTxn);
+                if (gatewayIp.isPresent()) {
+                    gwMac = getGatewayMacAddressForInterface(vpnName, interfaceName, gatewayIp.get());
+                    if (gwMac.isPresent()) {
+                        addArpResponderFlow(dpnId, lPortTag, vpnName, vpnId, interfaceName, subnetId,
+                                gwMac.get(), gatewayIp.get(), writeInvTxn);
+                    } else {
+                        LOG.warn("Gateway MAC for subnet ID {} could not be obtained, cannot create "
+                                        + "ARP responder flow for interface name {}, vpnName {}, gwIp {}",
+                                interfaceName, vpnName, gatewayIp.get());
+                    }
+                } else {
+                    LOG.warn("Gateway IP for subnet ID {} could not be obtained, cannot create ARP responder flow "
+                            + "for interface name {}, vpnName {}", subnetId, interfaceName, vpnName);
+                }
             } else {
                 //Extra route adjacency
                 LOG.trace("Adding prefix {} and nextHopList {} as extra-route for vpn", nextHop.getIpAddress(), nextHop.getNextHopIpList(), vpnName);
