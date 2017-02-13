55e98391a2a6ab1f152e382f65e94651a6630c9b
cgowdru <chetan.arakere@altencalsoftlabs.com>
2017-02-13 13:37:38 +0530

    Bug 7780 : NAT RPC's for getting SNAT/DNAT translation information.
    Description : With the Current DS structure, it's is bit difficult to
    identify information about what is the current DNAT/SNAT configuration
    done fo the given router-uuid/vpn-uuid.
    
    It would be good if we have a RPC's which can provide a information as
    below.
    
    input- router-uuid/vpn-uuid
    
    SNAT output - router-name, internal-ip/port, external-ip/port,protocol
    DNAT output - router-name,internal-ip,floating-ip
    
    Change-Id: I00b6576d2a752b53e94ad28c611a06794c65456c
    Signed-off-by: cgowdru <chetan.arakere@altencalsoftlabs.com>

--- vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatUtil.java
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatUtil.java
@@ -9,7 +9,6 @@
 package org.opendaylight.netvirt.natservice.internal;
 
 import java.math.BigInteger;
-
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
@@ -105,6 +104,8 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev15060
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.subnetmaps.SubnetmapKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.vpnmaps.VpnMap;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.vpnmaps.VpnMapKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.l3.rev150712.routers.attributes.routers.Router;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.l3.rev150712.routers.attributes.routers.RouterKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.port.attributes.FixedIps;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.ports.Port;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
@@ -1640,13 +1641,78 @@ public class NatUtil {
   */
     static String getExtGwMacAddFromRouterId(DataBroker broker, long routerId) {
 
-     String routerName = getRouterName(broker, routerId);
-     InstanceIdentifier id = buildRouterIdentifier(routerName);
-     Optional<Routers> routerData = read(broker, LogicalDatastoreType.CONFIGURATION, id);
-     if (routerData.isPresent()) {
-         return routerData.get().getExtGwMacAddress();
-     }
-     return null;
-  }
+        String routerName = getRouterName(broker, routerId);
+        InstanceIdentifier id = buildRouterIdentifier(routerName);
+        Optional<Routers> routerData = read(broker, LogicalDatastoreType.CONFIGURATION, id);
+        if (routerData.isPresent()) {
+            return routerData.get().getExtGwMacAddress();
+        }
+        return null;
+    }
 
+    static InstanceIdentifier<Router> buildNeutronRouterIdentifier(Uuid routerUuid) {
+        InstanceIdentifier<Router> routerInstanceIdentifier = InstanceIdentifier.create(Neutron.class)
+             .child(org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.l3.rev150712.routers.attributes.Routers.class)
+             .child(Router.class, new RouterKey(routerUuid));
+        return routerInstanceIdentifier;
+    }
+
+    public static String getNeutronRouterNamebyUuid(DataBroker broker, Uuid routerUuid) {
+        InstanceIdentifier<Router> neutronRouterIdentifier = NatUtil.buildNeutronRouterIdentifier(routerUuid);
+        Optional<Router> neutronRouterData = NatUtil.read(broker, LogicalDatastoreType.CONFIGURATION,
+                neutronRouterIdentifier);
+        if (neutronRouterData.isPresent()) {
+            return neutronRouterData.get().getName();
+        }
+        return null;
+    }
+
+    public static List<Ports> getFloatingIpPortsForRouter(DataBroker broker, Uuid routerUuid) {
+
+        InstanceIdentifier<RouterPorts> routerPortsIdentifier = getRouterPortsId(routerUuid.getValue());
+        Optional<RouterPorts> routerPortsData = NatUtil.read(broker, LogicalDatastoreType.CONFIGURATION,
+                routerPortsIdentifier);
+        if (routerPortsData.isPresent()) {
+            return routerPortsData.get().getPorts();
+        }
+        return null;
+    }
+
+    public static List<Uuid> getRouterUuIdsForVpn(DataBroker broker, Uuid vpnUuid) {
+        InstanceIdentifier<ExternalNetworks> externalNwIdentifier = InstanceIdentifier.create(ExternalNetworks.class);
+        Optional<ExternalNetworks> externalNwData = NatUtil.read(broker, LogicalDatastoreType.CONFIGURATION,
+                externalNwIdentifier);
+        if (externalNwData.isPresent()) {
+            for (Networks externalNw : externalNwData.get().getNetworks()) {
+                if (externalNw.getVpnid() != null && externalNw.getVpnid().equals(vpnUuid)) {
+                    return externalNw.getRouterIds();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean isIpInSubnet(String ipAddress, String start, String end) {
+
+        try {
+            long ipLo = ipToLong(InetAddress.getByName(start));
+            long ipHi = ipToLong(InetAddress.getByName(end));
+            long ipToTest = ipToLong(InetAddress.getByName(ipAddress));
+            return (ipToTest >= ipLo && ipToTest <= ipHi);
+        } catch (UnknownHostException e) {
+            LOG.error("NAT Service : isIpInSubnet failed for IP {}. Exception {}", ipAddress, e.getMessage());
+            return false;
+        }
+    }
+
+    private static long ipToLong(InetAddress ip) {
+        byte[] octets = ip.getAddress();
+        long result = 0;
+        for (byte octet : octets) {
+            result <<= 8;
+            result |= octet & 0xff;
+        }
+        return result;
+    }
+>>>>>>> e4923ed... Bug 7780 : NAT RPC's for getting SNAT/DNAT translation information.
 }
