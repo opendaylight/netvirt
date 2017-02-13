a9f0cb8484c1a48b9f8a63bf58373a0ed4c7660c
Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>
2017-02-13 16:09:38 +0530

    releasing dcn thread once tunnel interface state dcn delivered
    * This change is about releasing dataTreeChangeHandlerExecutor's thread
    immediately after DCN is delivered to service module. This allows to
    improve DataTreeChangeListener notification time when there are multiple
    listerners listening to same data tree path.
    * For ELAN, DJC is used to run tunnel interface state DCN related tasks in async
    way using ElanUtils#getElanInterfaceJobKey
    
    Change-Id: If3128e623ac3eae81fbec17320bf54d425b7f6f5
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInterfaceStateChangeListener.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInterfaceStateChangeListener.java
@@ -9,6 +9,7 @@ package org.opendaylight.netvirt.elan.internal;
 
 
 import java.math.BigInteger;
+import java.util.Collections;
 import java.util.List;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
@@ -81,20 +82,23 @@ public class ElanInterfaceStateChangeListener
             return;
         }
         if (update.getType().equals(Tunnel.class)) {
-            if (!original.getOperStatus().equals(Interface.OperStatus.Unknown)
-                    && !update.getOperStatus().equals(Interface.OperStatus.Unknown)) {
-                if (update.getOperStatus().equals(Interface.OperStatus.Up)) {
-                    InternalTunnel internalTunnel = getTunnelState(interfaceName);
-                    if (internalTunnel != null) {
-                        try {
-                            elanInterfaceManager.handleInternalTunnelStateEvent(internalTunnel.getSourceDPN(),
-                                    internalTunnel.getDestinationDPN());
-                        } catch (ElanException e) {
-                            LOG.error("Failed to update interface: " + identifier.toString(), e);
+            DataStoreJobCoordinator.getInstance().enqueueJob(interfaceName, () -> {
+                if (!original.getOperStatus().equals(Interface.OperStatus.Unknown)
+                        && !update.getOperStatus().equals(Interface.OperStatus.Unknown)) {
+                    if (update.getOperStatus().equals(Interface.OperStatus.Up)) {
+                        InternalTunnel internalTunnel = getTunnelState(interfaceName);
+                        if (internalTunnel != null) {
+                            try {
+                                elanInterfaceManager.handleInternalTunnelStateEvent(internalTunnel.getSourceDPN(),
+                                        internalTunnel.getDestinationDPN());
+                            } catch (ElanException e) {
+                                LOG.error("Failed to update interface: " + identifier.toString(), e);
+                            }
                         }
                     }
                 }
-            }
+                return Collections.emptyList();
+            }, ElanConstants.JOB_MAX_RETRIES);
         }
     }
 
@@ -105,17 +109,21 @@ public class ElanInterfaceStateChangeListener
         ElanInterface elanInterface = ElanUtils.getElanInterfaceByElanInterfaceName(broker, interfaceName);
         if (elanInterface == null) {
             if (intrf.getType() != null && intrf.getType().equals(Tunnel.class)) {
-                if (intrf.getOperStatus().equals(Interface.OperStatus.Up)) {
-                    InternalTunnel internalTunnel = getTunnelState(interfaceName);
-                    if (internalTunnel != null) {
-                        try {
-                            elanInterfaceManager.handleInternalTunnelStateEvent(internalTunnel.getSourceDPN(),
-                                internalTunnel.getDestinationDPN());
-                        } catch (ElanException e) {
-                            LOG.error("Failed to add interface: " + identifier.toString(), e);
+                DataStoreJobCoordinator.getInstance().enqueueJob(interfaceName,
+                    () -> {
+                        if (intrf.getOperStatus().equals(Interface.OperStatus.Up)) {
+                            InternalTunnel internalTunnel = getTunnelState(interfaceName);
+                            if (internalTunnel != null) {
+                                try {
+                                    elanInterfaceManager.handleInternalTunnelStateEvent(internalTunnel.getSourceDPN(),
+                                            internalTunnel.getDestinationDPN());
+                                } catch (ElanException e) {
+                                    LOG.error("Failed to add interface: " + identifier.toString(), e);
+                                }
+                            }
                         }
-                    }
-                }
+                        return Collections.emptyList();
+                    }, ElanConstants.JOB_MAX_RETRIES);
             }
             return;
         }
