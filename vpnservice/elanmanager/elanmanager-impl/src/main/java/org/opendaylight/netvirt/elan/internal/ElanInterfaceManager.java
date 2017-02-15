650f8e295e0c6bc9c36bbf5e68da3a3c0e09ff20
Alon Kochba <alonko@hpe.com>
2017-02-15 21:16:03 +0200

    Fix potential NPEs in ELAN tunnel handling
    We encountered null ElanInfos in CSIT after deletion
    of all the interfaces via REST in BFD Monitoring suite,
    this should not prevent other elanDpns from processing.
    
    Change-Id: I465b62cb5133e6aea5326eb4219fc82a70eab70e
    Signed-off-by: Alon Kochba <alonko@hpe.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInterfaceManager.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInterfaceManager.java
@@ -1512,22 +1512,32 @@ public class ElanInterfaceManager extends AsyncDataTreeChangeListenerBase<ElanIn
             if (dpnInterfaces == null) {
                 continue;
             }
+            DpnInterfaces dstDpnIf = null;
             for (DpnInterfaces dpnIf : dpnInterfaces) {
-                if (dpnIf.getDpId().equals(srcDpId) || dpnIf.getDpId().equals(dstDpId)) {
+                BigInteger dpnIfDpId = dpnIf.getDpId();
+                if (dpnIfDpId.equals(srcDpId)) {
                     cnt++;
+                } else if (dpnIfDpId.equals(dstDpId)) {
+                    cnt++;
+                    dstDpnIf = dpnIf;
                 }
             }
             if (cnt == 2) {
                 LOG.debug("Elan instance:{} is present b/w srcDpn:{} and dstDpn:{}", elanName, srcDpId, dstDpId);
                 ElanInstance elanInfo = ElanUtils.getElanInstanceByName(broker, elanName);
+                if (elanInfo == null) {
+                    LOG.warn("ELAN Info is null for elanName {} that does exist in elanDpnInterfaceList, "
+                            + "skipping this ELAN for tunnel handling", elanName);
+                    continue;
+                }
                 DataStoreJobCoordinator dataStoreCoordinator = DataStoreJobCoordinator.getInstance();
+                final DpnInterfaces finalDstDpnIf = dstDpnIf; // var needs to be final so it can be accessed in lambda
                 dataStoreCoordinator.enqueueJob(elanName, () -> {
                     // update Remote BC Group
                     setupElanBroadcastGroups(elanInfo, srcDpId);
 
-                    DpnInterfaces dpnInterface = elanUtils.getElanInterfaceInfoByElanDpn(elanName, dstDpId);
                     Set<String> interfaceLists = new HashSet<>();
-                    interfaceLists.addAll(dpnInterface.getInterfaces());
+                    interfaceLists.addAll(finalDstDpnIf.getInterfaces());
                     for (String ifName : interfaceLists) {
                         dataStoreCoordinator.enqueueJob(ifName, () -> {
                             InterfaceInfo interfaceInfo = interfaceManager.getInterfaceInfo(ifName);
