ffe6ada092d7b238dee21302ca06217ab7f28296
Vishal Thapar <vishal.thapar@ericsson.com>
2017-02-16 21:47:57 +0530

    AbstractDataChangeListener clean-up
    Reference:
    https://wiki.opendaylight.org/view/Weather#Genius_AbstractDataChangeListener_clean-up
    
    Change-Id: If92640071cbbfcf65df6a7695a56e726c6eab682
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/listeners/L2GatewayConnectionListener.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/listeners/L2GatewayConnectionListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ * Copyright (c) 2016, 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -7,58 +7,27 @@
  */
 package org.opendaylight.netvirt.elan.l2gw.listeners;
 
-import org.opendaylight.controller.md.sal.binding.api.ClusteredDataChangeListener;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.genius.datastoreutils.AsyncClusteredDataChangeListenerBase;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
 import org.opendaylight.netvirt.elan.l2gw.utils.L2GatewayConnectionUtils;
 import org.opendaylight.netvirt.elan.utils.ElanUtils;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.l2gateways.rev150712.l2gateway.connections.attributes.L2gatewayConnections;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.l2gateways.rev150712.l2gateway.connections.attributes.l2gatewayconnections.L2gatewayConnection;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class L2GatewayConnectionListener extends AsyncClusteredDataChangeListenerBase<L2gatewayConnection,
+public class L2GatewayConnectionListener extends AsyncClusteredDataTreeChangeListenerBase<L2gatewayConnection,
         L2GatewayConnectionListener> implements AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(L2GatewayConnectionListener.class);
 
-    private ListenerRegistration<DataChangeListener> listenerRegistration;
     private final DataBroker broker;
     private final L2GatewayConnectionUtils l2GatewayConnectionUtils;
 
-    public L2GatewayConnectionListener(final DataBroker db,
-                                       ElanUtils elanUtils) {
-        super(L2gatewayConnection.class, L2GatewayConnectionListener.class);
-        broker = db;
+    public L2GatewayConnectionListener(final DataBroker db, ElanUtils elanUtils) {
+        this.broker = db;
         this.l2GatewayConnectionUtils = elanUtils.getL2GatewayConnectionUtils();
-        registerListener(db);
-    }
-
-    @Override
-    @SuppressWarnings("checkstyle:IllegalCatch")
-    public void close() {
-        // TODO use https://git.opendaylight.org/gerrit/#/c/44145/ when merged, and remove @SuppressWarnings
-        if (listenerRegistration != null) {
-            try {
-                listenerRegistration.close();
-            } catch (final Exception e) {
-                LOG.error("Error when cleaning up DataChangeListener.", e);
-            }
-            listenerRegistration = null;
-        }
-        LOG.info("L2 Gateway Connection listener Closed");
-    }
-
-    private void registerListener(final DataBroker db) {
-        listenerRegistration = db.registerDataChangeListener(
-                LogicalDatastoreType.CONFIGURATION, InstanceIdentifier.create(Neutron.class)
-                        .child(L2gatewayConnections.class).child(L2gatewayConnection.class),
-                L2GatewayConnectionListener.this, DataChangeScope.SUBTREE);
     }
 
     @Override
@@ -87,16 +56,12 @@ public class L2GatewayConnectionListener extends AsyncClusteredDataChangeListene
 
     @Override
     protected InstanceIdentifier<L2gatewayConnection> getWildCardPath() {
-        return InstanceIdentifier.create(L2gatewayConnection.class);
+        return InstanceIdentifier.create(Neutron.class).child(L2gatewayConnections.class)
+            .child(L2gatewayConnection.class);
     }
 
     @Override
-    protected ClusteredDataChangeListener getDataChangeListener() {
-        return L2GatewayConnectionListener.this;
-    }
-
-    @Override
-    protected DataChangeScope getDataChangeScope() {
-        return DataChangeScope.BASE;
+    protected L2GatewayConnectionListener getDataTreeChangeListener() {
+        return this;
     }
 }
--- vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/l2gw/L2GatewayListener.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/l2gw/L2GatewayListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 - 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ * Copyright (c) 2016, 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -12,13 +12,10 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.List;
 import java.util.Set;
-import org.opendaylight.controller.md.sal.binding.api.ClusteredDataChangeListener;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipService;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.genius.datastoreutils.AsyncClusteredDataChangeListenerBase;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
 import org.opendaylight.genius.mdsalutil.MDSALUtil;
 import org.opendaylight.genius.utils.clustering.ClusteringUtils;
 import org.opendaylight.genius.utils.hwvtep.HwvtepSouthboundConstants;
@@ -37,7 +34,7 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class L2GatewayListener extends AsyncClusteredDataChangeListenerBase<L2gateway, L2GatewayListener>
+public class L2GatewayListener extends AsyncClusteredDataTreeChangeListenerBase<L2gateway, L2GatewayListener>
         implements AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(L2GatewayListener.class);
     private final DataBroker dataBroker;
@@ -46,7 +43,6 @@ public class L2GatewayListener extends AsyncClusteredDataChangeListenerBase<L2ga
 
     public L2GatewayListener(final DataBroker dataBroker, final EntityOwnershipService entityOwnershipService,
                              ItmRpcService itmRpcService) {
-        super(L2gateway.class, L2GatewayListener.class);
         this.dataBroker = dataBroker;
         this.entityOwnershipService = entityOwnershipService;
         this.itmRpcService = itmRpcService;
@@ -176,6 +172,7 @@ public class L2GatewayListener extends AsyncClusteredDataChangeListenerBase<L2ga
                     // Cleaning up the config DS
                     NodeId nodeId = new NodeId(l2GwDevice.getHwvtepNodeId());
                     NodeId psNodeId = HwvtepSouthboundUtils.createManagedNodeId(nodeId, l2DeviceName);
+                    //FIXME: These should be removed
                     MDSALUtil.syncDelete(dataBroker, LogicalDatastoreType.CONFIGURATION,
                             HwvtepSouthboundUtils.createInstanceIdentifier(nodeId));
                     MDSALUtil.syncDelete(dataBroker, LogicalDatastoreType.CONFIGURATION,
@@ -193,12 +190,7 @@ public class L2GatewayListener extends AsyncClusteredDataChangeListenerBase<L2ga
     }
 
     @Override
-    protected ClusteredDataChangeListener getDataChangeListener() {
+    protected L2GatewayListener getDataTreeChangeListener() {
         return this;
-    }
-
-    @Override
-    protected DataChangeScope getDataChangeScope() {
-        return AsyncDataBroker.DataChangeScope.SUBTREE;
     }
 }
