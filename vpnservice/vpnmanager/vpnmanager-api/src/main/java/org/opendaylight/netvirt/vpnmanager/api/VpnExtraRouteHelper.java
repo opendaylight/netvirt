b1cdf9f2dae4bc5da35ac3250b6820e29e204fae
Gobinath <gobinath@ericsson.com>
2017-02-17 15:08:40 +0530

    Fib changes for the ecmp feature
        * Split the traffic b/w extra routes behind the VMs present in the
        * same DC in the same DPN
    
        The local fib entries of the extra route would now point to the load
        balancing select group which would contain in turn the buckets
        pointing to the local next hop groups.
    
    Change-Id: I85bd5263327b20cc030e34ca97c8336405935d36
    Signed-off-by: Gobinath <gobinath@ericsson.com>
    Signed-off-by: gobinath <gobinath@ericsson.com>

---
+++ vpnservice/vpnmanager/vpnmanager-api/src/main/java/org/opendaylight/netvirt/vpnmanager/api/VpnExtraRouteHelper.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.vpnmanager.api;
+
+import com.google.common.base.Optional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.mdsalutil.MDSALUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetTunnelTypeInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetTunnelTypeOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.OdlInterfaceRpcService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.ExtrarouteRdsMap;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.extraroute.rds.map.ExtrarouteRds;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.extraroute.rds.map.ExtrarouteRdsKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.extraroute.rds.map.extraroute.rds.DestPrefixes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.extraroute.rds.map.extraroute.rds.DestPrefixesKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.VpnToExtraroutes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.to.extraroutes.Vpn;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.to.extraroutes.VpnKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.to.extraroutes.vpn.ExtraRoutes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.to.extraroutes.vpn.ExtraRoutesKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.to.extraroutes.vpn.extra.routes.Routes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.to.extraroutes.vpn.extra.routes.RoutesKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VpnExtraRouteHelper {
+    private static final Logger LOG = LoggerFactory.getLogger(VpnExtraRouteHelper.class);
+
+    public static  List<Routes> getVpnExtraroutes(DataBroker broker, String vpnName, String vpnRd) {
+        InstanceIdentifier<ExtraRoutes> vpnExtraRoutesId = getVpnToExtrarouteIdentifier(vpnName, vpnRd);
+        Optional<ExtraRoutes> vpnOpc = MDSALUtil.read(broker, LogicalDatastoreType.OPERATIONAL, vpnExtraRoutesId);
+        return vpnOpc.isPresent() ? vpnOpc.get().getRoutes() : new ArrayList<Routes>();
+    }
+
+    public static Optional<Routes> getVpnExtraroutes(DataBroker broker, String vpnName,
+                                                     String vpnRd, String destPrefix) {
+        InstanceIdentifier<Routes> vpnExtraRoutesId = getVpnToExtrarouteIdentifier(vpnName, vpnRd, destPrefix);
+        return MDSALUtil.read(broker, LogicalDatastoreType.OPERATIONAL, vpnExtraRoutesId);
+    }
+
+    public static  InstanceIdentifier<Routes> getVpnToExtrarouteIdentifier(String vpnName, String vrfId,
+            String ipPrefix) {
+        return InstanceIdentifier.builder(VpnToExtraroutes.class)
+                .child(Vpn.class, new VpnKey(vpnName)).child(ExtraRoutes.class,
+                        new ExtraRoutesKey(vrfId)).child(Routes.class, new RoutesKey(ipPrefix)).build();
+    }
+
+    public static  InstanceIdentifier<ExtraRoutes> getVpnToExtrarouteIdentifier(String vpnName, String vrfId) {
+        return InstanceIdentifier.builder(VpnToExtraroutes.class)
+                .child(Vpn.class, new VpnKey(vpnName)).child(ExtraRoutes.class,
+                        new ExtraRoutesKey(vrfId)).build();
+    }
+
+    public static  InstanceIdentifier<Vpn> getVpnToExtrarouteIdentifier(String vpnName) {
+        return InstanceIdentifier.builder(VpnToExtraroutes.class)
+                .child(Vpn.class, new VpnKey(vpnName)).build();
+    }
+
+    public static List<Routes> getAllVpnExtraRoutes(DataBroker dataBroker, String vpnName,
+            List<String> usedRds, String destPrefix) {
+        List<Routes> routes = new ArrayList<>();
+        for (String rd : usedRds) {
+            Optional<Routes> extraRouteInfo =
+                    MDSALUtil.read(dataBroker, LogicalDatastoreType.OPERATIONAL,
+                            VpnExtraRouteHelper.getVpnToExtrarouteIdentifier(vpnName, rd, destPrefix));
+            if (extraRouteInfo.isPresent()) {
+                routes.add(extraRouteInfo.get());
+            }
+        }
+        return routes;
+    }
+
+    public static  List<String> getUsedRds(DataBroker broker, long vpnId, String destPrefix) {
+        InstanceIdentifier<DestPrefixes> usedRdsId = getUsedRdsIdentifier(vpnId, destPrefix);
+        Optional<DestPrefixes> usedRds = MDSALUtil.read(broker, LogicalDatastoreType.OPERATIONAL, usedRdsId);
+        return usedRds.isPresent() ? usedRds.get().getRds() : new ArrayList<String>();
+    }
+
+    public static  InstanceIdentifier<DestPrefixes> getUsedRdsIdentifier(long vpnId, String destPrefix) {
+        return InstanceIdentifier.builder(ExtrarouteRdsMap.class)
+                .child(ExtrarouteRds.class, new ExtrarouteRdsKey(vpnId))
+                .child(DestPrefixes.class, new DestPrefixesKey(destPrefix)).build();
+    }
+
+    public static List<Routes> getAllExtraRoutes(DataBroker broker, String vpnName, String vrfId) {
+        Optional<ExtraRoutes> extraRoutes = MDSALUtil.read(broker,LogicalDatastoreType.OPERATIONAL,
+                getVpnToExtrarouteIdentifier(vpnName, vrfId));
+        List<Routes> extraRoutesList = new ArrayList<>();
+        if (extraRoutes.isPresent()) {
+            extraRoutesList = extraRoutes.get().getRoutes();
+        }
+        return extraRoutesList;
+    }
+
+    public static Class<? extends TunnelTypeBase> getTunnelType(OdlInterfaceRpcService interfaceManager,
+            String ifName) {
+        try {
+            Future<RpcResult<GetTunnelTypeOutput>> result = interfaceManager.getTunnelType(
+                    new GetTunnelTypeInputBuilder().setIntfName(ifName).build());
+            RpcResult<GetTunnelTypeOutput> rpcResult = result.get();
+            if (!rpcResult.isSuccessful()) {
+                LOG.warn("RPC Call to getTunnelInterfaceId returned with Errors {}", rpcResult.getErrors());
+            } else {
+                return rpcResult.getResult().getTunnelType();
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            LOG.warn("Exception when getting tunnel interface Id for tunnel type", e);
+        }
+        return null;
+    }
+
+}
