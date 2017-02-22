3c3e60ba7e35608043f65517f710808927aac643
karthikeyan <karthikeyan.k@altencalsoftlabs.com>
2017-02-22 16:03:45 +0530

    Ethernet VPN (EVPN_RT5) Enhancement to Non-NAPT to NAPT
    This review is part of EVPN_RT5 support in NAT feature.
    
    Changes are made in the NAT code for accomplishing the below
    
    Intra DC Communication:
    =========================
    For Non-NAPT to NAPT Traffic, Tunnel id will be setting with
    Router lPortTag which will be carved out per router for External
    VXLAN Provider Type only
    
    Note: In NatUtil class having following methods which is used for SNAT and
    DNAT support
    
    getExtNwProviderType()
    getL3Vni()
    getVpnInstanceOpData()
    makeL3GwMacTableEntry()
    removeL3GwMacTableEntry()
    
    Change-Id: Idc5e8b1956311541ae7bae255d023c5f3543f9a3
    Signed-off-by: karthikeyan <karthikeyan.k@altencalsoftlabs.com>

--- vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatUtil.java
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatUtil.java
@@ -29,16 +29,22 @@ import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
+import org.opendaylight.genius.interfacemanager.globals.IfmConstants;
 import org.opendaylight.genius.mdsalutil.ActionInfo;
 import org.opendaylight.genius.mdsalutil.FlowEntity;
 import org.opendaylight.genius.mdsalutil.MDSALUtil;
 import org.opendaylight.genius.mdsalutil.MatchInfo;
+import org.opendaylight.genius.mdsalutil.MetaDataUtil;
 import org.opendaylight.genius.mdsalutil.NwConstants;
 import org.opendaylight.genius.mdsalutil.actions.ActionNxResubmit;
 import org.opendaylight.genius.mdsalutil.actions.ActionOutput;
 import org.opendaylight.genius.mdsalutil.actions.ActionPushVlan;
 import org.opendaylight.genius.mdsalutil.actions.ActionRegLoad;
 import org.opendaylight.genius.mdsalutil.actions.ActionSetFieldVlanVid;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.genius.mdsalutil.matches.MatchEthernetDestination;
+import org.opendaylight.genius.mdsalutil.matches.MatchEthernetSource;
+import org.opendaylight.genius.mdsalutil.matches.MatchMetadata;
 import org.opendaylight.netvirt.bgpmanager.api.IBgpManager;
 import org.opendaylight.netvirt.fibmanager.api.IFibManager;
 import org.opendaylight.netvirt.fibmanager.api.RouteOrigin;
@@ -49,15 +55,20 @@ import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev14081
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpPrefix;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.OutputActionCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.PushVlanActionCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.SetFieldCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.Instruction;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.AllocateIdInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.AllocateIdInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.AllocateIdOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.ReleaseIdInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.ReleaseIdInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetDpidFromInterfaceInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetDpidFromInterfaceInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetDpidFromInterfaceOutput;
@@ -1735,4 +1746,124 @@ public class NatUtil {
         }
         return result;
     }
+
+    static VrfEntry.EncapType getExtNwProviderType(DataBroker broker, String rd) {
+        long l3Vni = getL3Vni(broker, rd);
+        if (l3Vni != NatConstants.INVALID_ID && l3Vni != 0) {
+            return VrfEntry.EncapType.Vxlan;
+        }
+        return VrfEntry.EncapType.Mplsgre;
+    }
+
+    static long getL3Vni(DataBroker broker, String rd) {
+        VpnInstanceOpDataEntry vpnInstanceOpDataEntry = getVpnInstanceOpData(broker, rd);
+        if (vpnInstanceOpDataEntry == null) {
+            return NatConstants.INVALID_ID;
+        }
+        Long l3Vni = vpnInstanceOpDataEntry.getL3vni();
+        if (l3Vni == null || l3Vni == NatConstants.INVALID_ID) {
+            return NatConstants.INVALID_ID;
+        }
+        return l3Vni;
+    }
+
+    static VpnInstanceOpDataEntry getVpnInstanceOpData(DataBroker broker, String rd) {
+        InstanceIdentifier<VpnInstanceOpDataEntry> id = getVpnInstanceOpDataIdentifier(rd);
+        Optional<VpnInstanceOpDataEntry> vpnInstanceOpData = read(broker, LogicalDatastoreType.OPERATIONAL, id);
+        if (vpnInstanceOpData.isPresent()) {
+            return vpnInstanceOpData.get();
+        }
+        return null;
+    }
+
+    static long getLPortTagForRouter(String routerIdKey, IdManagerService idManager) {
+        LOG.debug("NAT Service : Get Router_lPort_Tag from ID Manager for Non-NAPT Switch to NAPT Switch to use "
+                + "Tunnel ID");
+        AllocateIdInput getIdInput = new AllocateIdInputBuilder()
+                .setPoolName(IfmConstants.IFM_IDPOOL_NAME).setIdKey(routerIdKey)
+                .build();
+        try {
+            Future<RpcResult<AllocateIdOutput>> result = idManager.allocateId(getIdInput);
+            RpcResult<AllocateIdOutput> rpcResult = result.get();
+            return rpcResult.getResult().getIdValue();
+        } catch (NullPointerException | InterruptedException | ExecutionException e) {
+            LOG.trace("NAT Service : idManager Failed with exception {} while getting Router_lPort_Tag "
+                    + "from pool with key {} ", e.getStackTrace(), routerIdKey);
+        }
+        return 0;
+    }
+
+    static void removeLPortTagForRouter(String routerIdKey, IdManagerService idManager) {
+        LOG.debug("NAT Service : Remove Router_lPort_Tag ID Pool for Non-NAPT Switch to NAPT Switch to use Tunnel ID");
+        ReleaseIdInput getIdInput = new ReleaseIdInputBuilder()
+                .setPoolName(IfmConstants.IFM_IDPOOL_NAME).setIdKey(routerIdKey)
+                .build();
+        try {
+            Future<RpcResult<Void>> result = idManager.releaseId(getIdInput);
+            RpcResult<Void> rpcResult = result.get();
+            if (!rpcResult.isSuccessful()) {
+                LOG.error("NAT Service : idManager Failed  {} to remove Router_lPort_Tag from pool",
+                        rpcResult.getErrors());
+            }
+            LOG.debug("NAT Service : idManager successfully removed Router_lPort_Tag from pool with key {}",
+                    routerIdKey);
+        } catch (NullPointerException | InterruptedException | ExecutionException e) {
+            LOG.trace("NAT Service : idManager Failed with exception {} while removing Router_lPort_Tag from pool "
+                    + "with key {} ", e.getStackTrace(), routerIdKey);
+        }
+    }
+
+    static ProviderTypes getExtNwProviderTypeFromRouterName(DataBroker dataBroker, String routerName) {
+        ProviderTypes extNwProviderType = null;
+        Uuid externalNetworkId = getNetworkIdFromRouterName(dataBroker,routerName);
+        if (externalNetworkId == null) {
+            LOG.error("NAT Service : Could not retrieve external network UUID for router {}"
+                    + " in NatUtil.getExtNwProviderTypeFromRouterName()", routerName);
+            return extNwProviderType;
+        }
+        extNwProviderType = getProviderTypefromNetworkId(dataBroker, externalNetworkId);
+        if (extNwProviderType == null) {
+            LOG.error("NAT Service : Could not retrieve provider type for external network {} "
+                    + "in NatUtil.getExtNwProviderTypeFromRouterName()", externalNetworkId);
+            return extNwProviderType;
+        }
+        return extNwProviderType;
+    }
+
+    static void makeL3GwMacTableEntry(final BigInteger dpnId, final long vpnId, String gwMacAddress,
+                                      List<Instruction> customInstructions, IMdsalApiManager mdsalManager) {
+        List<MatchInfo> matchInfo = new ArrayList<>();
+        matchInfo.add(new MatchMetadata(MetaDataUtil.getVpnIdMetadata(vpnId), MetaDataUtil.METADATA_MASK_VRFID));
+        matchInfo.add(new MatchEthernetDestination(new MacAddress(gwMacAddress)));
+        LOG.debug("NAT Service : Create SNAT Reverse Traffic Flow in table {} on DpnId = {} "
+                + "for External Vpn Id = {} and GwMacAddress = {}", NwConstants.INBOUND_NAPT_TABLE, dpnId, vpnId,
+                gwMacAddress);
+        // Install the flow entry in L3_GW_MAC_TABLE
+        String flowRef = getFlowRef(dpnId, NwConstants.L3_GW_MAC_TABLE, vpnId, gwMacAddress);
+        Flow l3GwMacTableFlowEntity = MDSALUtil.buildFlowNew(NwConstants.L3_GW_MAC_TABLE,
+                flowRef, 21, flowRef, 0, 0, NwConstants.COOKIE_L3_GW_MAC_TABLE, matchInfo, customInstructions);
+
+        mdsalManager.installFlow(dpnId, l3GwMacTableFlowEntity);
+        LOG.debug("NAT Service : L3_GW_MAC_TABLE entry for SNAT Reverse Traffic flow created Successfully "
+                + "on DPN = {}", dpnId);
+    }
+
+    static void removeL3GwMacTableEntry(final BigInteger dpnId, final long vpnId, final String gwMacAddress,
+                                        IMdsalApiManager mdsalManager) {
+        List<MatchInfo> matchInfo = new ArrayList<>();
+        matchInfo.add(new MatchMetadata(MetaDataUtil.getVpnIdMetadata(vpnId), MetaDataUtil.METADATA_MASK_VRFID));
+        matchInfo.add(new MatchEthernetSource(new MacAddress(gwMacAddress)));
+        LOG.debug("NAT Service : Remove SNAT Reverse Traffic Flow in table {} on DpnId = {} "
+                + "for External Vpn Id = {} and gwMacAddress = {}", NwConstants.INBOUND_NAPT_TABLE, dpnId, vpnId,
+                gwMacAddress);
+        // Remove the flow entry in L3_GW_MAC_TABLE
+        String flowRef = getFlowRef(dpnId, NwConstants.L3_GW_MAC_TABLE, vpnId, gwMacAddress);
+        Flow l3GwMacTableFlowEntity = MDSALUtil.buildFlowNew(NwConstants.L3_GW_MAC_TABLE,
+                flowRef, 21, flowRef, 0, 0, NwConstants.COOKIE_L3_GW_MAC_TABLE, matchInfo, null);
+
+        mdsalManager.removeFlow(dpnId, l3GwMacTableFlowEntity);
+        LOG.debug("NAT Service : L3_GW_MAC_TABLE entry for SNAT Reverse Traffic flow removed Successfully "
+                + "on DPN = {}", dpnId);
+    }
+
 }
