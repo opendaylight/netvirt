031b37e4e479d08c7f52488ff3c21886bbc8a9d1
Julien Courtat <julien.courtat@6wind.com>
2017-02-24 11:46:31 +0100

    Yang changes for Inter DC connectivity using BGPVPNv6
    This patchset includes the yang changes required to implement
    Spec to support IPv6 Inter DC L3VPN connectivity using BGPVPN [1].
    
    [1] https://git.opendaylight.org/gerrit/#/c/50359
    
    Change-Id: I70fb5460bde2a852dc965525fb37ae8003bea109
    Signed-off-by: Julien Courtat <julien.courtat@6wind.com>

--- vpnservice/bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/netvirt/bgpmanager/BgpConfigurationManager.java
+++ vpnservice/bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/netvirt/bgpmanager/BgpConfigurationManager.java
@@ -1830,14 +1830,16 @@ public class BgpConfigurationManager {
     }
 
     public synchronized void addPrefix(String rd, String macAddress, String pfx, List<String> nhList,
-              VrfEntry.EncapType encapType, int lbl, long l3vni, String gatewayMac) {
+              VrfEntry.EncapType encapType, int lbl, long l3vni, String gatewayMac, int addressFamily) {
         for (String nh : nhList) {
             Ipv4Address nexthop = nh != null ? new Ipv4Address(nh) : null;
             Long label = (long) lbl;
+            Long afi = (long) addressFamily;
             InstanceIdentifier<Networks> iid = InstanceIdentifier.builder(Bgp.class)
-                    .child(Networks.class, new NetworksKey(pfx, rd)).build();
+                    .child(Networks.class, new NetworksKey(afi, pfx, rd)).build();
             NetworksBuilder networksBuilder = new NetworksBuilder().setRd(rd).setPrefixLen(pfx).setNexthop(nexthop)
-                                                .setLabel(label).setEthtag(BgpConstants.DEFAULT_ETH_TAG);
+                                                .setLabel(label).setEthtag(BgpConstants.DEFAULT_ETH_TAG)
+                                                .setAfi(afi);
             buildVpnEncapSpecificInfo(networksBuilder, encapType, label, l3vni, macAddress, gatewayMac);
             update(iid, networksBuilder.build());
         }
@@ -1938,10 +1940,10 @@ public class BgpConfigurationManager {
         delete(iid);
     }
 
-    public synchronized void delPrefix(String rd, String pfx) {
+    public synchronized void delPrefix(String rd, String pfx, int afi) {
         InstanceIdentifier.InstanceIdentifierBuilder<Networks> iib =
                 InstanceIdentifier.builder(Bgp.class)
-                        .child(Networks.class, new NetworksKey(pfx, rd));
+                        .child(Networks.class, new NetworksKey((long)afi, pfx, rd));
         InstanceIdentifier<Networks> iid = iib.build();
         delete(iid);
     }
