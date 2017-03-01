ed3455c592a4a21a42ab9f20cda8e305d5c731b5
Koby Aizer <koby.aizer@hpe.com>
2017-03-01 12:02:16 +0200

    Bug 7878: provider interface MACs are installed on remote DPNs
    MACs learnt on provider interfaces must not be installed on remote
    DPNs.
    
    This change includes 2 fixes:
    
    1. handleInternalTunnelState event initiated installation on remote
    DMAC flows for all elans in the remote DPN, without verifying that
    this elan is not Flat/Vlan.
    
    2. Protection on setupRemoteDmacFlow to make sure no other code flows
    will install remote DMAC flows on MACs learnt on provider interfaces.
    
    Change-Id: I5ae288ed525dc1ae2f5be1bec5215f2e8f01f8c5
    Signed-off-by: Koby Aizer <koby.aizer@hpe.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInterfaceManager.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInterfaceManager.java
@@ -1518,6 +1518,16 @@ public class ElanInterfaceManager extends AsyncDataTreeChangeListenerBase<ElanIn
         for (ElanDpnInterfacesList elanDpns : elanDpnIf) {
             int cnt = 0;
             String elanName = elanDpns.getElanInstanceName();
+            ElanInstance elanInfo = ElanUtils.getElanInstanceByName(broker, elanName);
+            if (elanInfo == null) {
+                LOG.warn("ELAN Info is null for elanName {} that does exist in elanDpnInterfaceList, "
+                        + "skipping this ELAN for tunnel handling", elanName);
+                continue;
+            }
+            if (ElanUtils.isFlat(elanInfo) || ElanUtils.isVlan(elanInfo)) {
+                LOG.debug("Ignoring internal tunnel state event for Flat/Vlan elan {}", elanName);
+                continue;
+            }
             List<DpnInterfaces> dpnInterfaces = elanDpns.getDpnInterfaces();
             if (dpnInterfaces == null) {
                 continue;
@@ -1534,12 +1544,6 @@ public class ElanInterfaceManager extends AsyncDataTreeChangeListenerBase<ElanIn
             }
             if (cnt == 2) {
                 LOG.debug("Elan instance:{} is present b/w srcDpn:{} and dstDpn:{}", elanName, srcDpId, dstDpId);
-                ElanInstance elanInfo = ElanUtils.getElanInstanceByName(broker, elanName);
-                if (elanInfo == null) {
-                    LOG.warn("ELAN Info is null for elanName {} that does exist in elanDpnInterfaceList, "
-                            + "skipping this ELAN for tunnel handling", elanName);
-                    continue;
-                }
                 DataStoreJobCoordinator dataStoreCoordinator = DataStoreJobCoordinator.getInstance();
                 final DpnInterfaces finalDstDpnIf = dstDpnIf; // var needs to be final so it can be accessed in lambda
                 dataStoreCoordinator.enqueueJob(elanName, () -> {
