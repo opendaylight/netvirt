d4e85abcbf5e6b6cbcfe8fc6afc123d9e2986cf1
ehvkand <hanamantagoud.v.kandagal@ericsson.com>
2017-03-01 12:59:14 +0530

    Bug 7876 : After router association to L3vpn, one of the VM ip is not removed from router interface to BGPVPN
    When a VPN associated to router is deleted and a same VPN is associated
    back to same router , there is race condition , where interface is not
    found in VpnInterfaceOp and remove event is being ignored. Hence interface
    remains in router VPN.
    
    It is also seen that , one of interface from same subnet is swapped and
    others are not. Its due to race condition that exist in NeutronVpnManager
    where create and update can race with each other. Currently create uses
    DJC , where as update doesn't .
    
    Change-Id: I354c3ce93f965580c735f1153a043e2514d68af7
    Signed-off-by: ehvkand <hanamantagoud.v.kandagal@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnInterfaceManager.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnInterfaceManager.java
@@ -1012,48 +1012,44 @@ public class VpnInterfaceManager extends AsyncDataTreeChangeListenerBase<VpnInte
 
         final VpnInterfaceKey key = identifier.firstKeyOf(VpnInterface.class, VpnInterfaceKey.class);
         final String interfaceName = key.getName();
-        InstanceIdentifier<VpnInterface> interfaceId = VpnUtil.getVpnInterfaceIdentifier(interfaceName);
+        BigInteger dpId = BigInteger.ZERO;
 
-        final Optional<VpnInterface> optVpnInterface =
-                VpnUtil.read(dataBroker, LogicalDatastoreType.OPERATIONAL, interfaceId);
         Interface interfaceState = InterfaceUtils.getInterfaceStateFromOperDS(dataBroker, interfaceName);
-        if (optVpnInterface.isPresent()) {
-            BigInteger dpnId = BigInteger.ZERO;
-            Boolean dpnIdRetrieved = Boolean.FALSE;
-            if (interfaceState != null) {
-                try {
-                    dpnId = InterfaceUtils.getDpIdFromInterface(interfaceState);
-                    dpnIdRetrieved = Boolean.TRUE;
-                } catch (Exception e) {
-                    LOG.error(
-                        "Unable to retrieve dpnId from interface operational data store for interface {}. Fetching "
-                            + "from vpn interface op data store. ",
-                        interfaceName, e);
-                }
-            } else {
-                LOG.error("Unable to retrieve interfaceState for interface {} , quitting ", interfaceName);
-                return;
+        if (interfaceState != null) {
+            try {
+                dpId = InterfaceUtils.getDpIdFromInterface(interfaceState);
+            } catch (Exception e) {
+                LOG.error(
+                    "Unable to retrieve dpnId from interface operational data store for interface {}. Fetching "
+                        + "from vpn interface op data store. ",
+                    interfaceName, e);
+                dpId = BigInteger.ZERO;
             }
-            final VpnInterface vpnOpInterface = optVpnInterface.get();
-            if (dpnIdRetrieved == Boolean.FALSE) {
-                LOG.info("dpnId for {} has not been retrieved yet. Fetching from vpn interface operational DS",
-                    interfaceName);
-                dpnId = vpnOpInterface.getDpnId();
-            }
+
             final int ifIndex = interfaceState.getIfIndex();
-            final BigInteger dpId = dpnId;
+            final BigInteger dpnId = dpId;
             DataStoreJobCoordinator dataStoreCoordinator = DataStoreJobCoordinator.getInstance();
             dataStoreCoordinator.enqueueJob("VPNINTERFACE-" + interfaceName,
                 () -> {
                     WriteTransaction writeConfigTxn = dataBroker.newWriteOnlyTransaction();
                     WriteTransaction writeOperTxn = dataBroker.newWriteOnlyTransaction();
                     WriteTransaction writeInvTxn = dataBroker.newWriteOnlyTransaction();
-                    processVpnInterfaceDown(dpId, interfaceName, ifIndex, false, true, writeConfigTxn, writeOperTxn,
-                        writeInvTxn);
                     List<ListenableFuture<Void>> futures = new ArrayList<>();
-                    futures.add(writeOperTxn.submit());
-                    futures.add(writeConfigTxn.submit());
-                    futures.add(writeInvTxn.submit());
+
+                    InstanceIdentifier<VpnInterface> interfaceId = VpnUtil.getVpnInterfaceIdentifier(interfaceName);
+                    final Optional<VpnInterface> optVpnInterface =
+                            VpnUtil.read(dataBroker, LogicalDatastoreType.OPERATIONAL, interfaceId);
+                    if (optVpnInterface.isPresent()) {
+                        VpnInterface vpnOpInterface = optVpnInterface.get();
+                        processVpnInterfaceDown(dpnId.equals(BigInteger.ZERO) ? vpnOpInterface.getDpnId() : dpnId ,
+                                interfaceName, ifIndex, false, true, writeConfigTxn, writeOperTxn, writeInvTxn);
+                        futures.add(writeOperTxn.submit());
+                        futures.add(writeConfigTxn.submit());
+                        futures.add(writeInvTxn.submit());
+                    } else {
+                        LOG.warn("VPN interface {} was unavailable in operational data store to handle remove event",
+                                interfaceName);
+                    }
                     return futures;
                 });
 
@@ -1068,8 +1064,7 @@ public class VpnInterfaceManager extends AsyncDataTreeChangeListenerBase<VpnInte
                     return futures;
                 });
         } else {
-            LOG.warn("VPN interface {} was unavailable in operational data store to handle remove event",
-                interfaceName);
+            LOG.warn("Handling removal of VPN interface {} skipped as interfaceState is not available", interfaceName);
         }
     }
 
