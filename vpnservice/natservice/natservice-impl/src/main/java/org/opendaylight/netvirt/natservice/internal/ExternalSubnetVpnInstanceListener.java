3420abc030dd51dc2da15eb7ecda5d253d20d9bd
Tomer Pearl <tomer.pearl@hpe.com>
2017-03-02 18:06:56 +0200

    Enable SubnetRoute in non BGPVPN usecase
    First commit in a series of commits, part of the discovery
    of directly connented PNFs in Flat/Vlan provider networks.
    
    This commit enables the creation of VrfEntry with a subnetroute
    augmentation, in non BGPVPN use case, with respect to the recently
    changes made by the hairpinning spec.
    
    A VrfEntry is created in both external networks and private tenant
    networks.
    
    Change-Id: Ida689b6e5317e45e6a9c8a4f6887e5e9f2438161
    Signed-off-by: Tomer Pearl <tomer.pearl@hpe.com>
    
    <WIP> Discovery of directly connected PNFs.
    First commit this this feature
    
    TODO: add list of changes
    
    Change-Id: Ida689b6e5317e45e6a9c8a4f6887e5e9f2438161
    Signed-off-by: Tomer Pearl <tomer.pearl@hpe.com>

--- vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/ExternalSubnetVpnInstanceListener.java
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/ExternalSubnetVpnInstanceListener.java
@@ -10,15 +10,21 @@ package org.opendaylight.netvirt.natservice.internal;
 import com.google.common.base.Optional;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.NotificationPublishService;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
 import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.netvirt.elanmanager.api.IElanService;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.instances.ElanInstance;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.VpnInstanceToVpnId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.to.vpn.id.VpnInstance;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ExternalSubnets;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.external.subnets.Subnets;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.external.subnets.SubnetsKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.SubnetAddedToVpnBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.SubnetDeletedFromVpnBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.subnetmaps.Subnetmap;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,11 +34,17 @@ public class ExternalSubnetVpnInstanceListener extends AsyncDataTreeChangeListen
     private static final Logger LOG = LoggerFactory.getLogger(ExternalSubnetVpnInstanceListener.class);
     private final DataBroker dataBroker;
     private final SNATDefaultRouteProgrammer snatDefaultRouteProgrammer;
+    private final IElanService elanService;
+    private final NotificationPublishService notificationPublishService;
 
     public ExternalSubnetVpnInstanceListener(final DataBroker dataBroker,
-            final SNATDefaultRouteProgrammer snatDefaultRouteProgrammer) {
+            final SNATDefaultRouteProgrammer snatDefaultRouteProgrammer,
+            final IElanService elanService,
+            final NotificationPublishService notificationPublishService) {
         this.dataBroker = dataBroker;
         this.snatDefaultRouteProgrammer = snatDefaultRouteProgrammer;
+        this.elanService = elanService;
+        this.notificationPublishService = notificationPublishService;
     }
 
     @Override
@@ -50,7 +62,13 @@ public class ExternalSubnetVpnInstanceListener extends AsyncDataTreeChangeListen
     protected void remove(InstanceIdentifier<VpnInstance> key, VpnInstance vpnInstance) {
         LOG.trace("NAT Service : External Subnet VPN Instance remove mapping method - key:{}. value={}",
                 key, vpnInstance);
-        addOrDelDefaultFibRouteToSNATFlow(vpnInstance, NwConstants.DEL_FLOW);
+        String vpnInstanceName = vpnInstance.getVpnInstanceName();
+        Optional<Subnets> optionalSubnets = NatUtil.read(dataBroker, LogicalDatastoreType.CONFIGURATION,
+                createSubnetIID(new Uuid(vpnInstanceName)));
+        if (optionalSubnets.isPresent()) {
+            addOrDelDefaultFibRouteToSNATFlow(vpnInstance, optionalSubnets.get(), NwConstants.DEL_FLOW);
+            tryToPublishSubnetDeleteddFromVpn(vpnInstanceName);
+        }
     }
 
     @Override
@@ -64,25 +82,70 @@ public class ExternalSubnetVpnInstanceListener extends AsyncDataTreeChangeListen
     protected void add(InstanceIdentifier<VpnInstance> key, VpnInstance vpnInstance) {
         LOG.trace("NAT Service : External Subnet VPN Instance OP Data Entry add mapping method - key:{}. value={}",
                 key, vpnInstance);
-        addOrDelDefaultFibRouteToSNATFlow(vpnInstance, NwConstants.ADD_FLOW);
+        String vpnInstanceName = vpnInstance.getVpnInstanceName();
+
+        Optional<Subnets> optionalSubnets = NatUtil.read(dataBroker, LogicalDatastoreType.CONFIGURATION,
+                 createSubnetIID(new Uuid(vpnInstanceName)));
+        if (optionalSubnets.isPresent()) {
+            addOrDelDefaultFibRouteToSNATFlow(vpnInstance, optionalSubnets.get(), NwConstants.ADD_FLOW);
+            tryToPublishSubnetAddedToVpn(vpnInstanceName);
+        }
     }
 
-    private void addOrDelDefaultFibRouteToSNATFlow(VpnInstance vpnInstance, int flowAction) {
+    private void tryToPublishSubnetAddedToVpn(String externalSubnetId) {
+        Uuid externalSubnetUuid = new Uuid(externalSubnetId);
+        Subnetmap subnetMap = NatUtil.getSubnetMap(dataBroker, externalSubnetUuid);
+        ElanInstance elanInstance = elanService.getElanInstance(subnetMap.getNetworkId().getValue());
+
+        SubnetAddedToVpnBuilder builder = new SubnetAddedToVpnBuilder();
+        builder.setSubnetId(externalSubnetUuid);
+        builder.setSubnetIp(subnetMap.getSubnetIp());
+        builder.setVpnName(externalSubnetId);
+        builder.setBgpVpn(false);
+        builder.setElanTag(elanInstance.getElanTag());
+        LOG.trace("publish SubnetAddedToVpn for subnet {}", subnetMap);
+
+        try {
+            notificationPublishService.putNotification(builder.build());
+        } catch (InterruptedException e) {
+            LOG.error("Cannot publish SubnetAddedToVpn notification for subnet-id {} in vpn-id {}",
+                    externalSubnetUuid, externalSubnetId);
+        }
+    }
+
+    private void tryToPublishSubnetDeleteddFromVpn(String externalSubnetId) {
+        Uuid externalSubnetUuid = new Uuid(externalSubnetId);
+        Subnetmap subnetMap = NatUtil.getSubnetMap(dataBroker, externalSubnetUuid);
+        ElanInstance elanInstance = elanService.getElanInstance(subnetMap.getNetworkId().getValue());
+
+        SubnetDeletedFromVpnBuilder builder = new SubnetDeletedFromVpnBuilder();
+        builder.setSubnetId(externalSubnetUuid);
+        builder.setSubnetIp(subnetMap.getSubnetIp());
+        builder.setVpnName(externalSubnetId);
+        builder.setBgpVpn(false);
+        builder.setElanTag(elanInstance.getElanTag());
+        LOG.trace("publish SubnetDeletedFromVpn for subnet {}", subnetMap);
+
+        try {
+            notificationPublishService.putNotification(builder.build());
+        } catch (InterruptedException e) {
+            LOG.error("Cannot publish SubnetDeletedFromVpn notification for subnet-id {} in vpn-id {}",
+                    externalSubnetUuid, externalSubnetId);
+        }
+    }
+
+    private void addOrDelDefaultFibRouteToSNATFlow(VpnInstance vpnInstance, Subnets subnet, int flowAction) {
         String vpnInstanceName = vpnInstance.getVpnInstanceName();
-        Uuid possibleExternalSubnetUuid = new Uuid(vpnInstanceName);
+        LOG.debug("NAT Service : VpnInstance {} for external subnet {}.", vpnInstanceName, subnet);
+        Long vpnId = vpnInstance.getVpnId();
+        snatDefaultRouteProgrammer.addOrDelDefaultFibRouteToSNATForSubnet(subnet,
+                subnet.getExternalNetworkId().getValue(), flowAction, vpnId);
+    }
+
+    private InstanceIdentifier<Subnets> createSubnetIID(Uuid possibleExternalSubnetUuid) {
         InstanceIdentifier<Subnets> subnetsIdentifier = InstanceIdentifier.builder(ExternalSubnets.class)
                 .child(Subnets.class, new SubnetsKey(possibleExternalSubnetUuid)).build();
-        Optional<Subnets> optionalSubnets = NatUtil.read(dataBroker, LogicalDatastoreType.CONFIGURATION,
-                subnetsIdentifier);
-        if (optionalSubnets.isPresent()) {
-            LOG.debug("NAT Service : VpnInstance {} for external subnet {}.", vpnInstanceName, optionalSubnets.get());
-            Subnets subnet = optionalSubnets.get();
-            Long vpnId = vpnInstance.getVpnId();
-            snatDefaultRouteProgrammer.addOrDelDefaultFibRouteToSNATForSubnet(subnet,
-                    subnet.getExternalNetworkId().getValue(), flowAction, vpnId);
-        } else {
-            LOG.trace("NAT Service : VpnInstance {} is not for an external subnet.", vpnInstanceName);
-        }
+        return subnetsIdentifier;
     }
 
     @Override
