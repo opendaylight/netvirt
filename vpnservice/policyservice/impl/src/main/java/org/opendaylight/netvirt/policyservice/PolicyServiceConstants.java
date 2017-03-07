f8965bcbcfd4a1d3950d0d57fc3d0da24045d28b
Tali <tali.ben-meir@hpe.com>
2017-03-07 10:08:42 +0200

    Policy classifier table ACL flows
    This patch depends on genius patch
    https://git.opendaylight.org/gerrit/#/c/52933/
    
    and netvirt yang changes
    https://git.opendaylight.org/gerrit/#/c/52406/
    
    Change-Id: Ia46f09bf8aa25206617effae0e0e1a634e9e2c15
    Signed-off-by: Tali <tali.ben-meir@hpe.com>

---
+++ vpnservice/policyservice/impl/src/main/java/org/opendaylight/netvirt/policyservice/PolicyServiceConstants.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017 Hewlett Packard Enterprise, Co. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.policyservice;
+
+public class PolicyServiceConstants {
+
+    public static final String POLICY_CLASSIFIER_POOL_NAME = "policyClassifierPool";
+    public static final Long POLICY_CLASSIFIER_LOW_ID = 1L;
+    public static final Long POLICY_CLASSIFIER_HIGH_ID = 30000L;
+    public static final long INVALID_ID = 0;
+    public static final int POLICY_FLOW_PRIOPITY = 42;
+}
