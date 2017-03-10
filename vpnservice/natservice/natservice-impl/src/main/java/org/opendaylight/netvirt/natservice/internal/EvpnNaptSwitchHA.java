4603bcef1ee24aa475c6c6d1eeb606b979fa00e6
karthikeyan <karthikeyan.k@altencalsoftlabs.com>
2017-03-10 17:02:41 +0530

    Ethernet VPN (EVPN_RT5) Enhancement to SNAT_HA
    This code review (4th Review) is part of EVPN_RT5 enhancement to NAT
    Feature.
    
    This code changes is responsible for SNAT HA during NAPT Switch
    down/restart scenario
    
    1) Remove all the SNAT flows from old NAPT switch
    
    2) Install all the SNAT flows on newly elected NAPT switch
    
    Note: This review is dependent on below review
    https://git.opendaylight.org/gerrit/#/c/51338/
    
    Change-Id: I4201096772af11b111f7387bb1bfce1535ee0ca5
    Signed-off-by: karthikeyan <karthikeyan.k@altencalsoftlabs.com>

---
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/EvpnNaptSwitchHA.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.natservice.internal;
+
+import java.math.BigInteger;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.genius.mdsalutil.FlowEntity;
+import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EvpnNaptSwitchHA {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EvpnNaptSwitchHA.class);
+    private final DataBroker dataBroker;
+    private final IMdsalApiManager mdsalManager;
+    private final IdManagerService idManager;
+    private final EvpnSnatFlowProgrammer evpnSnatFlowProgrammer;
+
+    public EvpnNaptSwitchHA(final DataBroker dataBroker, final IMdsalApiManager mdsalManager,
+                        final EvpnSnatFlowProgrammer evpnSnatFlowProgrammer,
+                        final IdManagerService idManager) {
+        this.dataBroker = dataBroker;
+        this.mdsalManager = mdsalManager;
+        this.evpnSnatFlowProgrammer = evpnSnatFlowProgrammer;
+        this.idManager = idManager;
+    }
+
+    public void evpnRemoveSnatFlowsInOldNaptSwitch(String routerName, long routerId, String vpnName,
+                                                   BigInteger naptSwitch) {
+        //Handling VXLAN Provider type flow removal from old NAPT switch
+        Long vpnId = NatUtil.getVpnId(dataBroker, routerId);
+        if (vpnId == null) {
+            LOG.error("NAT Service: Unable to retrieved vpnId for routerId {}", routerId);
+            return;
+        }
+        String rd = NatUtil.getVpnRd(dataBroker, vpnName);
+        if (rd == null) {
+            LOG.error("NAT Service : Could not retrieve RD value from VPN Name {} ", vpnName);
+            return;
+        }
+        long l3Vni = NatEvpnUtil.getL3Vni(dataBroker, rd);
+        if (l3Vni == NatConstants.DEFAULT_L3VNI_VALUE) {
+            LOG.error("NAT Service : Could not retrieve L3VNI value from RD {} ", rd);
+            return;
+        }
+        String gwMacAddress = NatUtil.getExtGwMacAddFromRouterId(dataBroker, routerId);
+        if (gwMacAddress == null) {
+            LOG.error("NAT Service : Unable to Get External Gateway MAC address for External Router ID {} ", routerId);
+            return;
+        }
+        //Remove the L3_GW_MAC_TABLE which forwards the packet to Inbound NAPT Table (table19->44)
+        NatEvpnUtil.removeL3GwMacTableEntry(naptSwitch, vpnId, gwMacAddress, mdsalManager);
+
+        //Remove the PDNAT_TABLE which forwards the packet to Inbound NAPT Table (table25->44)
+        evpnSnatFlowProgrammer.removePreDnatToSnatTableEntry(naptSwitch);
+
+        //Remove the PSNAT_TABLE which forwards the packet to Outbound NAPT Table (table26->46)
+        String flowRef = getFlowRefSnat(naptSwitch, NwConstants.PSNAT_TABLE, routerName);
+        FlowEntity flowEntity = NatUtil.buildFlowEntity(naptSwitch, NwConstants.PSNAT_TABLE, flowRef);
+        LOG.info("NAT Service: Remove the flow (table26->46) in table {} for the old napt switch with the DPN ID "
+                + "{} and router ID {}", NwConstants.PSNAT_TABLE, naptSwitch, routerId);
+        mdsalManager.removeFlow(flowEntity);
+
+        //Remove the Terminating Service table entry which forwards the packet to Inbound NAPT Table (table36->44)
+        LOG.info("NAT Service: Remove the flow (table36->44) in table {} for the old napt switch with the DPN ID "
+                + "{} and router ID {}", NwConstants.INTERNAL_TUNNEL_TABLE, naptSwitch, routerId);
+        evpnSnatFlowProgrammer.removeTunnelTableEntry(naptSwitch, l3Vni);
+
+        //Remove the INTERNAL_TUNNEL_TABLE entry which forwards the packet to Outbound NAPT Table (table36->46)
+        long tunnelId = NatEvpnUtil.getTunnelIdForRouter(idManager, dataBroker, routerName, routerId);
+
+        String tsFlowRef = getFlowRefTs(naptSwitch, NwConstants.INTERNAL_TUNNEL_TABLE, tunnelId);
+        FlowEntity tsNatFlowEntity = NatUtil.buildFlowEntity(naptSwitch, NwConstants.INTERNAL_TUNNEL_TABLE, tsFlowRef);
+        LOG.info("NAT Service : Remove the flow in the {} for the active switch with the DPN ID {} and router ID {}",
+                NwConstants.INTERNAL_TUNNEL_TABLE, naptSwitch, routerId);
+        mdsalManager.removeFlow(tsNatFlowEntity);
+
+    }
+
+    private String getFlowRefTs(BigInteger dpnId, short tableId, long routerID) {
+        return NatConstants.NAPT_FLOWID_PREFIX + dpnId + NatConstants.FLOWID_SEPARATOR + tableId + NatConstants
+                .FLOWID_SEPARATOR + routerID;
+    }
+
+    private String getFlowRefSnat(BigInteger dpnId, short tableId, String routerID) {
+        return NatConstants.SNAT_FLOWID_PREFIX + dpnId + NatConstants.FLOWID_SEPARATOR + tableId + NatConstants
+                .FLOWID_SEPARATOR + routerID;
+    }
+}
