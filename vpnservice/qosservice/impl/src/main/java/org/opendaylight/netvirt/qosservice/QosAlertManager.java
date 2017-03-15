5b3ddb6ae099b4a66df79ae173eb1cd4aa83fa19
Arun Sharma <arun.e.sharma@ericsson.com>
2017-03-15 17:52:54 +0530

    Netvirt: Karaf CLI for Neutron Qos Alert
    *** patch #3 - Adding karaf CLI for Neutron Qos Alert ***
    
    Added following new karaf CLI
    
    qos:enable-qos-alert <true|false>
    qos:drop-packet-threshold <threshold value in %>
    qos:alert-poll-interval <polling interval in minutes>
    
    Spec Review Link:
    https://git.opendaylight.org/gerrit/50689
    
    Change-Id: Ife146e6a52c0a63c0b7f4cfbabfc986fb2cde487
    Signed-off-by: Arun Sharma <arun.e.sharma@ericsson.com>

--- vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosAlertManager.java
+++ vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosAlertManager.java
@@ -9,13 +9,20 @@
 package org.opendaylight.netvirt.qosservice;
 
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.direct.statistics.rev160511.OpendaylightDirectStatisticsService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netvirt.qosalert.config.rev170301.QosalertConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.netvirt.qosalert.config.rev170301.QosalertConfigBuilder;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,6 +39,24 @@ public final class QosAlertManager implements Runnable {
     private final OpendaylightDirectStatisticsService odlDirectStatisticsService;
     private Thread thread;
     private static final Logger LOG = LoggerFactory.getLogger(QosAlertManager.class);
+
+    private static final FutureCallback<Void> DEFAULT_FUTURE_CALLBACK;
+
+    static {
+        DEFAULT_FUTURE_CALLBACK = new FutureCallback<Void>() {
+
+            @Override
+            public void onSuccess(Void result) {
+                LOG.info("Datastore operation completed successfully");
+            }
+
+            @Override
+            public void onFailure(Throwable error) {
+                LOG.error("Error in datastore operation {}", error);
+            }
+
+        };
+    }
 
     @Inject
     public QosAlertManager(final DataBroker dataBroker, OpendaylightDirectStatisticsService odlDirectStatisticsService,
@@ -108,6 +133,44 @@ public final class QosAlertManager implements Runnable {
         thread.interrupt();
     }
 
+    public void setThreshold(short threshold) {
+        LOG.info("setting threshold {} in config data store", threshold);
+        this.threshold = threshold;
+        writeConfigDataStore();
+    }
+
+    public void setPollInterval(int pollInterval) {
+        LOG.info("setting interval {} in config data store", pollInterval);
+        this.pollInterval = pollInterval;
+        writeConfigDataStore();
+    }
+
+    public void setEnable(boolean enable) {
+        LOG.info("setting QoS poll to {} in config data store", enable);
+        this.enable = enable;
+        writeConfigDataStore();
+    }
+
+    private static <T extends DataObject> void asyncWrite(LogicalDatastoreType datastoreType,
+                                                          InstanceIdentifier<T> path, T data, DataBroker broker,
+                                                          FutureCallback<Void> callback) {
+        WriteTransaction tx = broker.newWriteOnlyTransaction();
+        tx.put(datastoreType, path, data, true);
+        Futures.addCallback(tx.submit(), callback);
+    }
+
+    private void writeConfigDataStore() {
+
+        InstanceIdentifier<QosalertConfig> path = InstanceIdentifier.builder(QosalertConfig.class).build();
+
+        QosalertConfig qosAlertConfig = new QosalertConfigBuilder()
+                .setQosDropPacketThreshold(threshold)
+                .setQosAlertEnabled(enable)
+                .setQosAlertPollInterval(pollInterval)
+                .build();
+
+        asyncWrite(LogicalDatastoreType.CONFIGURATION, path, qosAlertConfig, dataBroker, DEFAULT_FUTURE_CALLBACK);
+    }
 
     private void pollDirectStatisticsForAllNodes() {
         LOG.debug("Polling direct statistics from all nodes");
