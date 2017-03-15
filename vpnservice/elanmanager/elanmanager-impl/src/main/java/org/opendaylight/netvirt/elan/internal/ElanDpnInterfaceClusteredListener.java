72e3e247636fa9ea0ec6fbd5598e974f9f68ecbc
Akash Kumar Sahu <a.k.sahu@ericsson.com>
2017-03-15 16:48:38 +0530

    Bug 7988 - Cluster reboot fix
    Elan Instance , Physical Switch , L2gateWay and L2gatewayConnection all
    have dependency among each other.But if upon cluster reboot the order is
    not proper (L2gw cache is updated at the last , then flows are not being programmed as expected.
    To take care of the situation
    Introduced new ElanInstance Listner (Was already present but not
    registered with xml to be instantiated )
    Handle scenario if L2gW shows up last after cluster reboot .
    Handle scenario if l2gwConnection is updated but Elan instance is not
    available.
    
    Change-Id: I6194d7cb22d6659803b17078d57c8ad694c0c201
    Signed-off-by: Akash Kumar Sahu <a.k.sahu@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanDpnInterfaceClusteredListener.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanDpnInterfaceClusteredListener.java
@@ -84,6 +84,7 @@ public class ElanDpnInterfaceClusteredListener
         final String elanName = getElanName(identifier);
         LOG.debug("Received ElanDpnInterface removed for for elan {} with Dp Id ", elanName,
             dpnInterfaces.getDpId());
+        ElanUtils.removeDPNInterfaceFromElanInCache(getElanName(identifier), dpnInterfaces);
 
         if (ElanL2GwCacheUtils.getInvolvedL2GwDevices(elanName).isEmpty()) {
             LOG.debug("dpnInterface removed, no external l2 devices to update for elan {} with Dp Id:", elanName,
@@ -106,6 +107,7 @@ public class ElanDpnInterfaceClusteredListener
                           final DpnInterfaces dpnInterfaces) {
         LOG.debug("dpninterfaces update fired new size {}", dpnInterfaces.getInterfaces().size());
         if (dpnInterfaces.getInterfaces().size() == 0) {
+            ElanUtils.removeDPNInterfaceFromElanInCache(getElanName(identifier), dpnInterfaces);
             LOG.debug("dpninterfaces last dpn interface on this elan {} ", dpnInterfaces.getKey());
             // this is the last dpn interface on this elan
             handleUpdate(identifier, dpnInterfaces);
@@ -114,6 +116,7 @@ public class ElanDpnInterfaceClusteredListener
 
     @Override
     protected void add(InstanceIdentifier<DpnInterfaces> identifier, final DpnInterfaces dpnInterfaces) {
+        ElanUtils.addDPNInterfaceToElanInCache(getElanName(identifier), dpnInterfaces);
         if (dpnInterfaces.getInterfaces().size() == 1) {
             LOG.debug("dpninterfaces first dpn interface on this elan {} {} ", dpnInterfaces.getKey(),
                 dpnInterfaces.getInterfaces().get(0));
