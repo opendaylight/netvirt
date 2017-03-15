f92054161e144a1404d88f0f2a118db7ced0d5e9
Arun Sharma <arun.e.sharma@ericsson.com>
2017-03-15 17:52:54 +0530

    Netvirt: Karaf CLI for Qos Alert patch3
    *** patch #3 - Adding karaf CLI for Neutron Qos Alert ***
    
    Added following new karaf CLI
    
    qos:enable-qos-alert <true|false>
    qos:drop-packet-threshold <threshold value in %>
    qos:alert-poll-interval <polling interval in minutes>
    
    Spec Review Link:
    https://git.opendaylight.org/gerrit/50689
    
    Change-Id: Ife146e6a52c0a63c0b7f4cfbabfc986fb2cde487
    Signed-off-by: Arun Sharma <arun.e.sharma@ericsson.com>

---
+++ vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosAlertPollIntervalCLI.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.qosservice;
+
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Command(scope = "qos", name = "alert-poll-interval", description = "configure polling interval in minutes")
+public class QosAlertPollIntervalCLI extends OsgiCommandSupport {
+
+    @Argument(index = 0, name = "<interval>", description = "polling interval in minutes",
+            required = true, multiValued = false)
+    private String interval;
+
+    private static final Logger LOG = LoggerFactory.getLogger(QosAlertPollIntervalCLI.class);
+
+    private QosAlertManager qosAlertManager;
+
+    public void setQosAlertManager(QosAlertManager qosAlertManager) {
+        this.qosAlertManager = qosAlertManager;
+        LOG.info("Qos manager:{} set", qosAlertManager);
+    }
+
+    @Override
+    protected Object doExecute() throws Exception {
+        LOG.info("setting threshold in qos alert manager:{}", interval);
+        qosAlertManager.setPollInterval(Integer.parseInt(interval));
+        return null;
+    }
+
+}
