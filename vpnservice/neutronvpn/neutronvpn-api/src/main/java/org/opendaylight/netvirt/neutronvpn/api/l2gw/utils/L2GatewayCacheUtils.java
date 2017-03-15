61b34d54b5fe528eb907f257ad4677537b1b6805
Akash Kumar Sahu <a.k.sahu@ericsson.com>
2017-03-15 16:48:38 +0530

    Bug 7988 - Cluster reboot fix
    Elan Instance , Physical Switch , L2gateWay and L2gatewayConnection all
    have dependency among each other.But if upon cluster reboot the order is
    not proper (L2gw cache is updated at the last , then flows are not being programmed as expected.
    To take care of the situation
    Introduced new ElanInstance Listner (Was already present but not
    registered with xml to be instantiated )
    Handle scenario if L2gW shows up last after cluster reboot .
    Handle scenario if l2gwConnection is updated but Elan instance is not
    available.
    
    Change-Id: I6194d7cb22d6659803b17078d57c8ad694c0c201
    Signed-off-by: Akash Kumar Sahu <a.k.sahu@ericsson.com>

--- vpnservice/neutronvpn/neutronvpn-api/src/main/java/org/opendaylight/netvirt/neutronvpn/api/l2gw/utils/L2GatewayCacheUtils.java
+++ vpnservice/neutronvpn/neutronvpn-api/src/main/java/org/opendaylight/netvirt/neutronvpn/api/l2gw/utils/L2GatewayCacheUtils.java
@@ -7,10 +7,14 @@
  */
 package org.opendaylight.netvirt.neutronvpn.api.l2gw.utils;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentMap;
 
 import org.opendaylight.netvirt.neutronvpn.api.l2gw.L2GatewayDevice;
 import org.opendaylight.genius.utils.cache.CacheUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.physical._switch.attributes.TunnelIps;
 
 public class L2GatewayCacheUtils {
     public static final String L2GATEWAY_CACHE_NAME = "L2GW";
@@ -48,4 +52,36 @@ public class L2GatewayCacheUtils {
                 .getCache(L2GatewayCacheUtils.L2GATEWAY_CACHE_NAME);
     }
 
+    public static synchronized  L2GatewayDevice updateCacheUponL2GatewayAdd(final String psName, final Uuid l2gwUuid) {
+        L2GatewayDevice l2GwDevice = L2GatewayCacheUtils.getL2DeviceFromCache(psName);
+        if (l2GwDevice == null) {
+            l2GwDevice = new L2GatewayDevice();
+            l2GwDevice.setDeviceName(psName);
+            l2GwDevice.addL2GatewayId(l2gwUuid);
+        } else {
+            l2GwDevice.addL2GatewayId(l2gwUuid);
+        }
+        addL2DeviceToCache(psName, l2GwDevice);
+        return l2GwDevice;
+    }
+
+    public static synchronized  L2GatewayDevice updateCacheUponSwitchConnect(final String psName, final String
+            hwvtepNodeId, final List<TunnelIps> tunnelIps) {
+        L2GatewayDevice l2GwDevice = L2GatewayCacheUtils.getL2DeviceFromCache(psName);
+        if (l2GwDevice == null) {
+            l2GwDevice = new L2GatewayDevice();
+            l2GwDevice.setDeviceName(psName);
+        }
+        l2GwDevice.setConnected(true);
+        l2GwDevice.setHwvtepNodeId(hwvtepNodeId);
+
+        if (tunnelIps != null && !tunnelIps.isEmpty()) {
+            for (TunnelIps tunnelIp : tunnelIps) {
+                IpAddress tunnelIpAddr = tunnelIp.getTunnelIpsKey();
+                l2GwDevice.addTunnelIp(tunnelIpAddr);
+            }
+        }
+        addL2DeviceToCache(psName, l2GwDevice);
+        return l2GwDevice;
+    }
 }
