7fc787b6870d863bf42fc44b40512ffe430235e4
Tali <tali.ben-meir@hpe.com>
2017-03-19 18:14:49 +0200

    Egress policy service binding and policy table miss flows
    Change-Id: I56b9bee5d794832b68f1522f502dcc169c1f0ab2
    Signed-off-by: Tali <tali.ben-meir@hpe.com>

--- vpnservice/policyservice/impl/src/main/java/org/opendaylight/netvirt/policyservice/PolicyServiceConstants.java
+++ vpnservice/policyservice/impl/src/main/java/org/opendaylight/netvirt/policyservice/PolicyServiceConstants.java
@@ -17,5 +17,6 @@ public class PolicyServiceConstants {
     public static final Long POLICY_GROUP_LOW_ID = 310000L;
     public static final Long POLICY_GROUP_HIGH_ID = 320000L;
     public static final long INVALID_ID = 0;
+    public static final int POLICY_DEFAULT_FLOW_PRIORITY = 10;
     public static final int POLICY_FLOW_PRIOPITY = 42;
 }
