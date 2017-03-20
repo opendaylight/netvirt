caadfedb2d037b772696869598d071405e5290e1
Tali <tali.ben-meir@hpe.com>
2017-03-20 18:38:57 +0200

    Policy routing table flow installation
    Route each combination of policy classifier and logical interface
    to the corresponding policy classifier group
    
    Change-Id: I74d30a2b0a7da1c0023c47bcd8b81c533ee67b81
    Signed-off-by: Tali <tali.ben-meir@hpe.com>

--- vpnservice/policyservice/impl/src/main/java/org/opendaylight/netvirt/policyservice/listeners/TunnelStateChangeListener.java
+++ vpnservice/policyservice/impl/src/main/java/org/opendaylight/netvirt/policyservice/listeners/TunnelStateChangeListener.java
@@ -9,6 +9,7 @@
 package org.opendaylight.netvirt.policyservice.listeners;
 
 import java.math.BigInteger;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -17,12 +18,16 @@ import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
+import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.netvirt.policyservice.PolicyRouteFlowProgrammer;
 import org.opendaylight.netvirt.policyservice.util.PolicyServiceUtil;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeLogicalGroup;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeVxlan;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.TepInfoAttributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.TunnelsState;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.tunnels_state.StateTunnelList;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.policy.rev170207.policy.profiles.PolicyProfile;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.policy.rev170207.underlay.networks.underlay.network.DpnToInterface;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
@@ -30,7 +35,9 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Listen on operational {@link StateTunnelList} changes and update
- * {@link DpnToInterface} accordingly for tunnel interfaces of type VxLAN.
+ * {@link DpnToInterface} accordingly for tunnel interfaces of type VxLAN.<br>
+ * When logical tunnel interface state is added or removed, the corresponding
+ * POLICY_ROUTING_TABLE entries will be updated.
  *
  */
 @Singleton
@@ -40,11 +47,14 @@ public class TunnelStateChangeListener
 
     private final DataBroker dataBroker;
     private final PolicyServiceUtil policyServiceUtil;
+    private final PolicyRouteFlowProgrammer routeFlowProgrammer;
 
     @Inject
-    public TunnelStateChangeListener(DataBroker dataBroker, final PolicyServiceUtil policyServiceUtil) {
+    public TunnelStateChangeListener(DataBroker dataBroker, final PolicyServiceUtil policyServiceUtil,
+            final PolicyRouteFlowProgrammer routeFlowProgrammer) {
         this.dataBroker = dataBroker;
         this.policyServiceUtil = policyServiceUtil;
+        this.routeFlowProgrammer = routeFlowProgrammer;
     }
 
     @Override
@@ -66,8 +76,12 @@ public class TunnelStateChangeListener
 
     @Override
     protected void remove(InstanceIdentifier<StateTunnelList> key, StateTunnelList tunnelState) {
-        LOG.debug("Tunnel state {} removed", tunnelState);
-        handleTunnelUpdate(tunnelState, false);
+        LOG.trace("Tunnel state {} removed", tunnelState);
+        if (isLogicalGroupTunnel(tunnelState)) {
+            populatePolicyRoutesToDpn(tunnelState, NwConstants.DEL_FLOW);
+        } else if (isVxlanTunnel(tunnelState)) {
+            updateTunnelToUnderlayNetworkOperDs(tunnelState, false);
+        }
     }
 
     @Override
@@ -77,15 +91,47 @@ public class TunnelStateChangeListener
 
     @Override
     protected void add(InstanceIdentifier<StateTunnelList> key, StateTunnelList tunnelState) {
-        LOG.debug("Tunnel state {} added", tunnelState);
-        handleTunnelUpdate(tunnelState, true);
+        LOG.trace("Tunnel state {} added", tunnelState);
+        if (isVxlanTunnel(tunnelState)) {
+            updateTunnelToUnderlayNetworkOperDs(tunnelState, true);
+        } else if (isLogicalGroupTunnel(tunnelState)) {
+            populatePolicyRoutesToDpn(tunnelState, NwConstants.ADD_FLOW);
+        }
     }
 
-    private void handleTunnelUpdate(StateTunnelList tunnelState, boolean isAdded) {
-        if (!isVxlanTunnel(tunnelState)) {
+    private void populatePolicyRoutesToDpn(StateTunnelList tunnelState, int addOrRemove) {
+        BigInteger srcDpId = getTepDpnId(tunnelState.getSrcInfo());
+        BigInteger dstDpId = getTepDpnId(tunnelState.getDstInfo());
+        String tunnelInterfaceName = tunnelState.getTunnelInterfaceName();
+        if (BigInteger.ZERO.equals(srcDpId) || BigInteger.ZERO.equals(dstDpId)) {
+            LOG.warn("No valid DPN found for logical tunnel {}", tunnelInterfaceName);
             return;
         }
 
+        List<PolicyProfile> policyProfiles = policyServiceUtil.getAllPolicyProfiles();
+        if (policyProfiles == null || policyProfiles.isEmpty()) {
+            LOG.debug("No policy profiles found on addition of {}", tunnelInterfaceName);
+            return;
+        }
+
+        policyProfiles.forEach(policyProfile -> {
+            String policyClassifier = policyProfile.getPolicyClassifier();
+            List<String> underlayNetworks = PolicyServiceUtil
+                    .getUnderlayNetworksFromPolicyRoutes(policyProfile.getPolicyRoute());
+            underlayNetworks.forEach(underlayNetwork -> {
+                if (policyServiceUtil.underlayNetworkContainsDpn(underlayNetwork, srcDpId)
+                        && policyServiceUtil.underlayNetworkContainsRemoteDpn(underlayNetwork, dstDpId)) {
+                    routeFlowProgrammer.programPolicyClassifierFlow(policyClassifier, srcDpId, dstDpId, addOrRemove,
+                            true);
+                } else {
+                    LOG.trace("logical tunnel {} source DPN {} dest DPN {} not associated to policy classifier {}",
+                            tunnelInterfaceName, srcDpId, dstDpId, policyClassifier);
+                }
+            });
+        });
+    }
+
+    private void updateTunnelToUnderlayNetworkOperDs(StateTunnelList tunnelState, boolean isAdded) {
         BigInteger srcDpId = getTepDpnId(tunnelState.getSrcInfo());
         BigInteger dstDpId = getTepDpnId(tunnelState.getDstInfo());
         String tunnelInterfaceName = tunnelState.getTunnelInterfaceName();
@@ -103,10 +149,11 @@ public class TunnelStateChangeListener
         String underlayNetwork = policyServiceUtil.getTunnelUnderlayNetwork(srcDpId, tunnelIp);
         if (underlayNetwork == null) {
             LOG.debug("No underlay networks defined for tunnel {} DPN {}", tunnelInterfaceName, srcDpId);
+            return;
         }
 
-        LOG.info("Handle tunnel state update for interface {} on DPN {} underlay network", tunnelInterfaceName, srcDpId,
-                underlayNetwork);
+        LOG.info("Handle tunnel state update for interface {} on DPN {} underlay network {}", tunnelInterfaceName,
+                srcDpId, underlayNetwork);
         policyServiceUtil.updateTunnelInterfaceForUnderlayNetwork(underlayNetwork, srcDpId, dstDpId,
                 tunnelInterfaceName, isAdded);
     }
@@ -114,6 +161,11 @@ public class TunnelStateChangeListener
     private static boolean isVxlanTunnel(StateTunnelList tunnelState) {
         return tunnelState.getTransportType() != null
                 && tunnelState.getTransportType().isAssignableFrom(TunnelTypeVxlan.class);
+    }
+
+    private static boolean isLogicalGroupTunnel(StateTunnelList tunnelState) {
+        return tunnelState.getTransportType() != null
+                && tunnelState.getTransportType().isAssignableFrom(TunnelTypeLogicalGroup.class);
     }
 
     private static BigInteger getTepDpnId(TepInfoAttributes tepInfoAttributes) {
