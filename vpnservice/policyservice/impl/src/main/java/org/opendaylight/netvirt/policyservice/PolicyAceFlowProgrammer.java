977711555809ce2c73ec46a5da3faf19b6e9a65b
Tali <tali.ben-meir@hpe.com>
2017-03-22 22:42:19 +0200

    <WIP> Add custom policy ACL matches
    Change-Id: I1a1fa604e0d35ef84e1464c2fdaf7272e71b4da7
    Signed-off-by: Tali <tali.ben-meir@hpe.com>

--- vpnservice/policyservice/impl/src/main/java/org/opendaylight/netvirt/policyservice/PolicyAceFlowProgrammer.java
+++ vpnservice/policyservice/impl/src/main/java/org/opendaylight/netvirt/policyservice/PolicyAceFlowProgrammer.java
@@ -8,17 +8,23 @@
 
 package org.opendaylight.netvirt.policyservice;
 
+import com.google.common.base.Objects;
+
 import java.math.BigInteger;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.genius.datastoreutils.DataStoreJobCoordinator;
+import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
 import org.opendaylight.genius.mdsalutil.InstructionInfo;
 import org.opendaylight.genius.mdsalutil.MatchInfoBase;
 import org.opendaylight.genius.mdsalutil.NwConstants;
@@ -26,6 +32,12 @@ import org.opendaylight.netvirt.aclservice.api.utils.IAclServiceUtil;
 import org.opendaylight.netvirt.policyservice.util.PolicyServiceFlowUtil;
 import org.opendaylight.netvirt.policyservice.util.PolicyServiceUtil;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.Ace;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.Matches;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.policy.rev170207.IngressInterface;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.policy.rev170207.L2vpnServiceType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.policy.rev170207.L3vpnServiceType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.policy.rev170207.Service;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.policy.rev170207.ServiceTypeBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,6 +47,7 @@ public class PolicyAceFlowProgrammer {
 
     private final DataBroker dataBroker;
     private final IAclServiceUtil aclServiceUtil;
+    private final IInterfaceManager interfaceManager;
     private final PolicyIdManager policyIdManager;
     private final PolicyServiceUtil policyServiceUtil;
     private final PolicyServiceFlowUtil policyFlowUtil;
@@ -42,10 +55,11 @@ public class PolicyAceFlowProgrammer {
 
     @Inject
     public PolicyAceFlowProgrammer(final DataBroker dataBroker, final IAclServiceUtil aclServiceUtil,
-            final PolicyIdManager policyIdManager, final PolicyServiceUtil policyServiceUtil,
-            final PolicyServiceFlowUtil policyFlowUtil) {
+            final IInterfaceManager interfaceManager, final PolicyIdManager policyIdManager,
+            final PolicyServiceUtil policyServiceUtil, final PolicyServiceFlowUtil policyFlowUtil) {
         this.dataBroker = dataBroker;
         this.aclServiceUtil = aclServiceUtil;
+        this.interfaceManager = interfaceManager;
         this.policyIdManager = policyIdManager;
         this.policyServiceUtil = policyServiceUtil;
         this.policyFlowUtil = policyFlowUtil;
@@ -85,11 +99,12 @@ public class PolicyAceFlowProgrammer {
     }
 
     public void programAceFlows(Ace ace, List<InstructionInfo> instructions, BigInteger dpId, int addOrRemove) {
-        Map<String, List<MatchInfoBase>> flowMap = aclServiceUtil.programIpFlow(ace.getMatches());
-        if (flowMap == null) {
-            LOG.error("Failed to create flows for ACE rule {}", ace.getRuleName());
-            return;
-        }
+        Map<String, List<MatchInfoBase>> aclflowMap = aclServiceUtil.programIpFlow(ace.getMatches());
+        Map<String, List<MatchInfoBase>> policyFlowMap = programPolicyAclFlows(ace.getMatches(), dpId);
+        Map<String, List<MatchInfoBase>> flowMap = new HashMap<>();
+        flowMap.putAll(Optional.ofNullable(aclflowMap).orElse(Collections.emptyMap()));
+        flowMap.putAll(policyFlowMap);
+
         coordinator.enqueueJob(ace.getRuleName(), () -> {
             WriteTransaction tx = dataBroker.newWriteOnlyTransaction();
 
@@ -104,6 +119,81 @@ public class PolicyAceFlowProgrammer {
             });
             return Collections.singletonList(tx.submit());
         });
+    }
+
+    private Map<String, List<MatchInfoBase>> programPolicyAclFlows(Matches matches, BigInteger dpId) {
+        Map<String, List<MatchInfoBase>> flowsMap = new HashMap<>();
+        IngressInterface ingressInterface = matches.getAugmentation(IngressInterface.class);
+        if (ingressInterface != null) {
+            Pair<String,
+                    List<MatchInfoBase>> ingressInterfaceFlow = programIngressInterfaceFlow(ingressInterface, dpId);
+            if (ingressInterfaceFlow != null) {
+                flowsMap.put(ingressInterfaceFlow.getKey(), ingressInterfaceFlow.getValue());
+            }
+        }
+
+        Service service = matches.getAugmentation(Service.class);
+        if (service != null) {
+            Pair<String, List<MatchInfoBase>> serviceFlow = programServiceFlow(service);
+            if (serviceFlow != null) {
+                flowsMap.put(serviceFlow.getKey(), serviceFlow.getValue());
+            }
+        }
+
+        return flowsMap;
+    }
+
+    private Pair<String, List<MatchInfoBase>> programIngressInterfaceFlow(IngressInterface ingressInterface,
+            BigInteger dpId) {
+        String interfaceName = ingressInterface.getName();
+        if (interfaceName == null) {
+            LOG.error("Invalid ingress interface augmentation. missing interface name");
+            return null;
+        }
+
+        BigInteger interfaceDpId = interfaceManager.getDpnForInterface(interfaceName);
+        if (!Objects.equal(dpId, interfaceDpId)) {
+            LOG.trace("Ingress interface match will be installed only on DPN {}. Ignoring DPN {}", ingressInterface,
+                    dpId);
+            return null;
+        }
+
+        List<MatchInfoBase> matches = policyFlowUtil.getIngressInterfaceMatches(interfaceName);
+        if (matches == null) {
+            LOG.error("Failed to get ingress interface {} matches", interfaceName);
+            return null;
+        }
+
+        String flowName = "INGRESS_INTERFACE_" + interfaceName;
+        return Pair.of(flowName, matches);
+        // TODO vlan
+    }
+
+    private Pair<String, List<MatchInfoBase>> programServiceFlow(Service service) {
+        // TODO Auto-generated method stub
+        String serviceName = service.getServiceName();
+        Class<? extends ServiceTypeBase> serviceType = service.getServiceType();
+        if (serviceName == null || serviceType == null) {
+            LOG.error("Invalid policy service augmentation {}", service);
+            return null;
+        }
+
+        String flowName = null;
+        List<MatchInfoBase> matches = null;
+        if (serviceType.isAssignableFrom(L2vpnServiceType.class)) {
+            flowName = "L2VPN_" + serviceName;
+            matches = policyFlowUtil.getElanInstanceMatches(serviceName);
+        } else if (serviceType.isAssignableFrom(L3vpnServiceType.class)) {
+            flowName = "L3VPN_" + serviceName;
+            matches = policyFlowUtil.getVpnInstanceMatches(serviceName);
+        }
+
+        if (flowName != null && matches != null) {
+            return Pair.of(flowName, matches);
+        }
+
+        LOG.debug("Failed to get service {} matches", serviceName);
+        return null;
     }
 
     // TODO table miss flow
