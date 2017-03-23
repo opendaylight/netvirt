a3c51cc1639e30e8c156d5e1dc4d80d060b8e428
Aswin Suryanarayanan <asuryana@redhat.com>
2017-03-23 10:38:38 +0530

    Added a new framework for selecting centralized SNAT router.
    Added a new HA framework for selecting the centralized SNAT router which
    will use a weighted round robin to select the NAPT switch
    
    A pseudo port will be added once the router is elected as the designated
    switch and will be delete on router delete.
    
    The switch selection/release happens in response to external router
    create/delete or snat enable/disable
    
    The flow programming will be done in response to   changes in
    centralized switch model RouterToNaptSwitch.
    
    Change-Id: I868b71f8a666925f72beddc811f4cc6b144bccc7
    Signed-off-by: Aswin Suryanarayanan <asuryana@redhat.com>

---
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/ha/SnatNodeEventListener.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.natservice.ha;
+
+import java.math.BigInteger;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
+import org.opendaylight.genius.mdsalutil.MDSALUtil;
+import org.opendaylight.netvirt.natservice.api.CentralizedSwitchScheduler;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * CentralizedSwitchChangeListener adds/removes the switches to scheduler pool when a switch is
+ * added/removed.
+ */
+
+public class SnatNodeEventListener  extends AsyncDataTreeChangeListenerBase<FlowCapableNode, SnatNodeEventListener>
+    implements AutoCloseable {
+    private final CentralizedSwitchScheduler  centralizedSwitchScheduler;
+    private static final Logger LOG = LoggerFactory.getLogger(SnatNodeEventListener.class);
+    private final DataBroker dataBroker;
+
+    @Inject
+    public SnatNodeEventListener(final DataBroker dataBroker,
+            final CentralizedSwitchScheduler centralizedSwitchScheduler) {
+        super(FlowCapableNode.class, SnatNodeEventListener.class);
+        this.dataBroker = dataBroker;
+        this.centralizedSwitchScheduler = centralizedSwitchScheduler;
+    }
+
+    @Override
+    @PostConstruct
+    public void init() {
+        LOG.info("{} start", getClass().getSimpleName());
+        registerListener(LogicalDatastoreType.OPERATIONAL, dataBroker);
+    }
+
+    @Override
+    protected InstanceIdentifier<FlowCapableNode> getWildCardPath() {
+        return InstanceIdentifier.create(Nodes.class).child(Node.class).augmentation(FlowCapableNode.class);
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<FlowCapableNode> key, FlowCapableNode dataObjectModification) {
+        NodeKey nodeKey = key.firstKeyOf(Node.class);
+        BigInteger dpnId = MDSALUtil.getDpnIdFromNodeName(nodeKey.getId());
+        centralizedSwitchScheduler.removeSwitch(dpnId);
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<FlowCapableNode> key, FlowCapableNode dataObjectModificationBefore,
+            FlowCapableNode dataObjectModificationAfter) {
+        /*Do Nothing */
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<FlowCapableNode> key, FlowCapableNode dataObjectModification) {
+        NodeKey nodeKey = key.firstKeyOf(Node.class);
+        BigInteger dpnId = MDSALUtil.getDpnIdFromNodeName(nodeKey.getId());
+        centralizedSwitchScheduler.addSwitch(dpnId);
+
+    }
+
+    @Override
+    protected org.opendaylight.netvirt.natservice.ha.SnatNodeEventListener getDataTreeChangeListener() {
+        return SnatNodeEventListener.this;
+    }
+
+}
---
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/SnatServiceManagerImpl.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.natservice.internal;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.netvirt.natservice.api.SnatServiceListener;
+import org.opendaylight.netvirt.natservice.api.SnatServiceManager;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ext.routers.Routers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SnatServiceManagerImpl implements SnatServiceManager {
+
+    private final List<SnatServiceListener> snatServiceListeners = new ArrayList<>();
+    private static final Logger LOG = LoggerFactory.getLogger(SnatServiceManagerImpl.class);
+
+    @Override
+    public void addNatServiceListener(SnatServiceListener natServiceListner) {
+        snatServiceListeners.add(natServiceListner);
+    }
+
+    @Override
+    public void removeNatServiceListener(SnatServiceListener natServiceListner) {
+        snatServiceListeners.remove(natServiceListner);
+    }
+
+    @Override
+    public void notify(Routers router, BigInteger primarySwitchId, BigInteger dpnId, Action action) {
+        for (SnatServiceListener snatServiceListener : snatServiceListeners) {
+            boolean result = false;
+            switch (action) {
+                case SNAT_ALL_SWITCH_ENBL:
+                    result = snatServiceListener.handleSnatAllSwitch(router, primarySwitchId, NwConstants.ADD_FLOW);
+                    break;
+
+                case SNAT_ALL_SWITCH_DISBL:
+                    result = snatServiceListener.handleSnatAllSwitch(router, primarySwitchId, NwConstants.DEL_FLOW);
+                    break;
+
+                case SNAT_ROUTER_ENBL:
+                    result = snatServiceListener.handleSnat(router, primarySwitchId, dpnId,  NwConstants.ADD_FLOW);
+                    break;
+
+                case SNAT_ROUTER_DISBL:
+                    result = snatServiceListener.handleSnat(router, primarySwitchId, dpnId, NwConstants.DEL_FLOW);
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (result) {
+                LOG.debug("Nat action {} invoking listener {} succeeded", action,
+                    snatServiceListener.getClass().getName());
+            } else {
+                LOG.warn("Nat action {} invoking listener {} failed", action, snatServiceListener.getClass().getName());
+            }
+        }
+    }
+
+}
