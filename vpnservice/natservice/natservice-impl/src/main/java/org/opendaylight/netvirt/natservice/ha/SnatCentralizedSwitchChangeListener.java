95ec8f2853871cb75cba8b27307e31a9642fd0b0
Aswin Suryanarayanan <asuryana@redhat.com>
2017-03-23 10:38:38 +0530

    Added a new framework for selecting centralized SNAT router.
    Added a new HA framework for selecting the centralized SNAT router which
    will use a weighted round robin to select the NAPT switch
    
    A pseudo port will be added once the router is elected as the designated
    switch and will be delete on router delete.
    
    The switch selection/release happens in response to external router
    create/delete or snat enable/disable
    
    The flow programming will be done in response to   changes in
    centralized switch model RouterToNaptSwitch.
    
    Change-Id: I868b71f8a666925f72beddc811f4cc6b144bccc7
    Signed-off-by: Aswin Suryanarayanan <asuryana@redhat.com>

---
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/ha/SnatCentralizedSwitchChangeListener.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.natservice.ha;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
+import org.opendaylight.netvirt.natservice.api.SnatServiceManager;
+import org.opendaylight.netvirt.natservice.internal.NatUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.NaptSwitches;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ext.routers.Routers;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.napt.switches.RouterToNaptSwitch;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * CentralizedSwitchChangeListener detect changes in switch:router mapping and
+ * update flows accordingly.
+ */
+public class SnatCentralizedSwitchChangeListener
+        extends AsyncDataTreeChangeListenerBase<RouterToNaptSwitch, SnatCentralizedSwitchChangeListener> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SnatCentralizedSwitchChangeListener.class);
+
+    private final DataBroker dataBroker;
+    private final SnatServiceManager snatServiceManger;
+
+    /** TODO This shall be a temporary map, the attributes consumed in Snat Service can be made
+     * later a part of RouterToNaptSwitch model.
+     */
+    private final Map<String,Routers> routerMap = new HashMap<>();
+
+    public SnatCentralizedSwitchChangeListener(final DataBroker dataBroker,
+            final SnatServiceManager snatServiceManger) {
+        super(RouterToNaptSwitch.class, SnatCentralizedSwitchChangeListener.class);
+        this.dataBroker = dataBroker;
+        this.snatServiceManger = snatServiceManger;
+    }
+
+    @Override
+    public void init() {
+        LOG.info("{} init", getClass().getSimpleName());
+        registerListener(LogicalDatastoreType.CONFIGURATION, dataBroker);
+    }
+
+    @Override
+    protected InstanceIdentifier<RouterToNaptSwitch> getWildCardPath() {
+        return InstanceIdentifier.create(NaptSwitches.class).child(RouterToNaptSwitch.class);
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<RouterToNaptSwitch> key, RouterToNaptSwitch routerToNaptSwitch) {
+        LOG.debug("Deleting ", routerToNaptSwitch);
+        BigInteger primarySwitchId = routerToNaptSwitch.getPrimarySwitchId();
+        Routers router =   routerMap.get(routerToNaptSwitch.getRouterName());
+        snatServiceManger.notify(router, primarySwitchId, null, SnatServiceManager.Action.SNAT_ALL_SWITCH_DISBL);
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<RouterToNaptSwitch> key, RouterToNaptSwitch origRouterToNaptSwitch,
+            RouterToNaptSwitch updatedRouterToNaptSwitch) {
+        LOG.debug("Updating old {} new {}", origRouterToNaptSwitch, updatedRouterToNaptSwitch);
+        BigInteger primarySwitchId = origRouterToNaptSwitch.getPrimarySwitchId();
+        Routers router = NatUtil.getRoutersFromConfigDS(dataBroker, origRouterToNaptSwitch.getRouterName());
+        routerMap.put(router.getRouterName(), router);
+        snatServiceManger.notify(router, primarySwitchId, null, SnatServiceManager.Action.SNAT_ALL_SWITCH_DISBL);
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<RouterToNaptSwitch> key, RouterToNaptSwitch routerToNaptSwitch) {
+        LOG.debug("Adding ", routerToNaptSwitch);
+        BigInteger primarySwitchId = routerToNaptSwitch.getPrimarySwitchId();
+        Routers router = NatUtil.getRoutersFromConfigDS(dataBroker, routerToNaptSwitch.getRouterName());
+        routerMap.put(router.getRouterName(), router);
+        snatServiceManger.notify(router, primarySwitchId, null, SnatServiceManager.Action.SNAT_ALL_SWITCH_ENBL);
+    }
+
+    @Override
+    protected SnatCentralizedSwitchChangeListener getDataTreeChangeListener() {
+        return this;
+    }
+}
