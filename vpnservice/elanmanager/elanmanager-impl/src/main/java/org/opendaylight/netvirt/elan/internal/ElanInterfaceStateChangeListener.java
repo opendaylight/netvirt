38d8709c77b710492245847372d68d2d89261f8c
Ravindra Thakur <ravindra.nath.thakur@ericsson.com>
2017-03-24 15:18:25 +0530

    Netvirt: enhancement to vlan provider networks
    Spec link:
    http://docs.opendaylight.org/en/latest/submodules/netvirt/docs/specs/vlan-provider-enhancement.html
    
    Change-Id: Iad7479835a7bd97a4f827c8365601d2a20efe477
    Signed-off-by: Ravindra Thakur <ravindra.nath.thakur@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInterfaceStateChangeListener.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInterfaceStateChangeListener.java
@@ -8,17 +8,22 @@
 package org.opendaylight.netvirt.elan.internal;
 
 import java.math.BigInteger;
+import java.util.Collections;
+import java.util.List;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
 import org.opendaylight.genius.datastoreutils.DataStoreJobCoordinator;
 import org.opendaylight.genius.interfacemanager.globals.InterfaceInfo;
+import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
 import org.opendaylight.genius.mdsalutil.MDSALUtil;
 import org.opendaylight.netvirt.elan.utils.ElanConstants;
 import org.opendaylight.netvirt.elan.utils.ElanUtils;
+import org.opendaylight.netvirt.elanmanager.api.IElanService;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.InterfacesState;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.dpn.interfaces.elan.dpn.interfaces.list.DpnInterfaces;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.instances.ElanInstance;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.interfaces.ElanInterface;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
@@ -32,11 +37,17 @@ public class ElanInterfaceStateChangeListener
 
     private final DataBroker broker;
     private final ElanInterfaceManager elanInterfaceManager;
+    private final IInterfaceManager interfaceManager;
+    private final IElanService elanService;
 
-    public ElanInterfaceStateChangeListener(final DataBroker db, final ElanInterfaceManager ifManager) {
+    public ElanInterfaceStateChangeListener(final DataBroker db, final ElanInterfaceManager ifManager,
+                                            final IInterfaceManager interfaceManager,
+                                            final IElanService elanService) {
         super(Interface.class, ElanInterfaceStateChangeListener.class);
         broker = db;
         elanInterfaceManager = ifManager;
+        this.interfaceManager = interfaceManager;
+        this.elanService = elanService;
     }
 
     @Override
@@ -47,7 +58,7 @@ public class ElanInterfaceStateChangeListener
     @Override
     protected void remove(InstanceIdentifier<Interface> identifier, Interface delIf) {
         LOG.trace("Received interface {} Down event", delIf);
-        String interfaceName =  delIf.getName();
+        String interfaceName = delIf.getName();
         ElanInterface elanInterface = ElanUtils.getElanInterfaceByElanInterfaceName(broker, interfaceName);
         if (elanInterface == null) {
             LOG.debug("No Elan Interface is created for the interface:{} ", interfaceName);
@@ -67,8 +78,28 @@ public class ElanInterfaceStateChangeListener
         }
         DataStoreJobCoordinator coordinator = DataStoreJobCoordinator.getInstance();
         InterfaceRemoveWorkerOnElan removeWorker = new InterfaceRemoveWorkerOnElan(elanInstanceName, elanInstance,
-            interfaceName, interfaceInfo, true, elanInterfaceManager);
+                interfaceName, interfaceInfo, true, elanInterfaceManager);
         coordinator.enqueueJob(elanInstanceName, removeWorker, ElanConstants.JOB_MAX_RETRIES);
+
+        // trigger deletion of vlan provider intf if this is the last VM on this DPN in the ELAN
+        if (!elanInstance.isExternal() && ElanUtils.isVlan(elanInstance)) {
+            coordinator.enqueueJob(dpId.toString(), () -> {
+                DpnInterfaces dpnInterfaces = elanService.getElanDpnInterfaces(elanInstanceName, dpId);
+                List<String> interfaces = dpnInterfaces.getInterfaces();
+                int count = 0;
+                for (String intf : interfaces) {
+                    if (!interfaceManager.isExternalInterface(intf)) {
+                        count++;
+                    }
+                }
+                // this VM intf is the last intf on the DPN
+                if (count == 1) {
+                    LOG.debug("deleting vlan prv intf for elan {}, dpn {}", elanInstanceName, dpId);
+                    elanService.deleteExternalElanNetwork(elanInstance, dpId);
+                }
+                return Collections.emptyList();
+            });
+        }
     }
 
     @Override
@@ -87,6 +118,18 @@ public class ElanInterfaceStateChangeListener
         InstanceIdentifier<ElanInterface> elanInterfaceId = ElanUtils
                 .getElanInterfaceConfigurationDataPathId(interfaceName);
         elanInterfaceManager.add(elanInterfaceId, elanInterface);
+
+        // create vlan member interface on patch port for internal VLAN provider network
+        ElanInstance elanInstance = ElanUtils.getElanInstanceByName(broker, elanInterface.getElanInstanceName());
+        if (!elanInstance.isExternal() && ElanUtils.isVlan(elanInstance)) {
+            BigInteger dpId = interfaceManager.getDpnForInterface(interfaceName);
+            DataStoreJobCoordinator.getInstance().enqueueJob(dpId.toString(), () -> {
+                LOG.debug("creating vlan member intf for network {}, dpn {}",
+                        elanInstance.getPhysicalNetworkName(), dpId);
+                elanService.createExternalElanNetwork(elanInstance, dpId);
+                return Collections.emptyList();
+            });
+        }
     }
 
     @Override
