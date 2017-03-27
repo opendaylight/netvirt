c4e809cc1c8594e591336b214a50aeaad5430549
Shlomi <shlomi.alfasi@hpe.com>
2017-03-27 19:15:22 +0300

    hwvtep federation support
    Change-Id: I1e3ef68ca45c9ab3998d2e0402989098c1aca137
    Signed-off-by: Shlomi <shlomi.alfasi@hpe.com>

--- vpnservice/federation-plugin/impl/src/main/java/org/opendaylight/netvirt/federation/plugin/identifiers/FederationTopologyHwvtepNodeIdentifier.java
+++ vpnservice/federation-plugin/impl/src/main/java/org/opendaylight/netvirt/federation/plugin/identifiers/FederationTopologyHwvtepNodeIdentifier.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.netvirt.federation.plugin.identifiers;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -16,28 +17,40 @@ import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
-public class FederationTopologyNodeIdentifier implements FederationPluginIdentifier<Node, Topology, NetworkTopology> {
+public class FederationTopologyHwvtepNodeIdentifier
+        implements FederationPluginIdentifier<Node, Topology, NetworkTopology> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FederationTopologyHwvtepNodeIdentifier.class);
 
     @Inject
-    public FederationTopologyNodeIdentifier() {
-        FederationPluginIdentifierRegistry.registerIdentifier(FederationPluginConstants.TOPOLOGY_NODE_CONFIG_KEY,
-                LogicalDatastoreType.CONFIGURATION, this);
-        FederationPluginIdentifierRegistry.registerIdentifier(FederationPluginConstants.TOPOLOGY_NODE_OPER_KEY,
-                LogicalDatastoreType.OPERATIONAL, this);
+    public FederationTopologyHwvtepNodeIdentifier() {
+        FederationPluginIdentifierRegistry.registerIdentifier(
+                FederationPluginConstants.TOPOLOGY_HWVTEP_NODE_CONFIG_KEY, LogicalDatastoreType.CONFIGURATION, this);
+        FederationPluginIdentifierRegistry.registerIdentifier(
+                FederationPluginConstants.TOPOLOGY_HWVTEP_NODE_OPER_KEY, LogicalDatastoreType.OPERATIONAL, this);
+    }
+
+    @PostConstruct
+    public void init() {
+        LOG.info("{} start", getClass().getSimpleName());
     }
 
     @Override
     public InstanceIdentifier<Node> getInstanceIdentifier() {
-        return InstanceIdentifier.create(NetworkTopology.class)
-                .child(Topology.class, FederationPluginConstants.OVSDB_TOPOLOGY_KEY).child(Node.class);
+        InstanceIdentifier<Node> nodeId = InstanceIdentifier.create(NetworkTopology.class)
+                .child(Topology.class, FederationPluginConstants.HWVTEP_TOPOLOGY_KEY).child(Node.class);
+        LOG.info("InstanceId: {}", nodeId);
+        return nodeId;
     }
 
     @Override
     public InstanceIdentifier<Topology> getParentInstanceIdentifier() {
         return InstanceIdentifier.create(NetworkTopology.class).child(Topology.class,
-                FederationPluginConstants.OVSDB_TOPOLOGY_KEY);
+                FederationPluginConstants.HWVTEP_TOPOLOGY_KEY);
     }
 
     @Override
