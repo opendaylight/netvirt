9a2ccaacf326c051f8005ac8ff869eccf0d5c496
Riyazahmed D Talikoti <riyazahmed.d.talikoti@ericsson.com>
2017-03-27 14:53:51 +0530

    EVPN RT2 elan changes to adv/withdraw RT2 routes
    1. Elan instance changes when network is attached and detached to EVPN
    2. Elan instance changes to advertise and withdraw routes on attaching and
    detaching network to EVPN
    
    Change-Id: I147da46e5d869cde560f25452e9e7511206a61dd
    Signed-off-by: Riyazahmed D Talikoti <riyazahmed.d.talikoti@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-api/src/main/java/org/opendaylight/netvirt/vpnmanager/api/IVpnManager.java
+++ vpnservice/vpnmanager/vpnmanager-api/src/main/java/org/opendaylight/netvirt/vpnmanager/api/IVpnManager.java
@@ -12,12 +12,16 @@ import java.math.BigInteger;
 import java.util.Collection;
 import java.util.List;
 
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.genius.mdsalutil.MatchInfoBase;
 import org.opendaylight.netvirt.fibmanager.api.IFibManager;
 import org.opendaylight.netvirt.fibmanager.api.RouteOrigin;
+import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.vpn.instances.VpnInstance;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.subnetmaps.Subnetmap;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.neutron.vpn.portip.port.data.VpnPortipToPort;
+
 
 public interface IVpnManager {
     void setFibManager(IFibManager fibManager);
@@ -75,5 +79,13 @@ public interface IVpnManager {
     void onSubnetDeletedFromVpn(Subnetmap subnetmap, boolean isBgpVpn);
 
     List<MatchInfoBase> getEgressMatchesForVpn(String vpnName);
+
+    VpnInstance getVpnInstance(DataBroker broker, String vpnInstanceName);
+
+    String getVpnRd(DataBroker broker, String vpnName);
+
+    VpnPortipToPort getNeutronPortFromVpnPortFixedIp(DataBroker broker, String vpnName, String fixedIp);
+
+    String getMacAddressForInterface(DataBroker dataBroker, String interfaceName);
 
 }
