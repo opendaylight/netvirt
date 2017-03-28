203cb7688c6847b86337394e79caa58e9d5408f4
Tomer Pearl <tomer.pearl@hpe.com>
2017-03-28 17:15:33 +0300

    Bind Tenant provider VLAN port to L3-Pipeline
    NetVirt currently missing binding of L3 services for provider
    ports configured in Flat/Vlan private tenant networks.
    
    A problems rises when trying to identify the rotuer instance that
    we are receiving traffic from, as a private network can be connected
    to multiple routers (when using multiple subnets in a network).
    
    In order to identify the right router, we have added the creation of
    VpnInstances for private networks (similar to what we do with external
    networks). The VpnId of the private network is coded in metadata in
    the dispatcher table(t17), before entering the L3 service.
    
    In L3_GW_MAC_TABLE (t19) there is a match for a router_inteface mac
    + vpn-id of private network, then the metadata is overrided with
    the vpn-id of the router.
    
    Change-Id: I03a3e1d679333982eae9cafaa705415257f6c317
    Signed-off-by: Tomer Pearl <tomer.pearl@hpe.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/SubnetMacHandler.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/SubnetMacHandler.java
@@ -9,6 +9,7 @@
 package org.opendaylight.netvirt.vpnmanager;
 
 import java.math.BigInteger;
+import java.util.List;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
@@ -16,8 +17,11 @@ import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
 import org.opendaylight.genius.mdsalutil.NwConstants;
 import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.adjacency.list.Adjacency;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.NeutronVpnPortipPortData;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.neutron.vpn.portip.port.data.VpnPortipToPort;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.subnets.rev150712.subnets.attributes.subnets.Subnet;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 public class SubnetMacHandler extends AsyncDataTreeChangeListenerBase<VpnPortipToPort, SubnetMacHandler> {
@@ -46,6 +50,14 @@ public class SubnetMacHandler extends AsyncDataTreeChangeListenerBase<VpnPortipT
             WriteTransaction writeTx = dataBroker.newWriteOnlyTransaction();
             VpnUtil.setupSubnetMacIntoVpnInstance(dataBroker, mdsalManager, value.getVpnName(), null,
                     value.getMacAddress(), BigInteger.ZERO /* On all DPNs */, writeTx, NwConstants.DEL_FLOW);
+
+            String networkName = getNetworkInstanceName(value.getPortName());
+            if (networkName != null
+                    && VpnUtil.isFlatOrVlanNetwork(VpnUtil.getNeutronNetwork(dataBroker, new Uuid(networkName)))) {
+                VpnUtil.setupSubnetMacIntoVpnInstanceOnDpns(dataBroker, mdsalManager, networkName,
+                        value.getVpnName(), value.getMacAddress(), VpnUtil.getDpnsOnVpn(dataBroker, value.getVpnName()),
+                        writeTx, NwConstants.DEL_FLOW);
+            }
             writeTx.submit();
         }
     }
@@ -61,8 +73,40 @@ public class SubnetMacHandler extends AsyncDataTreeChangeListenerBase<VpnPortipT
             WriteTransaction writeTx = dataBroker.newWriteOnlyTransaction();
             VpnUtil.setupSubnetMacIntoVpnInstance(dataBroker, mdsalManager, value.getVpnName(), null,
                     value.getMacAddress(), BigInteger.ZERO /* On all DPNs */, writeTx, NwConstants.ADD_FLOW);
+
+            String networkName = getNetworkInstanceName(value.getPortName());
+            if (networkName != null
+                    && VpnUtil.isFlatOrVlanNetwork(VpnUtil.getNeutronNetwork(dataBroker, new Uuid(networkName)))) {
+                VpnUtil.setupSubnetMacIntoVpnInstanceOnDpns(dataBroker, mdsalManager, networkName,
+                        value.getVpnName(), value.getMacAddress(), VpnUtil.getDpnsOnVpn(dataBroker, value.getVpnName()),
+                        writeTx, NwConstants.ADD_FLOW);
+            }
+
             writeTx.submit();
         }
+    }
+
+    private String getNetworkInstanceName(String routerInterface) {
+        Uuid subnetUuid = getSubnetIdfFromVpnInterfaceName(routerInterface);
+        if (subnetUuid != null) {
+            Subnet subnet = VpnUtil.getSubnetById(dataBroker, subnetUuid);
+            if (subnet != null) {
+                return subnet.getNetworkId().getValue();
+            }
+        }
+        return null;
+    }
+
+    private Uuid getSubnetIdfFromVpnInterfaceName(String rotuerInterfaceId) {
+        List<Adjacency> adjacencies = VpnUtil.getAdjacenciesForVpnInterfaceFromConfig(dataBroker, rotuerInterfaceId);
+        if (adjacencies != null) {
+            for (Adjacency adj : adjacencies) {
+                if (adj.isPrimaryAdjacency()) {
+                    return adj.getSubnetId();
+                }
+            }
+        }
+        return null;
     }
 
     @Override
