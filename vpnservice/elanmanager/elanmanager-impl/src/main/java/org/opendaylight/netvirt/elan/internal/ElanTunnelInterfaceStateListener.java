5e0a0124a44418ede4fd91231397b969a4565d8d
Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>
2017-03-28 12:48:06 +0530

    Making ELAN to use StateTunnelList listener
    * Currently ELAN uses Interface state DCN for tunnel events to program
    remote DMAC and BC group when VxLAN tunnel is created later. So making it
    to use listen StateTunnelList
    * This change would be useful when ITM supports multiple VxLAN tunnels and
    avoid race conditions
    
    Change-Id: Idce3dd3a548b95feadf43fbe2c1311c3fafdb70a
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>

---
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanTunnelInterfaceStateListener.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.elan.internal;
+
+import java.math.BigInteger;
+import java.util.Collections;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
+import org.opendaylight.genius.datastoreutils.DataStoreJobCoordinator;
+import org.opendaylight.netvirt.elan.ElanException;
+import org.opendaylight.netvirt.elan.utils.ElanConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.TepTypeInternal;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.TunnelOperStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.TunnelsState;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.op.rev160406.tunnels_state.StateTunnelList;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ElanTunnelInterfaceStateListener extends AsyncDataTreeChangeListenerBase<StateTunnelList,
+    ElanTunnelInterfaceStateListener> implements AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(ElanTunnelInterfaceStateListener.class);
+    private final DataBroker dataBroker;
+    private final ElanInterfaceManager elanInterfaceManager;
+
+    public ElanTunnelInterfaceStateListener(final DataBroker dataBroker,
+            final ElanInterfaceManager elanInterfaceManager) {
+        super(StateTunnelList.class, ElanTunnelInterfaceStateListener.class);
+        this.dataBroker = dataBroker;
+        this.elanInterfaceManager = elanInterfaceManager;
+    }
+
+    @Override
+    public void init() {
+        registerListener(LogicalDatastoreType.OPERATIONAL, dataBroker);
+    }
+
+    @Override
+    protected InstanceIdentifier<StateTunnelList> getWildCardPath() {
+        return InstanceIdentifier.create(TunnelsState.class).child(StateTunnelList.class);
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<StateTunnelList> key, StateTunnelList delete) {
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<StateTunnelList> key, StateTunnelList original,
+            StateTunnelList update) {
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<StateTunnelList> key, StateTunnelList add) {
+        LOG.info("processing add state for StateTunnelList {}", add);
+        if (!isInternalTunnel(add)) {
+            LOG.trace("tunnel {} is not a internal vxlan tunnel", add);
+        }
+        TunnelOperStatus tunOpStatus = add.getOperState();
+        if (tunOpStatus != TunnelOperStatus.Down && tunOpStatus != TunnelOperStatus.Up) {
+            LOG.trace("Returning because unsupported tunnelOperStatus {}", tunOpStatus);
+            return;
+        }
+        DataStoreJobCoordinator.getInstance().enqueueJob(add.getTunnelInterfaceName(), () -> {
+            BigInteger srcDpId = new BigInteger(add.getSrcInfo().getTepDeviceId());
+            BigInteger dstDpId = new BigInteger(add.getDstInfo().getTepDeviceId());
+            try {
+                LOG.info("Handling tunnel state event for srcDpId {} and dstDpId {} ",
+                        srcDpId, dstDpId);
+                elanInterfaceManager.handleInternalTunnelStateEvent(srcDpId, dstDpId);
+            } catch (ElanException e) {
+                LOG.error("Failed handle tunnel state event between srcDpId {} and dstDpId {} ",
+                        srcDpId, dstDpId, e);
+            }
+            return Collections.emptyList();
+        }, ElanConstants.JOB_MAX_RETRIES);
+    }
+
+    @Override
+    protected ElanTunnelInterfaceStateListener getDataTreeChangeListener() {
+        return this;
+    }
+
+    private boolean isInternalTunnel(StateTunnelList stateTunnelList) {
+        return stateTunnelList.getDstInfo().getTepDeviceType() == TepTypeInternal.class;
+    }
+
+}
