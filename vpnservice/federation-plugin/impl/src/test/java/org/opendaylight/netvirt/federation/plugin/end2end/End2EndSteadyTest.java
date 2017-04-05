46a3de5c3ed0692e2fe090b1e73de212cd5c674f
Tali <tali.ben-meir@hpe.com>
2017-04-05 20:28:00 +0300

    <WIP> Federation plugin component test
    Change-Id: I113010993b9b22dfb9178207c00de14cc708c82d
    Signed-off-by: Tali <tali.ben-meir@hpe.com>

--- vpnservice/federation-plugin/impl/src/test/java/org/opendaylight/netvirt/federation/plugin/end2end/End2EndSteadyTest.java
+++ vpnservice/federation-plugin/impl/src/test/java/org/opendaylight/netvirt/federation/plugin/end2end/End2EndSteadyTest.java
@@ -8,12 +8,11 @@
 package org.opendaylight.netvirt.federation.plugin.end2end;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
-
-import com.google.common.base.Optional;
-import com.google.common.util.concurrent.Futures;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +25,7 @@ import org.opendaylight.federation.service.api.message.SubscribeMessage;
 import org.opendaylight.netvirt.federation.plugin.FederationPluginConstants;
 import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.vpn.interfaces.VpnInterface;
 import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.vpn.interfaces.VpnInterfaceBuilder;
+import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.vpn.interfaces.VpnInterfaceKey;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana._if.type.rev140508.L2vlan;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.Interface;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.InterfaceBuilder;
@@ -38,6 +38,11 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.sn
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.interfaces.ElanInterface;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.interfaces.ElanInterfaceBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.interfaces.ElanInterfaceKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.federation.plugin.rev170219.ElanShadowProperties;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.federation.plugin.rev170219.IfShadowProperties;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.federation.plugin.rev170219.InventoryNodeShadowProperties;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.federation.plugin.rev170219.TopologyNodeShadowProperties;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.federation.plugin.rev170219.VpnShadowProperties;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.Adjacencies;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.AdjacenciesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.adjacency.list.AdjacencyBuilder;
@@ -47,6 +52,7 @@ import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPointBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 @RunWith(MockitoJUnitRunner.class)
 public class End2EndSteadyTest extends AbstractEnd2EndTest {
@@ -56,112 +62,160 @@ public class End2EndSteadyTest extends AbstractEnd2EndTest {
     }
 
     @Before
-    public void setUp() {
-        super.initialization();
-        prepareMocks();
+    public void setUp() throws Exception {
+        setupMocks();
         producer.handleSubscribeMsg(
                 new SubscribeMessage(CONSUMER1_QUEUE, "netvirt", CONSUMER1_QUEUE, "1.2.3.4", CONSUMER1_QUEUE));
-        sentMessages.clear();// Clear the commit message
     }
 
-    private void prepareMocks() {
-        when(mockReadTx.read(any(), any())).thenReturn(Futures.immediateCheckedFuture(Optional.absent()));
-        setGenerationNumberMock();
+    private void setupMocks() throws Exception {
+        setGenerationNumber();
     }
 
     @Test
-    public void topologyNodeWithFilteredInName() { // Every nodeId should pass
+    public void topologyNodeWithFilteredInName() throws Exception {
+        // Every nodeId should pass
         Node node = new NodeBuilder().setNodeId(new NodeId(INTEGRATION_BRIDGE_PREFIX)).build();
-        dcn(FederationPluginConstants.TOPOLOGY_NODE_CONFIG_KEY, node);
-        assertEquals(1, sentMessages.size());
-        assertEquals(INTEGRATION_BRIDGE_PREFIX, removeLastMerged(Node.class).getNodeId().getValue());
+        InstanceIdentifier<Node> instanceIdentifier = getTopologyNodeInstanceIdentifier(INTEGRATION_BRIDGE_PREFIX);
+        addFederationEntity(FederationPluginConstants.INVENTORY_NODE_CONFIG_KEY, instanceIdentifier, node);
+        assertSentMessages(1);
+        Node federatedNode = getFederationEntity(FederationPluginConstants.INVENTORY_NODE_CONFIG_KEY,
+                instanceIdentifier);
+        assertNotNull(federatedNode);
+        TopologyNodeShadowProperties shadowProperties = federatedNode
+                .getAugmentation(TopologyNodeShadowProperties.class);
+        assertNotNull(shadowProperties);
+        assertTrue(shadowProperties.isShadow());
+        assertEquals(INTEGRATION_BRIDGE_PREFIX, federatedNode.getNodeId().getValue());
     }
 
     @Test
-    public void vpnInterfaceWithoutAdjacenciesShouldBeFilteredOut() {
-        VpnInterface vpnIface = new VpnInterfaceBuilder().build();
-        dcn(FederationPluginConstants.VPN_INTERFACE_KEY, vpnIface);
-        assertEquals(0, sentMessages.size());
+    public void vpnInterfaceWithoutAdjacenciesShouldBeFilteredOut() throws Exception {
+        VpnInterface vpnIface = new VpnInterfaceBuilder().setKey(new VpnInterfaceKey(DUMMYINTERFACE)).build();
+        addFederationEntity(FederationPluginConstants.VPN_INTERFACE_KEY,
+                getVpnInterfaceInstanceIdentifier(DUMMYINTERFACE), vpnIface);
+        assertNoSentMessages();
     }
 
     @Test
-    public void vpnInterfaceValid() {
+    public void vpnInterfaceValid() throws Exception {
         Adjacencies adjacencies = new AdjacenciesBuilder().setAdjacency(Arrays.asList(new AdjacencyBuilder()
                 .setSubnetId(new Uuid(PRODUCER_SUBNET_ID)).setPrimaryAdjacency(true).setIpAddress("7.7.7.7").build()))
                 .build();
         VpnInterface vpnIface = new VpnInterfaceBuilder().addAugmentation(Adjacencies.class, adjacencies)
                 .setName(DUMMYINTERFACE).build();
-        dcn(FederationPluginConstants.VPN_INTERFACE_KEY, vpnIface);
-        assertEquals(1, sentMessages.size());
-        assertEquals(CONSUMER_SUBNET_ID, removeLastMerged(VpnInterface.class).getAugmentation(Adjacencies.class)
-                .getAdjacency().get(0).getSubnetId().getValue());
+        InstanceIdentifier<VpnInterface> instanceIdentifier = getVpnInterfaceInstanceIdentifier(DUMMYINTERFACE);
+        addFederationEntity(FederationPluginConstants.VPN_INTERFACE_KEY, instanceIdentifier, vpnIface);
+        assertSentMessages(1);
+        VpnInterface federatedVpnInterface = getFederationEntity(FederationPluginConstants.VPN_INTERFACE_KEY,
+                instanceIdentifier);
+        assertNotNull(federatedVpnInterface);
+        VpnShadowProperties shadowProperties = federatedVpnInterface.getAugmentation(VpnShadowProperties.class);
+        assertNotNull(shadowProperties);
+        assertTrue(shadowProperties.isShadow());
+        Adjacencies federatedAdjacencies = federatedVpnInterface.getAugmentation(Adjacencies.class);
+        assertNotNull(federatedAdjacencies);
+        assertEquals(CONSUMER_SUBNET_ID, federatedAdjacencies.getAdjacency().get(0).getSubnetId().getValue());
     }
 
     @SuppressWarnings("deprecation")
     @Test
-    public void inventoryNodeTruncatedStatisticsAugmentations() {
-        org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeBuilder
-            nodeBuilder = new org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeBuilder()
+    public void inventoryNodeTruncatedStatisticsAugmentations() throws Exception {
+        org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeBuilder nodeBuilder =
+                new org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeBuilder()
                 .setId(new org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId("dummynode"));
         FlowCapableStatisticsGatheringStatus statistics = new FlowCapableStatisticsGatheringStatusBuilder()
                 .setSnapshotGatheringStatusEnd(new SnapshotGatheringStatusEndBuilder()
                         .setEnd(new DateAndTime("1986-09-04T15:12:10.5Z")).build())
                 .build();
         nodeBuilder.addAugmentation(FlowCapableStatisticsGatheringStatus.class, statistics);
-        dcn(FederationPluginConstants.INVENTORY_NODE_CONFIG_KEY, nodeBuilder.build());
-        assertEquals(1, sentMessages.size());
-        org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node lastMergedNode = removeLastMerged(
-                org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node.class);
-        assertNull(lastMergedNode.getAugmentation(FlowCapableStatisticsGatheringStatus.class));
+        InstanceIdentifier<
+                org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node> instanceIdentifier =
+                getInventoryNodeInstanceIdentifier("dummynode");
+        addFederationEntity(FederationPluginConstants.INVENTORY_NODE_CONFIG_KEY, instanceIdentifier,
+                nodeBuilder.build());
+        assertSentMessages(1);
+        org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node federatedNode =
+                getFederationEntity(FederationPluginConstants.INVENTORY_NODE_CONFIG_KEY, instanceIdentifier);
+        assertNotNull(federatedNode);
+        InventoryNodeShadowProperties shadowProperties = federatedNode
+                .getAugmentation(InventoryNodeShadowProperties.class);
+        assertNotNull(shadowProperties);
+        assertTrue(shadowProperties.isShadow());
+        assertNull(federatedNode.getAugmentation(FlowCapableStatisticsGatheringStatus.class));
     }
 
     @Test
-    public void topologyNodeWithFilteredOutTerminationPoint() {
+    public void topologyNodeWithFilteredOutTerminationPoint() throws Exception {
         List<TerminationPoint> tps = Arrays.asList(new TerminationPointBuilder().setTpId(new TpId("tunDummy")).build());
         Node node = new NodeBuilder().setNodeId(new NodeId(INTEGRATION_BRIDGE_PREFIX)).setTerminationPoint(tps).build();
-        dcn(FederationPluginConstants.TOPOLOGY_NODE_CONFIG_KEY, node);
-
-        assertEquals(1, sentMessages.size());
-        assertEquals(0, removeLastMerged(Node.class).getTerminationPoint().size());
+        InstanceIdentifier<Node> instanceIdentifier = getTopologyNodeInstanceIdentifier(INTEGRATION_BRIDGE_PREFIX);
+        addFederationEntity(FederationPluginConstants.INVENTORY_NODE_CONFIG_KEY, instanceIdentifier, node);
+        assertSentMessages(1);
+        Node federatedNode = getFederationEntity(FederationPluginConstants.INVENTORY_NODE_CONFIG_KEY,
+                instanceIdentifier);
+        assertNotNull(federatedNode);
+        assertEquals(0, federatedNode.getTerminationPoint().size());
     }
 
     @Test
-    public void topologyNodeWithFilteredInTerminationPoint() {
+    public void topologyNodeWithFilteredInTerminationPoint() throws Exception {
         List<TerminationPoint> tps = Arrays
                 .asList(new TerminationPointBuilder().setTpId(new TpId("nottunDummy")).build());
         Node node = new NodeBuilder().setNodeId(new NodeId(INTEGRATION_BRIDGE_PREFIX)).setTerminationPoint(tps).build();
-        dcn(FederationPluginConstants.TOPOLOGY_NODE_CONFIG_KEY, node);
-        assertEquals(1, sentMessages.size());
-        assertEquals("nottunDummy", removeLastMerged(Node.class).getTerminationPoint().get(0).getTpId().getValue());
+        InstanceIdentifier<Node> instanceIdentifier = getTopologyNodeInstanceIdentifier(INTEGRATION_BRIDGE_PREFIX);
+        addFederationEntity(FederationPluginConstants.INVENTORY_NODE_CONFIG_KEY, instanceIdentifier, node);
+        assertSentMessages(1);
+        Node federatedNode = getFederationEntity(FederationPluginConstants.INVENTORY_NODE_CONFIG_KEY,
+                instanceIdentifier);
+        assertNotNull(federatedNode);
+        assertEquals("nottunDummy", federatedNode.getTerminationPoint().get(0).getTpId().getValue());
     }
 
     @Test
-    public void elanInterfaceWrongNetworkFilteredOut() {
+    public void elanInterfaceWrongNetworkFilteredOut() throws Exception {
         ElanInterface elanIface = new ElanInterfaceBuilder().setElanInstanceName("dummynetwork").setName(DUMMYINTERFACE)
                 .setKey(new ElanInterfaceKey(DUMMYINTERFACE)).build();
-        dcn(FederationPluginConstants.ELAN_INTERFACE_KEY, elanIface);
-        assertEquals(0, sentMessages.size());
+        addFederationEntity(FederationPluginConstants.ELAN_INTERFACE_KEY,
+                getElanInterfaceInstanceIdentifier(DUMMYINTERFACE), elanIface);
+        assertNoSentMessages();
     }
 
+
     @Test
-    public void interfaceQueuedAndThenReleased() {
+    public void interfaceQueuedAndThenReleased() throws Exception {
         Interface iface = new InterfaceBuilder().setType(L2vlan.class).setName(DUMMYINTERFACE)
                 .setKey(new InterfaceKey(DUMMYINTERFACE)).build();
         when(elanService.isExternalInterface(any())).thenReturn(false);
         when(elanService.getElanInterfaceByElanInterfaceName(any())).thenReturn(null);
-        dcn(FederationPluginConstants.IETF_INTERFACE_KEY, iface);
-        assertEquals(0, sentMessages.size());
+        addFederationEntity(FederationPluginConstants.IETF_INTERFACE_KEY,
+                getIetfInterfaceInstanceIdentifier(DUMMYINTERFACE), iface);
+        assertNoSentMessages();
 
         ElanInterface elanIface = new ElanInterfaceBuilder().setElanInstanceName(PRODUCER_NETWORK_ID)
                 .setName(DUMMYINTERFACE).setKey(new ElanInterfaceKey(DUMMYINTERFACE)).build();
-        dcn(FederationPluginConstants.ELAN_INTERFACE_KEY, elanIface);
-        assertEquals(2, sentMessages.size());
-        assertEquals(DUMMYINTERFACE, removeFirstMerged(Interface.class).getName());
-        assertEquals(CONSUMER_NETWORK_ID, removeFirstMerged(ElanInterface.class).getElanInstanceName());
+        InstanceIdentifier<ElanInterface> instanceIdentifier = getElanInterfaceInstanceIdentifier(DUMMYINTERFACE);
+        addFederationEntity(FederationPluginConstants.ELAN_INTERFACE_KEY, instanceIdentifier, elanIface);
+        assertSentMessages(2);
+        ElanInterface federatedElanInterface = getFederationEntity(FederationPluginConstants.ELAN_INTERFACE_KEY,
+                instanceIdentifier);
+        assertNotNull(federatedElanInterface);
+        ElanShadowProperties shadowProperties = federatedElanInterface.getAugmentation(ElanShadowProperties.class);
+        assertNotNull(shadowProperties);
+        assertTrue(shadowProperties.isShadow());
+        assertEquals(DUMMYINTERFACE, federatedElanInterface.getName());
+        assertEquals(CONSUMER_NETWORK_ID, federatedElanInterface.getElanInstanceName());
 
         when(elanService.getElanInterfaceByElanInterfaceName(any())).thenReturn(elanIface);
-        dcn(FederationPluginConstants.IETF_INTERFACE_KEY, iface);
+        InstanceIdentifier<Interface> interfaceIdentifier = getIetfInterfaceInstanceIdentifier(DUMMYINTERFACE);
+        addFederationEntity(FederationPluginConstants.IETF_INTERFACE_KEY, interfaceIdentifier, iface);
         assertEquals(3, sentMessages.size());
-        assertEquals(DUMMYINTERFACE, removeFirstMerged(Interface.class).getName());
+        Interface federatedInterface = getFederationEntity(FederationPluginConstants.IETF_INTERFACE_KEY,
+                interfaceIdentifier);
+        assertNotNull(federatedInterface);
+        IfShadowProperties ifaceShadowProperties = federatedInterface.getAugmentation(IfShadowProperties.class);
+        assertNotNull(ifaceShadowProperties);
+        assertTrue(ifaceShadowProperties.isShadow());
+        assertEquals(DUMMYINTERFACE, federatedInterface.getName());
     }
 }
