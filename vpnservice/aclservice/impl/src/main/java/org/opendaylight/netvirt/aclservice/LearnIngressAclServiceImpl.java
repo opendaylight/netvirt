db8f4e1d0b1a8a382f9a76c3ab1dd5005a7f949e
Slava Radune <slava.radune@hpe.com>
2017-04-06 10:20:19 +0300

    Fix remote acl bugs
    1. Merging metadatas of lPort and aclId.
    2. Handling reg6Match for ingress
    3. Fixing dupplicate AclInterfaces in AclDataUtil
    
    Depends on: https://git.opendaylight.org/gerrit/54398
    
    Change-Id: I64b0e294317e0ac1a3444192a92f8058f2604f1d
    Signed-off-by: Slava Radune <slava.radune@hpe.com>

--- vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/LearnIngressAclServiceImpl.java
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/LearnIngressAclServiceImpl.java
@@ -28,6 +28,7 @@ import org.opendaylight.netvirt.aclservice.utils.AclServiceUtils;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.Ace;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.actions.PacketHandling;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.actions.packet.handling.Permit;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.servicebinding.rev160406.ServiceModeIngress;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.interfaces._interface.AllowedAddressPairs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,7 +52,7 @@ public class LearnIngressAclServiceImpl extends AbstractIngressAclServiceImpl {
     protected String syncSpecificAclFlow(BigInteger dpId, int lportTag, int addOrRemove, Ace ace, String portId,
             Map<String, List<MatchInfoBase>> flowMap, String flowName) {
         List<MatchInfoBase> flowMatches = flowMap.get(flowName);
-        flowMatches.add(buildLPortTagMatch(lportTag));
+        AclServiceUtils.addLportTagMetadataMatch(lportTag, flowMatches, ServiceModeIngress.class);
         List<ActionInfo> actionsInfos = new ArrayList<>();
         PacketHandling packetHandling = ace.getActions() != null ? ace.getActions().getPacketHandling() : null;
         int priority = getIngressSpecificAclFlowPriority(dpId, addOrRemove, flowName, packetHandling);
