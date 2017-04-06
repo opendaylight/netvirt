900cd3fa0a6f7fb6e156a689e667e48d4b635446
eupakir <kiran.n.upadhyaya@ericsson.com>
2017-04-06 14:17:56 +0530

    Bug 8165 - Learnt IP route does not reappear on DC-GW after OVSRestart
    On OVS Restart, a learnt IP will have to be deleted by Liveness
    Monitor, and then re-learnt once the OVS comes up.
    But this IP was not learned again.
    
    Cyclic events were getting spawned while processing removal of
    Learnt IPs.  This was causing deletion of re-learnt IPs.
    
    Change-Id: I17b87fef05de8d3eb51f2a4720df98ff647e8937
    Signed-off-by: eupakir <kiran.n.upadhyaya@ericsson.com>
    Signed-off-by: Vivekanandan Narasimhan <n.vivekanandan@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/ArpMonitoringHandler.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/ArpMonitoringHandler.java
@@ -118,8 +118,8 @@ public class ArpMonitoringHandler
                 }
                 MacAddress srcMacAddress = MacAddress.getDefaultInstance(value.getMacAddress());
                 String vpnName =  value.getVpnName();
-                String interfaceName =  value.getPortName();
-                MacEntry macEntry = new MacEntry(vpnName, srcMacAddress, srcInetAddr, interfaceName);
+                MacEntry macEntry = new MacEntry(vpnName, srcMacAddress, srcInetAddr, value.getPortName(),
+                        value.getCreationTime());
                 DataStoreJobCoordinator coordinator = DataStoreJobCoordinator.getInstance();
                 coordinator.enqueueJob(buildJobKey(srcInetAddr.toString(), vpnName),
                         new ArpMonitorStartTask(macEntry, arpMonitorProfileId, dataBroker, alivenessManager,
@@ -139,10 +139,18 @@ public class ArpMonitoringHandler
                     LOG.warn("The mac address received is null for LearntVpnVipToPort {}, ignoring the DTCN", value);
                     return;
                 }
-                MacAddress srcMacAddress = MacAddress.getDefaultInstance(value.getMacAddress());
                 String vpnName =  value.getVpnName();
+                String learntIp = srcInetAddr.getHostAddress();
+                LearntVpnVipToPort vpnVipToPort = VpnUtil.getLearntVpnVipToPort(dataBroker, vpnName, learntIp);
+                if (vpnVipToPort != null && !vpnVipToPort.getCreationTime().equals(value.getCreationTime())) {
+                    LOG.warn("The MIP {} over vpn {} has been learnt again and processed. "
+                            + "Ignoring this remove event.", learntIp, vpnName);
+                    return;
+                }
+                MacAddress srcMacAddress = MacAddress.getDefaultInstance(value.getMacAddress());
                 String interfaceName =  value.getPortName();
-                MacEntry macEntry = new MacEntry(vpnName, srcMacAddress, srcInetAddr, interfaceName);
+                MacEntry macEntry = new MacEntry(vpnName, srcMacAddress, srcInetAddr, interfaceName,
+                        value.getCreationTime());
                 DataStoreJobCoordinator coordinator = DataStoreJobCoordinator.getInstance();
                 coordinator.enqueueJob(buildJobKey(srcInetAddr.toString(), vpnName),
                         new ArpMonitorStopTask(macEntry, dataBroker, alivenessManager));
