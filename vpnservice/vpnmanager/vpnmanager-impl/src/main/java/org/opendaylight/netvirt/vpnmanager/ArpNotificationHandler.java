98c7e65d089bcdb6d62a58142682f22b5790408b
eupakir <kiran.n.upadhyaya@ericsson.com>
2017-04-06 14:17:56 +0530

    Bug 8165 - Learnt IP route does not reappear on DC-GW after OVSRestart
    On OVS Restart, a learnt IP will have to be deleted by Liveness
    Monitor, and then re-learnt once the OVS comes up.
    But this IP was not learned again.
    
    Cyclic events were getting spawned while processing removal of
    Learnt IPs.  This was causing deletion of re-learnt IPs.
    
    Change-Id: I17b87fef05de8d3eb51f2a4720df98ff647e8937
    Signed-off-by: eupakir <kiran.n.upadhyaya@ericsson.com>
    Signed-off-by: Vivekanandan Narasimhan <n.vivekanandan@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/ArpNotificationHandler.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/ArpNotificationHandler.java
@@ -287,7 +287,8 @@ public class ArpNotificationHandler implements OdlArputilListener {
                         new VpnInterfaceKey(vpnInterface)).augmentation(Adjacencies.class).child(Adjacency.class,
                         new AdjacencyKey(ip)).build();
                 MDSALUtil.syncDelete(dataBroker, LogicalDatastoreType.CONFIGURATION, adjacencyIdentifier);
-                LOG.trace("Successfully Deleted Adjacency into VpnInterface {}", vpnInterface);
+                LOG.info("Successfully deleted the learned-ip-adjacency for prefix {} on vpn {} for interface {}",
+                        ip, vpnName, vpnInterface);
             }
         }
     }
