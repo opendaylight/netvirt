d4bbe2f844c9a1997dca688623c02f5fb0b608db
Arun Sharma <arun.e.sharma@ericsson.com>
2017-04-09 19:58:05 +0530

    Netvirt: Qos Alert patch5
    *** patch #5 - Port direct statistics polling  ***
    
    Added support of  retrieval of port statistics data using OpenflowPlugin
    direct-statistics RPC and log the alert message if packet drop ratio is
    greater than the configured threshold value.
    
    Spec Review Link:
    https://git.opendaylight.org/gerrit/50689
    
    Change-Id: I0a708ccd7eeb10c3d71fd8c08d5a2efeacdd3e25
    Signed-off-by: Arun Sharma <arun.e.sharma@ericsson.com>

--- vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosAlertPortData.java
+++ vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosAlertPortData.java
@@ -9,13 +9,68 @@
 package org.opendaylight.netvirt.qosservice;
 
 
+import java.math.BigInteger;
+import org.opendaylight.netvirt.neutronvpn.interfaces.INeutronVpnManager;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.ports.Port;
-
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.qos.rev160613.qos.attributes.qos.policies.QosPolicy;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev131214.node.connector.statistics.and.port.number.map.NodeConnectorStatisticsAndPortNumberMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class QosAlertPortData {
-    public  Port port;
+    private Port port;
+    private static INeutronVpnManager neutronVpnManager;
+    private BigInteger rxPackets;
+    private BigInteger rxDroppedPackets;
+    private static BigInteger   alertThreshold;
+    private boolean statsDataInit;
 
-    public QosAlertPortData(Port port) {
+    private static final BigInteger BIG_HUNDRED = new BigInteger("100");
+    private static final Logger LOG = LoggerFactory.getLogger(QosAlertPortData.class);
+
+
+    public QosAlertPortData(final Port port, final INeutronVpnManager neutronVpnManager) {
         this.port = port;
+        QosAlertPortData.neutronVpnManager = neutronVpnManager;
+        statsDataInit = false;
+    }
+
+    public static void setAlertThreshold(int threshold) {
+        alertThreshold = BigInteger.valueOf(threshold);
+        LOG.info("setAlertThreshold:{}", alertThreshold);
+    }
+
+    public void updatePortStatistics(NodeConnectorStatisticsAndPortNumberMap statsData) {
+        if (statsDataInit) {
+            calculateAlertCondition(statsData);
+        }
+        rxPackets = statsData.getPackets().getReceived();
+        rxDroppedPackets = statsData.getReceiveDrops();
+        statsDataInit = true;
+    }
+
+    private void calculateAlertCondition(NodeConnectorStatisticsAndPortNumberMap statsData)  {
+        BigInteger rxDiff = statsData.getPackets().getReceived().subtract(rxPackets);
+        BigInteger rxDroppedDiff = statsData.getReceiveDrops().subtract(rxDroppedPackets);
+
+        BigInteger rxTotalDiff = rxDiff.add(rxDroppedDiff);
+        LOG.trace("Port {} rxDiff:{} rxDropped diff:{} total diff:{}", port.getUuid(), rxDiff,
+                                                                            rxDroppedDiff, rxTotalDiff);
+        QosPolicy qosPolicy = QosNeutronUtils.getQosPolicy(neutronVpnManager, port);
+
+        if (qosPolicy == null) {
+            return;
+        }
+
+        if (rxDroppedDiff.multiply(BIG_HUNDRED).compareTo(rxTotalDiff.multiply(alertThreshold)) > 0) {
+            LOG.trace(QosConstants.alertMsgFormat, qosPolicy.getName(), qosPolicy.getUuid().getValue(),
+                    port.getUuid().getValue(), port.getNetworkId().getValue(), statsData.getPackets().getReceived(),
+                                                                                        statsData.getReceiveDrops());
+
+            QosAlertGenerator.raiseAlert(qosPolicy.getName(), qosPolicy.getUuid().getValue(),
+                    port.getUuid().getValue(), port.getNetworkId().getValue(), statsData.getPackets().getReceived(),
+                                                                                          statsData.getReceiveDrops());
+        }
+
     }
 }
