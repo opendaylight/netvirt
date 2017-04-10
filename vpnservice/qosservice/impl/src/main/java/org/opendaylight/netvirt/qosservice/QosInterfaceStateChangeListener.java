28a61134c09f0014c01e6c6136c430345b2ca234
Naveen Kumar Verma <naveen.kumar.verma@ericsson.com>
2017-04-10 19:00:22 +0530

    stale entry in flow rules, after deleting port associated to qos policy with dscp
    When we are delete the VM which has an associated qos policy with it,
    the stale entry of DSCP marking use to persist for the table number 90.
    
    The reason for this was that when QosNeutronPortChangeListner is called
    rpc called to fetch DPN id use to fail. By moving the code to
    QosInterfaceStateChangeLister we can use Interface to find out DPN id.
    
    Change-Id: I485d84806eae2bcc8341518f8186973efb171658
    Signed-off-by: Naveen Kumar Verma <naveen.kumar.verma@ericsson.com>

--- vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosInterfaceStateChangeListener.java
+++ vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosInterfaceStateChangeListener.java
@@ -118,7 +118,34 @@ public class QosInterfaceStateChangeListener extends AsyncDataTreeChangeListener
     }
 
     @Override
+    @SuppressWarnings("checkstyle:IllegalCatch")
     protected void remove(InstanceIdentifier<Interface> identifier, Interface intrf) {
+        try {
+            if (!Tunnel.class.equals(intrf.getType())) {
+                final String interfaceName = intrf.getName();
+                // Guava Optional asSet().forEach() emulates Java 8 Optional ifPresent()
+                getNeutronPort(interfaceName).asSet().forEach(port -> {
+                    LOG.trace("Qos Service : Received interface {} PORT DOWN event ", interfaceName);
+                    QosPortExtension removeQos = port.getAugmentation(QosPortExtension.class);
+                    if (removeQos != null) {
+                        QosNeutronUtils.handleNeutronPortRemove(dataBroker, odlInterfaceRpcService,
+                                mdsalUtils, port, removeQos.getQosPolicyId(), intrf);
+                        QosNeutronUtils.removeFromQosPortsCache(removeQos.getQosPolicyId(), port);
+                    } else {
+                        Network network = neutronVpnManager.getNeutronNetwork(port.getNetworkId());
+                        if (network != null && network.getAugmentation(QosNetworkExtension.class) != null) {
+                            Uuid networkQosUuid = network.getAugmentation(QosNetworkExtension.class).getQosPolicyId();
+                            if (networkQosUuid != null) {
+                                QosNeutronUtils.handleNeutronPortRemove(dataBroker, odlInterfaceRpcService,
+                                        mdsalUtils, port, networkQosUuid, intrf);
+                            }
+                        }
+                    }
+                });
+            }
+        } catch (Exception e) {
+            LOG.error("Qos:Exception caught in Interface Operational State Down event", e);
+        }
     }
 
     @Override
--- vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosNeutronPortChangeListener.java
+++ vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosNeutronPortChangeListener.java
@@ -16,12 +16,9 @@ import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
 import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
 import org.opendaylight.netvirt.neutronvpn.interfaces.INeutronVpnManager;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.OdlInterfaceRpcService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.networks.attributes.networks.Network;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.Ports;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.ports.Port;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.qos.ext.rev160613.QosNetworkExtension;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.qos.ext.rev160613.QosPortExtension;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
@@ -74,23 +71,7 @@ public class QosNeutronPortChangeListener extends AsyncDataTreeChangeListenerBas
 
     @Override
     protected void remove(InstanceIdentifier<Port> instanceIdentifier, Port port) {
-        //Remove DSCP Flow when the port is removed
-        //Qos Policy Deletion
-        QosPortExtension removeQos = port.getAugmentation(QosPortExtension.class);
-        if (removeQos != null) {
-            QosNeutronUtils.handleNeutronPortRemove(dataBroker, odlInterfaceRpcService,
-                    mdsalUtils, port, removeQos.getQosPolicyId());
-            QosNeutronUtils.removeFromQosPortsCache(removeQos.getQosPolicyId(), port);
-        } else {
-            Network network =  neutronVpnManager.getNeutronNetwork(port.getNetworkId());
-            if (network != null && network.getAugmentation(QosNetworkExtension.class) != null) {
-                Uuid networkQosUuid = network.getAugmentation(QosNetworkExtension.class).getQosPolicyId();
-                if (networkQosUuid != null) {
-                    QosNeutronUtils.handleNeutronPortRemove(dataBroker, odlInterfaceRpcService,
-                            mdsalUtils, port, networkQosUuid);
-                }
-            }
-        }
+        // do nothing
     }
 
     @Override
