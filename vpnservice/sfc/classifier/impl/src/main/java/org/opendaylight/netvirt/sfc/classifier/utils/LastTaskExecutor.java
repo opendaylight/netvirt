1a33a040ae1c98b95851aa8ff352d72db893cf1d
Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>
2017-04-11 16:40:15 +0200

    Factor out executor from ClassifierService and test
    Change-Id: I4cf5bffcd33e2f4190a699a9982f75ca2d3ad8fd
    Signed-off-by: Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>

---
+++ vpnservice/sfc/classifier/impl/src/main/java/org/opendaylight/netvirt/sfc/classifier/utils/LastTaskExecutor.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017 Ericsson Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.sfc.classifier.utils;
+
+import java.util.Objects;
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * An executor that only executes the last submitted task. Ongoing tasks wont
+ * be cancelled.
+ */
+public class LastTaskExecutor implements Executor {
+
+    private final Executor executor;
+    private final AtomicReference<Runnable> lastTask = new AtomicReference<>();
+
+    public LastTaskExecutor(Executor executor) {
+        this.executor = executor;
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        lastTask.set(command);
+        executor.execute(() -> {
+            Runnable task = lastTask.getAndSet(null);
+            if (Objects.nonNull(task)) {
+                task.run();
+            }
+        });
+    }
+}
