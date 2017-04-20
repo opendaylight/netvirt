72ceb433ba618b945d9b7c3ff78ecc0f4a230cef
Michael Vorburger <vorburger@redhat.com>
2017-04-20 18:20:20 +0200

    Minor Checkstyle fix Custom Import Order error
    This shows up as red in Eclipse, but the CLI mvn build missed this.
    This is due to a more recent version of Checkstyle.
    
    required for overall move to new Checkstyle version, see
    https://git.opendaylight.org/gerrit/#/q/topic:bumpCheckstyle
    
    also includes minor removal of (1) extra parenthesis, (2) generic <>
    clean-up and (3) missing @Override from automatic clean-up which happens
    on auto-save (technically unrelated to Checkstyle).
    
    Change-Id: Ic650ffe9ac42cfbc7cad4c1c31c7553faf5fe603
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/utils/ElanForwardingEntriesHandler.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/utils/ElanForwardingEntriesHandler.java
@@ -5,10 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.netvirt.elan.utils;
-
-
 
 import com.google.common.base.Optional;
 import javax.inject.Inject;
