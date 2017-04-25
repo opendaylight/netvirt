f1d04987a23746ffc4e0ad62f48c2da868ef08bb
Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>
2017-04-25 11:25:42 +0530

    Bug 7866 adding retries for remote dmac programming during tunnel up event
    * currently there is no retries in case of OptimisticLockFailedException
    while programming remote dmac during tunnel up event. This leads to remote
    dmac is programmed with drop action which leads to packet drop. Now added
    retries for jc job.
    * Also adding retries in case remote BC group fails (during tunnel up event) due to datastore exceptions.
    
    Change-Id: I2bc024fc7133170c321c4a5071d6b58e00a2666b
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInterfaceManager.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInterfaceManager.java
@@ -1065,8 +1065,8 @@ public class ElanInterfaceManager extends AsyncDataTreeChangeListenerBase<ElanIn
     }
 
     // Install DMAC entry on dst DPN
-    public void installDMacAddressTables(ElanInstance elanInfo, InterfaceInfo interfaceInfo, BigInteger dstDpId)
-            throws ElanException {
+    public void installDMacAddressTables(ElanInstance elanInfo, InterfaceInfo interfaceInfo, BigInteger dstDpId,
+            List<ListenableFuture<Void>> futures) throws ElanException {
         String interfaceName = interfaceInfo.getInterfaceName();
         ElanInterfaceMac elanInterfaceMac = elanUtils.getElanInterfaceMacByInterfaceName(interfaceName);
         if (elanInterfaceMac != null && elanInterfaceMac.getMacEntry() != null) {
@@ -1083,7 +1083,7 @@ public class ElanInterfaceManager extends AsyncDataTreeChangeListenerBase<ElanIn
                             writeFlowTx);
                 }
             }
-            writeFlowTx.submit();
+            futures.add(ElanUtils.waitForTransactionToComplete(writeFlowTx));
         }
     }
 
@@ -1582,6 +1582,7 @@ public class ElanInterfaceManager extends AsyncDataTreeChangeListenerBase<ElanIn
         return interfaceInfo.getAdminState() == InterfaceInfo.InterfaceAdminState.ENABLED;
     }
 
+    @SuppressWarnings("checkstyle:IllegalCatch")
     public void handleInternalTunnelStateEvent(BigInteger srcDpId, BigInteger dstDpId) throws ElanException {
         ElanDpnInterfaces dpnInterfaceLists = elanUtils.getElanDpnInterfacesList();
         LOG.trace("processing tunnel state event for srcDpId {} dstDpId {}"
@@ -1624,22 +1625,28 @@ public class ElanInterfaceManager extends AsyncDataTreeChangeListenerBase<ElanIn
                 dataStoreCoordinator.enqueueJob(elanName, () -> {
                     // update Remote BC Group
                     LOG.trace("procesing elan remote bc group for tunnel event {}", elanInfo);
-                    setupElanBroadcastGroups(elanInfo, srcDpId);
-
+                    List<ListenableFuture<Void>> elanFutures = new ArrayList<>();
+                    try {
+                        setupElanBroadcastGroups(elanInfo, srcDpId);
+                    } catch (RuntimeException e) {
+                        LOG.error("Error while adding remote bc group for {} on dpId {} ", elanName, srcDpId);
+                        ElanUtils.addToListenableFutureIfTxException(e, elanFutures);
+                    }
                     Set<String> interfaceLists = new HashSet<>();
                     interfaceLists.addAll(finalDstDpnIf.getInterfaces());
                     for (String ifName : interfaceLists) {
                         dataStoreCoordinator.enqueueJob(ifName, () -> {
                             LOG.info("Processing tunnel up event for elan {} and interface {}", elanName, ifName);
                             InterfaceInfo interfaceInfo = interfaceManager.getInterfaceInfo(ifName);
+                            List<ListenableFuture<Void>> elanInterfacefutures = new ArrayList<>();
                             if (isOperational(interfaceInfo)) {
-                                installDMacAddressTables(elanInfo, interfaceInfo, srcDpId);
+                                installDMacAddressTables(elanInfo, interfaceInfo, srcDpId, elanInterfacefutures);
                             }
-                            return null;
-                        });
+                            return elanInterfacefutures;
+                        }, ElanConstants.JOB_MAX_RETRIES);
                     }
-                    return null;
-                });
+                    return elanFutures;
+                }, ElanConstants.JOB_MAX_RETRIES);
             }
 
         }
