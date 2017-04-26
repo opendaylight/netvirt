29bcb7d57d6b6cc072ce5f88005507ddaf37c301
Arun Sharma <arun.e.sharma@ericsson.com>
2017-04-27 00:39:07 +0530

    Netvirt: Qos Alert patch6
    *** patch #6 - Added log4j configuraton support ***
    
    1. Added a new log4j appender with default logging properties in qosalert.cfg
    2. Added listener method update in QosAlertGenerator class to handle any chage in qosalert.cfg
    
    Spec Review Link:
    https://git.opendaylight.org/gerrit/50689
    
    Change-Id: I2008a0a2dd22661e0f45251f0aac1088270d3d96
    Signed-off-by: Arun Sharma <arun.e.sharma@ericsson.com>

--- vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosConstants.java
+++ vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosConstants.java
@@ -12,4 +12,8 @@ public class QosConstants {
     public static final int QOS_DEFAULT_FLOW_PRIORITY = 10;
     public static String alertMsgFormat = "Packet drop threshold hit for qos policy {} with qos-id {} for port port-{}"
         + " on network network-{} rx_received {} rx_dropped {}";
+    public static final String QOS_ALERT_PROPERTIES_PID = "org.opendaylight.netvirt.qosservice";
+    public static final String ORG_OPS4J_PAX_LOGGING = "org.ops4j.pax.logging";
+    public static final String FELIX_FILEINSTALL_FILENAME = "felix.fileinstall.filename";
+    public static final String SERVICE_PID = "service.pid";
 }
