2966a98cfaf35f11de2214135e7c761cff2f4a4f
Akash <a.k.sahu@ericsson.com>
2017-05-09 16:01:51 +0530

    bug 7599  hwvtep ucast mac add performance improv
    Changes for performance improvement.
    Added new Cmd TunnelIpCommand and also changed function getKey to return
    actual key Identifier in place of String value.
    
    Change-Id: I0fd84c87db6feefcba4c119fe0c05193751ab8cb
    Signed-off-by: Akash <a.k.sahu@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/ha/merge/PSAugmentationMerger.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/ha/merge/PSAugmentationMerger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ * Copyright (c) 2016, 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -8,11 +8,13 @@
 package org.opendaylight.netvirt.elan.l2gw.ha.merge;
 
 import org.opendaylight.netvirt.elan.l2gw.ha.commands.TunnelCmd;
+import org.opendaylight.netvirt.elan.l2gw.ha.commands.TunnelIpCmd;
 
 public class PSAugmentationMerger
         extends MergeCommandsAggregator {
     public PSAugmentationMerger() {
         commands.add(new TunnelCmd());
+        commands.add(new TunnelIpCmd());
     }
 
     static PSAugmentationMerger instance = new PSAugmentationMerger();
