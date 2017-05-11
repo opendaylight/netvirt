ea7a8d7dd1e9b3cf03f3dadb7e4456fe0e83f2a2
Achuth Maniyedath <achuth.m@altencalsoftlabs.com>
2017-05-12 00:02:50 +0530

    Neutron Port allocation for DHCP Service
    The feature enables DHCP service to make use of the
    newly created Neutron ports provisioned by Openstack
    networking-odl driver at the time of Subnet creation.
    Additionaly an ARPResponder flow is also introduced to
    service the ARP requests for these ports during VM
    DHCP renewal.
    
    All ArpResponder handling for Router interface gateway,
    floating IP and DHCP ports will be moved to ElanService,
    introducing a pipeline change for ARP handling.
    
    LPort Dispatcher Table(17) -> ARP Check Table(43) -> ARPResponder Group
    -> ELAN Base Table(48), ARP Responder Table(81), Controller
    -> Egress Dispatcher Tabel (220)
    
    Change-Id: I8ebbe05c37f5e7e4c5ae1dd1d17faa12965d5e65
    Depends-On: I2dce5e253d3c94180dd18e51125c5c18170b5c69
    Signed-off-by: Achuth Maniyedath <achuth.m@altencalsoftlabs.com>
    Signed-off-by: Vijayalakshmi Chickkamenahalli Nagaraju <vijayalakshmi.c@altencalsoftlabs.com>
    Signed-off-by: karthik.p <karthik.p@altencalsoftlabs.com>
    Signed-off-by: Achuth Maniyedath <achuth.m@altencalsoftlabs.com>

--- vpnservice/elanmanager/elanmanager-api/src/main/java/org/opendaylight/netvirt/elanmanager/api/ElanHelper.java
+++ vpnservice/elanmanager/elanmanager-api/src/main/java/org/opendaylight/netvirt/elanmanager/api/ElanHelper.java
@@ -7,15 +7,70 @@
  */
 package org.opendaylight.netvirt.elanmanager.api;
 
+import java.math.BigInteger;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
+import org.opendaylight.genius.mdsalutil.MetaDataUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.ElanDpnInterfaces;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.ElanInstances;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.dpn.interfaces.ElanDpnInterfacesList;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.dpn.interfaces.ElanDpnInterfacesListKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.instances.ElanInstance;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.instances.ElanInstanceKey;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ElanHelper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ElanHelper.class);
+
+    private ElanHelper() {
+        throw new AssertionError(ElanHelper.class.getName() + " cannot be initialized.");
+    }
+
     public static InstanceIdentifier<ElanInstance> getElanInstanceConfigurationDataPath(String elanInstanceName) {
         return InstanceIdentifier.builder(ElanInstances.class)
                 .child(ElanInstance.class, new ElanInstanceKey(elanInstanceName)).build();
+    }
+
+    public static BigInteger getElanMetadataLabel(long elanTag) {
+        return MetaDataUtil.getElanTagMetadata(elanTag);
+    }
+
+    public static BigInteger getElanMetadataLabel(long elanTag, int lportTag) {
+        return getElanMetadataLabel(elanTag).or(MetaDataUtil.getLportTagMetaData(lportTag));
+    }
+
+    public static BigInteger getElanMetadataMask() {
+        return MetaDataUtil.METADATA_MASK_SERVICE.or(MetaDataUtil.METADATA_MASK_LPORT_TAG);
+    }
+
+    public static List<String> getDpnInterfacesInElanInstance(DataBroker broker, String elanInstanceName) {
+
+        InstanceIdentifier<ElanDpnInterfacesList> elanDpnInterfaceId = getElanDpnOperationDataPath(elanInstanceName);
+        try {
+            ElanDpnInterfacesList existingElanDpnInterfaces = SingleTransactionDataBroker.syncRead(broker,
+                    LogicalDatastoreType.OPERATIONAL, elanDpnInterfaceId);
+            if (existingElanDpnInterfaces != null) {
+                return existingElanDpnInterfaces.getDpnInterfaces().stream().flatMap(v -> v.getInterfaces().stream())
+                        .collect(Collectors.toList());
+            }
+        } catch (ReadFailedException e) {
+            LOG.warn("Failed to read ElanDpnInterfacesList with error {}", e.getMessage());
+        }
+        return Collections.emptyList();
+    }
+
+    public static InstanceIdentifier<ElanDpnInterfacesList> getElanDpnOperationDataPath(String elanInstanceName) {
+        return InstanceIdentifier.builder(ElanDpnInterfaces.class)
+                .child(ElanDpnInterfacesList.class, new ElanDpnInterfacesListKey(elanInstanceName)).build();
+
     }
 }
