90922bea2083df4dece5346709ad400b020f5363
eupakir <kiran.n.upadhyaya@ericsson.com>
2017-05-12 19:59:39 +0530

    iRT/eRT changes for L3VPN Over VxLAN
    *First patchset to change existing iRT/eRT code to work with the newly
    defined data structures.
    
    Change-Id: I186031f1fa1546fe0aaa39e499942fe5bcf53e89
    Signed-off-by: eupakir <kiran.n.upadhyaya@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/populator/impl/L3vpnOverMplsGrePopulator.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/populator/impl/L3vpnOverMplsGrePopulator.java
@@ -73,9 +73,9 @@ public class L3vpnOverMplsGrePopulator extends L3vpnPopulator {
         // and internalVpnForExtraRoute (where rd is DpnId)
         if (VpnUtil.isEligibleForBgp(rd, input.getVpnName(), input.getDpnId(), input.getNetworkName())) {
             // the DpnId is set as rd in case of extra routes present in router based VPN
-            vpnInterfaceManager.addToLabelMapper(label, input.getDpnId(), nextHopIpAddress,
-                    Arrays.asList(nextHopIp), vpnId, input.getInterfaceName(), null,false,
-                    primaryRd);
+            vpnInterfaceManager.addToIpPrefixInfo(primaryRd, nextHopIpAddress, vpnName,
+                    Collections.singletonList(nextHopIp), vpnId, false /*isSubnetRoute*/, input.getDpnId(),
+                    input.getInterfaceName(), writeOperTxn);
             Objects.requireNonNull(input.getRouteOrigin(), "RouteOrigin is mandatory");
             addPrefixToBGP(rd, primaryRd, null /*macAddress*/, nextHopIpAddress, nextHopIp, encapType,
                     label, 0 /*l3vni*/, input.getGatewayMac(), input.getRouteOrigin(), writeConfigTxn);
@@ -86,8 +86,8 @@ public class L3vpnOverMplsGrePopulator extends L3vpnPopulator {
                     LOG.debug("Exporting route with rd {} prefix {} nexthop {} label {} to VPN {}", vpnRd,
                             nextHopIpAddress, nextHopIp, label, vpn);
                     fibManager.addOrUpdateFibEntry(broker, vpnRd, null /*macAddress*/,
-                            nextHopIpAddress, Arrays.asList(nextHopIp), encapType, (int) label,
-                            0 /*l3vni*/, input.getGatewayMac(), null /*parentVpnRd*/, RouteOrigin.SELF_IMPORTED,
+                            nextHopIpAddress, Collections.singletonList(nextHopIp), encapType, (int) label,
+                            0 /*l3vni*/, input.getGatewayMac(), primaryRd, RouteOrigin.SELF_IMPORTED,
                             writeConfigTxn);
                 }
             }
@@ -95,7 +95,7 @@ public class L3vpnOverMplsGrePopulator extends L3vpnPopulator {
             // ### add FIB route directly
             fibManager.addOrUpdateFibEntry(broker, vpnName, null /*macAddress*/,
                     nextHopIpAddress, Arrays.asList(nextHopIp), encapType, (int) label,
-                    0 /*l3vni*/, input.getGatewayMac(), null /*parentVpnRd*/, input.getRouteOrigin(), writeConfigTxn);
+                    0 /*l3vni*/, input.getGatewayMac(), primaryRd, input.getRouteOrigin(), writeConfigTxn);
         }
     }
 
