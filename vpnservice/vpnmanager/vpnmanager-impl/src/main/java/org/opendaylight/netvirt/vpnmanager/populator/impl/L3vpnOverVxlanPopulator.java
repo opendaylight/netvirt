94b5461292af93129d8ab9ba15f7d1d671323480
eupakir <kiran.n.upadhyaya@ericsson.com>
2017-05-12 19:59:39 +0530

    iRT/eRT changes for L3VPN Over VxLAN
    *First patchset to change existing iRT/eRT code to work with the newly
    defined data structures.
    
    Change-Id: I186031f1fa1546fe0aaa39e499942fe5bcf53e89
    Signed-off-by: eupakir <kiran.n.upadhyaya@ericsson.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/populator/impl/L3vpnOverVxlanPopulator.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/populator/impl/L3vpnOverVxlanPopulator.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.netvirt.vpnmanager.populator.impl;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -23,6 +24,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev15033
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.adjacency.list.Adjacency;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.adjacency.list.AdjacencyBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.adjacency.list.AdjacencyKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.op.data.VpnInstanceOpDataEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -54,8 +56,28 @@ public class L3vpnOverVxlanPopulator extends L3vpnPopulator {
         String rd = input.getRd();
         String primaryRd = input.getPrimaryRd();
         Adjacency nextHop = input.getNextHop();
+        String nextHopIp = input.getNextHopIp();
+        String nextHopIpAddress = nextHop.getIpAddress();
+        long l3vni = input.getL3vni();
+        String vpnName = input.getVpnName();
+        long vpnId = VpnUtil.getVpnId(broker, vpnName);
         LOG.info("populateFib : Found Interface Adjacency with prefix {} rd {}", nextHop.getIpAddress(), primaryRd);
-        if (!rd.equalsIgnoreCase(input.getVpnName()) && !rd.equals(input.getNetworkName())) {
+        if (!rd.equalsIgnoreCase(vpnName) && !rd.equals(input.getNetworkName())) {
+            vpnInterfaceManager.addToIpPrefixInfo(primaryRd, nextHopIpAddress, vpnName,
+                    Collections.singletonList(nextHopIp), vpnId, false /*isSubnetRoute*/, input.getDpnId(),
+                    input.getInterfaceName(), writeOperTxn);
+            List<VpnInstanceOpDataEntry> vpnsToImportRoute = vpnInterfaceManager.getVpnsImportingMyRoute(vpnName);
+            for (VpnInstanceOpDataEntry vpn : vpnsToImportRoute) {
+                String vpnRd = vpn.getVrfId();
+                if (vpnRd != null) {
+                    LOG.debug("Exporting route with rd {} prefix {} nexthop {} l3vni {} to VPN {}", vpnRd,
+                            nextHopIpAddress, nextHopIp, l3vni, vpn);
+                    fibManager.addOrUpdateFibEntry(broker, vpnRd, null /*macAddress*/,
+                            nextHopIpAddress, Collections.singletonList(nextHopIp), input.getEncapType(), 0 /*label*/,
+                            l3vni, input.getGatewayMac(), primaryRd, RouteOrigin.SELF_IMPORTED,
+                            writeConfigTxn);
+                }
+            }
             Objects.requireNonNull(input.getRouteOrigin(), "RouteOrigin is mandatory");
             addPrefixToBGP(rd, primaryRd, nextHop.getMacAddress(), nextHop.getIpAddress(), input.getNextHopIp(),
                     input.getEncapType(), 0 /*label*/, input.getL3vni(), input.getGatewayMac(),
