6e6aeed52efb422ffc7e6164a9b7309def05e712
Philippe Guibert <philippe.guibert@6wind.com>
2017-05-19 11:14:53 +0200

    natservice: fallback default rule implementation
    This utility function is used by external networks to create an IPv6
    fallback rule for external VPN.
    This api should support the creation and destruction of fallback rule.
    
    Change-Id: Ib197b8ba293cae4b9cc3c9732c812e0922056644
    Signed-off-by: Noel de Prandières <prandieres@6wind.com>
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatUtil.java
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatUtil.java
@@ -1909,4 +1909,10 @@ public class NatUtil {
         }
         return false;
     }
+
+    public static String getIPv6FlowRefL3(BigInteger dpnId, short tableId, /*InetAddress destPrefix,*/ long vpnId) {
+        return "L3." + dpnId.toString() + NwConstants.FLOWID_SEPARATOR + tableId
+                + NwConstants.FLOWID_SEPARATOR + vpnId;
+    }
+
 }
