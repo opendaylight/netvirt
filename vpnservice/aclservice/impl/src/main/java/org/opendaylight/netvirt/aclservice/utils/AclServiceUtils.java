d6088fd0e69821231cd1befabf914de53cf2f770
eupakir <kiran.n.upadhyaya@ericsson.com>
2017-05-23 20:20:01 +0530

    Bug 8398 - communication between two tenant network failed while using default SG (stateful mode)
    This fixes the remote ACL indirection flows for the case where a
    router/bgpvpn is involved in communication. It enables the dispatcher
    tables to write vpnId to the metadata so that Remote ACL flows can
    correctly match and send the packet to the intended destination.
    
    The fix makes use of the vpn footprint service to program ACL flows for
    every host addition/deletion.
    
    Depends-On: b3e21d27e8f593b4ca326dd93239db38e1ead4cd
    Change-Id: I1cbee44f951a75d4bbb382fed4e882162016c946
    Signed-off-by: eupakir <kiran.n.upadhyaya@ericsson.com>
    Signed-off-by: Vivekanandan Narasimhan <n.vivekanandan@ericsson.com>

--- vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclServiceUtils.java
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclServiceUtils.java
@@ -977,11 +977,17 @@ public final class AclServiceUtils {
                 AclConstants.PacketHandlingType.DENY));
     }
 
-    public static List<? extends MatchInfoBase> buildIpAndElanSrcMatch(long elanTag, AllowedAddressPairs ip,
-            DataBroker dataBroker) {
+    public static List<? extends MatchInfoBase> buildIpAndSrcServiceMatch(long elanTag, AllowedAddressPairs ip,
+            DataBroker dataBroker, Long vpnId) {
         List<MatchInfoBase> flowMatches = new ArrayList<>();
-        MatchMetadata metadatMatch =
-                new MatchMetadata(MetaDataUtil.getElanTagMetadata(elanTag), MetaDataUtil.METADATA_MASK_SERVICE);
+        MatchMetadata metadatMatch = null;
+        if (vpnId == null) {
+            metadatMatch =
+                    new MatchMetadata(MetaDataUtil.getElanTagMetadata(elanTag), MetaDataUtil.METADATA_MASK_SERVICE);
+        } else {
+            metadatMatch =
+                    new MatchMetadata(MetaDataUtil.getVpnIdMetadata(vpnId), MetaDataUtil.METADATA_MASK_VRFID);
+        }
         flowMatches.add(metadatMatch);
         if (ip.getIpAddress().getIpAddress() != null) {
             if (ip.getIpAddress().getIpAddress().getIpv4Address() != null) {
@@ -1013,11 +1019,17 @@ public final class AclServiceUtils {
         return flowMatches;
     }
 
-    public static List<? extends MatchInfoBase> buildIpAndElanDstMatch(Long elanTag, AllowedAddressPairs ip,
-            DataBroker dataBroker) {
+    public static List<? extends MatchInfoBase> buildIpAndDstServiceMatch(Long elanTag, AllowedAddressPairs ip,
+            DataBroker dataBroker, Long vpnId) {
         List<MatchInfoBase> flowMatches = new ArrayList<>();
-        MatchMetadata metadatMatch =
-                new MatchMetadata(MetaDataUtil.getElanTagMetadata(elanTag), MetaDataUtil.METADATA_MASK_SERVICE);
+        MatchMetadata metadatMatch = null;
+        if (vpnId == null) {
+            metadatMatch =
+                    new MatchMetadata(MetaDataUtil.getElanTagMetadata(elanTag), MetaDataUtil.METADATA_MASK_SERVICE);
+        } else {
+            metadatMatch =
+                    new MatchMetadata(MetaDataUtil.getVpnIdMetadata(vpnId), MetaDataUtil.METADATA_MASK_VRFID);
+        }
         flowMatches.add(metadatMatch);
 
         if (ip.getIpAddress().getIpAddress() != null) {
@@ -1068,5 +1080,14 @@ public final class AclServiceUtils {
             Class<? extends ServiceModeBase> serviceMode) {
         MatchInfoBase lportMatch = buildLPortTagMatch(lportTag, serviceMode);
         InterfaceServiceUtil.mergeMetadataMatchsOrAdd(flowMatches, lportMatch);
+    }
+
+    static AclInterface buildAclInterfaceState(String interfaceId, InterfaceAcl aclInPort) {
+        AclInterface aclInterface = new AclInterface();
+        aclInterface.setInterfaceId(interfaceId);
+        aclInterface.setPortSecurityEnabled(aclInPort.isPortSecurityEnabled());
+        aclInterface.setSecurityGroups(aclInPort.getSecurityGroups());
+        aclInterface.setAllowedAddressPairs(aclInPort.getAllowedAddressPairs());
+        return aclInterface;
     }
 }
