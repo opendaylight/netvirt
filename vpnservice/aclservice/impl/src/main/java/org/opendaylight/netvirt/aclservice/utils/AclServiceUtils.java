3f6c7fe28fdef20706ff3ae73c41f06bb9d2a35e
Philippe Guibert <philippe.guibert@6wind.com>
2017-05-24 15:34:48 +0100

    vpnmanager: vpn-interfaces yang container has vpn-instance-name list
    In order to handle internal VPN or BGPVPN in a two router case solution,
    some yang changes is needed.
    Config DS is being added a list of vpn-instance configuration for one
    VPN interface. The need is to have two vpn instances configurable for one VPN.
    This commit change only handles the compilation by adapting java code
    to as to care about the first element of vpn-instance.
    
    Change-Id: I87a4919899b0c702267977e5942ff8589f77de55
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclServiceUtils.java
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclServiceUtils.java
@@ -806,7 +806,8 @@ public final class AclServiceUtils {
     public static Long getVpnIdFromInterface(DataBroker broker, String vpnInterfaceName) {
         VpnInterface vpnInterface = VpnHelper.getVpnInterface(broker, vpnInterfaceName);
         if (vpnInterface != null) {
-            return VpnHelper.getVpnId(broker, vpnInterface.getVpnInstanceName());
+            return VpnHelper.getVpnId(broker,
+                    VpnHelper.getFirstVpnNameFromVpnInterface(vpnInterface));
         }
         return null;
     }
