fba0581e83ef2bbbf83f3ab98e9585a8e5bec3d3
Philippe Guibert <philippe.guibert@6wind.com>
2017-05-24 15:34:48 +0100

    vpnmanager: vpn-interfaces yang container has vpn-instance-name list
    In order to handle internal VPN or BGPVPN in a two router case solution,
    some yang changes is needed.
    Config DS is being added a list of vpn-instance configuration for one
    VPN interface. The need is to have two vpn instances configurable for one VPN.
    This commit change only handles the compilation by adapting java code
    to as to care about the first element of vpn-instance.
    
    Change-Id: I87a4919899b0c702267977e5942ff8589f77de55
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/ArpNotificationHandler.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/ArpNotificationHandler.java
@@ -212,7 +212,7 @@ public class ArpNotificationHandler implements OdlArputilListener {
                 Adjacencies aug = VpnUtil.getVpnInterfaceAugmentation(adjacencyList);
                 VpnInterface newVpnIntf =
                         new VpnInterfaceBuilder().setKey(new VpnInterfaceKey(vpnInterface))
-                        .setName(vpnInterface).setVpnInstanceName(vpnName).addAugmentation(Adjacencies.class, aug)
+                        .setName(vpnInterface).addAugmentation(Adjacencies.class, aug)
                         .build();
                 VpnUtil.syncUpdate(dataBroker, LogicalDatastoreType.CONFIGURATION, vpnIfId, newVpnIntf);
                 LOG.debug(" Successfully stored subnetroute Adjacency into VpnInterface {}", vpnInterface);
@@ -250,7 +250,7 @@ public class ArpNotificationHandler implements OdlArputilListener {
                     Adjacencies aug = VpnUtil.getVpnInterfaceAugmentation(adjacencyList);
                     VpnInterface newVpnIntf =
                         new VpnInterfaceBuilder().setKey(new VpnInterfaceKey(vpnInterface)).setName(
-                            vpnInterface).setVpnInstanceName(vpnName).addAugmentation(Adjacencies.class, aug).build();
+                            vpnInterface).addAugmentation(Adjacencies.class, aug).build();
                     VpnUtil.syncUpdate(dataBroker, LogicalDatastoreType.CONFIGURATION, vpnIfId, newVpnIntf);
                     LOG.debug(" Successfully stored subnetroute Adjacency into VpnInterface {}", vpnInterface);
                 }
