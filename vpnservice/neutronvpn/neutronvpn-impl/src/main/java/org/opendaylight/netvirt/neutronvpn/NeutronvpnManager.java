5938916c23270c002c4808e1335951c10de880df
Philippe Guibert <philippe.guibert@6wind.com>
2017-05-24 15:34:48 +0100

    vpnmanager: yang modify vpn-interfaces container. vpn-instance-name is a list
    In order to handle internal VPN or BGPVPN in a two router case solution,
    some yang changes is needed.
    In a dual router case solution, one vpn Instance and two vpn Interfaces
    will lead to create one structure for config DS and two structures for
    oper DS.
    Especially, config DS is being added a list of vpn-instance-name.
    The need is to have two vpn instances configurable for one VPN.
    This commit change only handles the compilation by adapting java code
    to as to care about the first element of vpn-instance.
    
    Change-Id: I87a4919899b0c702267977e5942ff8589f77de55
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnManager.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnManager.java
@@ -796,7 +796,7 @@ public class NeutronvpnManager implements NeutronvpnService, AutoCloseable, Even
                     .CONFIGURATION, vpnIfIdentifier);
             if (optionalVpnInterface.isPresent()) {
                 VpnInterfaceBuilder vpnIfBuilder = new VpnInterfaceBuilder(optionalVpnInterface.get())
-                        .setVpnInstanceName(vpnId.getValue());
+                                      .setVpnInstanceName(Arrays.asList(vpnId.getValue()));
                 LOG.debug("Updating vpn interface {}", infName);
                 if (!isBeingAssociated) {
                     Adjacencies adjs = vpnIfBuilder.getAugmentation(Adjacencies.class);
@@ -2322,7 +2322,7 @@ public class NeutronvpnManager implements NeutronvpnService, AutoCloseable, Even
         InstanceIdentifier<VpnInterface> vpnIfIdentifier = NeutronvpnUtils.buildVpnInterfaceIdentifier(infName);
         VpnInterfaceBuilder vpnb = new VpnInterfaceBuilder().setKey(new VpnInterfaceKey(infName))
                 .setName(infName)
-                .setVpnInstanceName(vpnId.getValue())
+                .setVpnInstanceName(Arrays.asList(vpnId.getValue()))
                 .setRouterInterface(isRouterInterface);
         if (adjacencies != null) {
             vpnb.addAugmentation(Adjacencies.class, adjacencies);
