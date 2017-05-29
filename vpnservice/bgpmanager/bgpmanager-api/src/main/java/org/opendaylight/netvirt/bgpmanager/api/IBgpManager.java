ade900cc86894f61c1b8786aeefc6bdd9a30b1f5
Philippe Guibert <philippe.guibert@6wind.com>
2017-05-29 10:45:00 +0100

    bgpmanager: change API of bgpmanager to add VRF IPv4 or IPv6
    This change adds a change in IBgpManager API, since it adds afi and safi
    parameters to VRF creation and deletion. This information is propagated
    to BGP so as to create and delete VPN context tables in function of the
    AFI and SAFI settings.
    Instead of passing a layerType value, an AddressFamily type is used.
    AddressFamily is mapped to internal afi/safi values.
    The values are appended or removed to the VRF objects of BGP Manager.
    The VRF object is then written to the config DS.
    Subsequently, the Quagga will be called upon the various changes in the VRF object.
    If the object is added or updated, the Quagga will be called with the appropriate
    afi/safi values. If the object is removed, all the afi/safi values are removed from
    the Quagga. If the object is added, the afi/safi values are added to the quagga.
    
    Change-Id: Ife754ce561f69dbb67fcdf74308b5153cbde53e6
    Signed-off-by: Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>
    Signed-off-by: Noel de Prandieres <prandieres@6wind.com>
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- vpnservice/bgpmanager/bgpmanager-api/src/main/java/org/opendaylight/netvirt/bgpmanager/api/IBgpManager.java
+++ vpnservice/bgpmanager/bgpmanager-api/src/main/java/org/opendaylight/netvirt/bgpmanager/api/IBgpManager.java
@@ -11,15 +11,34 @@ package org.opendaylight.netvirt.bgpmanager.api;
 import java.util.Collection;
 import java.util.List;
 import org.opendaylight.netvirt.fibmanager.api.RouteOrigin;
-import org.opendaylight.yang.gen.v1.urn.ericsson.params.xml.ns.yang.ebgp.rev150901.LayerType;
+import org.opendaylight.yang.gen.v1.urn.ericsson.params.xml.ns.yang.ebgp.rev150901.AddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.vrfentries.VrfEntry;
 
 public interface IBgpManager {
 
-    void addVrf(String rd, Collection<String> importRts, Collection<String> exportRts, LayerType layerType)
+    /** Create one VPN Context per address-family.
+     * VPN contexts apply to MPLS or VXLAN overlays.
+     * Passing IPv4 or IPv6 will create VPN context for MPLS, with IPv4 or IPv6 (or both)
+     * If L2VPN is passed as parameter, then IPv4 EVPN will be set too.
+     * @param rd is the route distinguisher to used for this vrf for the VPN.
+     * @param importRts the import rd(s) for this vrf
+     * @param exportRts the export rd(s) for this vrf
+     * @param addressFamily  is used to pass the nature of the VPN context : IPv4, IPv6, or EVPN.
+     * @throws Exception an exception could be raise (see the implementation method).
+     */
+    void addVrf(String rd, Collection<String> importRts, Collection<String> exportRts, AddressFamily addressFamily)
             throws Exception;
 
-    void deleteVrf(String rd, boolean removeFibTable);
+    /** Delete onv VPN table.
+     * VPN contexts apply to MPLS or VXLAN overlays.
+     * Passing IPv4 or IPv6 will unset VPN context for MPLS, with IPv4 or IPv6 (or both)
+     * If L2VPN is passed as parameter, then IPv4 EVPN will be unset too.
+     *
+     * @param rd the route distinguisher to define the vrf to delete
+     * @param removeFibTable true to remove to fib table
+     * @param addressFamily is used to pass the nature of the VPN context : IPv4, IPv6, or EVPN.
+     */
+    void deleteVrf(String rd, boolean removeFibTable, AddressFamily addressFamily);
 
     /**
      * Adds one or more routes, as many as nexthops provided, in a BGP neighbour. It persists VrfEntry in datastore
@@ -65,3 +84,4 @@ public interface IBgpManager {
 
     void bgpRestarted();
 }
+
