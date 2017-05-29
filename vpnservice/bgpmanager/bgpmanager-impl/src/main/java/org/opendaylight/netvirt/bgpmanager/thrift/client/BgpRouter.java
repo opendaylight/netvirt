0a87ca36884cce0328b01467dfb4000a7513aed8
Philippe Guibert <philippe.guibert@6wind.com>
2017-05-29 10:45:00 +0100

    bgpmanager: change API of bgpmanager to add VRF IPv4 or IPv6
    This change adds a change in IBgpManager API, since it adds afi and safi
    parameters to VRF creation and deletion. This information is propagated
    to BGP so as to create and delete VPN context tables in function of the
    AFI and SAFI settings.
    Instead of passing a layerType value, an AddressFamily type is used.
    AddressFamily is mapped to internal afi/safi values.
    The values are appended or removed to the VRF objects of BGP Manager.
    The VRF object is then written to the config DS.
    Subsequently, the Quagga will be called upon the various changes in the VRF object.
    If the object is added or updated, the Quagga will be called with the appropriate
    afi/safi values. If the object is removed, all the afi/safi values are removed from
    the Quagga. If the object is added, the afi/safi values are added to the quagga.
    
    Change-Id: Ife754ce561f69dbb67fcdf74308b5153cbde53e6
    Signed-off-by: Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>
    Signed-off-by: Noel de Prandieres <prandieres@6wind.com>
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- vpnservice/bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/netvirt/bgpmanager/thrift/client/BgpRouter.java
+++ vpnservice/bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/netvirt/bgpmanager/thrift/client/BgpRouter.java
@@ -96,6 +96,7 @@ public class BgpRouter {
         public String routermac;
         public af_afi afi;
         public int delayEOR;
+        public af_safi safi;
 
         BgpOp() {
             strs = new String[3];
@@ -318,14 +319,16 @@ public class BgpRouter {
         dispatch(bop);
     }
 
-    public synchronized void addVrf(LayerType layerType, String rd, List<String> irts, List<String> erts)
-            throws TException, BgpRouterException {
+    public synchronized void addVrf(LayerType layerType, String rd, List<String> irts, List<String> erts,
+            long afi, long safi) throws TException, BgpRouterException {
         bop.thriftLayerType = layerType == LayerType.LAYER2 ? layer_type.LAYER_2 : layer_type.LAYER_3;
         bop.type = Optype.VRF;
         bop.add = true;
         bop.strs[0] = rd;
         bop.irts = irts;
         bop.erts = erts;
+        bop.afi = af_afi.findByValue((int)afi);
+        bop.safi = af_safi.findByValue((int)safi);
         LOG.debug("Adding BGP VRF rd: {} ", rd);
         dispatch(bop);
     }
