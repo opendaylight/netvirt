73bfededbebf08e9f4fb97026c4c0bcd87e1dae9
ehvkand <hanamantagoud.v.kandagal@ericsson.com>
2017-05-30 14:18:47 +0530

    VrfEntryListener refactoring using chain of responsibility pattern
    This is a first review where BGP Route handling is separated into a new
    handler class BgpRouteVrfEntryHandler.
    
    As part of chain of responsibility pattern , VRF Entry will go through the
    chain of handlers which are interested in processing it.
    
    Subsequent dependent patches will contain handlers for subnetroute ,
    import-export route , inter-vpn link route etc
    
    Change-Id: Idd57572fd697113eaa504fe65ab5096220d91b97
    Signed-off-by: ehvkand <hanamantagoud.v.kandagal@ericsson.com>

---
+++ vpnservice/fibmanager/fibmanager-impl/src/main/java/org/opendaylight/netvirt/fibmanager/VrfEntryHandler.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.fibmanager;
+
+
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.genius.utils.batching.SubTransaction;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.vrfentries.VrfEntry;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public interface VrfEntryHandler {
+    /*VrfEntry processing status.
+    * VRF_PROCESSING_COMPLETED - means no more handlers are required to process the VRF entry.
+    * VRF_PROCESSING_CONTINUE - means next VRF Handler can process the VRF entry if required.
+    * */
+    short VRF_PROCESSING_COMPLETED = 1;
+    short VRF_PROCESSING_CONTINUE = 2;
+
+    void subscribeWithVrfListener();
+
+    void unSubscribeWithVrfListener();
+
+    boolean wantToProcessVrfEntry(VrfEntry vrfEntry);
+
+    String getHandlerType();
+
+    short createFlows(InstanceIdentifier<VrfEntry> identifier, VrfEntry vrfEntry, String rd);
+
+    short updateFlows(InstanceIdentifier<VrfEntry> identifier, VrfEntry original, VrfEntry update, String rd);
+
+    short removeFlows(InstanceIdentifier<VrfEntry> identifier, VrfEntry vrfEntry, String rd);
+
+    void createRemoteFibEntry(BigInteger remoteDpnId, long vpnId, String rd,
+                               VrfEntry vrfEntry, WriteTransaction tx, List<SubTransaction> txnObjects);
+}
