aebc62ed4bce90135619fca07efd87943cbdc330
Shashidhar Raja <shashidharr@altencalsoftlabs.com>
2017-06-07 18:46:17 +0530

    WIP: Bug 8398 - communication between two tenant network failed while using default SG (stateful mode)
    This commit fixes issues observed in
    https://git.opendaylight.org/gerrit/#/c/57705/
    
    Depends-On: 0c01eacb8100ee5a224d52e44362fce98bd875f5
    Change-Id: I827e99835c36509bad5b48f03034103d377102f8
    Signed-off-by: Shashidhar Raja <shashidharr@altencalsoftlabs.com>

--- vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclServiceUtilFacade.java
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclServiceUtilFacade.java
@@ -8,58 +8,26 @@
 
 package org.opendaylight.netvirt.aclservice.utils;
 
-import com.google.common.base.Optional;
-import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.genius.mdsalutil.MatchInfoBase;
-import org.opendaylight.netvirt.aclservice.api.AclServiceManager;
-import org.opendaylight.netvirt.aclservice.api.utils.AclInterface;
 import org.opendaylight.netvirt.aclservice.api.utils.IAclServiceUtil;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.Matches;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.Interface;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.InterfaceAcl;
 
 @Singleton
 public class AclServiceUtilFacade implements IAclServiceUtil {
-    private final AclServiceManager aclServiceManager;
-    private final DataBroker broker;
 
     @Inject
-    public AclServiceUtilFacade(DataBroker broker, AclServiceManager aclServiceManager) {
-        this.broker = broker;
-        this.aclServiceManager = aclServiceManager;
+    public AclServiceUtilFacade() {
     }
 
     @Override
     public Map<String, List<MatchInfoBase>> programIpFlow(Matches matches) {
         return AclServiceOFFlowBuilder.programIpFlow(matches);
-    }
-
-    @Override
-    public void updateBoundServicesFlow(String interfaceName, Long vpnId) {
-        Optional<Interface> intfc = AclServiceUtils.getInterface(broker, interfaceName);
-        InterfaceAcl aclInPort = intfc.get().getAugmentation(InterfaceAcl.class);
-        if (aclInPort != null && aclInPort.isPortSecurityEnabled()) {
-            aclServiceManager.bindAclTableForVpn(AclServiceUtils.buildAclInterfaceState(interfaceName, aclInPort),
-                    vpnId);
-        }
-    }
-
-    @Override
-    public void updateRemoteAclFilterTable(String interfaceName, Long vpnId, BigInteger dpnId, int addOrDelete) {
-        Optional<Interface> intfc = AclServiceUtils.getInterface(broker, interfaceName);
-        InterfaceAcl aclInPort = intfc.get().getAugmentation(InterfaceAcl.class);
-        if (aclInPort != null && aclInPort.isPortSecurityEnabled()) {
-            AclInterface aclInterface = AclServiceUtils.buildAclInterfaceState(interfaceName, aclInPort);
-            aclInterface.setDpId(dpnId);
-            aclServiceManager.updateRemoteAclFilterTable(aclInterface, addOrDelete, vpnId);
-        }
     }
 
 }
