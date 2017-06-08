014ec176809fb4032574c3ce4a986bcec9273de3
Miguel Perez <francisco.miguel.perez@ericsson.com>
2017-06-08 15:41:56 +0200

    Bug 8374: NPE observed on fib-show command
     + Havent been able to reproduce the NPE, but did a bit of
       refactoring with special care about possible NPEs
    
     + First of all, it's not VrfEntryListener's responsibility to
       print anything on screen. This logic's been moved to the
       fib-show command implementation.
    
     + I took the change to remove dependency from 'shell' to 'impl'
    
     + L3VPNTransportTypes defines types that are used in 'shell', so I
       moved it from 'impl' to 'api'.
    
     + Also moved the dependency to 'itm-api' from 'impl' to 'api'
    
    Change-Id: Ie792c40cf61af0bcefb83be9b0d6c5fcb6fb755c
    Signed-off-by: Miguel Perez <francisco.miguel.perez@ericsson.com>

--- vpnservice/fibmanager/fibmanager-api/src/main/java/org/opendaylight/netvirt/fibmanager/api/L3VPNTransportTypes.java
+++ vpnservice/fibmanager/fibmanager-api/src/main/java/org/opendaylight/netvirt/fibmanager/api/L3VPNTransportTypes.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.netvirt.fibmanager;
+package org.opendaylight.netvirt.fibmanager.api;
 
 import java.util.HashMap;
 import java.util.Map;
