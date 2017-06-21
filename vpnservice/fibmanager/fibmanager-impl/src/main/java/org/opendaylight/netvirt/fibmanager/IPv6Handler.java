219dcbe7727236c0b0362664b2c91991314f97c8
ehvkand <hanamantagoud.v.kandagal@ericsson.com>
2017-06-21 18:32:24 +0530

    VrfEntryListener refactoring using chain of responsibility pattern
    This is a secod review where RouteInterface handling is separated into a
    new handler class RouterInterfaceVrfEntryHandler.
    
    Change-Id: If4bc6ded02e23dc12f6217a7c412ac5cc1188f50
    Signed-off-by: ehvkand <hanamantagoud.v.kandagal@ericsson.com>

---
+++ vpnservice/fibmanager/fibmanager-impl/src/main/java/org/opendaylight/netvirt/fibmanager/IPv6Handler.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright © 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.fibmanager;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.FlowEntity;
+import org.opendaylight.genius.mdsalutil.InstructionInfo;
+import org.opendaylight.genius.mdsalutil.MDSALUtil;
+import org.opendaylight.genius.mdsalutil.MatchInfo;
+import org.opendaylight.genius.mdsalutil.MetaDataUtil;
+import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.genius.mdsalutil.actions.ActionMoveSourceDestinationEth;
+import org.opendaylight.genius.mdsalutil.actions.ActionMoveSourceDestinationIpv6;
+import org.opendaylight.genius.mdsalutil.actions.ActionNxLoadInPort;
+import org.opendaylight.genius.mdsalutil.actions.ActionNxResubmit;
+import org.opendaylight.genius.mdsalutil.actions.ActionSetFieldEthernetSource;
+import org.opendaylight.genius.mdsalutil.actions.ActionSetIcmpv6Type;
+import org.opendaylight.genius.mdsalutil.actions.ActionSetSourceIpv6;
+import org.opendaylight.genius.mdsalutil.instructions.InstructionApplyActions;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.genius.mdsalutil.matches.MatchEthernetType;
+import org.opendaylight.genius.mdsalutil.matches.MatchIcmpv6;
+import org.opendaylight.genius.mdsalutil.matches.MatchIpProtocol;
+import org.opendaylight.genius.mdsalutil.matches.MatchIpv6Destination;
+import org.opendaylight.genius.mdsalutil.matches.MatchMetadata;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv6Prefix;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+@Singleton
+public class IPv6Handler {
+    private static final Logger LOG = LoggerFactory.getLogger(IPv6Handler.class);
+    private final DataBroker dataBroker;
+    private final IMdsalApiManager mdsalManager;
+
+
+    @Inject
+    public IPv6Handler(final DataBroker dataBroker,
+                       final IMdsalApiManager mdsalManager) {
+        this.dataBroker = dataBroker;
+        this.mdsalManager = mdsalManager;
+    }
+
+    public void installPing6ResponderFlowEntry(BigInteger dpnId, long vpnId, String routerInternalIp,
+                                               MacAddress routerMac, long label, int addOrRemove) {
+
+        List<MatchInfo> matches = new ArrayList<>();
+        matches.add(MatchIpProtocol.ICMPV6);
+        matches.add(new MatchMetadata(MetaDataUtil.getVpnIdMetadata(vpnId), MetaDataUtil.METADATA_MASK_VRFID));
+        matches.add(new MatchIcmpv6((short) 128, (short) 0));
+        matches.add(MatchEthernetType.IPV6);
+        matches.add(new MatchIpv6Destination(routerInternalIp));
+
+        List<ActionInfo> actionsInfos = new ArrayList<>();
+        // Set Eth Src and Eth Dst
+        actionsInfos.add(new ActionMoveSourceDestinationEth());
+        actionsInfos.add(new ActionSetFieldEthernetSource(routerMac));
+
+        // Move Ipv6 Src to Ipv6 Dst
+        actionsInfos.add(new ActionMoveSourceDestinationIpv6());
+        actionsInfos.add(new ActionSetSourceIpv6(new Ipv6Prefix(routerInternalIp)));
+
+        // Set the ICMPv6 type to 129 (echo reply)
+        actionsInfos.add(new ActionSetIcmpv6Type((short) 129));
+        actionsInfos.add(new ActionNxLoadInPort(BigInteger.ZERO));
+        actionsInfos.add(new ActionNxResubmit(NwConstants.L3_FIB_TABLE));
+        List<InstructionInfo> instructions = new ArrayList<>();
+        instructions.add(new InstructionApplyActions(actionsInfos));
+
+        int priority = FibConstants.DEFAULT_FIB_FLOW_PRIORITY + FibConstants.DEFAULT_IPV6_PREFIX_LENGTH;
+        String flowRef = FibUtil.getFlowRef(dpnId, NwConstants.L3_FIB_TABLE, label, priority);
+
+        FlowEntity flowEntity = MDSALUtil.buildFlowEntity(dpnId, NwConstants.L3_FIB_TABLE, flowRef, priority, flowRef,
+                0, 0, NwConstants.COOKIE_VM_FIB_TABLE, matches, instructions);
+
+        if (addOrRemove == NwConstants.ADD_FLOW) {
+            mdsalManager.installFlow(flowEntity);
+        } else {
+            mdsalManager.removeFlow(flowEntity);
+        }
+    }
+
+}
