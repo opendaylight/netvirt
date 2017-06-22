bcf6ee67a72c3c88fbe360ff5fe9bdb8a2fbc9f6
Philippe Guibert <philippe.guibert@6wind.com>
2017-06-22 15:29:18 +0100

    cloud-service-chain: update VpnToPseudoPortData with VpnInterfaces
    Because VpnInterface can support more than one VPN, when a new VPN
    interface is created or removed, the list of its VPN should be parsed
    against the VpnToPseudoPortData contexts.
    
    Change-Id: I016cd2d4c5b7aa43940367eb6c5771eae8f0d2a9
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- vpnservice/cloud-servicechain/cloud-servicechain-impl/src/main/java/org/opendaylight/netvirt/cloudservicechain/listeners/CloudScVpnInterfaceListener.java
+++ vpnservice/cloud-servicechain/cloud-servicechain-impl/src/main/java/org/opendaylight/netvirt/cloudservicechain/listeners/CloudScVpnInterfaceListener.java
@@ -58,13 +58,18 @@ public class CloudScVpnInterfaceListener
 
     @Override
     protected void remove(InstanceIdentifier<VpnInterface> key, VpnInterface vpnIfaceRemoved) {
-        String vpnName = vpnIfaceRemoved.getVpnRouterIds().get(0);
-        Optional<VpnToPseudoPortData> optScfInfoForVpn = vpnScHandler.getScfInfoForVpn(vpnName);
-        if (!optScfInfoForVpn.isPresent()) {
-            LOG.trace("Vpn {} is not related to ServiceChaining. No further action", vpnName);
-            return;
+        Optional<VpnToPseudoPortData> optScfInfoForVpn;
+        for (String vpnName : vpnIfaceRemoved.getVpnRouterIds().get()) {
+        {
+            optScfInfoForVpn = vpnScHandler.getScfInfoForVpn(vpnName);
+            if (!optScfInfoForVpn.isPresent()) {
+                LOG.trace("Vpn {} is not related to ServiceChaining.", vpnName);
+                continue;
+            }
         }
-
+        if (!optScfInfoForVpn.isPresent()) {
+            LOG.trace("No Vpn related to ServiceChaining. No further action");
+        }
         vpnScHandler.unbindScfOnVpnInterface(vpnIfaceRemoved.getKey().getName());
     }
 
@@ -75,14 +80,14 @@ public class CloudScVpnInterfaceListener
 
     @Override
     protected void add(InstanceIdentifier<VpnInterface> key, VpnInterface vpnIfaceAdded) {
-        String vpnName = vpnIfaceAdded.getVpnRouterIds().get(0);
-        Optional<VpnToPseudoPortData> optScfInfoForVpn = vpnScHandler.getScfInfoForVpn(vpnName);
-        if (!optScfInfoForVpn.isPresent()) {
-            LOG.trace("Vpn {} is not related to ServiceChaining. No further action", vpnName);
-            return;
+        for (String vpnName :vpnIfaceAdded.getVpnRouterIds()) {
+            Optional<VpnToPseudoPortData> optScfInfoForVpn = vpnScHandler.getScfInfoForVpn(vpnName);
+            if (!optScfInfoForVpn.isPresent()) {
+                LOG.trace("Vpn {} is not related to ServiceChaining. No further action", vpnName);
+                return;
+            }
+            vpnScHandler.bindScfOnVpnInterface(vpnIfaceAdded.getKey().getName(), optScfInfoForVpn.get().getScfTag());
         }
-
-        vpnScHandler.bindScfOnVpnInterface(vpnIfaceAdded.getKey().getName(), optScfInfoForVpn.get().getScfTag());
     }
 
 }
