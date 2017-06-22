0add2347bb7ef47e2d6e2d19b79a07497cfa4802
Philippe Guibert <philippe.guibert@6wind.com>
2017-06-22 14:35:42 +0100

    vpnservice: various changes to support vpn-interface-op-data-entry
    - natservice: misc nat changes while using vpn-interface-op-data-entry
    On some cases where a VM interface is connected to two routers ( case
    with dual stack IPs), then the router which has nat enabled is searched.
    The changes parse the list of router per VPN interface, and return the
    first router which has NAT enabled.
    Obviously, that kind of setup may have only one router with NAT enabled,
    at maximum.
    On EVPN flow programmation, on removing floating IPs, the vpnInterfaces
    operational is removed too.
    On Interface state change, the vpnInterfaces will be updated, added or
    removed.
    
    - cloud-service-chain: update VpnToPseudoPortData with VpnInterfaces
    Because VpnInterface can support more than one VPN, when a new VPN
    interface is created or removed, the list of its VPN should be parsed
    against the VpnToPseudoPortData contexts.
    
    - fib-manager: update operational Adjacencies by using vpnInterfaceOpDataEntry
    This commit deletes the adjacenciesOp entry related to a vrfEntry, in
    vpnInstanceOpDataEntry. The list of vpnRouterIds() is parsed in config
    DS vpnInstance, to check is there is a vpnInstanceOpDataEntry existing.
    Then if present, the associated adjacenciesOp is deleted.
    
    The commit introduces a new function called getAdjacencyIdentifierOp().
    It replaces getAdjacencyIdentifier, and will search for adjacencies that
    are relying on vpninterfaceopdataentry.
    
    Change-Id: I5a676b15dfdb415064a2e9daba5f4c7b4d584fe9
    Signed-off-by: Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Signed-off-by: Noel de Prandières <prandieres@6wind.com>

--- vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatUtil.java
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatUtil.java
@@ -100,6 +100,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.Pre
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.VpnIdToVpnInstance;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.VpnInstanceOpData;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.VpnInstanceToVpnId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.VpnInterfaceOpData;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.dpn.routers.DpnRoutersList;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.dpn.routers.DpnRoutersListBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.dpn.routers.DpnRoutersListKey;
@@ -119,6 +120,8 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.neu
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.prefix.to._interface.vpn.ids.Prefixes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.prefix.to._interface.vpn.ids.PrefixesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.prefix.to._interface.vpn.ids.PrefixesKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn._interface.op.data.VpnInterfaceOpDataEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn._interface.op.data.VpnInterfaceOpDataEntryKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.id.to.vpn.instance.VpnIds;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.id.to.vpn.instance.VpnIdsKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.op.data.VpnInstanceOpDataEntry;
@@ -1845,6 +1848,18 @@ public class NatUtil {
 
     public static String validateAndAddNetworkMask(String ipAddress) {
         return ipAddress.contains("/32") ? ipAddress : (ipAddress + "/32");
+    }
+
+    public static InstanceIdentifier<VpnInterfaceOpDataEntry> getVpnInterfaceOpDataEntryIdentifier(
+            String vpnInterfaceName, String vpnName) {
+        return InstanceIdentifier.builder(VpnInterfaceOpData.class).child(VpnInterfaceOpDataEntry.class,
+        new VpnInterfaceOpDataEntryKey(vpnInterfaceName, vpnName)).build();
+    }
+
+    public static VpnInstanceOpDataEntry getVpnInstanceOpData(DataBroker broker, String rd) {
+        InstanceIdentifier<VpnInstanceOpDataEntry> id = NatUtil.getVpnInstanceOpDataIdentifier(rd);
+        return SingleTransactionDataBroker.syncReadOptionalAndTreatReadFailedExceptionAsAbsentOptional(
+                 broker, LogicalDatastoreType.OPERATIONAL, id).orNull();
     }
 
     public static boolean checkForRoutersWithSameExtNetAndNaptSwitch(DataBroker broker, Uuid networkId,
