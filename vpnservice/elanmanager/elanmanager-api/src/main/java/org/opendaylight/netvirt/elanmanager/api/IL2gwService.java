1643b8ad8173bb310bf41b08c82e8fd776ce9009
eaksahu <a.k.sahu@ericsson.com>
2017-07-03 18:21:33 +0530

    Bug 8791 - L2gateway delete is not clearing l2gwCo
    L2gw delete was not clearing associated L2gateway Connection.
    CLearing associated l2gwConnection as part of the review.
    
    Change-Id: Ie8b6a9668c7a943041bb2cc7f20bbcab7e68b51d
    Signed-off-by: eaksahu <a.k.sahu@ericsson.com>

--- vpnservice/elanmanager/elanmanager-api/src/main/java/org/opendaylight/netvirt/elanmanager/api/IL2gwService.java
+++ vpnservice/elanmanager/elanmanager-api/src/main/java/org/opendaylight/netvirt/elanmanager/api/IL2gwService.java
@@ -10,6 +10,10 @@ package org.opendaylight.netvirt.elanmanager.api;
 
 import org.opendaylight.netvirt.neutronvpn.api.l2gw.L2GatewayDevice;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.l2gateways.rev150712.l2gateway.connections.attributes.l2gatewayconnections.L2gatewayConnection;
+
+import java.util.List;
 
 /**
  * Created by eaksahu on 3/15/2017.
@@ -17,4 +21,6 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.
 public interface IL2gwService {
     void provisionItmAndL2gwConnection(L2GatewayDevice l2GwDevice, String psName,
                                               String hwvtepNodeId, IpAddress tunnelIpAddr) ;
+
+    List<L2gatewayConnection> getL2GwConnectionsByL2GatewayId(Uuid l2GatewayId);
 }
