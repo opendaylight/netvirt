abe4398e8de1bd9f819067a5e4917820ef43505c
Moshe Levi <moshele@mellanox.com>
2017-07-12 22:50:17 +0300

    Add hardware offload support for openvswitch
    Handle 'direct' vnic type with 'switchdev' (hw offload) capability
    ports in NeutronPortChangeListner.
    
    Change-Id: I0d2a5aa32b9f909d5cf56a1c220ca2cdcbc80071
    Signed-off-by: Moshe Levi <moshele@mellanox.com>
    Co-Authored-By: Edan David <edand@mellanox.com>
    Depends-On: I65644f32551124c61e30c02831ac4193d12471d2

--- vpnservice/neutronvpn/neutronvpn-api/src/main/java/org/opendaylight/netvirt/neutronvpn/api/utils/NeutronUtils.java
+++ vpnservice/neutronvpn/neutronvpn-api/src/main/java/org/opendaylight/netvirt/neutronvpn/api/utils/NeutronUtils.java
@@ -23,8 +23,13 @@ import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
+import org.opendaylight.ovsdb.utils.mdsal.utils.MdsalUtils;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.binding.rev150712.PortBindingExtension;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.binding.rev150712.binding.attributes.Profile;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.Hostconfigs;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.hostconfigs.Hostconfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.hostconfig.rev150712.hostconfig.attributes.hostconfigs.HostconfigKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.NetworkTypeBase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.networks.attributes.networks.Network;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.Ports;
@@ -42,6 +47,7 @@ public class NeutronUtils {
     private static final Logger LOG = LoggerFactory.getLogger(NeutronUtils.class);
 
     public static final String VNIC_TYPE_NORMAL = "normal";
+    public static final String VNIC_TYPE_DIRECT = "direct";
     public static final String PORT_STATUS_ACTIVE = "ACTIVE";
     public static final String PORT_STATUS_BUILD = "BUILD";
     public static final String PORT_STATUS_DOWN = "DOWN";
@@ -114,6 +120,81 @@ public class NeutronUtils {
         }
 
         return true;
+    }
+
+    public static String getPortHostId(final Port port) {
+        PortBindingExtension portBinding = port.getAugmentation(PortBindingExtension.class);
+        if (portBinding == null) {
+            return null;
+        }
+        return portBinding.getHostId();
+    }
+
+    private static InstanceIdentifier<Hostconfig> hostConfigIid(String hostId) {
+        // TODO: use hostconfig util host type constant
+        return InstanceIdentifier.builder(Neutron.class)
+                .child(Hostconfigs.class)
+                .child(Hostconfig.class, new HostconfigKey(hostId, "ODL L2"))
+                .build();
+    }
+
+    public static boolean isPortBound(final Port port) {
+        String hostId = getPortHostId(port);
+        return hostId != null && !hostId.isEmpty();
+    }
+
+    private static boolean isPortVnicTypeDirect(Port port) {
+        PortBindingExtension portBinding = port.getAugmentation(PortBindingExtension.class);
+        if (portBinding == null || portBinding.getVnicType() == null) {
+            // By default, VNIC_TYPE is NORMAL
+            return false;
+        }
+        String vnicType = portBinding.getVnicType().trim().toLowerCase();
+        return vnicType.equals(VNIC_TYPE_DIRECT);
+    }
+
+    public static boolean isSupportedVnicTypeByHost(final Port port,
+                                                    final String vnicType,
+                                                    final DataBroker dataBroker) {
+        String hostId = getPortHostId(port);
+        // validate that the host supports direct port binding
+        MdsalUtils mdsalUtils = new MdsalUtils(dataBroker);
+        InstanceIdentifier<Hostconfig> hostConfigKey = hostConfigIid(hostId);
+        Hostconfig hostConfig = mdsalUtils.read(LogicalDatastoreType.OPERATIONAL, hostConfigKey);
+        String supportStr = String.format("\"vnic_type\": \"%s\"", vnicType);
+        if (hostConfig != null && hostConfig.getConfig().contains(supportStr)) {
+            return true;
+        }
+        LOG.debug("Hostconfig for host {}: {}\n expected {}",
+                  hostId, hostConfig == null ? "null" : hostConfig.getConfig(), supportStr);
+        return false;
+    }
+
+    public static boolean isPortTypeSwitchdev(final Port port) {
+        // TODO: uncomment when https://git.opendaylight.org/gerrit/#/c/65551/ is merged
+        //       currently the binding profile attribute isn't passed by odl-neutron
+        //       and is treated as a String when openstack defines this attribute as a dictionary
+
+        // if (!NeutronUtils.isPortVnicTypeDirect(port)) {
+        //     return false;
+        // }
+
+        // PortBindingExtension portBinding = port.getAugmentation(PortBindingExtension.class);
+        // List<Profile> profiles = portBinding.getProfile();
+        // if (profiles == null) {
+        //     LOG.debug("Port {} hase no binding:profile values", port.getUuid());
+        //     return false;
+        // }
+
+        // for (Profile profile: profiles) {
+        //     if (profile.getKey().equals("capabilities")
+        //         && profile.getValue().equals("switchdev")) {
+        //             return true;
+        //         }
+        // }
+
+        // return false;
+        return NeutronUtils.isPortVnicTypeDirect(port);
     }
 
     public static boolean isPortVnicTypeNormal(Port port) {
