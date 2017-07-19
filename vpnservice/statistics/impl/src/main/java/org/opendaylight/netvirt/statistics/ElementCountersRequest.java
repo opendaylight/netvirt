c38d5f2cf35fdc75d0a230c9ffe5f31e47959b0c
Stephen Kitt <skitt@redhat.com>
2017-07-19 17:25:19 +0200

    Move statistics into org.opendaylight.netvirt
    Now that the BluePrint plugin only considers org.opendaylight.netvirt,
    we need to move all NetVirt’s packages into that namespace.
    
    Change-Id: I953e9df0a9c650161083239acb1889e529429f1a
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- vpnservice/statistics/impl/src/main/java/org/opendaylight/netvirt/statistics/ElementCountersRequest.java
+++ vpnservice/statistics/impl/src/main/java/org/opendaylight/netvirt/statistics/ElementCountersRequest.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.statistics;
+package org.opendaylight.netvirt.statistics;
 
 import java.math.BigInteger;
 import java.util.HashMap;
