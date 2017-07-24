6c782914e35ca9bb2e41574960ba0a6b12f03109
Stephen Kitt <skitt@redhat.com>
2017-07-24 13:36:16 +0200

    ElanUtils clean-up: make read() static
    It turns out no callers use the variant of read() which relies on
    ElanUtils’ own broker, so we can drop that and make read() static,
    which reduces the requirement for ElanUtils instances.
    
    Change-Id: I75841497b4247abb9f10c216956ba534bc5068ce
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanServiceProvider.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanServiceProvider.java
@@ -412,7 +412,7 @@ public class ElanServiceProvider extends AbstractLifecycle implements IElanServi
     public List<ElanInstance> getElanInstances() {
         InstanceIdentifier<ElanInstances> elanInstancesIdentifier = InstanceIdentifier.builder(ElanInstances.class)
                 .build();
-        return elanUtils.read(broker, LogicalDatastoreType.CONFIGURATION, elanInstancesIdentifier).transform(
+        return ElanUtils.read(broker, LogicalDatastoreType.CONFIGURATION, elanInstancesIdentifier).transform(
                 ElanInstances::getElanInstance).or(Collections.emptyList());
     }
 
@@ -421,7 +421,7 @@ public class ElanServiceProvider extends AbstractLifecycle implements IElanServi
         List<String> elanInterfaces = new ArrayList<>();
         InstanceIdentifier<ElanInterfaces> elanInterfacesIdentifier = InstanceIdentifier.builder(ElanInterfaces.class)
                 .build();
-        Optional<ElanInterfaces> elanInterfacesOptional = elanUtils.read(broker, LogicalDatastoreType.CONFIGURATION,
+        Optional<ElanInterfaces> elanInterfacesOptional = ElanUtils.read(broker, LogicalDatastoreType.CONFIGURATION,
                 elanInterfacesIdentifier);
         if (!elanInterfacesOptional.isPresent()) {
             return elanInterfaces;
