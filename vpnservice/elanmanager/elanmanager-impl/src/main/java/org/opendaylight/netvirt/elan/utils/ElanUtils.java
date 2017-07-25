d8d5091783a7fd4a101d2ecde8bcaf908c17bd82
Stephen Kitt <skitt@redhat.com>
2017-07-25 11:24:12 +0200

    ElanUtils clean-up: L2Gateway{Connection,}Utils
    We can now instantiate L2GatewayConnectionUtils and ElanL2GatewayUtils
    directly, without needing ElanUtils to mediate.
    
    Actually pulling everything apart still requires using interfaces for
    ElanInstanceManager and ElanInterfaceManager, so that they can be
    proxied (there’s still a dependency loop through
    ElanL2GatewayMulticastUtils).
    
    Change-Id: I23caf8f74c7a02df056dc914db9e7e4985230b9d
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/utils/ElanUtils.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/utils/ElanUtils.java
@@ -36,7 +36,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
-import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipService;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
@@ -67,9 +66,6 @@ import org.opendaylight.genius.mdsalutil.packet.IPv4;
 import org.opendaylight.genius.utils.ServiceIndex;
 import org.opendaylight.netvirt.elan.ElanException;
 import org.opendaylight.netvirt.elan.internal.ElanInstanceManager;
-import org.opendaylight.netvirt.elan.l2gw.utils.ElanL2GatewayMulticastUtils;
-import org.opendaylight.netvirt.elan.l2gw.utils.ElanL2GatewayUtils;
-import org.opendaylight.netvirt.elan.l2gw.utils.L2GatewayConnectionUtils;
 import org.opendaylight.netvirt.elanmanager.api.ElanHelper;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddressBuilder;
@@ -193,9 +189,6 @@ public class ElanUtils {
     private final ElanInstanceManager elanInstanceManager;
     private final OdlInterfaceRpcService interfaceManagerRpcService;
     private final ItmRpcService itmRpcService;
-    private final ElanL2GatewayUtils elanL2GatewayUtils;
-    private final ElanL2GatewayMulticastUtils elanL2GatewayMulticastUtils;
-    private final L2GatewayConnectionUtils l2GatewayConnectionUtils;
     private final IInterfaceManager interfaceManager;
     private final ElanConfig elanConfig;
     private final ElanItmUtils elanItmUtils;
@@ -216,10 +209,8 @@ public class ElanUtils {
     @Inject
     public ElanUtils(DataBroker dataBroker, IMdsalApiManager mdsalManager, ElanInstanceManager elanInstanceManager,
                      OdlInterfaceRpcService interfaceManagerRpcService, ItmRpcService itmRpcService,
-                     ElanConfig elanConfig,
-                     EntityOwnershipService entityOwnershipService, IInterfaceManager interfaceManager,
-                     ElanL2GatewayMulticastUtils elanL2GatewayMulticastUtils, ElanEtreeUtils elanEtreeUtils,
-                     ElanItmUtils elanItmUtils, ElanDmacUtils elanDmacUtils) {
+                     ElanConfig elanConfig, IInterfaceManager interfaceManager, ElanEtreeUtils elanEtreeUtils,
+                     ElanItmUtils elanItmUtils) {
         this.broker = dataBroker;
         this.mdsalManager = mdsalManager;
         this.elanInstanceManager = elanInstanceManager;
@@ -227,30 +218,8 @@ public class ElanUtils {
         this.itmRpcService = itmRpcService;
         this.interfaceManager = interfaceManager;
         this.elanConfig = elanConfig;
-
-        this.elanL2GatewayMulticastUtils = elanL2GatewayMulticastUtils;
         this.elanEtreeUtils = elanEtreeUtils;
         this.elanItmUtils = elanItmUtils;
-        this.elanL2GatewayUtils =
-                new ElanL2GatewayUtils(broker, this, elanDmacUtils, elanItmUtils, entityOwnershipService);
-        this.l2GatewayConnectionUtils = new L2GatewayConnectionUtils(broker,
-                elanInstanceManager, entityOwnershipService, this);
-    }
-
-    public void close() {
-        elanL2GatewayUtils.close();
-    }
-
-    public ElanL2GatewayUtils getElanL2GatewayUtils() {
-        return elanL2GatewayUtils;
-    }
-
-    public ElanL2GatewayMulticastUtils getElanL2GatewayMulticastUtils() {
-        return elanL2GatewayMulticastUtils;
-    }
-
-    public L2GatewayConnectionUtils getL2GatewayConnectionUtils() {
-        return l2GatewayConnectionUtils;
     }
 
     public final Boolean isOpenStackVniSemanticsEnforced() {
