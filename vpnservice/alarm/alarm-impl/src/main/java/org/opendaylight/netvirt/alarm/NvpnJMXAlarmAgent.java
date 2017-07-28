735ac051d562dbc414e396a4fcb7558a5f846a98
Philippe Guibert <philippe.guibert@6wind.com>
2017-07-28 15:58:37 +0100

    neutronvpn: send alarm upon extra route configuration fault using BGPVPN
    When extra routes are configured, through a router, if the number of
    nexthops is greater than the number of available RDs, then an alarm and
    an error is generated.
    
    Change-Id: Ib78b1ff82a9a2013ec2f81f28c0e0261ef971177
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Signed-off-by: Noel de Prandières <prandieres@6wind.com>

---
+++ vpnservice/alarm/alarm-impl/src/main/java/org/opendaylight/netvirt/alarm/NvpnJMXAlarmAgent.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2017 6WIND, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.alarm;
+
+import java.lang.management.ManagementFactory;
+
+import javax.management.InstanceNotFoundException;
+import javax.management.ListenerNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+
+import jline.internal.Log;
+
+public class NvpnJMXAlarmAgent {
+
+    private static final String BEANNAME = "SDNC.FM:name=NeutronvpnControlPathAlarmBean";
+
+    public static final String OP_RAISEALARM = "raiseAlarm";
+    public static final String OP_CLEARALARM = "clearAlarm";
+
+    public MBeanServer mbs = null;
+    private ObjectName alarmName = null;
+    private NvpnNbrControlPathAlarm alarmBean = new NvpnNbrControlPathAlarm();
+
+    public NvpnJMXAlarmAgent() {
+        mbs = ManagementFactory.getPlatformMBeanServer();
+        try {
+            alarmName = new ObjectName(BEANNAME);
+        } catch (MalformedObjectNameException e) {
+            Log.info("ObjectName instance creation failed for BEANAME " + BEANNAME + " ," + e);
+        }
+    }
+
+    public void invokeFMclearmethod(String alarmId, String text, String src, String details) {
+        try {
+            mbs.invoke(alarmName, OP_CLEARALARM, new Object[] {alarmId, text, src, details}, new String[] {
+                    String.class.getName(), String.class.getName(), String.class.getName(), String.class.getName()});
+            Log.debug("Invoked clearAlarm function for Mbean {} with source {}", BEANNAME, src, details);
+        } catch (InstanceNotFoundException | MBeanException | ReflectionException e) {
+            Log.info("Invoking clearAlarm method failed for Mbean {} :{}", alarmName, e);
+        }
+    }
+
+    public void invokeFMraisemethod(String alarmId, String text, String src, String details) {
+        try {
+            mbs.invoke(alarmName, OP_RAISEALARM, new Object[] {alarmId, text, src, details}, new String[] {
+                    String.class.getName(), String.class.getName(), String.class.getName(), String.class.getName()});
+            Log.debug("Invoked raiseAlarm function for Mbean {} with source {} for details {}",BEANNAME, src , details);
+        } catch (InstanceNotFoundException | MBeanException | ReflectionException e) {
+            Log.info("Invoking raiseAlarm method failed for Mbean {} :{}", alarmName, e);
+        }
+    }
+
+    public void registerMbean() {
+        try {
+            if (!mbs.isRegistered(alarmName)) {
+                mbs.registerMBean(alarmBean, alarmName);
+                Log.debug("Registered Mbean {} successfully", alarmName);
+            }
+        } catch (javax.management.InstanceAlreadyExistsException | javax.management.MBeanRegistrationException
+                | javax.management.NotCompliantMBeanException e) {
+            Log.info("Registeration failed for Mbean " + alarmName + " : " + e);
+        }
+        try {
+            mbs.addNotificationListener(/*alarmName*/alarmName, alarmBean, null, null);
+        } catch (InstanceNotFoundException e2) {
+            Log.info("Registeration of listener failed for Mbean " + alarmName + " err=> " + e2);
+        }
+    }
+
+    public void unregisterMbean() {
+        try {
+            mbs.removeNotificationListener(alarmName, alarmBean);
+        } catch (InstanceNotFoundException | ListenerNotFoundException e1) {
+            Log.info("unregisteration of listener failed for Mbean " + alarmName + " err=> " + e1);
+        }
+        try {
+            if (mbs.isRegistered(alarmName)) {
+                mbs.unregisterMBean(alarmName);
+                Log.debug("Unregistered Mbean {} successfully", alarmName);
+            }
+        } catch (InstanceNotFoundException | MBeanRegistrationException e2) {
+            Log.info("UnRegisteration failed for Mbean {} :{}", alarmName, e2);
+        }
+    }
+
+    public NvpnNbrControlPathAlarm getAlarmBean() {
+        return alarmBean;
+    }
+
+
+}
---
+++ vpnservice/alarm/alarm-impl/src/test/java/org/opendaylight/netvirt/alarm/test/AlarmTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2017 6WIND, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.alarm.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class AlarmTest {
+
+    private final NeutronvpnAlarmsTest neutronvpnAlarmTest = new NeutronvpnAlarmsTest();
+    private Uuid vpnId = new Uuid("ffaa8822-2222-3333-4444-ffffffffffff");
+
+    private String typeAlarm = "for vpnId: " + vpnId + " have exceeded next hops for prefixe";
+    private String detailsAlarm = "this is Test details to raise an alarm";
+    private String newLine = System.getProperty("line.separator");
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testNeutronvpnMoreNextHopThanRD() {
+
+        /***********************Step to raise alarm*/
+
+        neutronvpnAlarmTest.raiseNvpnNbrDownAlarm(typeAlarm, detailsAlarm);
+        neutronvpnAlarmTest.raiseNvpnNbrDownAlarm(typeAlarm + "_Second", detailsAlarm);
+        long startTime = System.currentTimeMillis();
+
+        while (startTime + 5000 > System.currentTimeMillis()) {
+            int nbreAlarm = neutronvpnAlarmTest.getAlarmAgent().getAlarmBean().getRaiseAlarmObjectMapSize();
+            if (nbreAlarm > 0) {
+                break;
+            } else {
+                pause(10L);
+            }
+        }
+
+
+        String messageError = "testNeutronvpnMoreNextHopThanRD: NeutronvpnAlarmTest.getAlarmAgent().alarmBean."
+                + "raiseAlarmObjectMap.size() = " + neutronvpnAlarmTest.getAlarmAgent().getAlarmBean()
+                .getRaiseAlarmObjectMapSize() + newLine;
+
+        Map<String, List<String>> map = neutronvpnAlarmTest.getAlarmAgent().getAlarmBean().getRaiseAlarmObjectMapCopy();
+        messageError += getDetailForMap(map);
+
+        assertEquals(newLine + messageError + "waiting 1 entry => "
+                + messageError + " this is the first method running", 2, map.size());
+
+
+
+
+        /***********************Step to clear alarm*/
+        messageError = newLine;
+
+        neutronvpnAlarmTest.clearNvpnNbrDownAlarm(typeAlarm, detailsAlarm);
+
+        neutronvpnAlarmTest.clearNvpnNbrDownAlarm(typeAlarm + "_Second", detailsAlarm);
+
+        startTime = System.currentTimeMillis();
+        while (startTime + 5000 > System.currentTimeMillis()) {
+            int nbreAlarm = neutronvpnAlarmTest.getAlarmAgent().getAlarmBean().getRaiseAlarmObjectMapSize();
+            if (nbreAlarm == 0) {
+                break;
+            } else {
+                pause(10L);
+            }
+        }
+
+        messageError += "testNeutronvpnClearMoreNextHopThanRD: NeutronvpnAlarmTest.getAlarmAgent().alarmBean."
+                + "raiseAlarmObjectMap.size() = " + neutronvpnAlarmTest.getAlarmAgent().getAlarmBean()
+                .getRaiseAlarmObjectMapSize() + newLine;
+
+        map = neutronvpnAlarmTest.getAlarmAgent().getAlarmBean().getRaiseAlarmObjectMapCopy();
+        messageError += getDetailForMap(map);
+
+        int size = map.size();
+        assertEquals(newLine + messageError + "waiting for 0 entry => this is the second method running", 0, size);
+    }
+
+    private void pause(long milliSec) {
+        try {
+            Thread.sleep(milliSec);
+        } catch (InterruptedException e) {
+            return;
+        }
+    }
+
+    private String getDetailForMap(Map<String, List<String>>  map) {
+        String res = "";
+
+        res += "================================================" + newLine;
+        for (Entry<String, List<String>> entry : map.entrySet()) {
+            res += "Key : " + entry.getKey() + newLine;
+            if (entry.getValue() != null) {
+                res += "Values : ";
+                List<String> list = entry.getValue();
+                for (int i = 0; i < list.size(); i++) {
+                    res += "value " + i + ", " + list.get(i)  + newLine;
+                }
+            }
+            res += "================================================" + newLine;
+        }
+        return res;
+    }
+}
