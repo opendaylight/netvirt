724af6767e0fb3c4abcd6908342d4a150b4fbec6
Philippe Guibert <philippe.guibert@6wind.com>
2017-07-28 15:58:37 +0100

    neutronvpn: send alarm upon extra route configuration fault using BGPVPN
    When extra routes are configured, through a router, if the number of
    nexthops is greater than the number of available RDs, then an alarm and
    an error is generated.
    
    Change-Id: Ib78b1ff82a9a2013ec2f81f28c0e0261ef971177
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Signed-off-by: Noel de Prandières <prandieres@6wind.com>

---
+++ vpnservice/alarm/alarm-impl/src/main/java/org/opendaylight/netvirt/alarm/NeutronvpnAlarms.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2017 6WIND, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.alarm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NeutronvpnAlarms {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NeutronvpnAlarms.class);
+    protected NvpnJMXAlarmAgent alarmAgent = new NvpnJMXAlarmAgent();
+    private static final String ALARM_TEXT = "Neutronvpn alarm";
+    private static final String ALARM_ID = "NeutronvpnControlPathFailure";
+    private static final String SOURCE_START = "Neutronvpn_type=";
+    private static final String CUSTOM_PROPERTY = "vpnservice.bgpspeaker.host.name";
+
+    public NeutronvpnAlarms() {
+        alarmAgent.registerMbean();
+    }
+
+    public void unregisterMbean() {
+        alarmAgent.unregisterMbean();
+    }
+
+    /**
+     * method to raise an alarm.
+     *
+     * @param typeAlarm the type of alarm as String
+     * @param detailsAlarm the details just to log more informations
+     */
+    public void raiseNvpnNbrDownAlarm(String typeAlarm, String detailsAlarm) {
+        if (!isAlarmEnabled() || typeAlarm == null || typeAlarm.isEmpty()) {
+            return;
+        }
+        StringBuilder source = new StringBuilder().append(SOURCE_START).append(typeAlarm);
+        LOG.trace("Raising neutronvpnControlPathFailure alarm. {} alarmtext {} ", source, ALARM_TEXT);
+        alarmAgent.invokeFMraisemethod(ALARM_ID, ALARM_TEXT, source.toString(), detailsAlarm);
+    }
+
+    /**
+     * method to clear an alarm from its typeAlarm.
+     *
+     * @param typeAlarm the type of alarm as String
+     * @param detailsAlarm the details just to log more information
+     */
+    public void clearNvpnNbrDownAlarm(String typeAlarm, String detailsAlarm) {
+        if (!isAlarmEnabled() || typeAlarm == null || typeAlarm.isEmpty()) {
+            return;
+        }
+        StringBuilder source = new StringBuilder().append(SOURCE_START).append(typeAlarm);
+        LOG.trace("Clearing NeutronvpnControlPathFailure alarm of source {} alarmtext {} ", source, ALARM_TEXT);
+        alarmAgent.invokeFMclearmethod(ALARM_ID, ALARM_TEXT, source.toString(), detailsAlarm);
+    }
+
+    /**
+     * get if this alarm is activated from a file as custom.properties.
+     *
+     * @return get true if this alarm is activated, false otherwise
+     */
+    public boolean isAlarmEnabled() {
+        final String enabledPropertyStr = System.getProperty(CUSTOM_PROPERTY, "yes");
+        return enabledPropertyStr != null && enabledPropertyStr.equalsIgnoreCase("yes");
+    }
+}
+
---
+++ vpnservice/alarm/alarm-impl/src/test/java/org/opendaylight/netvirt/alarm/test/NeutronvpnAlarmsTest.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017 6WIND, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.alarm.test;
+
+import org.opendaylight.netvirt.alarm.NeutronvpnAlarms;
+import org.opendaylight.netvirt.alarm.NvpnJMXAlarmAgent;
+
+public class NeutronvpnAlarmsTest extends NeutronvpnAlarms {
+
+    /**
+     * method to used only for unit test cases.
+     *
+     * @return the NvpnJMXAlarmAgent registered in MBeanServer
+     */
+    public NvpnJMXAlarmAgent getAlarmAgent() {
+        return alarmAgent;
+    }
+
+}
