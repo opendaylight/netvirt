fffd5f5c8c3b72a145bc4eb716272240faeda8e1
Philippe Guibert <philippe.guibert@6wind.com>
2017-07-28 15:58:37 +0100

    neutronvpn: send alarm upon extra route configuration fault using BGPVPN
    When extra routes are configured, through a router, if the number of
    nexthops is greater than the number of available RDs, then an alarm and
    an error is generated.
    
    Change-Id: Ib78b1ff82a9a2013ec2f81f28c0e0261ef971177
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Signed-off-by: Noel de Prandières <prandieres@6wind.com>

---
+++ vpnservice/alarm/alarm-impl/src/test/java/org/opendaylight/netvirt/alarm/test/AlarmTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2017 6WIND, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.alarm.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class AlarmTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testNeutronvpnMoreNextHopThanRD() {
+        assertEquals(1,1);
+    }
+}
