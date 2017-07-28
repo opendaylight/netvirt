f965beccff5091261856ff89122b227f6479c6b0
Philippe Guibert <philippe.guibert@6wind.com>
2017-07-28 15:58:37 +0100

    neutronvpn: send alarm upon extra route configuration fault using BGPVPN
    When extra routes are configured, through a router, if the number of
    nexthops is greater than the number of available RDs, then an alarm and
    an error is generated.
    
    Change-Id: Ib78b1ff82a9a2013ec2f81f28c0e0261ef971177
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Signed-off-by: Noel de Prandières <prandieres@6wind.com>

---
+++ vpnservice/alarm/alarm-impl/src/test/java/org/opendaylight/netvirt/alarm/test/AlarmTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2017 6WIND, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.alarm.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class AlarmTest {
+
+    private final NeutronvpnAlarmsTest neutronvpnAlarmTest = new NeutronvpnAlarmsTest();
+    private Uuid vpnId = new Uuid("ffaa8822-2222-3333-4444-ffffffffffff");
+
+    private String typeAlarm = "for vpnId: " + vpnId + " have exceeded next hops for prefixe";
+    private String detailsAlarm = "this is Test details to raise an alarm";
+    private String newLine = System.getProperty("line.separator");
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testNeutronvpnMoreNextHopThanRD() {
+
+        neutronvpnAlarmTest.raiseNvpnNbrDownAlarm(typeAlarm, detailsAlarm);
+        long startTime = System.currentTimeMillis();
+
+        while (startTime + 5000 > System.currentTimeMillis()) {
+            int nbreAlarm = neutronvpnAlarmTest.getAlarmAgent().getAlarmBean().getRaiseAlarmObjectMapSize();
+            if (nbreAlarm > 0) {
+                break;
+            } else {
+                pause(10L);
+            }
+        }
+
+
+        String messageError = "neutronvpnAlarmTest.getAlarmAgent().alarmBean.raiseAlarmObjectMap.size() = "
+                + neutronvpnAlarmTest.getAlarmAgent().getAlarmBean().getRaiseAlarmObjectMapSize() + newLine;
+
+        Map<String, List<String>> map = neutronvpnAlarmTest.getAlarmAgent().getAlarmBean().getRaiseAlarmObjectMapCopy();
+        for (Entry<String, List<String>> entry : map.entrySet()) {
+            messageError += "Key : " + entry.getKey() + ",,, Value : " + entry.getValue().size() + newLine;
+        }
+
+        assertEquals("waiting 1 entry => " + messageError + " this is the first method running", 1, map.size());
+    }
+
+    private void pause(long milliSec) {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            return;
+        }
+    }
+
+    @Test
+    public void testNeutronvpnClearMoreNextHopThanRD() {
+
+        neutronvpnAlarmTest.clearNvpnNbrDownAlarm(typeAlarm, detailsAlarm);
+
+        long startTime = System.currentTimeMillis();
+        while (startTime + 5000 > System.currentTimeMillis()) {
+            int nbreAlarm = neutronvpnAlarmTest.getAlarmAgent().getAlarmBean().getRaiseAlarmObjectMapSize();
+            if (nbreAlarm == 0) {
+                break;
+            } else {
+                pause(10L);
+            }
+        }
+
+        String messageError = "neutronvpnAlarmTest.getAlarmAgent().alarmBean.raiseAlarmObjectMap.size() = "
+                + neutronvpnAlarmTest.getAlarmAgent().getAlarmBean().getRaiseAlarmObjectMapSize() + newLine;
+
+        Map<String, List<String>> map = neutronvpnAlarmTest.getAlarmAgent().getAlarmBean().getRaiseAlarmObjectMapCopy();
+        for (Entry<String, List<String>> entry : map.entrySet()) {
+            messageError += "Key : " + entry.getKey() + ",,, Value : " + entry.getValue().size() + newLine;
+        }
+
+        assertEquals("waiting for 0 entry => this is the second method running",0, map.size());
+    }
+}
