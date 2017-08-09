44f9ee02b275bf26f7cf8c27bc647c73b4a7fc3b
eaksahu <a.k.sahu@ericsson.com>
2017-08-09 19:52:09 +0530

    Bug 8965 - L2gw update is not suported
    Currently l2gw once created , no update are on that are supported.
    With this patch following use case has been supported.
    Update existing l2gw vlanbindings
    Update existing l2gw with new ports / device .
    
    It takes care of configuring w.r.t associated l2gwconnection .
    
    Change-Id: I37dda9cf366b1fe59ccf95f64f0574448f156d47
    Signed-off-by: eaksahu <a.k.sahu@ericsson.com>

--- vpnservice/elanmanager/elanmanager-api/src/main/java/org/opendaylight/netvirt/elanmanager/api/IL2gwService.java
+++ vpnservice/elanmanager/elanmanager-api/src/main/java/org/opendaylight/netvirt/elanmanager/api/IL2gwService.java
@@ -9,14 +9,26 @@
 package org.opendaylight.netvirt.elanmanager.api;
 
 import java.util.List;
+import java.util.Set;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.netvirt.neutronvpn.api.l2gw.L2GatewayDevice;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.l2gateways.rev150712.l2gateway.connections.attributes.l2gatewayconnections.L2gatewayConnection;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.l2gateways.rev150712.l2gateways.attributes.l2gateways.L2gateway;
 
 public interface IL2gwService {
     void provisionItmAndL2gwConnection(L2GatewayDevice l2GwDevice, String psName,
-                                              String hwvtepNodeId, IpAddress tunnelIpAddr) ;
+                                       String hwvtepNodeId, IpAddress tunnelIpAddr);
 
     List<L2gatewayConnection> getL2GwConnectionsByL2GatewayId(Uuid l2GatewayId);
+
+    void addL2GatewayConnection(L2gatewayConnection input);
+
+    void addL2GatewayConnection(L2gatewayConnection input,
+                                String l2GwDeviceName,
+                                L2gateway l2Gateway);
+
+    List<L2gatewayConnection> getAssociatedL2GwConnections(DataBroker broker, Set<Uuid> l2GatewayIds);
 }
