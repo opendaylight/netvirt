aee7d0f517dbd96ebd9b9201fe5bcfbab06cc8a1
Abhinav Gupta <abhinav.gupta@ericsson.com>
2017-08-11 17:31:42 +0530

    Bug 8979: Logging enhancements for VPNService
    This fixes logging issues related to non-parametrization of logs, printing
    of exceptions, changing log levels for a few logs.
    These changes have been made across NeutronVPN primarily, and also a bit
    for VPNManager and FIBManager
    
    Change-Id: Ice4b206ada6cc591f96ef75eccd820683b21a647
    Signed-off-by: Abhinav Gupta <abhinav.gupta@ericsson.com>

--- vpnservice/neutronvpn/neutronvpn-shell/src/main/java/org/opendaylight/netvirt/neutronvpn/shell/ShowSubnet.java
+++ vpnservice/neutronvpn/neutronvpn-shell/src/main/java/org/opendaylight/netvirt/neutronvpn/shell/ShowSubnet.java
@@ -103,7 +103,7 @@ public class ShowSubnet extends OsgiCommandSupport {
             }
         } catch (Exception e) {
             System.out.println("Error fetching data for given subnetId ");
-            LOG.error("Error Fetching Data",e);
+            LOG.error("Error Fetching Data", e);
         }
 
         return null;
