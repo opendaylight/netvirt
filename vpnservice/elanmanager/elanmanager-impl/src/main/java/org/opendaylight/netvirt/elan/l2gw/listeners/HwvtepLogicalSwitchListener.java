57d216bd016787e93fecb63f76cda53b9106ba91
eaksahu <a.k.sahu@ericsson.com>
2017-08-16 18:03:48 +0530

    Bug 8998 - L2GW:Vlan bindings missing on reboot
    Vlan bindings are missin upon cluster reboot.
    Because of the async dependecy of PhysicalSwitch Listner and
    HAOPNodeListners .
    Fixed by running job when both Opnode and Physical Switch Node are
    available.
    Also fixed the use case if the Global nodes gets connected without any
    Tunnel IP for ITM configuration.
    Updating the cache from all the time to on all listners
    
    Change-Id: Id6fdbbfe852a6fc0d89f360eb498fece8f00bdb5
    Signed-off-by: eaksahu <a.k.sahu@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/listeners/HwvtepLogicalSwitchListener.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/listeners/HwvtepLogicalSwitchListener.java
@@ -152,7 +152,8 @@ public class HwvtepLogicalSwitchListener extends
                 logicalSwitchNew);
         try {
             L2GatewayDevice elanDevice = L2GatewayConnectionUtils.addL2DeviceToElanL2GwCache(broker,
-                    logicalSwitchNew.getHwvtepNodeName().getValue(), l2GatewayDevice, l2GwConnId,physicalDevice);
+                    logicalSwitchNew.getHwvtepNodeName().getValue(), elanUtils,
+                    l2GatewayDevice, l2GwConnId,physicalDevice);
 
             LogicalSwitchAddedJob logicalSwitchAddedWorker = new LogicalSwitchAddedJob(broker, elanL2GatewayUtils,
                     elanUtils, elanL2GatewayMulticastUtils, logicalSwitchName, physicalDevice, elanDevice,
