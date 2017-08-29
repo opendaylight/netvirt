a34f0a8e40702a2c1befe0aa9305cd71cb6c0c3e
Arun Sharma <arun.e.sharma@ericsson.com>
2017-08-29 14:11:49 +0530

    Cluster support for netvirt QoS
    -Changed QoS policy configuration change listeners from
     AsyncDataTreeChangeListenerBase to
     AsyncClusteredDataTreeChangeListenerBase so that all nodes receive
     QoS configuration change notifications.
    
    -Rate limit and DSCP marking is done only by QoS entity owner. Other
     nodes just update the cache. This ensures that cache is always in
     sync between all the nodes.
    
    Change-Id: Id2215e4b98cfcb238c437402a02ece640db11413
    Signed-off-by: Arun Sharma <arun.e.sharma@ericsson.com>

--- vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosConstants.java
+++ vpnservice/qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosConstants.java
@@ -16,5 +16,5 @@ public class QosConstants {
     public static final String ORG_OPS4J_PAX_LOGGING = "org.ops4j.pax.logging";
     public static final String FELIX_FILEINSTALL_FILENAME = "felix.fileinstall.filename";
     public static final String SERVICE_PID = "service.pid";
-    public static final String QOS_ALERT_OWNER_ENTITY_TYPE = "netvirt-qosalert-owner-entity";
+    public static final String QOS_ALERT_OWNER_ENTITY_TYPE = "netvirt-qos-owner-entity";
 }
