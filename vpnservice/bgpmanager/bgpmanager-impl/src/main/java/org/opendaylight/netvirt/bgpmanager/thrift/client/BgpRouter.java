568d861523d443662226424c4d510540aba2e40b
Vyshakh Krishnan CH <vyshakh.krishnan.c.h@ericsson.com>
2017-08-31 11:24:17 +0530

    Bug 9012 : BGP not connecting to config server
    Issue was there with previos commit where config_server_updated flag was
    never set to false. Making it to false at correct place now
    
    Change-Id: I5c49bf02dac8cb66033eb8a38d453ac0922cbca9
    Signed-off-by: Vyshakh Krishnan CH <vyshakh.krishnan.c.h@ericsson.com>

--- vpnservice/bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/netvirt/bgpmanager/thrift/client/BgpRouter.java
+++ vpnservice/bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/netvirt/bgpmanager/thrift/client/BgpRouter.java
@@ -129,6 +129,7 @@ public class BgpRouter {
 
         final int numberOfConnectRetries = 180;
         RetryOnException connectRetry = new RetryOnException(numberOfConnectRetries);
+        BgpConfigurationManager.config_server_updated = false;
 
         disconnect();
         setConnectTS(System.currentTimeMillis());
