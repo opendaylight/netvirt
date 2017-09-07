dd2529d66f4c677baa01a79a82dda8905e112485
Sam Hague <shague@redhat.com>
2017-09-07 10:39:44 -0400

    Cleanup cherry-pick of 61888
    Change-Id: I475dc18033f85e41414cc9b8c62b27145d015e00
    Signed-off-by: Sam Hague <shague@redhat.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/SubnetRouteInterfaceStateChangeListener.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/SubnetRouteInterfaceStateChangeListener.java
@@ -16,6 +16,7 @@ import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.datastoreutils.AsyncDataTreeChangeListenerBase;
 import org.opendaylight.genius.datastoreutils.DataStoreJobCoordinator;
+import org.opendaylight.netvirt.neutronvpn.api.utils.NeutronUtils;
 import org.opendaylight.netvirt.neutronvpn.interfaces.INeutronVpnManager;
 import org.opendaylight.netvirt.vpnmanager.utilities.InterfaceUtils;
 import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.vpn.interfaces.VpnInterface;
@@ -223,8 +224,8 @@ public class SubnetRouteInterfaceStateChangeListener extends AsyncDataTreeChange
 
     protected Uuid getSubnetId(Interface intrf) {
 
-        if (!VpnUtil.isUuidValidPattern(intrf.getName())) {
-            LOG.error("SubnetRouteInterfaceListener: Interface {} doesnt have valid uuid pattern", intrf.getName());
+        if (!NeutronUtils.isUuid(intrf.getName())) {
+            LOG.debug("SubnetRouteInterfaceListener: Interface {} doesnt have valid uuid pattern", intrf.getName());
             return null;
         }
 
