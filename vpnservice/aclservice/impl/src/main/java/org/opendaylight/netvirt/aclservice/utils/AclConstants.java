6bc65cef4308d2e3651694a129613bb35341754b
vinothb <vinothb@hcl.com>
2017-09-13 19:10:00 -0400

    Bug 8400 - ACL changes doesn't affect the existing connections
    Problem:
        Change in SG rules does not affect the existing traffic
    
    Fix provided:
        * "ct_mark" flag of conntrack module will be used to identify the
    change made in acl rules of specific instance.
        * When a Acl rule removed from the instance, Netvirt will reset the
    value of "ct_mark" flag value. Reset of ct_mark value will skip the further
    packets of existing traffic to not be matched by conntrack flow and these
    packets will be dropped by the DROP flow.
    
    Change-Id: If55070a7810b580af1cb922abde2880cf434b371
    Signed-off-by: vinothb <vinothb@hcl.com>

--- vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclConstants.java
+++ vpnservice/aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/utils/AclConstants.java
@@ -55,11 +55,17 @@ public final class AclConstants {
     public static final int TRACKED_REL_CT_STATE = 0x24;
     public static final int TRACKED_NEW_CT_STATE = 0x21;
     public static final int TRACKED_INV_CT_STATE = 0x30;
+    public static final int TRACKED_RPL_CT_STATE = 0x20;
 
     public static final int TRACKED_EST_CT_STATE_MASK = 0x37;
     public static final int TRACKED_REL_CT_STATE_MASK = 0x37;
     public static final int TRACKED_NEW_CT_STATE_MASK = 0x21;
     public static final int TRACKED_INV_CT_STATE_MASK = 0x30;
+    public static final int TRACKED_RPL_CT_STATE_MASK = 0x28;
+
+    public static final int CT_MARK_NEW_STATE = 0x0;
+    public static final int CT_MARK_EST_STATE = 0x1;
+    public static final int CT_MARK_EST_STATE_MASK = 0x1;
 
     public static final String IPV4_ALL_NETWORK = "0.0.0.0/0";
     public static final String IPV6_ALL_NETWORK = "::/0";
