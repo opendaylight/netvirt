604e6a562558ba815e99c5ce12a5c06aefb17e16
Philippe Guibert <philippe.guibert@6wind.com>
2017-09-15 08:27:27 +0100

    vpnmanager: for each vrf sub family deleted, partial FIB removed
    Upon VRF sub family deletion, associate FIB entries are looked up and
    removed.
    
    Change-Id: I20ad53b9671358d645a1a9715e4d8a667593fe04
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnInstanceListener.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnInstanceListener.java
@@ -366,7 +366,7 @@ public class VpnInstanceListener extends AsyncDataTreeChangeListenerBase<VpnInst
             }
         } else {
             rds.parallelStream().forEach(rd -> {
-                bgpManager.deleteVrf(rd, false, family);
+                bgpManager.deleteVrf(rd, true, family);
             });
         }
         return true;
