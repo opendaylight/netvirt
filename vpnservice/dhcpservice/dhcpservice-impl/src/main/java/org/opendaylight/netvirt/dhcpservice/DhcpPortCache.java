6a5419269effee10e691a8ca280fb8ae6214ffdf
Achuth Maniyedath <achuth.m@altencalsoftlabs.com>
2017-09-23 18:20:14 +0530

    NETVIRT-1033: Adding ArpResponder flows for SR-IOV VMs
    DHCP Neutron Port ARP handling for SR-IOV VMs.
    Handled removal of stale flows entry from ArpResponderTable.
    
    New pipeline for processing Neutron port DHCP ARPs
    Table 0 -> Table 18 -> Table 38 -> Table 81
    
    Change-Id: Id5ee3160240cfcb6caec5790734d9f003c16640e
    Signed-off-by: Achuth Maniyedath <achuth.maniyedath@gmail.com>
    Signed-off-by: Vijayalakshmi Chickkamenahalli Nagaraju <vijayalakshmi.c@altencalsoftlabs.com>

---
+++ vpnservice/dhcpservice/dhcpservice-impl/src/main/java/org/opendaylight/netvirt/dhcpservice/DhcpPortCache.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright © 2015, 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.dhcpservice;
+
+import java.util.concurrent.ConcurrentHashMap;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.ports.Port;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class DhcpPortCache {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DhcpPortCache.class);
+    private final ConcurrentHashMap<String, Port> portMap = new ConcurrentHashMap<String, Port>();
+
+    @PostConstruct
+    public void init() {
+        LOG.trace("Initialize DhcpPortCache. ");
+    }
+
+    public  void put(@Nonnull  String interfaceName, Port port) {
+        portMap.put(interfaceName, port);
+        LOG.trace("Added the interface {} to DhcpPortCache",interfaceName);
+    }
+
+    @Nullable
+    public Port get(@Nonnull String interfaceName) {
+        return portMap.get(interfaceName);
+    }
+
+    public void remove(@Nonnull String interfaceName) {
+        portMap.remove(interfaceName);
+    }
+}
