6cb2c4deb4891bff74321394799d39762df74d3c
HANAMANTAGOUD V Kandagal <hanamantagoud.v.kandagal@ericsson.com>
2017-09-25 18:00:58 +0530

    Bug 9181: Code changes for conflicting modifications exceptions of table=19
    Change-Id: I38d6b12bcec4ade471c99571028265d868a1e3a2
    Signed-off-by: HANAMANTAGOUD V Kandagal <hanamantagoud.v.kandagal@ericsson.com>
    Signed-off-by: ehvkand <hanamantagoud.v.kandagal@ericsson.com>
    Signed-off-by: HANAMANTAGOUD V Kandagal <hanamantagoud.v.kandagal@ericsson.com>

--- vpnservice/fibmanager/fibmanager-impl/src/main/java/org/opendaylight/netvirt/fibmanager/VrfEntryListener.java
+++ vpnservice/fibmanager/fibmanager-impl/src/main/java/org/opendaylight/netvirt/fibmanager/VrfEntryListener.java
@@ -1557,7 +1557,7 @@ public class VrfEntryListener extends AsyncDataTreeChangeListenerBase<VrfEntry, 
                         }
                         RouterInterface routerInt = vrfEntry.getAugmentation(RouterInterface.class);
                         if (routerInt != null) {
-                            LOG.trace("Router augmented vrfentry found rd:{}, uuid:{}, ip:{}, mac:{}",
+                            LOG.info("populateFibOnNewDpn: Router augmented vrfentry found rd:{}, uuid:{}, ip:{}, mac:{}",
                                 rd, routerInt.getUuid(), routerInt.getIpAddress(), routerInt.getMacAddress());
                             routerInterfaceVrfEntryHandler.installRouterFibEntry(vrfEntry, dpnId, vpnId,
                                     routerInt.getIpAddress(), new MacAddress(routerInt.getMacAddress()),
@@ -1735,7 +1735,7 @@ public class VrfEntryListener extends AsyncDataTreeChangeListenerBase<VrfEntry, 
                             // ping responder for router interfaces
                             RouterInterface routerInt = vrfEntry.getAugmentation(RouterInterface.class);
                             if (routerInt != null) {
-                                LOG.trace("Router augmented vrfentry found for rd:{}, uuid:{}, ip:{}, mac:{}",
+                                LOG.trace("cleanUpDpnForVpn: Router augmented vrfentry found for rd:{}, uuid:{}, ip:{}, mac:{}",
                                     rd, routerInt.getUuid(), routerInt.getIpAddress(), routerInt.getMacAddress());
                                 routerInterfaceVrfEntryHandler.installRouterFibEntry(vrfEntry, dpnId, vpnId,
                                         routerInt.getIpAddress(), new MacAddress(routerInt.getMacAddress()),
