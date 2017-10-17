1301fbf60ddde62b770e69815c82742e6996ccb4
Philippe Guibert <philippe.guibert@6wind.com>
2017-10-17 13:45:08 +0100

    vpnmanager: add ipv4 and ipv6 sub-services for L3VPN
    In the case with two router solution, two default VPNids have to be
    chosen, so that outgoing packets can go to the corret VPN. Indeed, the
    solution is chosen to select the VPN IPv6 ( or arbitrarily choose one)
    with the highest priority, on table 17. Then packets reach table 19. If
    there are no matches in table 19, then packet will be resubmitted to
    table 19, where a second rule with a second VPN (IPv4) will be set.
    
    Change-Id: I9981db0308402099901671533eeb25dead26daa8
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnUtils.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnUtils.java
@@ -1381,16 +1381,18 @@ public class NeutronvpnUtils {
 
     public VpnInstanceOpDataEntry getVpnInstanceOpDataEntryFromVpnId(String vpnName) {
         String primaryRd = getVpnRd(vpnName);
+        String index = primaryRd;
         if (primaryRd == null) {
             LOG.error("getVpnInstanceOpDataEntryFromVpnId: Vpn Instance {} "
-                     + "Primary RD not found", vpnName);
-            return null;
+                     + "Primary RD not found. Reusing vpnName.", vpnName);
+            index = vpnName;
         }
         InstanceIdentifier<VpnInstanceOpDataEntry> id = InstanceIdentifier.builder(VpnInstanceOpData.class)
               .child(VpnInstanceOpDataEntry.class, new VpnInstanceOpDataEntryKey(primaryRd)).build();
-        Optional<VpnInstanceOpDataEntry> vpnInstanceOpDataEntryOptional = read(LogicalDatastoreType.OPERATIONAL, id);
+        Optional<VpnInstanceOpDataEntry> vpnInstanceOpDataEntryOptional =
+            read(LogicalDatastoreType.OPERATIONAL, id);
         if (!vpnInstanceOpDataEntryOptional.isPresent()) {
-            LOG.error("getVpnInstanceOpDataEntryFromVpnId: VpnInstance {} not found", primaryRd);
+            LOG.error("getVpnInstanceOpDataEntryFromVpnId: VpnInstance {} not found", index);
             return null;
         }
         return vpnInstanceOpDataEntryOptional.get();
@@ -1619,13 +1621,14 @@ public class NeutronvpnUtils {
 
     public void updateVpnInstanceOpVpn(VpnInstanceOpDataEntry.BgpvpnType choice, Uuid vpn) {
         String primaryRd = getVpnRd(vpn.getValue());
+        String index = primaryRd;
         if (primaryRd == null) {
             LOG.error("updateVpnInstanceOpVpn: Update BgpvpnType {} for {}."
-                    + "Primary RD not found", choice, vpn.getValue());
-            return;
+                    + "Primary RD not found. Using default VpnInstanceOpVpn", choice, vpn.getValue());
+            index = vpn.getValue();
         }
         InstanceIdentifier<VpnInstanceOpDataEntry> id = InstanceIdentifier.builder(VpnInstanceOpData.class)
-              .child(VpnInstanceOpDataEntry.class, new VpnInstanceOpDataEntryKey(primaryRd)).build();
+              .child(VpnInstanceOpDataEntry.class, new VpnInstanceOpDataEntryKey(index)).build();
 
         Optional<VpnInstanceOpDataEntry> vpnInstanceOpDataEntryOptional =
             read(LogicalDatastoreType.OPERATIONAL, id);
