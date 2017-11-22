e1be36e69891c8949ea4f0816be9572f9bafb9c2
Michael Vorburger <vorburger@redhat.com>
2017-11-22 10:58:20 +0100

    Fix the fix for the bad transaction leak in VpnInterfaceManager
    In 0c3a7e3103fea3dc039519ab4d4497fd218dc3d8 for
    I0f62fcc17c037365c9384e56c9f6f5a430693c20 there was a left-over
    tx.submit() that should have also been removed as part of it (because
    callWithNewWriteOnlyTransactionAndSubmit already does that, as its name
    says).  This fixes the follow new error which started appearing in CSIT
    logs since that was merged:
    
    2017-11-21 18:05:05,592 | ERROR | ChangeHandler-77 | L3vpnPopulator
    | 349 - org.opendaylight.netvirt.vpnmanager-impl - 0.6.0.SNAPSHOT |
    Future (eventually) failed: addToLabelMapper
    java.lang.UnsupportedOperationException: submit() cannot be used inside
    a Managed[New]TransactionRunner
    
    see https://jira.opendaylight.org/browse/NETVIRT-985
    
    Change-Id: Ic2c8b02670242f3a3f190860c205d7d70c2939b3
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/populator/impl/L3vpnPopulator.java
+++ vpnservice/vpnmanager/vpnmanager-impl/src/main/java/org/opendaylight/netvirt/vpnmanager/populator/impl/L3vpnPopulator.java
@@ -174,7 +174,6 @@ public abstract class L3vpnPopulator implements VpnPopulator {
                     InstanceIdentifier<LabelRouteInfo> lriIid = InstanceIdentifier.builder(LabelRouteMap.class)
                             .child(LabelRouteInfo.class, new LabelRouteInfoKey(label)).build();
                     tx.merge(LogicalDatastoreType.OPERATIONAL, lriIid, lri, true);
-                    tx.submit();
                     LOG.info("addToLabelMapper: Added label route info to label {} prefix {} nextHopList {} vpnId {}"
                             + " interface {} rd {} elantag {}", label, prefix, nextHopIpList, vpnId, vpnInterfaceName,
                             rd, elanTag);
