0bef6672956dc7d5ba8352c7b1ed378efc0abf64
Philippe Guibert <philippe.guibert@6wind.com>
2017-11-28 09:44:26 +0000

    neutronvpn: update bgpvpn fallback pipeline rules for ipv6
    When associating a BGPVPN to an external network, the identifier
    associated to that BGPVPN is already used for all the underlying
    external subnets declared in that network.
    
    For IPv6 GUA subnets that are "behind that external network", that is
    to say behind a router which has that external network linked, that same
    identifier is used to write a specific fallback route for those IPv6 GUA
    to access the Internet through BGPVPN. Indeed, when the router has no
    more entries in its FIB table, a fallback rule is written for IPv6 GU
    traffic against possibly BGPVPN IPv6 entries. That fallback rule
    resubmits the packet to the FIB table, but with an ID set to the BGPVPN
    id. Like that, all IPv6 traffic that has an entry associated may be
    redirected to the correct DC-GW.
    
    The modification here consists in adding that zero priority fallback,
    rule, in the case a BGPVPN is present on external network, on the
    presence of the first IPv6 subnet; removing the fallback rule, if the
    BGPVPN is detached or the last subnet ipv6 is removed.
    
    Change-Id: Iebd754ae10fa6db08512980a98e07c3a02ebb6d5
    Signed-off-by: Noel De Prandieres <prandieres@6wind.com>
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Signed-off-by: Valentina Krasnobaeva <valentina.krasnobaeva@6wind.com>

--- vpnservice/neutronvpn/neutronvpn-impl/src/test/java/org/opendaylight/netvirt/neutronvpn/NeutronPortChangeListenerTest.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/test/java/org/opendaylight/netvirt/neutronvpn/NeutronPortChangeListenerTest.java
@@ -85,6 +85,10 @@ public class NeutronPortChangeListenerTest {
     IInterfaceManager interfaceManager;
     @Mock
     IdManagerService idManager;
+    @Mock
+    NeutronvpnUtils neutronvpnUtils;
+    @Mock
+    IPV6InternetDefaultRouteProgrammer ipV6InternetDefRt;
 
     MetricProvider metricProvider = new TestMetricProviderImpl();
 
@@ -98,7 +102,7 @@ public class NeutronPortChangeListenerTest {
         when(mockReadTx.read(any(LogicalDatastoreType.class), any(InstanceIdentifier.class)))
             .thenReturn(Futures.immediateCheckedFuture(Optional.of(mockNetwork)));
         neutronPortChangeListener = new NeutronPortChangeListener(dataBroker, neutronvpnManager, neutronvpnNatManager,
-                gwMacResolver, elanService, jobCoordinator, new NeutronvpnUtils(dataBroker, idManager, jobCoordinator),
+                gwMacResolver, elanService, jobCoordinator, new NeutronvpnUtils(dataBroker, idManager, jobCoordinator, ipV6InternetDefRt),
                 new HostConfigCache(dataBroker, new GuavaCacheProvider(new CacheManagersRegistryImpl())));
         InstanceIdentifier<ElanInstance> elanIdentifierId = InstanceIdentifier.builder(ElanInstances.class)
                 .child(ElanInstance.class,
