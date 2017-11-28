c88007089a0694a4168bbf45641fa9ee9ac5fb9f
Philippe Guibert <philippe.guibert@6wind.com>
2017-11-28 09:44:26 +0000

    neutronvpn: update bgpvpn fallback pipeline rules for ipv6
    When associating a BGPVPN to an external network, the identifier
    associated to that BGPVPN is already used for all the underlying
    external subnets declared in that network.
    For IPv6 GUA subnets that are "behind that external network", that is
    to say behind a router which has that external network linked, that same
    identifier is used to write a specific fallback route for those IPv6 GUA
    to access the Internet through BGPVPN. Indeed, when the router has no
    more entries in its FIB table, a fallback rule is written for IPv6 GU
    traffic against possibly BGPVPN IPv6 entries. That fallback rule
    resubmits the packet to the FIB table, but with an ID set to the BGPVPN
    id. Like that, all IPv6 traffic that has an entry associated may be
    redirected to the correct DC-GW.
    The modification here consists in adding that zero priority fallback,
    rule, in the case a BGPVPN is present on external network, on the
    presence of the first IPv6 subnet; removing the fallback rule, if the
    BGPVPN is detached or the last subnet ipv6 is removed.
    
    Change-Id: Iebd754ae10fa6db08512980a98e07c3a02ebb6d5
    Signed-off-by: Noel De Prandieres <prandieres@6wind.com>
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnManager.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/NeutronvpnManager.java
@@ -2297,6 +2297,8 @@ public class NeutronvpnManager implements NeutronvpnService, AutoCloseable, Even
                                                             .getInternetVpnId().getValue(),
                                                    neutronvpnUtils.getIpVersionFromString(sn
                                                             .getSubnetIp()), true);
+                                            neutronvpnUtils.updateVpnInstanceWithFallback(
+                                                          sn.getInternetVpnId().getValue(), true);
                                         }
                                     }
                                     addSubnetToVpn(vpn, subnet, sn != null ? sn.getInternetVpnId() : null);
@@ -2330,6 +2332,8 @@ public class NeutronvpnManager implements NeutronvpnService, AutoCloseable, Even
                                         if (neutronvpnUtils.shouldVpnHandleIpVersionChangeToAdd(sm, vpn)) {
                                             neutronvpnUtils.updateVpnInstanceWithIpFamily(vpn.getValue(),
                                                     neutronvpnUtils.getIpVersionFromString(sm.getSubnetIp()), true);
+                                            neutronvpnUtils.updateVpnInstanceWithFallback(vpn.getValue(),
+                                                    true);
                                         }
                                     }
                                 }
@@ -2389,11 +2393,15 @@ public class NeutronvpnManager implements NeutronvpnService, AutoCloseable, Even
                                         vpnInstanceIpVersionsToRemove, false);
                             }
                             if (vpnInstanceInternetIpVersionRemoved) {
+                                neutronvpnUtils.updateVpnInstanceWithFallback(
+                                               vpnIdInternet.getValue(), false);
                                 neutronvpnUtils.updateVpnInstanceWithIpFamily(vpnIdInternet.getValue(),
                                                    IpVersionChoice.IPV6, false);
                             }
                         }
                         if (neutronvpnUtils.getIsExternal(network)) {
+                            neutronvpnUtils.updateVpnInstanceWithFallback(vpn.getValue(),
+                                                    false);
                             neutronvpnUtils.updateVpnInstanceOpWithType(VpnInstanceOpDataEntry
                                              .BgpvpnType.VPN, vpn);
                             LOG.debug("Removing IPv6 network subnets...");
