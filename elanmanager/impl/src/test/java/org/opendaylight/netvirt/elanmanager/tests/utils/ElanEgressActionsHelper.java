1d5629ce46f48064d4da435cc65b5050622a3b6e
Yugandhar Reddy Kaku <yugandhar.reddy.kaku@ericsson.com>
2017-12-05 19:05:12 +0530

    ELAN L2GW Junit Test Cases
    1. verifyL2gw1Connection
       verify the dpn macs are programed in tor remote ucast
       verify dpn remote broadcast group includes tor endpoint
       verify tor1 mcast group includes dpns
    2. verifyL2gwMac1InDpns
       verify tor mac is programed in dpn
    3. verifyL2gw2Connection
       verify tor1 mac is programed in tor2
       verify tor1 mcast group includes tor2 tep
    4. verifyL2gwMac2InTors
       verify new tor1 mac is programed in tor2
    5. verifyL2gwMacDeleteInTors
       verify tor1 mac is deleted from tor2
    6. verifyAddDpnAfterL2gwConnection
       verify tor macs are programed in new dpn
    7. verifyDeleteDpnAfterL2gwConnection
       verify tor macs are removed from dpn
       verify tor mcast group does not contain deleted dpn
       verify dpn mac is removed from tor
    8. verifyDeleteL2gw1Connection
       verify dpn bc group does not contain tor
       verify dpn macs are removed from tor
    
    Change-Id: I648e246079d0c432037a4b6851275e7986ebf753
    Signed-off-by: Yugandhar Reddy Kaku <yugandhar.reddy.kaku@ericsson.com>

--- elanmanager/impl/src/test/java/org/opendaylight/netvirt/elanmanager/tests/utils/ElanEgressActionsHelper.java
+++ elanmanager/impl/src/test/java/org/opendaylight/netvirt/elanmanager/tests/utils/ElanEgressActionsHelper.java
@@ -15,8 +15,12 @@ import java.util.concurrent.Future;
 import org.mockito.Mockito;
 import org.opendaylight.genius.interfacemanager.IfmUtil;
 import org.opendaylight.genius.interfacemanager.commons.InterfaceManagerCommonUtils;
+import org.opendaylight.genius.testutils.TestInterfaceManager;
 import org.opendaylight.genius.tools.mdsal.rpc.FutureRpcResults;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetDpidFromInterfaceInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetDpidFromInterfaceOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetDpidFromInterfaceOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetEgressActionsForInterfaceInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetEgressActionsForInterfaceOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.GetEgressActionsForInterfaceOutputBuilder;
@@ -30,10 +34,13 @@ import org.slf4j.LoggerFactory;
 public abstract class ElanEgressActionsHelper implements OdlInterfaceRpcService {
     private static final Logger LOG = LoggerFactory.getLogger(ElanEgressActionsHelper.class);
     private InterfaceManagerCommonUtils interfaceManagerCommonUtils;
+    private TestInterfaceManager testInterfaceManager;
 
-    public static ElanEgressActionsHelper newInstance(InterfaceManagerCommonUtils interfaceManagerCommonUtils) {
+    public static ElanEgressActionsHelper newInstance(InterfaceManagerCommonUtils interfaceManagerCommonUtils,
+                                                      TestInterfaceManager testInterfaceManager) {
         ElanEgressActionsHelper instance = Mockito.mock(ElanEgressActionsHelper.class, realOrException());
         instance.interfaceManagerCommonUtils = interfaceManagerCommonUtils;
+        instance.testInterfaceManager = testInterfaceManager;
         return instance;
     }
 
@@ -44,6 +51,15 @@ public abstract class ElanEgressActionsHelper implements OdlInterfaceRpcService 
             List<Action> actionsList = IfmUtil.getEgressActionsForInterface(input.getIntfName(), input.getTunnelKey(),
                     input.getActionKey(), interfaceManagerCommonUtils, false);
             return new GetEgressActionsForInterfaceOutputBuilder().setAction(actionsList);
+        }).build();
+    }
+
+    @Override
+    public Future<RpcResult<GetDpidFromInterfaceOutput>> getDpidFromInterface(
+            GetDpidFromInterfaceInput input) {
+        return FutureRpcResults.fromBuilder(LOG, input, () -> {
+            return new GetDpidFromInterfaceOutputBuilder().setDpid(
+                    testInterfaceManager.getDpnForInterface(input.getIntfName()));
         }).build();
     }
 }
