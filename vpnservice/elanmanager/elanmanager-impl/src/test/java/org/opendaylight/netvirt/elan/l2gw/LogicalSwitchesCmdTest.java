57d32b45d5cc30812bd4d997d88592e46599c62e
eaksahu <a.k.sahu@ericsson.com>
2017-12-13 16:36:01 +0530

    Bug 8998 - Vlan Binding missing on connection add
    missing child entries when connection is deleted and added immediately.
    
    Since we are using batch manager previous delete
    is still not finished by the time next add request comes.
    
    Since the data is not deleted yet this add request will become no op.
    
    delete from batch manager eventually succeeds and we end up entry missing.
    
    make sure that add request does not proceed until the previous delete
    request has not completed.
    
    All the mac updates are copied from NodeListener itself now
    instead of individual mac / logical switch liseners.
    
    Change-Id: I88c887a4f40ad96ae2230e3a5362e8cc1cbde87f
    Signed-off-by: eaksahu <a.k.sahu@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/test/java/org/opendaylight/netvirt/elan/l2gw/LogicalSwitchesCmdTest.java
+++ vpnservice/elanmanager/elanmanager-impl/src/test/java/org/opendaylight/netvirt/elan/l2gw/LogicalSwitchesCmdTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ * Copyright (c) 2016 ,2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -9,8 +9,12 @@ package org.opendaylight.netvirt.elan.l2gw;
 
 import static org.junit.Assert.assertEquals;
 
+import com.google.common.collect.Lists;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,7 +24,11 @@ import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.binding.test.AbstractConcurrentDataBrokerTest;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.genius.utils.batching.ResourceBatchingManager;
+import org.opendaylight.netvirt.elan.l2gw.ha.BatchedTransaction;
 import org.opendaylight.netvirt.elan.l2gw.ha.HwvtepHAUtil;
+import org.opendaylight.netvirt.elan.l2gw.ha.IidTracker;
 import org.opendaylight.netvirt.elan.l2gw.ha.commands.LogicalSwitchesCmd;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepGlobalAugmentation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepGlobalAugmentationBuilder;
@@ -29,6 +37,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hw
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.LogicalSwitchesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.LogicalSwitchesKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.Identifiable;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
@@ -41,7 +50,7 @@ public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
     // public @Rule RunUntilFailureRule repeater = new RunUntilFailureRule(classRepeater);
 
     DataBroker dataBroker;
-    ReadWriteTransaction tx;
+    BatchedTransaction tx;
     LogicalSwitchesCmd cmd = new LogicalSwitchesCmd();
 
     HwvtepGlobalAugmentationBuilder dstBuilder = new HwvtepGlobalAugmentationBuilder();
@@ -57,7 +66,9 @@ public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
     InstanceIdentifier<Node> d2NodePath = HwvtepHAUtil.convertToInstanceIdentifier("d2");
 
     LogicalSwitches[] logicalSwitches = new LogicalSwitches[4];
+    LogicalSwitches[] parentLogicalSwitches = new LogicalSwitches[4];
     InstanceIdentifier<LogicalSwitches>[] ids = new InstanceIdentifier[4];
+    Map<LogicalSwitches, InstanceIdentifier<LogicalSwitches>> iids = new HashMap<>();
 
     String[][] data = new String[][] {
             {"ls1", "100"},
@@ -66,19 +77,40 @@ public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
             {"ls4", "400"}
     };
 
+    private List<Identifiable> updatedLogicalSwitches = null;
+    private List<Identifiable> deletedData = null;
+
     @Before
     public void setupForHANode() {
         dataBroker = getDataBroker();
-        tx = Mockito.spy(dataBroker.newReadWriteTransaction());
+        tx = Mockito.spy(new BatchedTransaction(dataBroker, new IidTracker()));
         for (int i = 0 ; i < 4; i++) {
             logicalSwitches[i] = buildData(data[i][0], data[i][1]);
             ids[i] = haNodePath.augmentation(HwvtepGlobalAugmentation.class).child(LogicalSwitches.class,
                     new LogicalSwitchesKey(new HwvtepNodeName(data[i][0])));
+            iids.put(logicalSwitches[i], ids[i]);
+            parentLogicalSwitches[i] = new LogicalSwitchesBuilder(logicalSwitches[i])
+                    .setLogicalSwitchUuid(HwvtepHAUtil.getUUid(logicalSwitches[i].getHwvtepNodeName().getValue()))
+                    .build();
         }
+        updatedLogicalSwitches = new ArrayList<>();
+        deletedData = new ArrayList<>();
+        Map<Class<? extends Identifiable>, List<Identifiable>> updateMap = new HashMap<>();
+        tx.setUpdatedData(updateMap);
+        Map<Class<? extends Identifiable>, List<Identifiable>> deleteMap = new HashMap<>();
+        tx.setDeletedData(deleteMap);
+        Mockito.verify(tx).setUpdatedData(Mockito.any());
+        Mockito.verify(tx).setDeletedData(Mockito.any());
+        updateMap.put(LogicalSwitches.class, updatedLogicalSwitches);
+        deleteMap.put(LogicalSwitches.class, deletedData);
+        ResourceBatchingManager.getInstance().registerDefaultBatchHandlers(dataBroker);
     }
 
     @After
     public void teardown() {
+        for (ResourceBatchingManager.ShardResource i : ResourceBatchingManager.ShardResource.values()) {
+            ResourceBatchingManager.getInstance().deregisterBatchableResource(i.name());
+        }
     }
 
 
@@ -98,13 +130,27 @@ public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
         assertEquals("should copy the logical switches ", 2, dstBuilder.getLogicalSwitches().size());
     }
 
+    void addToUpdated(List<LogicalSwitches> added) {
+        updatedLogicalSwitches.addAll(getData(added.toArray(new LogicalSwitches[]{})).getLogicalSwitches());
+    }
+
+    void addToDeleted(List<LogicalSwitches> deleted) throws TransactionCommitFailedException {
+        for (LogicalSwitches deletedLs : deleted) {
+            ReadWriteTransaction tx = dataBroker.newReadWriteTransaction();
+            tx.put(LogicalDatastoreType.OPERATIONAL, iids.get(deletedLs), deletedLs, true);
+            tx.submit().checkedGet();
+        }
+        deletedData.addAll(getData(deleted.toArray(new LogicalSwitches[]{})).getLogicalSwitches());
+    }
+
     @Test
     public void testOneLogicalSwitchAddedUpdate() throws Exception {
         existingData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
         originalData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
         updatedData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1], logicalSwitches[2]});
+        addToUpdated(Lists.newArrayList(logicalSwitches[2]));
         cmd.mergeOpUpdate(existingData, updatedData, originalData, haNodePath, tx);
-        Mockito.verify(tx).put(LogicalDatastoreType.OPERATIONAL, ids[2], logicalSwitches[2],
+        Mockito.verify(tx).put(LogicalDatastoreType.OPERATIONAL, ids[2], parentLogicalSwitches[2],
                 WriteTransaction.CREATE_MISSING_PARENTS);
     }
 
@@ -114,10 +160,11 @@ public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
         originalData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
         updatedData = getData(new LogicalSwitches[]{logicalSwitches[0],
                 logicalSwitches[1], logicalSwitches[2], logicalSwitches[3]});
+        addToUpdated(Lists.newArrayList(logicalSwitches[2], logicalSwitches[3]));
         cmd.mergeOpUpdate(existingData, updatedData, originalData, haNodePath, tx);
-        Mockito.verify(tx).put(LogicalDatastoreType.OPERATIONAL, ids[2], logicalSwitches[2],
+        Mockito.verify(tx).put(LogicalDatastoreType.OPERATIONAL, ids[2], parentLogicalSwitches[2],
                 WriteTransaction.CREATE_MISSING_PARENTS);
-        Mockito.verify(tx).put(LogicalDatastoreType.OPERATIONAL, ids[3], logicalSwitches[3],
+        Mockito.verify(tx).put(LogicalDatastoreType.OPERATIONAL, ids[3], parentLogicalSwitches[3],
                 WriteTransaction.CREATE_MISSING_PARENTS);
     }
 
@@ -126,6 +173,7 @@ public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
         existingData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1], logicalSwitches[2]});
         originalData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1], logicalSwitches[2]});
         updatedData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
+        addToDeleted(Lists.newArrayList(logicalSwitches[2]));
         cmd.mergeOpUpdate(existingData, updatedData, originalData, haNodePath, tx);
         Mockito.verify(tx).delete(LogicalDatastoreType.OPERATIONAL, ids[2]);
     }
@@ -138,6 +186,7 @@ public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
         originalData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1],
                 logicalSwitches[2], logicalSwitches[3]});
         updatedData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
+        addToDeleted(Lists.newArrayList(logicalSwitches[2], logicalSwitches[3]));
         cmd.mergeOpUpdate(existingData, updatedData, originalData, haNodePath, tx);
         Mockito.verify(tx).delete(LogicalDatastoreType.OPERATIONAL, ids[2]);
         Mockito.verify(tx).delete(LogicalDatastoreType.OPERATIONAL, ids[3]);
@@ -148,10 +197,12 @@ public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
         existingData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
         originalData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
         updatedData = getData(new LogicalSwitches[]{logicalSwitches[2], logicalSwitches[3]});
+        addToUpdated(Lists.newArrayList(logicalSwitches[2], logicalSwitches[3]));
+        addToDeleted(Lists.newArrayList(logicalSwitches[0], logicalSwitches[1]));
         cmd.mergeOpUpdate(existingData, updatedData, originalData, haNodePath, tx);
-        Mockito.verify(tx).put(LogicalDatastoreType.OPERATIONAL, ids[2], logicalSwitches[2],
+        Mockito.verify(tx).put(LogicalDatastoreType.OPERATIONAL, ids[2], parentLogicalSwitches[2],
                 WriteTransaction.CREATE_MISSING_PARENTS);
-        Mockito.verify(tx).put(LogicalDatastoreType.OPERATIONAL, ids[3], logicalSwitches[3],
+        Mockito.verify(tx).put(LogicalDatastoreType.OPERATIONAL, ids[3], parentLogicalSwitches[3],
                 WriteTransaction.CREATE_MISSING_PARENTS);
         Mockito.verify(tx).delete(LogicalDatastoreType.OPERATIONAL, ids[0]);
         Mockito.verify(tx).delete(LogicalDatastoreType.OPERATIONAL, ids[1]);
@@ -162,6 +213,7 @@ public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
         existingData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
         originalData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
         updatedData = getData(new LogicalSwitches[]{});
+        addToDeleted(Lists.newArrayList(logicalSwitches[0], logicalSwitches[1]));
         cmd.mergeOpUpdate(existingData, updatedData, originalData, haNodePath, tx);
         Mockito.verify(tx).delete(LogicalDatastoreType.OPERATIONAL, ids[0]);
         Mockito.verify(tx).delete(LogicalDatastoreType.OPERATIONAL, ids[1]);
@@ -173,6 +225,8 @@ public class LogicalSwitchesCmdTest extends AbstractConcurrentDataBrokerTest {
         originalData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
         updatedData = getData(new LogicalSwitches[]{logicalSwitches[0], logicalSwitches[1]});
         cmd.mergeOpUpdate(existingData, updatedData, originalData, haNodePath, tx);
+        Mockito.verify(tx).getUpdatedData();
+        Mockito.verify(tx).getDeletedData();
         Mockito.verifyNoMoreInteractions(tx);
     }
 
