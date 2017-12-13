b3789da3ae9f6f2a9419b74c505ed40c81a31fa0
eaksahu <a.k.sahu@ericsson.com>
2017-12-13 16:36:01 +0530

    Bug 8998 - Vlan Binding missing on connection add
    missing child entries when connection is deleted and added immediately.
    
    Since we are using batch manager previous delete
    is still not finished by the time next add request comes.
    
    Since the data is not deleted yet this add request will become no op.
    
    delete from batch manager eventually succeeds and we end up entry missing.
    
    make sure that add request does not proceed until the previous delete
    request has not completed.
    
    All the mac updates are copied from NodeListener itself now
    instead of individual mac / logical switch liseners.
    
    Change-Id: I88c887a4f40ad96ae2230e3a5362e8cc1cbde87f
    Signed-off-by: eaksahu <a.k.sahu@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/ha/handlers/OpNodeUpdatedHandler.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/ha/handlers/OpNodeUpdatedHandler.java
@@ -8,15 +8,13 @@
 package org.opendaylight.netvirt.elan.l2gw.ha.handlers;
 
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.netvirt.elan.l2gw.ha.DataUpdates;
 import org.opendaylight.netvirt.elan.l2gw.ha.HwvtepHAUtil;
 import org.opendaylight.netvirt.elan.l2gw.ha.merge.GlobalAugmentationMerger;
 import org.opendaylight.netvirt.elan.l2gw.ha.merge.GlobalNodeMerger;
 import org.opendaylight.netvirt.elan.l2gw.ha.merge.PSAugmentationMerger;
 import org.opendaylight.netvirt.elan.l2gw.ha.merge.PSNodeMerger;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepGlobalAugmentation;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.PhysicalSwitchAugmentation;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
@@ -27,65 +25,38 @@ public class OpNodeUpdatedHandler {
     GlobalNodeMerger globalNodeMerger         = GlobalNodeMerger.getInstance();
     PSNodeMerger psNodeMerger             = PSNodeMerger.getInstance();
 
-    public void handle(Node updatedSrcNode, Node origSrcNode, InstanceIdentifier<Node> haPath, ReadWriteTransaction tx)
-            throws ReadFailedException {
-        if (updatedSrcNode.getAugmentation(HwvtepGlobalAugmentation.class) != null) {
-            copyChildGlobalOpUpdateToHAParent(updatedSrcNode, origSrcNode, haPath, tx);
-        } else {
-            copyChildPsOpUpdateToHAParent(updatedSrcNode, origSrcNode, haPath, tx);
-        }
-    }
-
     /**
      * Copy HA ps node update to HA child ps node of operational data tree.
      *
      * @param updatedSrcPSNode Updated HA child ps node
-     * @param origSrcPSNode Original HA ps node
      * @param haPath HA node path
+     * @param dataUpdates updated and deleted data
      * @param tx Transaction
      * @throws ReadFailedException  Exception thrown if read fails
      */
     public void copyChildPsOpUpdateToHAParent(Node updatedSrcPSNode,
-                                              Node origSrcPSNode,
                                               InstanceIdentifier<Node> haPath,
+                                              DataUpdates dataUpdates,
                                               ReadWriteTransaction tx) throws ReadFailedException {
 
         InstanceIdentifier<Node> haPSPath = HwvtepHAUtil.convertPsPath(updatedSrcPSNode, haPath);
-        Node existingHAPSNode = HwvtepHAUtil.readNode(tx, LogicalDatastoreType.OPERATIONAL, haPSPath);
-
-        PhysicalSwitchAugmentation updatedSrc   = HwvtepHAUtil.getPhysicalSwitchAugmentationOfNode(updatedSrcPSNode);
-        PhysicalSwitchAugmentation origSrc      = HwvtepHAUtil.getPhysicalSwitchAugmentationOfNode(origSrcPSNode);
-        PhysicalSwitchAugmentation existingData = HwvtepHAUtil.getPhysicalSwitchAugmentationOfNode(existingHAPSNode);
-
-        psAugmentationMerger.mergeOpUpdate(existingData, updatedSrc, origSrc, haPSPath, tx);
-        psNodeMerger.mergeOpUpdate(existingHAPSNode, updatedSrcPSNode, origSrcPSNode, haPSPath, tx);
+        psAugmentationMerger.mergeOpUpdate(haPSPath, dataUpdates, tx);
+        psNodeMerger.mergeOpUpdate(haPSPath, dataUpdates, tx);
     }
 
     /**
      * Copy updated data from HA node to child node of operational data tree.
      *
-     * @param updatedSrcNode Updated HA child node
-     * @param origSrcNode Original HA node
      * @param haPath HA node path
+     * @param dataUpdates updated and deleted data
      * @param tx Transaction
      * @throws ReadFailedException  Exception thrown if read fails
      */
-    public void copyChildGlobalOpUpdateToHAParent(Node updatedSrcNode,
-                                                  Node origSrcNode,
-                                                  InstanceIdentifier<Node> haPath,
+    public void copyChildGlobalOpUpdateToHAParent(InstanceIdentifier<Node> haPath,
+                                                  DataUpdates dataUpdates,
                                                   ReadWriteTransaction tx) throws ReadFailedException {
-
-        Node existingDstNode = HwvtepHAUtil.readNode(tx, LogicalDatastoreType.OPERATIONAL, haPath);
-        if (existingDstNode == null) {
-            //No dst present nothing to copy
-            return;
-        }
-        HwvtepGlobalAugmentation existingData    = HwvtepHAUtil.getGlobalAugmentationOfNode(existingDstNode);
-        HwvtepGlobalAugmentation updatedSrc = HwvtepHAUtil.getGlobalAugmentationOfNode(updatedSrcNode);
-        HwvtepGlobalAugmentation origSrc    = HwvtepHAUtil.getGlobalAugmentationOfNode(origSrcNode);
-
-        globalAugmentationMerger.mergeOpUpdate(existingData, updatedSrc, origSrc, haPath, tx);
-        globalNodeMerger.mergeOpUpdate(existingDstNode, updatedSrcNode, origSrcNode, haPath, tx);
+        globalAugmentationMerger.mergeOpUpdate(haPath, dataUpdates, tx);
+        globalNodeMerger.mergeOpUpdate(haPath, dataUpdates, tx);
     }
 
 }
