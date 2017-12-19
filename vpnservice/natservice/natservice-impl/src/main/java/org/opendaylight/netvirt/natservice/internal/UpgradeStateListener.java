eea4b15864738ea64da13f747e369db8942df733
Josh <jhershbe@redhat.com>
2017-12-19 12:21:12 +0200

    SNAT Ext. Router scheduling broken w/ upgrade
    Depends on this gerrit from genius:
    https://git.opendaylight.org/gerrit/#/c/65894/
    
    SNAT requires that all the nodes be in the operational datastore
    at the time the external router configurations are pushed to ODL.
    At this point the external router is "scheduled", that is, a
    switch is chosen to get the flows and track the sessions. In the
    case of upgrade, there are in fact no nodes connected to ODL.
    
    Solution:
    Wait until all the nodes are connected and only then schedule
    the external routers.
    1) if the upgrading flag is set, don't schedule external routers
    2) When the listener fires, schedule all the external routers
    
    Change-Id: Ib349450905ef9c33fe3640753bb88e65c8ebf967
    Signed-off-by: Josh <jhershbe@redhat.com>

---
+++ vpnservice/natservice/natservice-impl/src/main/java/org/opendaylight/netvirt/natservice/internal/UpgradeStateListener.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.natservice.internal;
+
+import java.util.List;
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
+import org.opendaylight.genius.datastoreutils.listeners.AbstractClusteredSyncDataTreeChangeListener;
+import org.opendaylight.netvirt.natservice.api.CentralizedSwitchScheduler;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.config.rev170206.NatserviceConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ExtRouters;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ext.routers.Routers;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ext.routers.routers.ExternalIps;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsalutil.rev170830.Config;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class UpgradeStateListener extends AbstractClusteredSyncDataTreeChangeListener<Config> {
+    private static final Logger LOG = LoggerFactory.getLogger(UpgradeStateListener.class);
+
+    private final DataBroker dataBroker;
+    private final CentralizedSwitchScheduler centralizedSwitchScheduler;
+    private final NatserviceConfig.NatMode natMode;
+
+    @Inject
+    public UpgradeStateListener(final DataBroker dataBroker,
+                                final CentralizedSwitchScheduler centralizedSwitchScheduler,
+                                final NatserviceConfig config) {
+        super(dataBroker, new DataTreeIdentifier<>(
+                LogicalDatastoreType.CONFIGURATION, InstanceIdentifier.create(Config.class)));
+        this.dataBroker = dataBroker;
+        this.centralizedSwitchScheduler = centralizedSwitchScheduler;
+        if (config != null) {
+            this.natMode = config.getNatMode();
+        } else {
+            this.natMode = NatserviceConfig.NatMode.Controller;
+        }
+        LOG.trace("UpgradeStateListener (nat) initialized");
+    }
+
+    @Override
+    public void add(@Nonnull Config newDataObject) {
+    }
+
+    @Override
+    public void remove(@Nonnull Config removedDataObject) {
+    }
+
+    @Override
+    public void update(@Nonnull Config original, Config updated) {
+        if (natMode != NatserviceConfig.NatMode.Conntrack) {
+            return;
+        }
+
+        LOG.info("UpgradeStateListener update from {} to {}", original, updated);
+        if (!(original.isUpgradeInProgress() && !updated.isUpgradeInProgress())) {
+            return;
+        }
+
+        SingleTransactionDataBroker reader = new SingleTransactionDataBroker(dataBroker);
+        ExtRouters routers;
+        try {
+            routers = reader.syncRead(LogicalDatastoreType.CONFIGURATION,
+                    InstanceIdentifier.create(ExtRouters.class));
+        } catch (ReadFailedException e) {
+            LOG.error("Error reading external routers", e);
+            return;
+        }
+
+        for (Routers router : routers.getRouters()) {
+            List<ExternalIps> externalIps = router.getExternalIps();
+            if (router.isEnableSnat() && externalIps != null && !externalIps.isEmpty()) {
+                centralizedSwitchScheduler.scheduleCentralizedSwitch(router);
+            }
+        }
+    }
+}
