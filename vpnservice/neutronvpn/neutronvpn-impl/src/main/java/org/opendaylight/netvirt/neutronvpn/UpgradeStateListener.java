c5b184a9b9db834badf3247e60ffe4f5d1b2fe01
Josh <jhershbe@redhat.com>
2017-12-28 12:28:15 +0200

    Upgrade: Remote FIB entry missing for ext. gateway
    Root cause of this is that the ARP to discover the
    external gateway's mac is triggered at router/subnet
    creation time and the code assumes the ovsdb models
    are in place. However, in the case of upgrade they are
    not yet present as the switches have not yet reconnected.
    
    Fix: At the point where the upgrading flag is unset we
    know that all switches have reconnected. Listen for that
    transition and trigger the ARP for all routers.
    
    Change-Id: I6cc6045937e2d9a936596e1a8bc36b7a0cbb70e3
    Signed-off-by: Josh <jhershbe@redhat.com>

---
+++ vpnservice/neutronvpn/neutronvpn-impl/src/main/java/org/opendaylight/netvirt/neutronvpn/UpgradeStateListener.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.neutronvpn;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.listeners.AbstractClusteredSyncDataTreeChangeListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsalutil.rev170830.Config;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class UpgradeStateListener extends AbstractClusteredSyncDataTreeChangeListener<Config> {
+    private static final Logger LOG = LoggerFactory.getLogger(UpgradeStateListener.class);
+
+    private final NeutronSubnetGwMacResolver neutronSubnetGwMacResolver;
+
+    @Inject
+    public UpgradeStateListener(final DataBroker dataBroker,
+                                final NeutronSubnetGwMacResolver neutronSubnetGwMacResolver) {
+        super(dataBroker, new DataTreeIdentifier<>(
+                LogicalDatastoreType.CONFIGURATION, InstanceIdentifier.create(Config.class)));
+        LOG.trace("UpgradeStateListener (neutronvpn) initialized");
+        this.neutronSubnetGwMacResolver = neutronSubnetGwMacResolver;
+    }
+
+    @Override
+    public void add(@Nonnull Config newDataObject) {
+    }
+
+    @Override
+    public void remove(@Nonnull Config removedDataObject) {
+    }
+
+    @Override
+    public void update(@Nonnull Config original, Config updated) {
+        LOG.info("UpgradeStateListener update from {} to {}", original, updated);
+        neutronSubnetGwMacResolver.sendArpRequestsToExtGateways();
+    }
+}
