a3d69350e449f8c7ff7b4858b668ad97dafc686a
Kiran Kumar Darapu <kiran.kumar@altencalsoftlabs.com>
2018-01-18 11:48:31 +0530

    Migrating BGP PM Counters to Infrautils Counters
    Existing BGP PM counters were using JMX MBeans,
    Now new BGP PM counters were imoplemented using
    Infrautils metrics counters APIs which will be common library
    to populate JMX MBeans for these counters by leveraging
    Codahale framework.
    
    Change-Id: Ib458d151ffed331ccd2b717ad3abef0ebecf3f80
    Signed-off-by: Kiran Kumar Darapu <kiran.kumar@altencalsoftlabs.com>

--- vpnservice/bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/netvirt/bgpmanager/BgpUtil.java
+++ vpnservice/bgpmanager/bgpmanager-impl/src/main/java/org/opendaylight/netvirt/bgpmanager/BgpUtil.java
@@ -24,6 +24,7 @@ import org.opendaylight.genius.utils.batching.ActionableResource;
 import org.opendaylight.genius.utils.batching.ActionableResourceImpl;
 import org.opendaylight.genius.utils.batching.DefaultBatchHandler;
 import org.opendaylight.genius.utils.batching.ResourceBatchingManager;
+import org.opendaylight.netvirt.bgpmanager.oam.BgpConstants;
 import org.opendaylight.netvirt.bgpmanager.thrift.gen.af_afi;
 import org.opendaylight.netvirt.bgpmanager.thrift.gen.af_safi;
 import org.opendaylight.netvirt.bgpmanager.thrift.gen.encap_type;
@@ -291,5 +292,41 @@ public class BgpUtil implements AutoCloseable {
                    .child(VrfTables.class, new VrfTablesKey(rd))
                    .child(VrfEntry.class, new VrfEntryKey(vrfEntry.getDestPrefix())).build();
         delete(LogicalDatastoreType.CONFIGURATION, vrfEntryId);
+    }
+
+    /**
+     * This method returns metric key.
+     * @param as string.
+     * @param rx BgpNeighborPacketsReceived value.
+     * @param tx BgpNeighborPacketsSent value.
+     * @param strIp neighbor-ip value.
+     * @param rd BgpRdRouteCount value.
+     * @return counterKey string.
+     */
+    public static String generateBgpCounterKey(
+            String as, String rx, String tx, String strIp, String rd) {
+        String moduleName = "odl.netvirt.bgpmanager";
+        String entityTypeBgpPeer = ".entitycounter.entitytype:bgp-peer.entityid:<asid=";
+        String neighborIp = ";neighborip=";
+        String angularBracket = ">.";
+        String aggregateCounter = ".aggregatecounter.[source:";
+        String totalPrefixes = "].totalprefixes";
+        String entityTypeBgpRd = ".entitycounter.entitytype:bgp-rd.entityid:<rd=";
+
+        String counterKey = moduleName;
+        if (rx != null) {
+            counterKey += entityTypeBgpPeer + as + neighborIp + strIp + angularBracket
+                    + BgpConstants.BGP_COUNTER_NBR_PKTS_RX;
+        } else if (tx != null) {
+            counterKey += entityTypeBgpPeer + as + neighborIp + strIp + angularBracket
+                    + BgpConstants.BGP_COUNTER_NBR_PKTS_TX;
+        } else if (rd != null) {
+            counterKey += entityTypeBgpRd + rd + angularBracket
+                    + BgpConstants.BGP_COUNTER_RD_ROUTE_COUNT;
+        } else {
+            counterKey += aggregateCounter + BgpConstants.BGP_COUNTER_TOTAL_PFX
+                    + totalPrefixes;
+        }
+        return counterKey;
     }
 }
