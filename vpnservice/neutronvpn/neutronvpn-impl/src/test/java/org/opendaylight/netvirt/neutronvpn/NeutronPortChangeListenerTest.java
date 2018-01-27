82da8f88368bb97de9c1e1e6f50272c0d97e3470
Faseela K <faseela.k@ericsson.com>
2018-01-28 01:06:52 +0530

    Pass MetricProvider to JC Test Module
    This requires Ie90562c2f8205bab8817d370d077def9a3419d52 and
    I530d20946a80933b05371c5220def447be1ee5e6 in infrautils in order to be
    able to compile, and must be merged together (with the 2nd, the first
    can go in before).
    
    Change-Id: If3c3bcdb8a972c7e57cff20fd24d5addefa7746b
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- vpnservice/neutronvpn/neutronvpn-impl/src/test/java/org/opendaylight/netvirt/neutronvpn/NeutronPortChangeListenerTest.java
+++ vpnservice/neutronvpn/neutronvpn-impl/src/test/java/org/opendaylight/netvirt/neutronvpn/NeutronPortChangeListenerTest.java
@@ -31,6 +31,8 @@ import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
 import org.opendaylight.infrautils.jobcoordinator.internal.JobCoordinatorImpl;
+import org.opendaylight.infrautils.metrics.MetricProvider;
+import org.opendaylight.infrautils.metrics.testimpl.TestMetricProviderImpl;
 import org.opendaylight.netvirt.elanmanager.api.IElanService;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
@@ -82,7 +84,9 @@ public class NeutronPortChangeListenerTest {
     @Mock
     IdManagerService idManager;
 
-    private final JobCoordinatorImpl jobCoordinator = new JobCoordinatorImpl();
+    MetricProvider metricProvider = new TestMetricProviderImpl();
+
+    private final JobCoordinatorImpl jobCoordinator = new JobCoordinatorImpl(metricProvider);
 
     @Before
     public void setUp() {
