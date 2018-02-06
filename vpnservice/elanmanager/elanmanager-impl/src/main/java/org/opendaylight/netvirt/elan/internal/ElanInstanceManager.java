ac93463bcd5e1b28713fd492fd8a25db7d4d20f6
eswanit <swati.udhavrao.niture@ericsson.com>
2018-02-06 23:50:21 +0530

    [WiP] Bug 9247 - Conflicting modification for path...
    *Conflicting path modification exception is seen for
    elan forwarding tables when two methods, removeElanInterface
    and removeElanStateForInterface, are trying two remove the
    same elan forwarding tables entry
    
    *This is fixed by adding djc for each elan interface for
    remove method
    
    Change-Id: I049659b8828bc2e92345492a3f7b1a807104f004
    Signed-off-by: eswanit <swati.udhavrao.niture@ericsson.com>

--- vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInstanceManager.java
+++ vpnservice/elanmanager/elanmanager-impl/src/main/java/org/opendaylight/netvirt/elan/internal/ElanInstanceManager.java
@@ -11,6 +11,7 @@ package org.opendaylight.netvirt.elan.internal;
 import static java.util.Collections.emptyList;
 
 import com.google.common.base.Optional;
+import com.google.common.util.concurrent.ListenableFuture;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -82,14 +83,19 @@ public class ElanInstanceManager extends AsyncDataTreeChangeListenerBase<ElanIns
         if (existingElan != null) {
             List<String> elanInterfaces = existingElan.getElanInterfaces();
             if (elanInterfaces != null && !elanInterfaces.isEmpty()) {
-                for (String elanInterfaceName : elanInterfaces) {
-                    InstanceIdentifier<ElanInterface> elanInterfaceId = ElanUtils
-                            .getElanInterfaceConfigurationDataPathId(elanInterfaceName);
-                    InterfaceInfo interfaceInfo = interfaceManager.getInterfaceInfo(elanInterfaceName);
-                    elanInterfaceManager.removeElanInterface(deletedElan, elanInterfaceName, interfaceInfo);
-                    ElanUtils.delete(broker, LogicalDatastoreType.CONFIGURATION,
-                            elanInterfaceId);
-                }
+                List<ListenableFuture<Void>> futureList = new ArrayList<>();
+                elanInterfaces.forEach(elanInterfaceName -> {
+                    jobCoordinator.enqueueJob(ElanUtils.getElanInterfaceJobKey(elanInterfaceName), () -> {
+                        InstanceIdentifier<ElanInterface> elanInterfaceId = ElanUtils
+                                .getElanInterfaceConfigurationDataPathId(elanInterfaceName);
+                        InterfaceInfo interfaceInfo = interfaceManager.getInterfaceInfo(elanInterfaceName);
+                        futureList.addAll(elanInterfaceManager.removeElanInterface(deletedElan, elanInterfaceName,
+                                interfaceInfo));
+                        ElanUtils.delete(broker, LogicalDatastoreType.CONFIGURATION,
+                                elanInterfaceId);
+                        return futureList;
+                    },ElanConstants.JOB_MAX_RETRIES);
+                });
             }
             ElanUtils.delete(broker, LogicalDatastoreType.OPERATIONAL,
                     ElanUtils.getElanInstanceOperationalDataPath(elanName));
