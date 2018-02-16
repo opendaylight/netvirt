7eb2d6b78411af4042b9e361ebc3ae09bb071005
Faseela K <faseela.k@ericsson.com>
2018-02-16 14:00:11 +0530

    NETVIRT-1122 - L3 ping between pods not working
    Change-Id: Ia89276382b27292f5891d958851380a4637b0508
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

---
+++ coe/impl/src/main/java/org/opendaylight/netvirt/coe/caches/PodsCache.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.coe.caches;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.mdsalutil.cache.DataObjectCache;
+import org.opendaylight.infrautils.caches.CacheProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.coe.northbound.pod.rev170611.Coe;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.coe.northbound.pod.rev170611.coe.Pods;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+@Singleton
+public class PodsCache extends DataObjectCache<Pods> {
+    @Inject
+    public PodsCache(DataBroker dataBroker, CacheProvider cacheProvider) {
+        super(Pods.class, dataBroker, LogicalDatastoreType.CONFIGURATION,
+                InstanceIdentifier.create(Coe.class).child(Pods.class), cacheProvider);
+    }
+}
