2c353536f8392d2a9e5e95deb2a664dde66d633b
Shashidhar Raja <shashidharr@altencalsoftlabs.com>
2018-03-02 15:52:03 +0530

    Service-Recovery: Below service recovery actions are supported for ACL (a) ACL service recovery: As part of this, all ACL listeners are re-registered (unregistered and registerd again) (b) ACL Interface recovery (c) ACL Instance recovery
    Updated with ACL-Instance recovery changes.
    
    Service recovery actions supported are defined in SRM module with below
    patch set:
    https://git.opendaylight.org/gerrit/#/c/68987/
    
    Depends-On: I4c91cd52e7595b92b494356dfa715dbdf15da3c5
    
    Change-Id: I21be0f9ed05eb3bf17ddb422ed10196e122b5cc8
    Signed-off-by: Shashidhar Raja <shashidharr@altencalsoftlabs.com>
    Signed-off-by: kiranvasudeva <kirankumar.v@altencalsoftlabs.com>

--- aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/listeners/AclNodeListener.java
+++ aclservice/impl/src/main/java/org/opendaylight/netvirt/aclservice/listeners/AclNodeListener.java
@@ -20,6 +20,8 @@ import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
 import org.opendaylight.genius.infra.ManagedNewTransactionRunnerImpl;
 import org.opendaylight.genius.mdsalutil.MDSALUtil;
 import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.genius.srm.RecoverableListener;
+import org.opendaylight.genius.srm.ServiceRecoveryRegistry;
 import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
 import org.opendaylight.netvirt.aclservice.utils.AclConstants;
 import org.opendaylight.netvirt.aclservice.utils.AclNodeDefaultFlowsTxBuilder;
@@ -39,7 +41,8 @@ import org.slf4j.LoggerFactory;
  * during when node is discovered.
  */
 @Singleton
-public class AclNodeListener extends AsyncDataTreeChangeListenerBase<FlowCapableNode, AclNodeListener> {
+public class AclNodeListener extends AsyncDataTreeChangeListenerBase<FlowCapableNode, AclNodeListener>
+        implements RecoverableListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(AclNodeListener.class);
 
@@ -54,7 +57,8 @@ public class AclNodeListener extends AsyncDataTreeChangeListenerBase<FlowCapable
 
     @Inject
     public AclNodeListener(final IMdsalApiManager mdsalManager, DataBroker dataBroker, AclserviceConfig config,
-            AclServiceUtils aclServiceUtils, JobCoordinator jobCoordinator) {
+            AclServiceUtils aclServiceUtils, JobCoordinator jobCoordinator,
+            ServiceRecoveryRegistry serviceRecoveryRegistry) {
         super(FlowCapableNode.class, AclNodeListener.class);
 
         this.mdsalManager = mdsalManager;
@@ -63,6 +67,7 @@ public class AclNodeListener extends AsyncDataTreeChangeListenerBase<FlowCapable
         this.config = config;
         this.aclServiceUtils = aclServiceUtils;
         this.jobCoordinator = jobCoordinator;
+        serviceRecoveryRegistry.addRecoverableListener(AclServiceUtils.getRecoverServiceRegistryKey(), this);
     }
 
     @Override
@@ -72,9 +77,20 @@ public class AclNodeListener extends AsyncDataTreeChangeListenerBase<FlowCapable
         if (config != null) {
             this.securityGroupMode = config.getSecurityGroupMode();
         }
+        registerListener();
+        LOG.info("AclserviceConfig: {}", this.config);
+    }
+
+    @Override
+    public void registerListener() {
         this.aclServiceUtils.createRemoteAclIdPool();
         registerListener(LogicalDatastoreType.OPERATIONAL, dataBroker);
-        LOG.info("AclserviceConfig: {}", this.config);
+    }
+
+    @Override
+    public  void deregisterListener() {
+        super.deregisterListener();
+        this.aclServiceUtils.deleteRemoteAclIdPool();
     }
 
     @Override
