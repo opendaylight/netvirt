cda80a0303e6c7edc3ba517418783b8f3d51d6ca
Stephen Kitt <skitt@redhat.com>
2018-03-02 11:15:57 +0100

    Use BgpConfigurationManager instead of BgpManager
    Most of BgpManager’s methods are delegations to
    BgpConfigurationManager, which can be replaced by direct calls to
    BgpConfigurationManager. This patch implements as many replacements as
    are viable, within bgpmanager.
    
    Change-Id: Id4b42b4ce6bd50dcb44482f4e5dd4cc9d1ecaf8c
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- bgpmanager/impl/src/main/java/org/opendaylight/netvirt/bgpmanager/BgpConfigurationManager.java
+++ bgpmanager/impl/src/main/java/org/opendaylight/netvirt/bgpmanager/BgpConfigurationManager.java
@@ -2903,7 +2903,7 @@ public class BgpConfigurationManager {
                     TimeUnit.MILLISECONDS);
             LOG.info("Bgp Counters task scheduled for every two minutes.");
 
-            bgpManager.setQbgpLog(BgpConstants.BGP_DEF_LOG_FILE, BgpConstants.BGP_DEF_LOG_LEVEL);
+            addLogging(BgpConstants.BGP_DEF_LOG_FILE, BgpConstants.BGP_DEF_LOG_LEVEL);
         }
     }
 
