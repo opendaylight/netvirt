957bf66aec9681a67f5c72ee93374f17049e5746
Vinh Nguyen <vinh.nguyen@hcl.com>
2018-03-05 18:14:56 -0800

    NETVIRT-1080 : Fix PNF discovery failure after FIP detached
    Problem:
      In conntrack NAT mode, deleting last FIP port on dpn also
      deleting the PNF flow entries ion the OVS node.
    Solution:
      The problem is caused by missing ExternalFixedIP entry
      in the vpn-to-dpn-list for the external subnet on the
      NAPT switch. The existence of this entry will prevent
      deleting the PNF flows when the FIP is disassociated.
      The patch fix the problem by adding/deleting the
      ExternalFixedIP entry when the external subnet is
      associated/disassociated with the router.
    
    Change-Id: Ib46e860448138c6808105573ae4c52be0bc2b437
    Signed-off-by: Vinh Nguyen <vinh.nguyen@hcl.com>

--- natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/FlatVlanConntrackBasedSnatService.java
+++ natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/FlatVlanConntrackBasedSnatService.java
@@ -12,6 +12,7 @@ import java.math.BigInteger;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
 import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.netvirt.vpnmanager.api.IVpnFootprintService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.OdlInterfaceRpcService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rpcs.rev160406.ItmRpcService;
@@ -27,9 +28,10 @@ public class FlatVlanConntrackBasedSnatService extends ConntrackBasedSnatService
     public FlatVlanConntrackBasedSnatService(DataBroker dataBroker, IMdsalApiManager mdsalManager,
                                              ItmRpcService itmManager, OdlInterfaceRpcService odlInterfaceRpcService,
                                              IdManagerService idManager, NAPTSwitchSelector naptSwitchSelector,
-                                             IInterfaceManager interfaceManager) {
+                                             IInterfaceManager interfaceManager,
+                                             IVpnFootprintService vpnFootprintService) {
         super(dataBroker, mdsalManager, itmManager, idManager, naptSwitchSelector,
-                odlInterfaceRpcService, interfaceManager);
+                odlInterfaceRpcService, interfaceManager, vpnFootprintService);
     }
 
     @Override
