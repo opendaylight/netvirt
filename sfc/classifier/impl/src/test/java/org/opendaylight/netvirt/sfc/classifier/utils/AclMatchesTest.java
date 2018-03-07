442274eab29e37433f47db46070093c25980f3bd
Jaime Caamaño Ruiz <jcaamano@suse.com>
2018-03-07 10:49:42 +0100

    NETVIRT-1139: Invert matches for reverse path classification
    When an ACL translated from a flow classifier includes a logical
    destination port, ACL matches should be inverted to classify traffic on
    that port to the reverse path, while the original matches are used to
    classify traffic on the logical source port to the forward path.
    
    Change-Id: Iaaff8106d9bfa6d1e8bd346b9c6f2f0ff0659817
    JIRA: NETVIRT-1139
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@suse.com>
    (cherry picked from commit 5beeb65)
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@suse.com>

--- sfc/classifier/impl/src/test/java/org/opendaylight/netvirt/sfc/classifier/utils/AclMatchesTest.java
+++ sfc/classifier/impl/src/test/java/org/opendaylight/netvirt/sfc/classifier/utils/AclMatchesTest.java
@@ -9,22 +9,30 @@
 package org.opendaylight.netvirt.sfc.classifier.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 import org.opendaylight.genius.mdsalutil.NwConstants;
 import org.opendaylight.genius.mdsalutil.packet.IPProtocols;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.Matches;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.MatchesBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.AceEth;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.AceEthBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.AceIp;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.AceIpBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.ace.ip.ace.ip.version.AceIpv4;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.ace.ip.ace.ip.version.AceIpv4Builder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.ace.ip.ace.ip.version.AceIpv6;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.ace.ip.ace.ip.version.AceIpv6Builder;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Dscp;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Prefix;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv6Prefix;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.packet.fields.rev160218.acl.transport.header.fields.DestinationPortRange;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.packet.fields.rev160218.acl.transport.header.fields.DestinationPortRangeBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.packet.fields.rev160218.acl.transport.header.fields.SourcePortRange;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.packet.fields.rev160218.acl.transport.header.fields.SourcePortRangeBuilder;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.MatchBuilder;
@@ -255,4 +263,118 @@ public class AclMatchesTest {
         assertNull(matchBuilder.getLayer4Match());
     }
 
+    @Test
+    public void invertEthMatchTest() {
+        AceEthBuilder aceEthBuilder = new AceEthBuilder();
+        aceEthBuilder.setDestinationMacAddress(new MacAddress(MAC_DST_STR));
+        aceEthBuilder.setSourceMacAddress(new MacAddress(MAC_SRC_STR));
+        AceEth aceEth = aceEthBuilder.build();
+
+        MatchesBuilder matchesBuilder = new MatchesBuilder();
+        matchesBuilder.setAceType(aceEth);
+        Matches matches = matchesBuilder.build();
+
+        Matches invertedMatches = AclMatches.invertMatches(matches);
+
+        assertNotEquals(matches, invertedMatches);
+
+        AceEth invertedAceEth = (AceEth) invertedMatches.getAceType();
+        assertEquals(invertedAceEth.getDestinationMacAddress(), aceEth.getSourceMacAddress());
+        assertEquals(invertedAceEth.getSourceMacAddress(), aceEth.getDestinationMacAddress());
+    }
+
+    @Test
+    public void invertIpv4MatchTest() {
+        AceIpBuilder aceIpBuilder = new AceIpBuilder();
+        aceIpBuilder.setProtocol(IPProtocols.UDP.shortValue());
+        aceIpBuilder.setDscp(new Dscp(DSCP_VALUE));
+
+        DestinationPortRangeBuilder dstPortRange = new DestinationPortRangeBuilder();
+        dstPortRange.setLowerPort(new PortNumber(UDP_DST_LOWER_PORT));
+        dstPortRange.setUpperPort(new PortNumber(UDP_DST_UPPER_PORT));
+        DestinationPortRange destinationPortRange = dstPortRange.build();
+        aceIpBuilder.setDestinationPortRange(destinationPortRange);
+
+        AceIpv4Builder aceIpv4Builder = new AceIpv4Builder();
+        aceIpv4Builder.setDestinationIpv4Network(new Ipv4Prefix(IPV4_DST_STR));
+        aceIpv4Builder.setSourceIpv4Network(new Ipv4Prefix(IPV4_SRC_STR));
+        AceIpv4 aceIpv4 = aceIpv4Builder.build();
+        aceIpBuilder.setAceIpVersion(aceIpv4);
+
+        MatchesBuilder matchesBuilder = new MatchesBuilder();
+        AceIp aceIp = aceIpBuilder.build();
+        matchesBuilder.setAceType(aceIp);
+
+        Matches matches = matchesBuilder.build();
+
+        Matches invertedMatches = AclMatches.invertMatches(matches);
+
+        assertNotEquals(matches, invertedMatches);
+
+        AceIp invertedAceIp = (AceIp) invertedMatches.getAceType();
+        assertEquals(invertedAceIp.getDscp(), aceIp.getDscp());
+        assertEquals(invertedAceIp.getProtocol(), aceIp.getProtocol());
+
+        DestinationPortRange invertedDestinationPortRange = invertedAceIp.getDestinationPortRange();
+        assertNull(invertedDestinationPortRange);
+
+        SourcePortRange invertedSourcePortRange = invertedAceIp.getSourcePortRange();
+        assertEquals(invertedSourcePortRange.getLowerPort(), destinationPortRange.getLowerPort());
+        assertEquals(invertedSourcePortRange.getUpperPort(), destinationPortRange.getUpperPort());
+
+        AceIpv4 invertedAceIpv4 = (AceIpv4) invertedAceIp.getAceIpVersion();
+        assertEquals(invertedAceIpv4.getDestinationIpv4Network(), aceIpv4.getSourceIpv4Network());
+        assertEquals(invertedAceIpv4.getSourceIpv4Network(), aceIpv4.getDestinationIpv4Network());
+    }
+
+    @Test
+    public void invertIpv6MatchTest() {
+        AceIpBuilder aceIpBuilder = new AceIpBuilder();
+        aceIpBuilder.setProtocol(IPProtocols.TCP.shortValue());
+        aceIpBuilder.setDscp(new Dscp(DSCP_VALUE));
+
+        SourcePortRangeBuilder srcPortRange = new SourcePortRangeBuilder();
+        srcPortRange.setLowerPort(new PortNumber(TCP_SRC_LOWER_PORT));
+        srcPortRange.setUpperPort(new PortNumber(TCP_SRC_UPPER_PORT));
+        SourcePortRange sourcePortRange = srcPortRange.build();
+        aceIpBuilder.setSourcePortRange(sourcePortRange);
+
+        DestinationPortRangeBuilder dstPortRange = new DestinationPortRangeBuilder();
+        dstPortRange.setLowerPort(new PortNumber(TCP_DST_LOWER_PORT));
+        dstPortRange.setUpperPort(new PortNumber(TCP_DST_UPPER_PORT));
+        DestinationPortRange destinationPortRange = dstPortRange.build();
+        aceIpBuilder.setDestinationPortRange(destinationPortRange);
+
+        AceIpv6Builder aceIpv6Builder = new AceIpv6Builder();
+        aceIpv6Builder.setDestinationIpv6Network(new Ipv6Prefix(IPV6_DST_STR));
+        aceIpv6Builder.setSourceIpv6Network(new Ipv6Prefix(IPV6_SRC_STR));
+        AceIpv6 aceIpv6 = aceIpv6Builder.build();
+        aceIpBuilder.setAceIpVersion(aceIpv6);
+
+        MatchesBuilder matchesBuilder = new MatchesBuilder();
+        AceIp aceIp = aceIpBuilder.build();
+        matchesBuilder.setAceType(aceIp);
+
+        Matches matches = matchesBuilder.build();
+
+        Matches invertedMatches = AclMatches.invertMatches(matches);
+
+        assertNotEquals(matches, invertedMatches);
+
+        AceIp invertedAceIp = (AceIp) invertedMatches.getAceType();
+        assertEquals(invertedAceIp.getDscp(), aceIp.getDscp());
+        assertEquals(invertedAceIp.getProtocol(), aceIp.getProtocol());
+
+        DestinationPortRange invertedDestinationPortRange = invertedAceIp.getDestinationPortRange();
+        assertEquals(invertedDestinationPortRange.getLowerPort(), sourcePortRange.getLowerPort());
+        assertEquals(invertedDestinationPortRange.getUpperPort(), sourcePortRange.getUpperPort());
+
+        SourcePortRange invertedSourcePortRange = invertedAceIp.getSourcePortRange();
+        assertEquals(invertedSourcePortRange.getLowerPort(), destinationPortRange.getLowerPort());
+        assertEquals(invertedSourcePortRange.getUpperPort(), destinationPortRange.getUpperPort());
+
+        AceIpv6 invertedAceIpv6 = (AceIpv6) invertedAceIp.getAceIpVersion();
+        assertEquals(invertedAceIpv6.getDestinationIpv6Network(), aceIpv6.getSourceIpv6Network());
+        assertEquals(invertedAceIpv6.getSourceIpv6Network(), aceIpv6.getDestinationIpv6Network());
+    }
 }
