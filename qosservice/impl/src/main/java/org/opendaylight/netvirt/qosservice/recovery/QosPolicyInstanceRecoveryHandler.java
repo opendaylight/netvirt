9e76710a8484e6204d4782bc62463b6536c28e8b
Naveen Kumar Verma <naveen.kumar.verma@ericsson.com>
2018-03-09 15:35:18 +0530

    Service Recover for QoS Service as a whole & QoS Policy Instance.
    For QoS service Recovery, listeners in the QoS service will be
    re-regisered. Following command will do service recovery for QoS
    service.
    -----
    srm:recover service qos
    -----
    
    For Qos Policy Instance Recovery, The policy id giving in the following
    command will be reapplied on all the networks and port on which it was
    applied earlier.
    -----
    srm:recover instance policy <policy-id>
    -----
    
    Change-Id: If02bc0a610d216f4387832ac21dac80aafb7d70e
    Signed-off-by: Naveen Kumar Verma <naveen.kumar.verma@ericsson.com>
    (cherry picked from commit 5ed03b3f0719cb75046f311d105a588ece4a49ec)

---
+++ qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/recovery/QosPolicyInstanceRecoveryHandler.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.qosservice.recovery;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.genius.srm.ServiceRecoveryInterface;
+import org.opendaylight.genius.srm.ServiceRecoveryRegistry;
+import org.opendaylight.netvirt.qosservice.QosPolicyChangeListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.NetvirtQosPolicyInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Singleton
+public class QosPolicyInstanceRecoveryHandler implements ServiceRecoveryInterface {
+
+    private static final Logger LOG = LoggerFactory.getLogger(QosPolicyInstanceRecoveryHandler.class);
+
+    private final QosPolicyChangeListener qosPolicyChangeListener;
+
+    @Inject
+    public QosPolicyInstanceRecoveryHandler(ServiceRecoveryRegistry serviceRecoveryRegistry,
+                                            QosPolicyChangeListener qosPolicyChangeListener) {
+
+        LOG.info("Registering for recovery of QosPolicy Instance");
+        this.qosPolicyChangeListener = qosPolicyChangeListener;
+        serviceRecoveryRegistry.registerServiceRecoveryRegistry(buildServiceRegistryKey(), this);
+    }
+
+    @Override
+    public void recoverService(String entityId) {
+        LOG.info("Recover Qos Policy instance {}", entityId);
+        qosPolicyChangeListener.reapplyPolicy(entityId);
+    }
+
+    private String buildServiceRegistryKey() {
+        return NetvirtQosPolicyInstance.class.toString();
+    }
+}
