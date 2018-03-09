e17a8822683d045dd21db0e9f080970bd7dadc67
Naveen Kumar Verma <naveen.kumar.verma@ericsson.com>
2018-03-09 15:35:18 +0530

    Service Recover for QoS Service as a whole & QoS Policy Instance.
    For QoS service Recovery, listeners in the QoS service will be
    re-regisered. Following command will do service recovery for QoS
    service.
    -----
    srm:recover service qos
    -----
    
    For Qos Policy Instance Recovery, The policy id giving in the following
    command will be reapplied on all the networks and port on which it was
    applied earlier.
    -----
    srm:recover instance policy <policy-id>
    -----
    
    Change-Id: If02bc0a610d216f4387832ac21dac80aafb7d70e
    Signed-off-by: Naveen Kumar Verma <naveen.kumar.verma@ericsson.com>

---
+++ qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/recovery/QosServiceRecoveryHandler.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.qosservice.recovery;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.genius.srm.ServiceRecoveryInterface;
+import org.opendaylight.genius.srm.ServiceRecoveryRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.srm.types.rev170711.NetvirtQos;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Singleton
+public class QosServiceRecoveryHandler implements ServiceRecoveryInterface {
+
+    private static final Logger LOG = LoggerFactory.getLogger(QosServiceRecoveryHandler.class);
+    private final ServiceRecoveryRegistry serviceRecoveryRegistry;
+
+    @Inject
+    public QosServiceRecoveryHandler(final ServiceRecoveryRegistry serviceRecoveryRegistry) {
+        LOG.info("Registering Qos for service recovery");
+        this.serviceRecoveryRegistry = serviceRecoveryRegistry;
+        serviceRecoveryRegistry.registerServiceRecoveryRegistry(buildServiceRegistryKey(), this);
+    }
+
+    @Override
+    public void recoverService(final String entityId) {
+        LOG.info("recover QOS service by de-registering and registering all relevant listeners");
+        deregisterListeners();
+        registerListeners();
+    }
+
+    private void deregisterListeners() {
+        LOG.trace("De-Registering QOS Listeners for recovery");
+        serviceRecoveryRegistry.getRecoverableListeners(buildServiceRegistryKey())
+            .forEach((recoverableListener -> recoverableListener.deregisterListener()));
+    }
+
+    private void registerListeners() {
+        LOG.trace("Re-Registering QOS Listeners for recovery");
+        serviceRecoveryRegistry.getRecoverableListeners(buildServiceRegistryKey())
+            .forEach((recoverableListener -> recoverableListener.registerListener()));
+    }
+
+    public String buildServiceRegistryKey() {
+        return NetvirtQos.class.toString();
+    }
+}
