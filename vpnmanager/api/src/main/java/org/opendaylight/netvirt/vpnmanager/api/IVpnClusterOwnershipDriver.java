d986c333a2b8ab508e61867e2358851f3ef5897a
enavive <vivek.konnect@gmail.com>
2018-03-21 16:02:24 +0530

    WIP: Provide a class in VPN to manage cluster ownership.
    The cluster ownership management can be accomplished
    by few approaches.  However, this initial review
    attempts to decouple the ownershipmanager from the
    rest of the vpn system.
    
    Area 1
    Review #1
    Refer l3vpn-robustness spec: https://git.opendaylight.org/gerrit/69526
    
    Change-Id: Id3f4258ea960728f438d09d41e38a4a677f9362f
    Signed-off-by: Vivekanandan Narasimhan <vivek.konnect@gmail.com>

---
+++ vpnmanager/api/src/main/java/org/opendaylight/netvirt/vpnmanager/api/IVpnClusterOwnershipDriver.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2015, 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.vpnmanager.api;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.genius.mdsalutil.MatchInfoBase;
+import org.opendaylight.netvirt.fibmanager.api.RouteOrigin;
+import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.vpn.instances.VpnInstance;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.vrfentries.VrfEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.adjacency.list.Adjacency;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.neutron.vpn.portip.port.data.VpnPortipToPort;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.subnetmaps.Subnetmap;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.List;
+
+
+public interface IVpnClusterOwnershipDriver {
+    boolean amIOwner();
+}
+
