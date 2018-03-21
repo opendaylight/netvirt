f7c545270cf6812f251fd2ff148740d974a1c918
enavive <vivek.konnect@gmail.com>
2018-03-21 16:02:24 +0530

    Provide a class in VPN to manage cluster ownership.
    The cluster ownership management can be accomplished
    by few approaches.  However, this initial review
    attempts to decouple the ownershipmanager from the
    rest of the vpn system.
    
    Area 1
    Review #1
    Refer l3vpn-robustness spec: https://git.opendaylight.org/gerrit/69526
    
    Change-Id: Id3f4258ea960728f438d09d41e38a4a677f9362f
    Signed-off-by: Vivekanandan Narasimhan <vivek.konnect@gmail.com>

---
+++ vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnClusterShardBasedOwnershipDriver.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015 - 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+/*
+ * Copyright © 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.vpnmanager;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.mdsal.eos.binding.api.EntityOwnershipService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Singleton
+public class VpnClusterShardBasedOwnershipDriver extends VpnClusterOwnershipDriverBase {
+
+    private static final Logger LOG = LoggerFactory.getLogger(VpnClusterShardBasedOwnershipDriver.class);
+
+    // This file resides to provide an implementation where it will drive amIOwner based on where
+    // the Default Operational Shard (or) Default Config is placed in the 3 PL environment
+    @Inject
+    public VpnClusterShardBasedOwnershipDriver(final EntityOwnershipService entityOwnershipService) {
+    }
+
+    @PostConstruct
+    public void start() {
+        LOG.info("{} start", getClass().getSimpleName());
+    }
+
+    @Override
+    @PreDestroy
+    public void close() throws Exception {
+        LOG.info("{} closed", getClass().getSimpleName());
+    }
+}
