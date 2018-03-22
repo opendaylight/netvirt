4a919c2915947025f8738649b653b2f416c65bf8
xcheara <chetan.arakere@altencalsoftlabs.com>
2018-03-22 12:49:31 +0530

    Retain NAPT Switch after Upgrade for SNAT
    Test Scenario1: All OVS are connected back after upgrade.
    
    1) Take the back-up of the Config DS and bring down the controller.
    2) Disconnect OVS(by blocking port 6640 & 6653)
    3) Upgrade ODL, set upgrade-flag=true and reload the config DS.
    4) Start connecting back the OVS with first connecting non-napt switches
    followed by Napt-Switches.
    5) set upgrade-flag=false once all the OVS are connected by to ODL.
    6) Verify earlier Napt Switches continues to work as Napt Switches and all
    earlier on-going SNAT session and new sessions are not impacted.
    
    Test Scenario2: Few OVS(which are elected as Napt) are not connected back
    after upgrade.
    
    1) Take the back-up of the Config DS and bring down the controller.
    2) Disconnect OVS(by blocking port 6640 & 6653)
    3) Upgrade ODL, set upgrade-flag=true and reload the config DS.
    4) Start connecting back the OVS with first connecting non-napt switches
    and do not connect few OVS elected as Napt Switches earlier.
    5) set upgrade-flag=false.
    6) Verify a new Napt Switch is elected for those router whose Napt
    Switches are not been connected back after upgrade.
    7) Verify new SNAT sessions are established and earlier on-going SNAT
    session gets migrated to newly elected Napt Switch.
    
    JIRA: NETVIRT-1222
    Change-Id: I194124596e987c43703be37707235a6c88c91013
    Signed-off-by: xcheara <chetan.arakere@altencalsoftlabs.com>
    Signed-off-by: shaik <shaik.b@altencalsoftlabs.com>
    Signed-off-by: xcheara <chetan.arakere@altencalsoftlabs.com>

--- natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/UpgradeStateListener.java
+++ natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/UpgradeStateListener.java
@@ -8,6 +8,14 @@
 
 package org.opendaylight.netvirt.natservice.internal;
 
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import static org.opendaylight.genius.infra.Datastore.CONFIGURATION;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
@@ -15,15 +23,28 @@ import javax.inject.Singleton;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunnerImpl;
+import org.opendaylight.genius.infra.TypedReadWriteTransaction;
+import org.opendaylight.genius.infra.TypedWriteTransaction;
+import org.opendaylight.genius.infra.Datastore.Configuration;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
 import org.opendaylight.netvirt.natservice.api.CentralizedSwitchScheduler;
 import org.opendaylight.serviceutils.tools.mdsal.listener.AbstractClusteredSyncDataTreeChangeListener;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.config.rev170206.NatserviceConfig;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ExtRouters;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.NaptSwitches;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ProviderTypes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ext.routers.Routers;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ext.routers.routers.ExternalIps;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.napt.switches.RouterToNaptSwitch;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.mdsalutil.rev170830.Config;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
@@ -36,15 +57,29 @@ public class UpgradeStateListener extends AbstractClusteredSyncDataTreeChangeLis
     private final DataBroker dataBroker;
     private final CentralizedSwitchScheduler centralizedSwitchScheduler;
     private final NatserviceConfig.NatMode natMode;
+    private SNATDefaultRouteProgrammer defaultRouteProgrammer;
+    private IMdsalApiManager mdsalManager;
+    private IdManagerService idManager;
+    private NaptSwitchHA naptSwitchHA;
+    private final JobCoordinator coordinator;
+    private final ManagedNewTransactionRunner txRunner;
 
     @Inject
     public UpgradeStateListener(final DataBroker dataBroker,
                                 final CentralizedSwitchScheduler centralizedSwitchScheduler,
-                                final NatserviceConfig config) {
+                                final SNATDefaultRouteProgrammer defaultRouteProgrammer,
+                                final IMdsalApiManager mdsalManager,
+                                final IdManagerService idManager,
+                                final NaptSwitchHA naptSwitchHA,
+                                final NatserviceConfig config, final JobCoordinator coordinator) {
         super(dataBroker, new DataTreeIdentifier<>(
                 LogicalDatastoreType.CONFIGURATION, InstanceIdentifier.create(Config.class)));
         this.dataBroker = dataBroker;
+        this.txRunner = new ManagedNewTransactionRunnerImpl(dataBroker);
         this.centralizedSwitchScheduler = centralizedSwitchScheduler;
+        this.defaultRouteProgrammer = defaultRouteProgrammer;
+        this.coordinator = coordinator;
+        this.naptSwitchHA = naptSwitchHA;
         if (config != null) {
             this.natMode = config.getNatMode();
         } else {
@@ -59,11 +94,34 @@ public class UpgradeStateListener extends AbstractClusteredSyncDataTreeChangeLis
 
     @Override
     public void remove(@Nonnull Config removedDataObject) {
+        if (natMode == NatserviceConfig.NatMode.Conntrack) {
+            return;
+        }
+        LOG.debug("Verify is all Elected Napt Switch and connected back post upgrade");
     }
 
     @Override
     public void update(@Nonnull Config original, Config updated) {
-        if (natMode != NatserviceConfig.NatMode.Conntrack) {
+        if (natMode == NatserviceConfig.NatMode.Controller) {
+            if (original.isUpgradeInProgress() && !updated.isUpgradeInProgress()) {
+                Optional<NaptSwitches> npatSwitches = NatUtil.getAllPrimaryNaptSwitches(dataBroker);
+                if (npatSwitches.isPresent()) {
+                    for (RouterToNaptSwitch routerToNaptSwitch : npatSwitches.get().getRouterToNaptSwitch()) {
+                        BigInteger primaryNaptDpnId = routerToNaptSwitch.getPrimarySwitchId();
+                        if (!NatUtil.getSwitchStatus(dataBroker, routerToNaptSwitch.getPrimarySwitchId())) {
+                            String routerUuid = routerToNaptSwitch.getRouterName();
+                            coordinator.enqueueJob((NatConstants.NAT_DJC_PREFIX + routerUuid),
+                                    () -> Collections.singletonList(
+                                            txRunner.callWithNewReadWriteTransactionAndSubmit(CONFIGURATION, confTx -> {
+                                    //hndlTepDelForSnatInEachRtr(router, routerId, srcDpnId, srcTepIp, isFipExists,
+                                    //        extNwProvType, removeFlowInvTx);
+                                    reElectNewNaptSwitch(routerUuid, primaryNaptDpnId, confTx);
+                                }
+                            )), NatConstants.NAT_DJC_MAX_RETRIES);
+                        }
+                    }
+                }
+            }
             return;
         }
 
@@ -89,4 +147,67 @@ public class UpgradeStateListener extends AbstractClusteredSyncDataTreeChangeLis
             }
         }
     }
+
+    private void reElectNewNaptSwitch(String routerName, BigInteger primaryNaptDpnId,
+            TypedReadWriteTransaction<Configuration> confTx) {
+        // Check if this is externalRouter else ignore
+        InstanceIdentifier<Routers> extRoutersId = NatUtil.buildRouterIdentifier(routerName);
+        Optional<Routers> routerData =
+                SingleTransactionDataBroker.syncReadOptionalAndTreatReadFailedExceptionAsAbsentOptional(dataBroker,
+                        LogicalDatastoreType.CONFIGURATION, extRoutersId);
+        if (!routerData.isPresent()) {
+            LOG.debug("reElectNewNaptSwitch : SNAT->Ignoring Re-election for router {} since its not External Router",
+                    routerName);
+            return;
+        }
+        Uuid networkId = routerData.get().getNetworkId();
+        if (networkId == null) {
+            LOG.error("hndlTepDelForSnatInEachRtr : SNAT -> Ignoring Re-election  with Napt {} for router {}"
+                    + "as external network configuraton is missing", primaryNaptDpnId, routerName);
+            return;
+        }
+        long routerId = NatUtil.getVpnId(dataBroker, routerName);
+        LOG.debug("hndlTepDelForSnatInEachRtr : SNAT->Router {} is associated with ext nw {}", routerId, networkId);
+        Uuid bgpVpnUuid = NatUtil.getVpnForRouter(dataBroker, routerName);
+        Long bgpVpnId;
+        if (bgpVpnUuid == null) {
+            LOG.debug("hndlTepDelForSnatInEachRtr : SNAT->Internal VPN-ID {} associated to router {}",
+                    routerId, routerName);
+            bgpVpnId = routerId;
+        } else {
+            bgpVpnId = NatUtil.getVpnId(dataBroker, bgpVpnUuid.getValue());
+            if (bgpVpnId == NatConstants.INVALID_ID) {
+                LOG.error("hndlTepDelForSnatInEachRtr :SNAT->Invalid Private BGP VPN ID returned for routerName {}",
+                        routerName);
+                return;
+            }
+        }
+        defaultRouteProgrammer.removeDefNATRouteInDPN(primaryNaptDpnId, bgpVpnId, confTx);
+        if (routerData.get().isEnableSnat()) {
+            LOG.info("hndlTepDelForSnatInEachRtr : SNAT enabled for router {}", routerId);
+
+            long routerVpnId = routerId;
+            if (bgpVpnId != NatConstants.INVALID_ID) {
+                LOG.debug("hndlTepDelForSnatInEachRtr : SNAT -> Private BGP VPN ID (Internal BGP VPN ID) {} "
+                        + "associated to the router {}", bgpVpnId, routerName);
+                routerVpnId = bgpVpnId;
+            } else {
+                LOG.debug("hndlTepDelForSnatInEachRtr : SNAT -> Internal L3 VPN ID (Router ID) {} "
+                        + "associated to the router {}", routerVpnId, routerName);
+            }
+            //Re-elect the other available switch as the NAPT switch and program the NAT flows.
+            ProviderTypes extNwProvType = NatEvpnUtil.getExtNwProvTypeFromRouterName(dataBroker,
+                    routerName, networkId);
+            if (extNwProvType == null) {
+                return;
+            }
+            NatUtil.removeSNATFromDPN(dataBroker, mdsalManager, idManager, naptSwitchHA, primaryNaptDpnId, routerName,
+                    routerId, routerVpnId, extNwProvType, confTx);
+
+        } else {
+            LOG.info("hndlTepDelForSnatInEachRtr : SNAT is not enabled for router {} to handle addDPN event {}",
+                    routerId, primaryNaptDpnId);
+        }
+    }
+
 }
