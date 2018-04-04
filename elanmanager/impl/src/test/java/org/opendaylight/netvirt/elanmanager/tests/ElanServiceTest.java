44518f4ccba5acd520695091d33b78a931c2f026
ashwinbc <ashwin.b.c@ericsson.com>
2018-04-04 14:35:53 +0530

    Issue: Table 51 MAC issue after CSC reboot
    Description: If MAC movement happens after a CSC reboot, the old entry in CSC gets cleared followed by a idle timeout.
    However, this pushes a flow delete to CSS which removes the working flow as well due to same match condition.
    The solution is to construct a flow key with ELAN tag and MAC address, which would update the same flow after MAC movement
    
    Change-Id: Icad172d2c446cbf8bb2e6d12a8a8117b02057925
    Signed-off-by: ashwinbc <ashwin.b.c@ericsson.com>

--- elanmanager/impl/src/test/java/org/opendaylight/netvirt/elanmanager/tests/ElanServiceTest.java
+++ elanmanager/impl/src/test/java/org/opendaylight/netvirt/elanmanager/tests/ElanServiceTest.java
@@ -219,10 +219,7 @@ public class ElanServiceTest extends  ElanServiceTestBase {
 
         // Read and Compare SMAC flow
         String flowId =  new StringBuffer()
-                .append(NwConstants.ELAN_SMAC_TABLE)
                 .append(actualElanInstances.getElanTag())
-                .append(DPN1_ID)
-                .append(interfaceInfo.getInterfaceTag())
                 .append(interfaceInfo.getMacAddress())
                 .toString();
         InstanceIdentifier<Flow> flowInstanceIidSrc = getFlowIid(NwConstants.ELAN_SMAC_TABLE,
@@ -252,10 +249,7 @@ public class ElanServiceTest extends  ElanServiceTestBase {
 
         // Read DMAC Flow in DPN1
         String flowId =  new StringBuffer()
-                .append(NwConstants.ELAN_DMAC_TABLE)
                 .append(actualElanInstances.getElanTag())
-                .append(DPN1_ID)
-                .append(interfaceInfo.getInterfaceTag())
                 .append(interfaceInfo.getMacAddress())
                 .toString();
         InstanceIdentifier<Flow> flowInstanceIidDst = getFlowIid(NwConstants.ELAN_DMAC_TABLE,
@@ -286,11 +280,9 @@ public class ElanServiceTest extends  ElanServiceTestBase {
         addElanInterface(ExpectedObjects.ELAN1, interfaceInfo, DPN2IP1);
 
         // Read and Compare DMAC flow in DPN1 for MAC1 of DPN2
-        String flowId = ElanUtils.getKnownDynamicmacFlowRef((short)51,
-                        DPN1_ID,
-                        DPN2_ID,
-                        interfaceInfo.getMacAddress().toString(),
-                        actualElanInstances.getElanTag());
+        String flowId = ElanUtils.getKnownDynamicmacFlowRef(
+                        actualElanInstances.getElanTag(),
+                        interfaceInfo.getMacAddress().toString());
 
         InstanceIdentifier<Flow> flowInstanceIidDst = getFlowIid(NwConstants.ELAN_DMAC_TABLE,
                 new FlowId(flowId), DPN1_ID);
