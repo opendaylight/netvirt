94ee991ee8794dc86bbe9a8998805cd3fc713c9a
Karthikeyan Krishnan <karthikeyangceb007@gmail.com>
2018-04-05 12:27:57 +0530

    NETVIRT-1171:IPv4 entries appearing in FIB though not associated to Router
    Problem Description:
    ====================
    IPv4 entries are appearing in FIB when subnet is not associated to router
    in a dualstack network
    
    Solution Description:
    =====================
    Dual Stack VM has both IPv4 and IPv6 Address, IPv6 or IPv4 subnet only
    associated to router means it should show only IPv6 or IPv4 address based
    on the subnet address family. To do this behaviour we need to create the
    VPN-Interface adjacency based on the subnet associated to router. Have
    done the code changes accordingly in VPN-Interface adjacency.
    
    Change-Id: Id1a4712dc1b8be0713fe7dcb526555c5e5021e39
    Signed-off-by: Karthikeyan Krishnan <karthikeyangceb007@gmail.com>

--- vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnInterfaceManager.java
+++ vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnInterfaceManager.java
@@ -1514,8 +1514,29 @@ public class VpnInterfaceManager extends AsyncDataTreeChangeListenerBase<VpnInte
                         }
                         for (Adjacency adj : copyOldAdjs) {
                             if (!isBgpVpnInternetVpn || VpnUtil.isAdjacencyEligibleToVpnInternet(dataBroker, adj)) {
-                                delAdjFromVpnInterface(vpnInterfaceOpIdentifier, adj, dpnId,
-                                       writeOperTxn, writeConfigTxn);
+                                if (adj.getAdjacencyType() == AdjacencyType.PrimaryAdjacency
+                                        && !adj.isPhysNetworkFunc()) {
+                                    delAdjFromVpnInterface(vpnInterfaceOpIdentifier, adj, dpnId,
+                                            writeOperTxn, writeConfigTxn);
+                                    Optional<VpnInterfaceOpDataEntry> optVpnInterface = VpnUtil.read(dataBroker,
+                                            LogicalDatastoreType.OPERATIONAL, vpnInterfaceOpIdentifier);
+                                    VpnInterfaceOpDataEntry vpnInterfaceOpDataEntry = optVpnInterface.get();
+                                    long vpnId = VpnUtil.getVpnId(dataBroker, newVpnName);
+                                    VpnUtil.removePrefixToInterfaceAdj(dataBroker, adj, vpnId, vpnInterfaceOpDataEntry,
+                                            writeOperTxn);
+                                    //remove FIB entry
+                                    String vpnRd = VpnUtil.getVpnRd(dataBroker, newVpnName);
+                                    LOG.debug("update: remove prefix {} from the FIB and BGP entry "
+                                            + "for the Vpn-Rd {} ", adj.getIpAddress(), vpnRd);
+                                    //remove BGP entry
+                                    fibManager.removeFibEntry(vpnRd, adj.getIpAddress(), writeConfigTxn);
+                                    if (vpnRd != null && !vpnRd.equalsIgnoreCase(newVpnName)) {
+                                        bgpManager.withdrawPrefix(vpnRd, adj.getIpAddress());
+                                    }
+                                } else {
+                                    delAdjFromVpnInterface(vpnInterfaceOpIdentifier, adj, dpnId,
+                                            writeOperTxn, writeConfigTxn);
+                                }
                             }
                             LOG.info("update: Adjacency {} with nextHop {} label {} subnet {} removed from"
                                 + " vpn interface {} on vpn {}", adj.getIpAddress(), adj.getNextHopIpList(),
@@ -1660,7 +1681,20 @@ public class VpnInterfaceManager extends AsyncDataTreeChangeListenerBase<VpnInte
             L3vpnInput input = new L3vpnInput().setNextHop(adj).setVpnName(vpnName)
                     .setInterfaceName(currVpnIntf.getName()).setPrimaryRd(primaryRd).setRd(primaryRd);
             Adjacency operationalAdjacency = null;
-            if (adj.getNextHopIpList() != null && !adj.getNextHopIpList().isEmpty()) {
+            //Handling dual stack neutron port primary adjacency
+            if (adj.getAdjacencyType() == AdjacencyType.PrimaryAdjacency && !adj.isPhysNetworkFunc()) {
+                LOG.trace("addNewAdjToVpnInterface: Adding prefix {} to existing interface {} for vpn {}", prefix,
+                        currVpnIntf.getName(), vpnName);
+                Interface interfaceState = InterfaceUtils.getInterfaceStateFromOperDS(dataBroker,
+                        currVpnIntf.getName());
+                if (interfaceState != null) {
+                    processVpnInterfaceAdjacencies(dpnId, currVpnIntf.getLportTag().intValue(), vpnName, primaryRd,
+                            currVpnIntf.getName(),
+                            vpnId, writeConfigTxn, writeOperTxn, null, interfaceState);
+                }
+            }
+            if (adj.getNextHopIpList() != null && !adj.getNextHopIpList().isEmpty()
+                    && adj.getAdjacencyType() != AdjacencyType.PrimaryAdjacency) {
                 RouteOrigin origin = adj.getAdjacencyType() == AdjacencyType.PrimaryAdjacency ? RouteOrigin.LOCAL
                         : RouteOrigin.STATIC;
                 String nh = adj.getNextHopIpList().get(0);
