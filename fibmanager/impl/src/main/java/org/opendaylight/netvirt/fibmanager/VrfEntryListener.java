8c80f460415eca4ae267b0f576dac289357f5e70
xcheara <chetan.arakere@altencalsoftlabs.com>
2018-04-13 13:46:07 +0530

    VNI based L3 forwarding support for BGPVPN
    Description: As part of this spec implementation earlier, VNI based
    forwarding for BGPVPN was kept out of scope. This Change addresses to
    support VNI based forwarding of packet over VXLAN tunnel covering Router
    associated and Network Associated BGPVPN and iRT/eRT use-cases.
    
    Additional changes to preventing backpulling of neutron subnet-map to
    retieve network-related attributes in vpn-engine is been addressed.
    
    Handled missing destination mac address in the remote fib flow for
    iRT/eRT use-cases.
    
    Issue : NETVIRT-1209
    
    Change-Id: Ib61d0c02129de980348436cdeba7939488c2da6e
    Signed-off-by: xcheara <chetan.arakere@altencalsoftlabs.com>

--- fibmanager/impl/src/main/java/org/opendaylight/netvirt/fibmanager/VrfEntryListener.java
+++ fibmanager/impl/src/main/java/org/opendaylight/netvirt/fibmanager/VrfEntryListener.java
@@ -885,8 +885,7 @@ public class VrfEntryListener extends AsyncDataTreeChangeListenerBase<VrfEntry, 
                 return Collections.singletonList(txRunner.callWithNewWriteOnlyTransactionAndSubmit(tx -> {
                     baseVrfEntryHandler.makeConnectedRoute(dpnId, vpnId, vrfEntry, rd, instructions,
                             NwConstants.ADD_FLOW, tx, null);
-                    if (!fibUtil.enforceVxlanDatapathSemanticsforInternalRouterVpn(localNextHopInfo.getSubnetId(),
-                            vpnName, rd)) {
+                    if  (!FibUtil.isVxlanNetwork(localNextHopInfo.getNetworkType())) {
                         optLabel.ifPresent(label -> {
                             if (RouteOrigin.value(vrfEntry.getOrigin()) != RouteOrigin.SELF_IMPORTED) {
                                 LOG.debug(
@@ -1133,8 +1132,7 @@ public class VrfEntryListener extends AsyncDataTreeChangeListenerBase<VrfEntry, 
                 () -> Collections.singletonList(txRunner.callWithNewWriteOnlyTransactionAndSubmit(tx -> {
                     baseVrfEntryHandler.makeConnectedRoute(dpnId, vpnId, vrfEntry, rd, null,
                             NwConstants.DEL_FLOW, tx, null);
-                    if (!fibUtil.enforceVxlanDatapathSemanticsforInternalRouterVpn(localNextHopInfo.getSubnetId(),
-                            vpnId, rd)) {
+                    if  (!FibUtil.isVxlanNetwork(localNextHopInfo.getNetworkType())) {
                         if (RouteOrigin.value(vrfEntry.getOrigin()) != RouteOrigin.SELF_IMPORTED) {
                             FibUtil.getLabelFromRoutePaths(vrfEntry).ifPresent(label -> {
                                 makeLFibTableEntry(dpnId, label, null /* instructions */, DEFAULT_FIB_FLOW_PRIORITY,
