1c47173b3aa6057325f1b7ae33d81d7bfe755c99
Dhiman <dhiman.ghosh@ericsson.com>
2018-04-20 16:45:35 +0530

    ECMP-fib entry not populated correctly
    1. File: NeutronRouterChangeListener
    - During Router Update, conslidating the original and updated routes, the logic to calculation of the Routes to be added and Routes to be deleted were incorrect. It was deleting everything from the list containing Routes to be deleted.
    
    2. File: VpnInterfaceManager
    - While deletion of adjucencies from odl-l3vpn:vpn-interface-op-data datastore, merge call was not removing it. Now usign delete instead, which is working.
    
    3. File: VpnUtils.jaba
    - Added InstanceIdentifier builder for reaching the correct adjacency which would be deleted after merge.
    
    Change-Id: I7374470ce4609be78782b25062dbe83ea9a48adb
    Signed-off-by: Dhiman <dhiman.ghosh@ericsson.com>

--- vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnUtil.java
+++ vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnUtil.java
@@ -1578,6 +1578,16 @@ public final class VpnUtil {
                 .augmentation(Adjacencies.class).child(Adjacency.class, new AdjacencyKey(ipAddress)).build();
     }
 
+    static InstanceIdentifier<Adjacency> getVpnInterfaceOpDataEntryAdjacencyIdentifier(String intfName,
+                                                                                       String vpnName,
+                                                                                       String ipAddress) {
+        LOG.debug("getVpnInterfaceO pDataEntryAdjacencyIdentifier intfName {}, vpnName {}, ipAddress {}",
+                intfName, vpnName, ipAddress);
+        return InstanceIdentifier.builder(VpnInterfaceOpData.class)
+                    .child(VpnInterfaceOpDataEntry.class, new VpnInterfaceOpDataEntryKey(intfName, vpnName))
+                    .augmentation(AdjacenciesOp.class).child(Adjacency.class, new AdjacencyKey(ipAddress)).build();
+    }
+
     public static List<String> getIpsListFromExternalIps(List<ExternalIps> externalIps) {
         if (externalIps == null) {
             return Collections.emptyList();
