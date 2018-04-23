87044a50e047fbf62a2a75ec5947a0305e5e5d05
Sam Hague <shague@redhat.com>
2018-04-23 09:08:44 -0400

    change logger format
    Change-Id: Ib937127e05f9e735b9e8e28fb3f9cf050e667a75
    Signed-off-by: Sam Hague <shague@redhat.com>

--- resources/tools/odltools/odltools/logg.py
+++ resources/tools/odltools/odltools/logg.py
@@ -4,7 +4,7 @@ import logging
 class Logger:
     def __init__(self):
         logger = logging.getLogger()
-        formatter = logging.Formatter('%(asctime)s - %(levelname).3s - %(name)-20s - %(lineno)04d - %(message)s')
+        formatter = logging.Formatter('%(asctime)s | %(levelname).3s | %(name)-20s | %(lineno)04d | %(message)s')
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
         ch.setFormatter(formatter)
