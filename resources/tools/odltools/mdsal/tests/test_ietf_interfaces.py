7de8f2aec5dfe614e0537e37a5e30c021223e982
Sam Hague <shague@redhat.com>
2018-04-23 12:07:36 -0400

    integrate model operations
    Change-Id: Ie1dc0946bbe906549a0ea71e59adec69cd2171f8
    Signed-off-by: Sam Hague <shague@redhat.com>

---
+++ resources/tools/odltools/mdsal/tests/test_ietf_interfaces.py
@@ -0,0 +1,22 @@
+import unittest
+from mdsal.model import Model
+from mdsal.ietf_interfaces import interfaces
+from odltools import logg
+
+ip = "127.0.0.1"
+port = "8080"
+path = "./resources"
+# path = "/tmp/robotjob/s1-t1_Create_VLAN_Network_net_1/models"
+
+
+class TestIetfInterfaces(unittest.TestCase):
+    def setUp(self):
+        logg.Logger()
+        self.interfaces = interfaces(Model.CONFIG, ip, port, path)
+
+    def test_get_interfaces_by_key(self):
+        d = self.interfaces.get_interfaces_by_key()
+        self.assertIsNotNone(d['tun95fee4d7132'])
+
+if __name__ == '__main__':
+    unittest.main()
--- resources/tools/odltools/mdsal/tests/test_itm_state.py
+++ resources/tools/odltools/mdsal/tests/test_itm_state.py
@@ -1,34 +1,36 @@
 import unittest
 from mdsal import itm_state
-from mdsal.models import Model
+from mdsal.model import Model
 from mdsal.itm_state import DpnEndpoints
 from odltools import logg
 
 ip = "127.0.0.1"
 port = "8080"
+path = "./resources"
+# path = "/tmp/robotjob/s1-t1_Create_VLAN_Network_net_1/models"
 
 
 class TestItmState(unittest.TestCase):
     def setUp(self):
         logg.Logger()
-        self.dpn_endpoints = itm_state.dpn_endpoints(Model.CONFIG, ip, port)
-        self.data = self.dpn_endpoints.read_file("./resources/itm-state_dpn-endpoints.json")
+        self.dpn_endpoints = itm_state.dpn_endpoints(Model.CONFIG, ip, port, path)
+        # self.data = self.dpn_endpoints.read_file("./resources/config_itm-state:dpn-endpoints.json")
 
     def test_read_file(self):
-        print "dpn-endpoints: {}".format(self.data)
-        print "dpn-endpoints: \n{}".format(self.dpn_endpoints.pretty_format(self.data))
+        print "dpn-endpoints: {}".format(self.dpn_endpoints.data)
+        print "dpn-endpoints: \n{}".format(self.dpn_endpoints.pretty_format(self.dpn_endpoints.data))
 
     def test_get_ip_address(self):
         dpn_ids = self.dpn_endpoints.get_dpn_ids()
         dpn_id = dpn_ids[0]
         ip_address = self.dpn_endpoints.get_ip_address(dpn_id)
         print "dpn_id: {}, ip_address: {}".format(dpn_id, ip_address)
-        self.assertEqual(dpn_id, "13878168265586")
+        self.assertEqual(dpn_id, 13878168265586)
         self.assertEqual(ip_address, "10.29.13.165")
 
     def test_get_all(self):
-        print "dpn-endpoints: {}".format(self.data)
-        print "dpn-endpoints: \n{}".format(self.dpn_endpoints.pretty_format(self.data))
+        print "dpn-endpoints: {}".format(self.dpn_endpoints.data)
+        print "dpn-endpoints: \n{}".format(self.dpn_endpoints.pretty_format(self.dpn_endpoints.data))
 
         dpn_ids = self.dpn_endpoints.get_dpn_ids()
         dpn_id = dpn_ids[0]
@@ -45,7 +47,7 @@ class TestItmState(unittest.TestCase):
         self.get_info(DpnEndpoints.DPN_ID)
 
     def get_info(self, key):
-        info = self.dpn_endpoints.get_kv(key, self.data, values=[])
+        info = self.dpn_endpoints.get_kv(key, self.dpn_endpoints.data, values=[])
         print "dpn info for {}: {}".format(key, info)
         return info
 
