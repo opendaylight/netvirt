7ddbbc6950dea5d32b7a3c457f82648b46e5d34f
Aswin Suryanarayanan <asuryana@redhat.com>
2018-04-27 09:58:27 +0530

    Fixes the fib flow for the g/w and PNF
    The mip adjacencies in the external interface and LearntVpnVipToPortData
    for  external n/w is removed when the last external router is deleted.
    
    This removes the stale flows in table21 and 51.
    
    NETVIRT-1157 Stale flows after clearing gateway info from a router
    
    Change-Id: I3c8db31b64beb123c21fab305cc457154e7d1c57
    Signed-off-by: Aswin Suryanarayanan <asuryana@redhat.com>

--- natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/VxlanGreConntrackBasedSnatService.java
+++ natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/VxlanGreConntrackBasedSnatService.java
@@ -39,6 +39,8 @@ import org.opendaylight.genius.mdsalutil.matches.MatchMetadata;
 import org.opendaylight.genius.mdsalutil.matches.MatchTunnelId;
 import org.opendaylight.genius.mdsalutil.nxmatches.NxMatchCtState;
 import org.opendaylight.netvirt.elanmanager.api.IElanService;
+import org.opendaylight.netvirt.fibmanager.api.IFibManager;
+import org.opendaylight.netvirt.natservice.ha.NatDataUtil;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.OdlInterfaceRpcService;
@@ -60,12 +62,12 @@ public class VxlanGreConntrackBasedSnatService extends ConntrackBasedSnatService
     private final IElanService elanManager;
 
     public VxlanGreConntrackBasedSnatService(DataBroker dataBroker, IMdsalApiManager mdsalManager,
-                                             ItmRpcService itmManager, OdlInterfaceRpcService odlInterfaceRpcService,
-                                             IdManagerService idManager, NAPTSwitchSelector naptSwitchSelector,
-                                             ExternalRoutersListener externalRouterListener, IElanService elanManager,
-                                             IInterfaceManager interfaceManager) {
-        super(dataBroker, mdsalManager, itmManager, idManager, naptSwitchSelector, odlInterfaceRpcService,
-                interfaceManager);
+            ItmRpcService itmManager, OdlInterfaceRpcService odlInterfaceRpcService, IdManagerService idManager,
+            NAPTSwitchSelector naptSwitchSelector, ExternalRoutersListener externalRouterListener,
+            IElanService elanManager, final IFibManager fibManager, final IInterfaceManager interfaceManager,
+            final NatDataUtil natDataUtil) {
+        super(dataBroker, mdsalManager, itmManager, odlInterfaceRpcService, idManager, naptSwitchSelector,
+                fibManager, interfaceManager, natDataUtil);
         this.externalRouterListener = externalRouterListener;
         this.elanManager = elanManager;
     }
