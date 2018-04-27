64469b66993b69cd56630266ccdd14aee303fe87
Aswin Suryanarayanan <asuryana@redhat.com>
2018-04-27 09:58:27 +0530

    Fixes the fib flow for the g/w and PNF
    The mip adjacencies in the external interface and LearntVpnVipToPortData
    for  external n/w is removed when the last external router is deleted.
    
    This removes the stale flows in table21 and 51.
    
    NETVIRT-1157 Stale flows after clearing gateway info from a router
    
    Change-Id: I3c8db31b64beb123c21fab305cc457154e7d1c57
    Signed-off-by: Aswin Suryanarayanan <asuryana@redhat.com>

--- natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/FlatVlanConntrackBasedSnatService.java
+++ natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/FlatVlanConntrackBasedSnatService.java
@@ -12,6 +12,8 @@ import java.math.BigInteger;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
 import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.netvirt.fibmanager.api.IFibManager;
+import org.opendaylight.netvirt.natservice.ha.NatDataUtil;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rpcs.rev160406.OdlInterfaceRpcService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.itm.rpcs.rev160406.ItmRpcService;
@@ -25,11 +27,11 @@ public class FlatVlanConntrackBasedSnatService extends ConntrackBasedSnatService
     private static final Logger LOG = LoggerFactory.getLogger(FlatVlanConntrackBasedSnatService.class);
 
     public FlatVlanConntrackBasedSnatService(DataBroker dataBroker, IMdsalApiManager mdsalManager,
-                                             ItmRpcService itmManager, OdlInterfaceRpcService odlInterfaceRpcService,
-                                             IdManagerService idManager, NAPTSwitchSelector naptSwitchSelector,
-                                             IInterfaceManager interfaceManager) {
-        super(dataBroker, mdsalManager, itmManager, idManager, naptSwitchSelector,
-                odlInterfaceRpcService, interfaceManager);
+            ItmRpcService itmManager, OdlInterfaceRpcService odlInterfaceRpcService, IdManagerService idManager,
+            NAPTSwitchSelector naptSwitchSelector, IFibManager fibManager,
+            IInterfaceManager interfaceManager, NatDataUtil natDataUtil) {
+        super(dataBroker, mdsalManager, itmManager, odlInterfaceRpcService, idManager, naptSwitchSelector,
+                fibManager, interfaceManager, natDataUtil);
     }
 
     @Override
