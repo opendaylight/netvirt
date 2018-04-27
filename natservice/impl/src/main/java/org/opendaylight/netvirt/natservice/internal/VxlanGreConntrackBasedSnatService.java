83a334de05103bfe0b0c28c16dd02343b78e00f8
Aswin Suryanarayanan <asuryana@redhat.com>
2018-04-27 09:58:27 +0530

    Fixes stale fib flows for the g/w and PNF
    The mip adjacency in the external interface and LearntVpnVipToPortData
    for  external n/w is removed when the last external router is deleted.
    
    This removes the stale flows in table21 and 51 and some exceptions.
    
    NETVIRT-1157 Stale flows after clearing gateway info from a router.
    
    Change-Id: I3c8db31b64beb123c21fab305cc457154e7d1c57
    Signed-off-by: Aswin Suryanarayanan <asuryana@redhat.com>

--- natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/VxlanGreConntrackBasedSnatService.java
+++ natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/VxlanGreConntrackBasedSnatService.java
@@ -42,6 +42,7 @@ import org.opendaylight.genius.mdsalutil.nxmatches.NxMatchCtState;
 import org.opendaylight.infrautils.utils.concurrent.ListenableFutures;
 import org.opendaylight.netvirt.elanmanager.api.IElanService;
 import org.opendaylight.netvirt.fibmanager.api.IFibManager;
+import org.opendaylight.netvirt.natservice.ha.NatDataUtil;
 import org.opendaylight.netvirt.vpnmanager.api.IVpnFootprintService;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
@@ -70,9 +71,9 @@ public class VxlanGreConntrackBasedSnatService extends ConntrackBasedSnatService
                                              ExternalRoutersListener externalRouterListener, IElanService elanManager,
                                              IInterfaceManager interfaceManager,
                                              IVpnFootprintService vpnFootprintService,
-                                             IFibManager fibManager) {
+                                             IFibManager fibManager, NatDataUtil natDataUtil) {
         super(dataBroker, mdsalManager, itmManager, idManager, naptSwitchSelector, odlInterfaceRpcService,
-                interfaceManager, vpnFootprintService, fibManager);
+                interfaceManager, vpnFootprintService, fibManager, natDataUtil);
         this.externalRouterListener = externalRouterListener;
         this.elanManager = elanManager;
         this.txRunner = new ManagedNewTransactionRunnerImpl(dataBroker);
