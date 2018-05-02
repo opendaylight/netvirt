08760e7b81bb8823c85b2b94efe64ae9923f47c6
Sam Hague <shague@redhat.com>
2018-05-02 17:41:33 -0400

    Use ifname in place of ifName
    JIRA: NETVIRT: 1232
    Change-Id: Ia65f2dd43ed2cfd14259617b644663b31381835b
    Signed-off-by: Sam Hague <shague@redhat.com>

--- resources/tools/odltools/odltools/netvirt/tests/__init__.py
+++ resources/tools/odltools/odltools/netvirt/tests/__init__.py
@@ -3,7 +3,7 @@ import os
 
 class Args:
     def __init__(self, transport="http", ip="localhost", port=8181, user="admin", pw="admin", path="/tmp",
-                 pretty_print=False, if_name=""):
+                 pretty_print=False, ifname=""):
         self.transport = transport
         self.ip = ip
         self.port = port
@@ -11,7 +11,7 @@ class Args:
         self.pw = pw
         self.path = path
         self.pretty_print = pretty_print
-        self.ifName = if_name
+        self.ifname = ifname
 
 
 def get_resources_path():
