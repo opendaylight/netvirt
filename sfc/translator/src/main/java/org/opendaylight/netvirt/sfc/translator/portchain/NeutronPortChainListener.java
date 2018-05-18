0367ef49082b20bbcc884c4ac1bd9548159ab260
gvrangan <venkatrangang@hcl.com>
2018-05-18 06:46:47 +0530

    Fix For SFC-220
    Change-Id: Ia6764eb1ad20da9e6245a89d82771c678f000e9b
    Signed-off-by: gvrangan <venkatrangang@hcl.com>

--- sfc/translator/src/main/java/org/opendaylight/netvirt/sfc/translator/portchain/NeutronPortChainListener.java
+++ sfc/translator/src/main/java/org/opendaylight/netvirt/sfc/translator/portchain/NeutronPortChainListener.java
@@ -84,8 +84,9 @@ public class NeutronPortChainListener extends DelegatingDataTreeListener<PortCha
      * @param updatePortChain     - changed PortChain (contain updates)
      */
     @Override
-    public void update(PortChain updatePortChain) {
-        //TODO: Add support for chain update
+    public void update(PortChain origPortChain,PortChain updatePortChain) {
+        // Handle Flow Classifier Updates first
+        processOldFlowClassifiers(origPortChain.getFlowClassifiers());
     }
 
     /**
@@ -173,10 +174,10 @@ public class NeutronPortChainListener extends DelegatingDataTreeListener<PortCha
         // The RSP will automatically be created from the SFP added above.
 
         // Add ACLs from flow classifiers
-        processFlowClassifiers(newPortChain, newPortChain.getFlowClassifiers(), sfp.getName().getValue());
+        processNewFlowClassifiers(newPortChain, newPortChain.getFlowClassifiers(), sfp.getName().getValue());
     }
 
-    private void processFlowClassifiers(PortChain pc, List<Uuid> flowClassifiers, String sfpName) {
+    private void processNewFlowClassifiers(PortChain pc, List<Uuid> flowClassifiers, String sfpName) {
         for (Uuid uuid : flowClassifiers) {
             SfcFlowClassifier fc = neutronMdsalHelper.getNeutronFlowClassifier(uuid);
             if (fc != null) {
@@ -193,4 +194,23 @@ public class NeutronPortChainListener extends DelegatingDataTreeListener<PortCha
             }
         }
     }
+
+    private void processOldFlowClassifiers(List<Uuid> flowClassifiers) {
+        for (Uuid uuid : flowClassifiers) {
+            SfcFlowClassifier fc = neutronMdsalHelper.getNeutronFlowClassifier(uuid);
+            if (fc != null) {
+                Acl acl = FlowClassifierTranslator.buildAcl(fc);
+                if (acl != null) {
+                    sfcMdsalHelper.removeAclFlowClassifier(acl);
+                } else {
+                    LOG.warn("Acl Removing failed for flow classifier {}", fc);
+                    LOG.warn("Traffic might not be redirected correctly");
+                }
+
+            } else {
+                LOG.error("Neutron Flow Classifier {} is not present in the neutron data store", uuid);
+            }
+        }
+    }
+
 }
