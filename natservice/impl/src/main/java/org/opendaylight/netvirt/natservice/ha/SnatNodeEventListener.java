8582e9cf16210fb81a3afec909244a0b7ac0057a
Ravindra Thakur <ravindra.nath.thakur@ericsson.com>
2018-05-18 15:25:28 +0530

    Rate limiting for controller based SNAT punts
    spec link:
    http://docs.opendaylight.org/projects/netvirt/en/latest/specs/fluorine/controller-punt-protection.html
    
    Issue: NETVIRT-1218
    
    Depends-On: https://git.opendaylight.org/gerrit/#/c/72006/
    Depends-On: https://git.opendaylight.org/gerrit/#/c/72007/
    
    Change-Id: Idc6e88ae8279f58eae525af95fab6f312b4d2586
    Signed-off-by: Ravindra Thakur <ravindra.nath.thakur@ericsson.com>

--- natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/ha/SnatNodeEventListener.java
+++ natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/ha/SnatNodeEventListener.java
@@ -8,6 +8,8 @@
 package org.opendaylight.netvirt.natservice.ha;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 
 import javax.inject.Inject;
@@ -16,12 +18,24 @@ import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.mdsalutil.ActionInfo;
+import org.opendaylight.genius.mdsalutil.FlowEntity;
+import org.opendaylight.genius.mdsalutil.InstructionInfo;
 import org.opendaylight.genius.mdsalutil.MDSALUtil;
+import org.opendaylight.genius.mdsalutil.MatchInfo;
+import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.genius.mdsalutil.actions.ActionDrop;
+import org.opendaylight.genius.mdsalutil.instructions.InstructionApplyActions;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.genius.mdsalutil.matches.MatchEthernetType;
+import org.opendaylight.genius.mdsalutil.matches.MatchIpProtocol;
 import org.opendaylight.genius.tools.mdsal.listener.AbstractClusteredAsyncDataTreeChangeListener;
 import org.opendaylight.netvirt.natservice.api.CentralizedSwitchScheduler;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.config.rev170206.NatserviceConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.config.rev170206.NatserviceConfig.NatMode;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,16 +48,28 @@ import org.slf4j.LoggerFactory;
 public class SnatNodeEventListener  extends AbstractClusteredAsyncDataTreeChangeListener<Node> {
     private static final Logger LOG = LoggerFactory.getLogger(SnatNodeEventListener.class);
     private final CentralizedSwitchScheduler  centralizedSwitchScheduler;
+    private final NatMode natmode;
+    private final IMdsalApiManager mdsalApiManager;
 
     @Inject
     public SnatNodeEventListener(final DataBroker dataBroker,
-            final CentralizedSwitchScheduler centralizedSwitchScheduler) {
+                                 final CentralizedSwitchScheduler centralizedSwitchScheduler,
+                                 final IMdsalApiManager mdsalApiManager,
+                                 final NatserviceConfig config) {
 
         super(dataBroker,new DataTreeIdentifier<>(LogicalDatastoreType.OPERATIONAL, InstanceIdentifier
                 .create(Nodes.class).child(Node.class)),
                 Executors.newSingleThreadExecutor());
         this.centralizedSwitchScheduler = centralizedSwitchScheduler;
+        this.mdsalApiManager = mdsalApiManager;
+        if (config != null) {
+            this.natmode = config.getNatMode();
+        } else {
+            this.natmode = NatMode.Controller;
+        }
     }
+
+
 
     @Override
     public void remove(Node dataObjectModification) {
@@ -65,6 +91,31 @@ public class SnatNodeEventListener  extends AbstractClusteredAsyncDataTreeChange
         BigInteger dpnId = MDSALUtil.getDpnIdFromNodeName(nodeKey.getId());
         LOG.info("Dpn added {}", dpnId);
         centralizedSwitchScheduler.addSwitch(dpnId);
+        if (natmode == NatMode.Controller) {
+            addIcmpDropFlow(dpnId);
+        }
+    }
 
+    private String getFlowRef(BigInteger dpnId, short tableId, String prefix) {
+        return new StringBuffer().append(dpnId).append(NwConstants.FLOWID_SEPARATOR).append(tableId)
+                .append(NwConstants.FLOWID_SEPARATOR).append(prefix).toString();
+    }
+
+    private void addIcmpDropFlow(BigInteger dpnId) {
+        List<MatchInfo> matches = new ArrayList<>();
+        matches.add(MatchEthernetType.IPV4);
+        matches.add(MatchIpProtocol.ICMP);
+
+        List<ActionInfo> actionInfos = new ArrayList<>();
+        actionInfos.add(new ActionDrop());
+
+        List<InstructionInfo> instructions = new ArrayList<>();
+        instructions.add(new InstructionApplyActions(actionInfos));
+
+        String flowRef = getFlowRef(dpnId, NwConstants.OUTBOUND_NAPT_TABLE, "icmp.drop");
+        FlowEntity flow = MDSALUtil.buildFlowEntity(dpnId, NwConstants.OUTBOUND_NAPT_TABLE, flowRef,
+                NwConstants.TABLE_MISS_PRIORITY, "icmp drop flow", 0, 0,
+                NwConstants.COOKIE_OUTBOUND_NAPT_TABLE, matches, instructions);
+        mdsalApiManager.installFlow(flow);
     }
 }
