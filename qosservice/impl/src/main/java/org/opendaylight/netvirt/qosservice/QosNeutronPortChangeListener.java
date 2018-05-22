4498ea52cb535babc940af5233e3bb00168e29e9
Ravindra Thakur <ravindra.nath.thakur@ericsson.com>
2018-05-22 19:38:15 +0530

    QoS: DSCP marking doesn't work for dual stack VMs
    Added support for DSCP marking for dual stack VM so that DSCP
    marking flows are present for both ipv4 and ipv6.
    
    Change-Id: If49662abfe8f6673c6f1c96d00376eadbefcfc51
    Signed-off-by: Ravindra Thakur <ravindra.nath.thakur@ericsson.com>

--- qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosNeutronPortChangeListener.java
+++ qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosNeutronPortChangeListener.java
@@ -7,18 +7,24 @@
  */
 package org.opendaylight.netvirt.qosservice;
 
+import java.math.BigInteger;
+import java.util.Collections;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
+import org.opendaylight.genius.mdsalutil.NwConstants;
 import org.opendaylight.genius.srm.RecoverableListener;
 import org.opendaylight.genius.srm.ServiceRecoveryRegistry;
+import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
 import org.opendaylight.netvirt.qosservice.recovery.QosServiceRecoveryHandler;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.Ports;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.ports.rev150712.ports.attributes.ports.Port;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.qos.ext.rev160613.QosPortExtension;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.qos.rev160613.qos.attributes.qos.policies.QosPolicy;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
@@ -30,14 +36,20 @@ public class QosNeutronPortChangeListener extends AsyncClusteredDataTreeChangeLi
     private static final Logger LOG = LoggerFactory.getLogger(QosNeutronPortChangeListener.class);
     private final DataBroker dataBroker;
     private final QosNeutronUtils qosNeutronUtils;
+    private final QosEosHandler qosEosHandler;
+    private final JobCoordinator jobCoordinator;
 
     @Inject
     public QosNeutronPortChangeListener(final DataBroker dataBroker,
             final QosNeutronUtils qosNeutronUtils, final QosServiceRecoveryHandler qosServiceRecoveryHandler,
-                                        final ServiceRecoveryRegistry serviceRecoveryRegistry) {
+                                        final ServiceRecoveryRegistry serviceRecoveryRegistry,
+                                        final QosEosHandler qosEosHandler,
+                                        final JobCoordinator jobCoordinator) {
         super(Port.class, QosNeutronPortChangeListener.class);
         this.dataBroker = dataBroker;
         this.qosNeutronUtils = qosNeutronUtils;
+        this.qosEosHandler = qosEosHandler;
+        this.jobCoordinator = jobCoordinator;
         serviceRecoveryRegistry.addRecoverableListener(qosServiceRecoveryHandler.buildServiceRegistryKey(),
                 this);
         LOG.debug("{} created",  getClass().getSimpleName());
@@ -99,5 +111,49 @@ public class QosNeutronPortChangeListener extends AsyncClusteredDataTreeChangeLi
             qosNeutronUtils.removeFromQosPortsCache(originalQos.getQosPolicyId(), original);
         }
 
+        if (qosEosHandler.isQosClusterOwner()) {
+            checkForPortIpAddressUpdate(original, update);
+        }
+    }
+
+
+
+    private void checkForPortIpAddressUpdate(Port original, Port update) {
+        QosPolicy qosPolicy = qosNeutronUtils.getQosPolicy(update);
+        if (qosPolicy == null || !qosNeutronUtils.hasDscpMarkingRule(qosPolicy)) {
+            return;
+        }
+        int origAddrMask = qosNeutronUtils.getIpVersions(original);
+        int updateAddrMask = qosNeutronUtils.getIpVersions(update);
+
+        if (origAddrMask == updateAddrMask) {
+            return;
+        }
+        jobCoordinator.enqueueJob("QosPort-" + update.getUuid().getValue(), () -> {
+            short dscpVal = qosPolicy.getDscpmarkingRules().get(0).getDscpMark();
+            String ifName = update.getUuid().getValue();
+            BigInteger dpnId = qosNeutronUtils.getDpnForInterface(ifName);
+            if (dpnId.equals(BigInteger.ZERO)) {
+                LOG.warn("dpnId not found for intf {}", ifName);
+                return Collections.emptyList();
+            }
+            Interface intf = qosNeutronUtils.getInterfaceStateFromOperDS(ifName);
+            if (qosNeutronUtils.hasIpv4Addr(origAddrMask) && !qosNeutronUtils.hasIpv4Addr(updateAddrMask)) {
+                LOG.debug("removing ipv4 flow for port: {}", update.getUuid().getValue());
+                qosNeutronUtils.removeFlow(dpnId, ifName, NwConstants.ETHTYPE_IPV4, intf);
+            } else if (!qosNeutronUtils.hasIpv4Addr(origAddrMask) && qosNeutronUtils.hasIpv4Addr(updateAddrMask)) {
+                LOG.debug("adding ipv4 flow for port: {}, dscp: {}", update.getUuid().getValue(), dscpVal);
+                qosNeutronUtils.addFlow(dpnId, dscpVal, ifName, NwConstants.ETHTYPE_IPV4, intf);
+            }
+
+            if (qosNeutronUtils.hasIpv6Addr(origAddrMask) && !qosNeutronUtils.hasIpv6Addr(updateAddrMask)) {
+                LOG.debug("removing ipv6 flow for port: {}", update.getUuid().getValue());
+                qosNeutronUtils.removeFlow(dpnId, ifName, NwConstants.ETHTYPE_IPV6, intf);
+            } else if (!qosNeutronUtils.hasIpv6Addr(origAddrMask) && qosNeutronUtils.hasIpv6Addr(updateAddrMask)) {
+                LOG.debug("adding ipv6 flow for port: {}, dscp: {}", update.getUuid().getValue(), dscpVal);
+                qosNeutronUtils.addFlow(dpnId, dscpVal, ifName, NwConstants.ETHTYPE_IPV6, intf);
+            }
+            return Collections.emptyList();
+        });
     }
 }
