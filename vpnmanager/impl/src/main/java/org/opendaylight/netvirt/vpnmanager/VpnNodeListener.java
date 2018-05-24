1af43dbbd2392fa10d22b75f70865f2abf0e0256
Karthikeyan Krishnan <karthikeyangceb007@gmail.com>
2018-05-24 10:26:46 +0530

    Rate limiting for subnet route punt packets for VPNv6
    Adds a learn action for subnet route punt flow which installs a learn
    flow with higher priority than the punt flow and matches on dst ipv6
    address
    + vpn id and drops the matching packets till the flow is deleted after
    hard timeout expiry.
    
    Depends-On: https://git.opendaylight.org/gerrit/#/c/72196/
    Depends-On: https://git.opendaylight.org/gerrit/#/c/72006/
    Depends-On: https://git.opendaylight.org/gerrit/#/c/72007/
    
    JIRA: NETVIRT-1213
    
    Spec:
    http://docs.opendaylight.org/projects/netvirt/en/latest/specs/fluorine/subnet-routing-for-hidden-ipv6.html
    
    Change-Id: I42cad4be7eada8d81cfca6407aea1f69f022ad2f
    Signed-off-by: Karthikeyan Krishnan <karthikeyangceb007@gmail.com>

--- vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnNodeListener.java
+++ vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnNodeListener.java
@@ -25,7 +25,9 @@ import org.opendaylight.genius.mdsalutil.FlowEntity;
 import org.opendaylight.genius.mdsalutil.InstructionInfo;
 import org.opendaylight.genius.mdsalutil.MDSALUtil;
 import org.opendaylight.genius.mdsalutil.MatchInfo;
+import org.opendaylight.genius.mdsalutil.MetaDataUtil;
 import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.genius.mdsalutil.actions.ActionLearn;
 import org.opendaylight.genius.mdsalutil.actions.ActionNxResubmit;
 import org.opendaylight.genius.mdsalutil.actions.ActionPuntToController;
 import org.opendaylight.genius.mdsalutil.instructions.InstructionApplyActions;
@@ -37,6 +39,7 @@ import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
 import org.opendaylight.netvirt.elan.arp.responder.ArpResponderUtil;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.vpn.config.rev161130.VpnConfig;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,14 +54,17 @@ public class VpnNodeListener extends AsyncClusteredDataTreeChangeListenerBase<No
     private final IMdsalApiManager mdsalManager;
     private final JobCoordinator jobCoordinator;
     private final List<BigInteger> connectedDpnIds;
+    private final VpnConfig vpnConfig;
 
     @Inject
-    public VpnNodeListener(DataBroker dataBroker, IMdsalApiManager mdsalManager, JobCoordinator jobCoordinator) {
+    public VpnNodeListener(DataBroker dataBroker, IMdsalApiManager mdsalManager, JobCoordinator jobCoordinator,
+                           VpnConfig vpnConfig) {
         super(Node.class, VpnNodeListener.class);
         this.broker = dataBroker;
         this.mdsalManager = mdsalManager;
         this.jobCoordinator = jobCoordinator;
         this.connectedDpnIds = new CopyOnWriteArrayList<>();
+        this.vpnConfig = vpnConfig;
     }
 
     @PostConstruct
@@ -107,6 +113,7 @@ public class VpnNodeListener extends AsyncClusteredDataTreeChangeListenerBase<No
                 makeTableMissFlow(writeFlowTx, dpId, NwConstants.ADD_FLOW);
                 makeL3IntfTblMissFlow(writeFlowTx, dpId, NwConstants.ADD_FLOW);
                 makeSubnetRouteTableMissFlow(writeFlowTx, dpId, NwConstants.ADD_FLOW);
+                makeIpv6SubnetRouteTableMissFlow(writeFlowTx, dpId, NwConstants.ADD_FLOW);
                 createTableMissForVpnGwFlow(writeFlowTx, dpId);
                 createL3GwMacArpFlows(writeFlowTx, dpId);
                 programTableMissForVpnVniDemuxTable(writeFlowTx, dpId, NwConstants.ADD_FLOW);
@@ -173,6 +180,45 @@ public class VpnNodeListener extends AsyncClusteredDataTreeChangeListenerBase<No
         }
     }
 
+    private void makeIpv6SubnetRouteTableMissFlow(WriteTransaction writeFlowTx, BigInteger dpnId, int addOrRemove) {
+        final BigInteger cookieTableMiss = new BigInteger("8000004", 16);
+        List<ActionInfo> actionsInfos = new ArrayList<>();
+        List<InstructionInfo> instructions = new ArrayList<>();
+        actionsInfos.add(new ActionPuntToController());
+        int learnTimeout = vpnConfig.getSubnetRoutePuntTimeout().intValue();
+        if (learnTimeout != 0) {
+            actionsInfos.add(new ActionLearn(0, learnTimeout, VpnConstants.DEFAULT_FLOW_PRIORITY, cookieTableMiss,
+                    0, NwConstants.L3_SUBNET_ROUTE_TABLE, 0, 0,
+                    Arrays.asList(
+                            new ActionLearn.MatchFromValue(NwConstants.ETHTYPE_IPV6,
+                                    NwConstants.NxmOfFieldType.NXM_OF_ETH_TYPE.getType(),
+                                    NwConstants.NxmOfFieldType.NXM_OF_ETH_TYPE.getFlowModHeaderLenInt()),
+                            new ActionLearn.MatchFromField(NwConstants.NxmOfFieldType.NXM_NX_IPV6_DST.getType(),
+                                    NwConstants.NxmOfFieldType.NXM_NX_IPV6_DST.getType(),
+                                    NwConstants.NxmOfFieldType.NXM_NX_IPV6_DST.getFlowModHeaderLenInt()),
+                            new ActionLearn.MatchFromField(NwConstants.NxmOfFieldType.OXM_OF_METADATA.getType(),
+                                    MetaDataUtil.METADATA_VPN_ID_OFFSET,
+                                    NwConstants.NxmOfFieldType.OXM_OF_METADATA.getType(),
+                                    MetaDataUtil.METADATA_VPN_ID_OFFSET,
+                                    MetaDataUtil.METADATA_VPN_ID_BITLEN))));
+        }
+
+        instructions.add(new InstructionApplyActions(actionsInfos));
+        List<MatchInfo> matches = new ArrayList<>();
+        matches.add(MatchEthernetType.IPV6);
+        String flowRef = getIpv6TableMissFlowRef(dpnId, NwConstants.L3_SUBNET_ROUTE_TABLE, NwConstants.ETHTYPE_IPV6,
+                NwConstants.TABLE_MISS_FLOW);
+        FlowEntity flowEntity = MDSALUtil.buildFlowEntity(dpnId, NwConstants.L3_SUBNET_ROUTE_TABLE, flowRef,
+                NwConstants.TABLE_MISS_PRIORITY, "IPv6 Subnet Route Table Miss", 0, 0,
+                cookieTableMiss, matches, instructions);
+        if (addOrRemove == NwConstants.ADD_FLOW) {
+            LOG.debug("makeIpv6SubnetRouteTableMissFlow: Install Ipv6 Subnet Route Table  Miss entries");
+            mdsalManager.addFlowToTx(flowEntity, writeFlowTx);
+        } else {
+            mdsalManager.removeFlowToTx(flowEntity, writeFlowTx);
+        }
+    }
+
     private void programTableMissForVpnVniDemuxTable(WriteTransaction writeFlowTx, BigInteger dpnId, int addOrRemove) {
         List<ActionInfo> actionsInfos = Collections.singletonList(new ActionNxResubmit(NwConstants
                 .LPORT_DISPATCHER_TABLE));
@@ -221,5 +267,12 @@ public class VpnNodeListener extends AsyncClusteredDataTreeChangeListenerBase<No
         return new StringBuffer().append(FLOWID_PREFIX).append(dpnId).append(NwConstants.FLOWID_SEPARATOR)
             .append(tableId).append(NwConstants.FLOWID_SEPARATOR).append(tableMiss)
             .append(FLOWID_PREFIX).toString();
+    }
+
+    private String getIpv6TableMissFlowRef(BigInteger dpnId, short tableId, int etherType, int tableMiss) {
+        return new StringBuffer().append(FLOWID_PREFIX).append(dpnId).append(NwConstants.FLOWID_SEPARATOR)
+                .append(tableId).append(NwConstants.FLOWID_SEPARATOR).append(etherType)
+                .append(NwConstants.FLOWID_SEPARATOR).append(tableMiss)
+                .append(FLOWID_PREFIX).toString();
     }
 }
