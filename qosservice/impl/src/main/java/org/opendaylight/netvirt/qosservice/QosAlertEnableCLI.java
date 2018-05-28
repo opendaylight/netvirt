8c70aaf0c46740c17ffb17fa49d5fce4fd339291
Meenakshi <meenakshi.c@ericsson.com>
2018-05-28 17:59:09 +0530

    QoS Log clean-up QoS log tags have been modified for better debugging.
    Change-Id: I6ec119429363735ccbd8cadc4e4628b2fb983f49
    Signed-off-by: Meenakshi <meenakshi.c@ericsson.com>

--- qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosAlertEnableCLI.java
+++ qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosAlertEnableCLI.java
@@ -29,7 +29,7 @@ public class QosAlertEnableCLI extends OsgiCommandSupport {
 
     public void setQosAlertManager(QosAlertManager qosAlertManager) {
         this.qosAlertManager = qosAlertManager;
-        LOG.debug("Qos manager:{} set", qosAlertManager);
+        LOG.trace("Qos manager:{} set", qosAlertManager);
     }
 
     @Override
