59d005b2ca70f18ff0d66e3156c9d48c2091718c
Loshmitha <loshmitha@ericsson.com>
2018-05-29 17:17:04 +0530

    NETVIRT-1163:No option to configure IPv6 on ODL No option was available to configure IPv6 in ConfigCli , so adding one
    Change-Id: If7b6ec769f7aa41c1494e0bd6b6f793120661a19
    Signed-off-by: Loshmitha <loshmitha@ericsson.com>

--- bgpmanager/impl/src/main/java/org/opendaylight/netvirt/bgpmanager/ConfigureBgpCli.java
+++ bgpmanager/impl/src/main/java/org/opendaylight/netvirt/bgpmanager/ConfigureBgpCli.java
@@ -345,30 +345,30 @@ public class ConfigureBgpCli extends OsgiCommandSupport {
                 session.getConsole().println("error: Address family must be lu/evpn/vpnv4/vpnv6 ");
                 return;
             }
-            try {
-                af_safi.valueOf(addressFamily);
-                af_afi afi;
-                af_safi safi;
-                if (addressFamily.equals("vpnv6")) {
-                    afi = af_afi.findByValue(2);
-                    safi = af_safi.findByValue(5);
-                } else if (addressFamily.equals("evpn")) {
-                    afi = af_afi.findByValue(3);
-                    safi = af_safi.findByValue(6);
-                } else if (addressFamily.equals("lu")) {
-                    afi = af_afi.findByValue(1);
-                    safi = af_safi.findByValue(4);
-                } else { // vpnv4
-                    afi = af_afi.findByValue(1);
-                    safi = af_safi.findByValue(5);
-                }
-                bgpManager.addAddressFamily(ip, afi, safi);
-            } catch (IllegalArgumentException e) {
+
+            af_afi afi ;
+            af_safi safi ;
+            if (addressFamily.equals("vpnv6")) {
+                afi = af_afi.findByValue(2);
+                safi = af_safi.findByValue(5);
+            } else if (addressFamily.equals("evpn")) {
+                afi = af_afi.findByValue(3);
+                safi = af_safi.findByValue(6);
+            } else if (addressFamily.equals("lu")) {
+                afi = af_afi.findByValue(1);
+                safi = af_safi.findByValue(4);
+            } else if  (addressFamily.equals("vpnv4")) {
+                afi = af_afi.findByValue(1);
+                safi = af_safi.findByValue(5);
+            } else {
+                afi = af_afi.findByValue(0);
+                safi = af_safi.findByValue(0);
                 session.getConsole().println(
                         "invalid addressFamily valid values SAFI_IPV4_LABELED_UNICAST | SAFI_MPLS_VPN");
                 printAddNeighborHelp();
-                return;
             }
+            bgpManager.addAddressFamily(ip, afi, safi);
+
         }
         if (getAsNumber(ip) != -1) {
             session.getConsole().println("neighbor with ip " + ip + " already exists");
