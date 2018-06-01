6c77b84cef9d155e3a64bb1fb1a08b553813a0b7
Michael Vorburger <vorburger@redhat.com>
2018-06-01 15:41:05 +0200

    unblock broken build by replacing some but not all Xtend from aclservice
    This rewrites some of the Xtend with Java.  It only replaces those
    *Builder classes, but does not (yet?) replace the expected objects with
    some TBD not Xtend based approach.
    
    As it is something related to Java generics in those Builder classes
    which seems to (suddenly) somehow be causing the build failure, this is
    sufficient, for now, to unblock broken build.
    
    JIRA: NETVIRT-1290
    Change-Id: Id76d24f1475ed3d3eeac622de8932fc8a63f8ee6
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ aclservice/impl/src/test/java/org/opendaylight/netvirt/aclservice/tests/IdentifiedAceBuilder.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.aclservice.tests;
+
+import static org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType.CONFIGURATION;
+
+import java.util.Optional;
+import javax.annotation.concurrent.NotThreadSafe;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.netvirt.aclservice.tests.infra.DataTreeIdentifierDataObjectPairBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.AccessLists;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.Ipv4Acl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.Acl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.AclKey;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.AccessListEntries;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.Ace;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.AceBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.AceKey;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.ActionsBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.Matches;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.actions.packet.handling.PermitBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.DirectionBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.SecurityRuleAttr;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.SecurityRuleAttrBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+@NotThreadSafe
+public class IdentifiedAceBuilder implements DataTreeIdentifierDataObjectPairBuilder<Ace> {
+
+    private String sgUuid;
+    private String ruleName;
+    private Matches matches;
+    private Class<? extends DirectionBase> direction;
+    private Optional<Uuid> optRemoteGroupId = Optional.empty();
+
+    @Override
+    public LogicalDatastoreType type() {
+        return CONFIGURATION;
+    }
+
+    @Override
+    public InstanceIdentifier<Ace> identifier() {
+        return InstanceIdentifier
+                .builder(AccessLists.class)
+                .child(Acl.class, new AclKey(sgUuid, Ipv4Acl.class))
+                .child(AccessListEntries.class)
+                .child(Ace.class, new AceKey(ruleName))
+                .build();
+    }
+
+    @Override
+    public Ace dataObject() {
+        return new AceBuilder()
+            .setKey(new AceKey(ruleName))
+            .setRuleName(ruleName)
+            .setMatches(matches)
+            .setActions(new ActionsBuilder()
+                .setPacketHandling(new PermitBuilder()
+                    .setPermit(true).build()
+                ).build()
+            )
+            .addAugmentation(SecurityRuleAttr.class, new SecurityRuleAttrBuilder()
+                .setDirection(direction)
+                .setRemoteGroupId(optRemoteGroupId.orElse(null)).build()
+            ).build();
+    }
+
+    // TODO use Immutables.org to generate below?
+
+    public IdentifiedAceBuilder sgUuid(String newSgUuid) {
+        this.sgUuid = newSgUuid;
+        return this;
+    }
+
+    public IdentifiedAceBuilder newRuleName(String newRuleName) {
+        this.ruleName = newRuleName;
+        return this;
+    }
+
+    public IdentifiedAceBuilder newMatches(Matches newMatches) {
+        this.matches = newMatches;
+        return this;
+    }
+
+    public IdentifiedAceBuilder newDirection(Class<? extends DirectionBase> newDirection) {
+        this.direction = newDirection;
+        return this;
+    }
+
+    public IdentifiedAceBuilder newRemoteGroupId(Uuid newRemoteGroupId) {
+        this.optRemoteGroupId = Optional.of(newRemoteGroupId);
+        return this;
+    }
+
+}
---
+++ aclservice/impl/src/test/java/org/opendaylight/netvirt/aclservice/tests/StateInterfaceBuilderHelper.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.aclservice.tests;
+
+import static java.util.Collections.singletonList;
+import static org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType.OPERATIONAL;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.iana._if.type.rev140508.L2vlan;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.InterfacesState;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface.AdminStatus;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.Interface.OperStatus;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.InterfaceBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state.InterfaceKey;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.state._interface.StatisticsBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.DateAndTime;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.PhysAddress;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Utility to create interfaces in tests.
+ *
+ * @author Michael Vorburger.ch
+ */
+public final class StateInterfaceBuilderHelper {
+    // TODO make this like IdentifiedInterfaceWithAclBuilder
+
+    private StateInterfaceBuilderHelper() {}
+
+    public static void putNewStateInterface(DataBroker dataBroker, String interfaceName, String mac)
+            throws TransactionCommitFailedException {
+        InterfaceBuilder stateInterfaceBuilder = new InterfaceBuilder();
+        stateInterfaceBuilder.setName(interfaceName);
+        stateInterfaceBuilder.setPhysAddress(new PhysAddress(mac));
+        stateInterfaceBuilder.setLowerLayerIf(singletonList("openflow:123:456"));
+        stateInterfaceBuilder.setIfIndex(987);
+        stateInterfaceBuilder.setOperStatus(OperStatus.Up);
+        stateInterfaceBuilder.setAdminStatus(AdminStatus.Up);
+        stateInterfaceBuilder.setType(L2vlan.class);
+        stateInterfaceBuilder.setStatistics(new StatisticsBuilder()
+                .setDiscontinuityTime(DateAndTime.getDefaultInstance("8330-42-22T79:08:74Z")).build());
+        Interface stateInterface = stateInterfaceBuilder.build();
+        InstanceIdentifier<Interface> id = InstanceIdentifier.builder(InterfacesState.class)
+                .child(Interface.class, new InterfaceKey(interfaceName)).build();
+        SingleTransactionDataBroker.syncWrite(dataBroker, OPERATIONAL, id, stateInterface);
+    }
+
+}
