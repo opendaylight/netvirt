9a1fae472895117dadf4d1fb5abc156b2086f9a0
Somashekar Byrappa <somashekar.b@altencalsoftlabs.com>
2018-06-14 19:47:26 +0530

    Refactor VpnManager module.
    + Refactoring VpnManager to support subnet routing for hidden IPv6
      addresses.
    + Moved all IP learning classes to package org.opendaylight.netvirt
      .vpnmanager.iplearn.
    + Renamed few classes to Ip*** instead of Arp*** to keep it common for
      both IPv4 and IPv6.
    + Refactored code in iplearn package to handle both IPv4 and IPv6.
    + Kept placeholders (TODO's) for supporting IPv6 subnet routing for
      hidden IPs.
    
    Depends-On: https://git.opendaylight.org/gerrit/#/c/72796/
    
    JIRA: NETVIRT-1213
    
    Change-Id: I7f2c7968d92d61b4d3e1d8a1d858df34368344e8
    Signed-off-by: Somashekar Byrappa <somashekar.b@altencalsoftlabs.com>

--- vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/iplearn/IpMonitorStopTask.java
+++ vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/iplearn/IpMonitorStopTask.java
@@ -5,25 +5,26 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.netvirt.vpnmanager;
+package org.opendaylight.netvirt.vpnmanager.iplearn;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-
+import org.opendaylight.netvirt.vpnmanager.VpnUtil;
+import org.opendaylight.netvirt.vpnmanager.iplearn.model.MacEntry;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.learnt.vpn.vip.to.port.data.LearntVpnVipToPort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ArpMonitorStopTask implements Callable<List<ListenableFuture<Void>>> {
+public class IpMonitorStopTask implements Callable<List<ListenableFuture<Void>>> {
+    private static final Logger LOG = LoggerFactory.getLogger(IpMonitorStopTask.class);
     private MacEntry macEntry;
     private final AlivenessMonitorUtils alivenessMonitorUtils;
-    private static final Logger LOG = LoggerFactory.getLogger(ArpMonitorStopTask.class);
     private boolean isRemoveMipAdjAndLearntIp;
     private final VpnUtil vpnUtil;
 
-    public ArpMonitorStopTask(MacEntry macEntry, boolean removeMipAdjAndLearntIp, VpnUtil vpnUtil,
+    public IpMonitorStopTask(MacEntry macEntry, boolean removeMipAdjAndLearntIp, VpnUtil vpnUtil,
                               AlivenessMonitorUtils alivenessMonitorUtils) {
         this.macEntry = macEntry;
         this.alivenessMonitorUtils = alivenessMonitorUtils;
@@ -36,7 +37,7 @@ public class ArpMonitorStopTask implements Callable<List<ListenableFuture<Void>>
         final List<ListenableFuture<Void>> futures = new ArrayList<>();
         java.util.Optional<Long> monitorIdOptional = AlivenessMonitorUtils.getMonitorIdFromInterface(macEntry);
         monitorIdOptional.ifPresent(monitorId -> {
-            alivenessMonitorUtils.stopArpMonitoring(monitorId);
+            alivenessMonitorUtils.stopIpMonitoring(monitorId);
         });
 
         if (this.isRemoveMipAdjAndLearntIp) {
