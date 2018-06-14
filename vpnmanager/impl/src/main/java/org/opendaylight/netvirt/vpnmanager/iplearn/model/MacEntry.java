1c07c3621b3a6125d5b64fbab4e13a2c253cfa35
Somashekar Byrappa <somashekar.b@altencalsoftlabs.com>
2018-06-14 19:47:26 +0530

    Refactor VpnManager module.
    + Refactoring VpnManager to support subnet routing for hidden IPv6
      addresses.
    + Moved all IP learning classes to package org.opendaylight.netvirt
      .vpnmanager.iplearn.
    + Renamed few classes to Ip*** instead of Arp*** to keep it common for
      both IPv4 and IPv6.
    + Refactored code in iplearn package to handle both IPv4 and IPv6.
    + Kept placeholders (TODO's) for supporting IPv6 subnet routing for
      hidden IPs.
    
    Depends-On: https://git.opendaylight.org/gerrit/#/c/72796/
    
    JIRA: NETVIRT-1213
    
    Change-Id: I7f2c7968d92d61b4d3e1d8a1d858df34368344e8
    Signed-off-by: Somashekar Byrappa <somashekar.b@altencalsoftlabs.com>

--- vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/iplearn/model/MacEntry.java
+++ vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/iplearn/model/MacEntry.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.netvirt.vpnmanager;
+package org.opendaylight.netvirt.vpnmanager.iplearn.model;
 
 import java.net.InetAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.MacAddress;
