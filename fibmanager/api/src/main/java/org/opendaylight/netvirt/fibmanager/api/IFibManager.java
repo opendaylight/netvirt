3862733e18e5664ca78c2d0ef080b78721a01957
Josh <jhershbe@redhat.com>
2018-06-27 10:28:14 +0300

    vpnmanager typed tx: VpnUtil
    But w/out the VpnInterfaceManager parts
    
    JIRA: NETVIRT-1346
    
    Change-Id: Ic1d9c7d434a58128c130c9772d3941ed57ac0081
    Signed-off-by: Josh <jhershbe@redhat.com>

--- fibmanager/api/src/main/java/org/opendaylight/netvirt/fibmanager/api/IFibManager.java
+++ fibmanager/api/src/main/java/org/opendaylight/netvirt/fibmanager/api/IFibManager.java
@@ -14,6 +14,8 @@ import java.math.BigInteger;
 import java.util.List;
 
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.genius.infra.Datastore;
+import org.opendaylight.genius.infra.TypedWriteTransaction;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.interfacemanager.rev160406.TunnelTypeBase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.RouterInterface;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.fibmanager.rev150330.vrfentries.VrfEntry;
@@ -68,7 +70,7 @@ public interface IFibManager {
 
     void addVrfTable(String rd, WriteTransaction writeConfigTxn);
 
-    void removeVrfTable(String rd, WriteTransaction writeConfigTxn);
+    void removeVrfTable(String rd, TypedWriteTransaction<Datastore.Configuration> writeConfigTxn);
 
     void removeInterVPNLinkRouteFlows(String interVpnLinkName,
                                       boolean isVpnFirstEndPoint,
