640654107e51b2e98eaee75b9c463822e74285d5
ananth <ananth.y@hcl.com>
2018-07-25 22:50:54 +0900

    WIP: Fix For NETVIRT-1389
    Implemented flows for TCP source/destination
    port-range matches. Port ranges are converted
    to a masked values and a flow entry is added
    for each such combination of TCP source/destination
    ports.
    
    Change-Id: I297deee3d64be9c82a63f4156dd6d1185a890f83
    Signed-off-by: ananth <ananth.y@hcl.com>

--- sfc/classifier/impl/src/main/java/org/opendaylight/netvirt/sfc/classifier/utils/AclMatches.java
+++ sfc/classifier/impl/src/main/java/org/opendaylight/netvirt/sfc/classifier/utils/AclMatches.java
@@ -8,6 +8,14 @@
 
 package org.opendaylight.netvirt.sfc.classifier.utils;
 
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import org.opendaylight.genius.mdsalutil.NwConstants;
 import org.opendaylight.genius.mdsalutil.packet.IPProtocols;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.Matches;
@@ -42,20 +50,29 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv4MatchBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv6Match;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._3.match.Ipv6MatchBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._4.match.TcpMatchBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.model.match.types.rev131026.match.layer._4.match.UdpMatchBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.GeneralAugMatchNodesNodeTableFlow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.GeneralAugMatchNodesNodeTableFlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.general.extension.grouping.ExtensionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.general.rev140714.general.extension.list.grouping.ExtensionListBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxAugMatchNodesNodeTableFlow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxAugMatchNodesNodeTableFlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxmOfTcpDstKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.NxmOfTcpSrcKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.nxm.of.tcp.dst.grouping.NxmOfTcpDstBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflowplugin.extension.nicira.match.rev140714.nxm.of.tcp.src.grouping.NxmOfTcpSrcBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AclMatches {
     private static final Logger LOG = LoggerFactory.getLogger(AclMatches.class);
-    private final MatchBuilder matchBuilder;
+    private MatchBuilder matchBuilder;
+    private Set<GeneralAugMatchNodesNodeTableFlow> tcpMatches;
     private final Matches matches;
     private boolean ipv4EtherTypeSet;
     private boolean ipv6EtherTypeSet;
 
     public AclMatches(Matches matches) {
-        this.matchBuilder = new MatchBuilder();
         this.matches = matches;
         this.ipv4EtherTypeSet = false;
         this.ipv6EtherTypeSet = false;
@@ -65,16 +82,26 @@ public class AclMatches {
      * Convert the ACL into an OpenFlow {@link MatchBuilder}.
      * @return {@link MatchBuilder}
      */
-    public MatchBuilder buildMatch() {
+    public List<MatchBuilder> buildMatch() {
+        matchBuilder = new MatchBuilder();
+        tcpMatches = new HashSet<>();
+        List<MatchBuilder> newMatches = new ArrayList<>();
         if (matches.getAceType() instanceof AceEth) {
             addEthMatch();
         } else if (matches.getAceType() instanceof AceIp) {
             addIpMatch();
         }
-
-        LOG.debug("buildMatch: {}", matchBuilder.build());
-
-        return matchBuilder;
+        if (tcpMatches.isEmpty()) {
+            newMatches.add(this.matchBuilder);
+        } else if (!tcpMatches.isEmpty()) {
+            for (GeneralAugMatchNodesNodeTableFlow tcpL4Match : tcpMatches) {
+                MatchBuilder matchBuilds = matchBuilder.addAugmentation(GeneralAugMatchNodesNodeTableFlow.class,
+                    tcpL4Match);
+                newMatches.add(new MatchBuilder(matchBuilds.build()));
+            }
+        }
+        LOG.debug("returned matches: {}", newMatches);
+        return newMatches;
     }
 
     private void addEthMatch() {
@@ -136,11 +163,14 @@ public class AclMatches {
         ipMatch.setIpProtocol(aceIp.getProtocol());
         matchBuilder.setIpMatch(mergeIpMatch(matchBuilder, ipMatch));
 
-        // TODO Ranges are not supported yet
-
         int srcPort = 0;
         if (aceIp.getSourcePortRange() != null && aceIp.getSourcePortRange().getLowerPort() != null) {
             srcPort = aceIp.getSourcePortRange().getLowerPort().getValue();
+        }
+
+        int srcPortMax = 0;
+        if (aceIp.getSourcePortRange() != null && aceIp.getSourcePortRange().getUpperPort() != null) {
+            srcPortMax = aceIp.getSourcePortRange().getUpperPort().getValue();
         }
 
         int dstPort = 0;
@@ -148,17 +178,49 @@ public class AclMatches {
             dstPort = aceIp.getDestinationPortRange().getLowerPort().getValue();
         }
 
+        int dstPortMax = 0;
+        if (aceIp.getDestinationPortRange() != null && aceIp.getDestinationPortRange().getUpperPort() != null) {
+            dstPortMax = aceIp.getDestinationPortRange().getUpperPort().getValue();
+        }
         // Match on a TCP/UDP src/dst port
         if (aceIp.getProtocol() == IPProtocols.TCP.shortValue()) {
-            TcpMatchBuilder tcpMatch = new TcpMatchBuilder();
+            NxAugMatchNodesNodeTableFlow nxAugMatch = null;
+            GeneralAugMatchNodesNodeTableFlow genAugMatch = null;
             if (srcPort != 0) {
-                tcpMatch.setTcpSourcePort(new PortNumber(srcPort));
-            }
-            if (dstPort != 0) {
-                tcpMatch.setTcpDestinationPort(new PortNumber(dstPort));
-            }
-            if (srcPort != 0 || dstPort != 0) {
-                matchBuilder.setLayer4Match(tcpMatch.build());
+                NxmOfTcpSrcBuilder tcpSrc = new NxmOfTcpSrcBuilder();
+                Map<Integer,Integer> srcPortMaskMap = getLayer4MaskForRange(srcPort, srcPortMax);
+                for (Entry<Integer, Integer> srcEntry : srcPortMaskMap.entrySet()) {
+                    Integer srcTcpPort = srcEntry.getKey();
+                    Integer srcTcpMask = srcEntry.getValue();
+                    tcpSrc.setMask(srcTcpMask);
+                    tcpSrc.setPort(new PortNumber(srcTcpPort));
+                    nxAugMatch = new NxAugMatchNodesNodeTableFlowBuilder().setNxmOfTcpSrc(tcpSrc.build()).build();
+                    genAugMatch = new GeneralAugMatchNodesNodeTableFlowBuilder()
+                    .setExtensionList(ImmutableList.of(new ExtensionListBuilder().setExtensionKey(NxmOfTcpSrcKey.class)
+                                                       .setExtension(new ExtensionBuilder()
+                                                       .addAugmentation(NxAugMatchNodesNodeTableFlow.class, nxAugMatch)
+                                                                     .build()).build())).build();
+
+                    tcpMatches.add(genAugMatch);
+                }
+            } else if (dstPort != 0) {
+                NxmOfTcpDstBuilder tcpDst = new NxmOfTcpDstBuilder();
+                Map<Integer,Integer> dstPortMaskMap = getLayer4MaskForRange(dstPort, dstPortMax);
+                for (Entry<Integer, Integer> dstEntry : dstPortMaskMap.entrySet()) {
+                    Integer dstTcpPort = dstEntry.getKey();
+                    Integer dstTcpMask = dstEntry.getValue();
+                    tcpDst.setMask(dstTcpMask);
+                    tcpDst.setPort(new PortNumber(dstTcpPort));
+                    nxAugMatch = new NxAugMatchNodesNodeTableFlowBuilder().setNxmOfTcpDst(tcpDst.build()).build();
+                    genAugMatch = new GeneralAugMatchNodesNodeTableFlowBuilder()
+                    .setExtensionList(ImmutableList.of(new ExtensionListBuilder()
+                                                       .setExtensionKey(NxmOfTcpDstKey.class)
+                                                       .setExtension(new ExtensionBuilder()
+                                                       .addAugmentation(NxAugMatchNodesNodeTableFlow.class, nxAugMatch)
+                                                                     .build()).build())).build();
+
+                    tcpMatches.add(genAugMatch);
+                }
             }
         } else if (aceIp.getProtocol() == IPProtocols.UDP.shortValue()) {
             UdpMatchBuilder udpMatch = new UdpMatchBuilder();
@@ -172,6 +234,62 @@ public class AclMatches {
                 matchBuilder.setLayer4Match(udpMatch.build());
             }
         }
+    }
+
+    public static Map<Integer,Integer>  getLayer4MaskForRange(int portMin, int portMax) {
+        final int[] offset = { 32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 };
+        final int[] mask = { 0x8000, 0xC000, 0xE000, 0xF000, 0xF800, 0xFC00, 0xFE00, 0xFF00, 0xFF80, 0xFFC0, 0xFFE0,
+            0xFFF0, 0xFFF8, 0xFFFC, 0xFFFE, 0xFFFF };
+        int noOfPorts = portMax - portMin + 1;
+        Map<Integer,Integer> portMap = new HashMap<>();
+        if (noOfPorts == 1) {
+            portMap.put(portMin, mask[15]);
+            return portMap;
+        } else if (noOfPorts == 65535) {
+            portMap.put(portMin, 0x0000);
+            return portMap;
+        }
+        if (noOfPorts < 0) { // TODO: replace with infrautils.counter in case of high repetitive usage
+            LOG.warn("Cannot convert port range into a set of masked port ranges - Illegal port range {}-{}", portMin,
+                    portMax);
+            return portMap;
+        }
+        String binaryNoOfPorts = Integer.toBinaryString(noOfPorts);
+        if (binaryNoOfPorts.length() > 16) { // TODO: replace with infrautils.counter in case of high repetitive usage
+            LOG.warn("Cannot convert port range into a set of masked port ranges - Illegal port range {}-{}", portMin,
+                    portMax);
+            return portMap;
+        }
+        int medianOffset = 16 - binaryNoOfPorts.length();
+        int medianLength = offset[medianOffset];
+        int median = 0;
+        for (int tempMedian = 0;tempMedian < portMax;) {
+            tempMedian = medianLength + tempMedian;
+            if (portMin < tempMedian) {
+                median = tempMedian;
+                break;
+            }
+        }
+        int tempMedian = 0;
+        int currentMedain = median;
+        for (int tempMedianOffset = medianOffset;16 > tempMedianOffset;tempMedianOffset++) {
+            tempMedian = currentMedain - offset[tempMedianOffset];
+            for (;portMin <= tempMedian;) {
+                portMap.put(tempMedian, mask[tempMedianOffset]);
+                currentMedain = tempMedian;
+                tempMedian = tempMedian - offset[tempMedianOffset];
+            }
+        }
+        currentMedain = median;
+        for (int tempMedianOffset = medianOffset;16 > tempMedianOffset;tempMedianOffset++) {
+            tempMedian = currentMedain + offset[tempMedianOffset];
+            for (;portMax >= tempMedian - 1;) {
+                portMap.put(currentMedain, mask[tempMedianOffset]);
+                currentMedain = tempMedian;
+                tempMedian = tempMedian  + offset[tempMedianOffset];
+            }
+        }
+        return portMap;
     }
 
     private void addIpV4Match(AceIp aceIp) {
