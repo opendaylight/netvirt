7a3b5e0cd06c173001fee995ec903a780348c800
Michael Vorburger <vorburger@redhat.com>
2018-07-26 12:59:31 +0200

    rm runtime logging change in QosAlertGenerator
    JIRA: CONTROLLER-1845
    Change-Id: I4b92254b0b47a718f9920166b5dbfc177cbf8dc7
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosAlertGenerator.java
+++ qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosAlertGenerator.java
@@ -8,95 +8,19 @@
 
 package org.opendaylight.netvirt.qosservice;
 
-import java.io.IOException;
 import java.math.BigInteger;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class QosAlertGenerator {
+public final class QosAlertGenerator {
     private static final Logger LOG = LoggerFactory.getLogger(QosAlertGenerator.class);
 
-    private final ConfigurationAdmin configurationAdmin;
-
-    public QosAlertGenerator(ConfigurationAdmin configurationAdmin) {
-        this.configurationAdmin = configurationAdmin;
-        try {
-            updateQoSAlertLog4jProperties(getPropertyMap(QosConstants.QOS_ALERT_PROPERTIES_PID));
-        } catch (IOException e) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Error initialising log4j properties ", e);
-            } else {
-                LOG.warn("Qos Alert properties could not be initialised");
-            }
-        }
-    }
-
-    public void update(Map<String, Object> qosAlertProperties) {
-        try {
-            updateQoSAlertLog4jProperties(qosAlertProperties);
-        } catch (IOException e) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Error updating log4j properties ", e);
-            } else {
-                LOG.warn("Qos Alert properties update failed");
-            }
-        }
-    }
+    private QosAlertGenerator() { }
 
     public static void raiseAlert(final String qosPolicyName, final String qosPolicyUuid, final String portUuid,
                                   final String networkUuid, final BigInteger rxPackets,
                                   final BigInteger rxDroppedPackets) {
-        // Log the alert message in a text file using log4j appender qosalertmsg
         LOG.debug(QosConstants.ALERT_MSG_FORMAT, qosPolicyName, qosPolicyUuid, portUuid, networkUuid,
                                                                                         rxPackets, rxDroppedPackets);
-    }
-
-    private Configuration getConfig(String pid) throws IOException {
-        return configurationAdmin.getConfiguration(pid);
-    }
-
-    private Map<String, Object> getPropertyMap(String pid) throws IOException {
-        Map<String, Object> propertyMap;
-        Configuration configurationInit = getConfig(pid);
-        Dictionary<String, Object> config = configurationInit.getProperties();
-        propertyMap = new HashMap<>(config.size());
-        Enumeration<String> keys = config.keys();
-        while (keys.hasMoreElements()) {
-            String key = keys.nextElement();
-            propertyMap.put(key, config.get(key));
-        }
-        return propertyMap;
-    }
-
-    private static Map<String, Object> removeNonLog4jProperties(Map<String, Object> qosAlertLog4jProperties) {
-        qosAlertLog4jProperties.remove(QosConstants.FELIX_FILEINSTALL_FILENAME);
-        qosAlertLog4jProperties.remove(QosConstants.SERVICE_PID);
-        return qosAlertLog4jProperties;
-    }
-
-    private void updateQoSAlertLog4jProperties(Map<String, Object> qosAlertLog4jProperties) throws IOException {
-        Map<String, Object> log4jProperties = getPropertyMap(QosConstants.ORG_OPS4J_PAX_LOGGING);
-        Hashtable<String, Object> updateLog4jProperties = new Hashtable<>();
-        updateLog4jProperties.putAll(log4jProperties);
-        updateLog4jProperties.putAll(removeNonLog4jProperties(qosAlertLog4jProperties));
-
-        Configuration log4jConfig = getConfig(QosConstants.ORG_OPS4J_PAX_LOGGING);
-        try {
-            log4jConfig.update(updateLog4jProperties);
-            log4jConfig.update();
-        } catch (IOException ioe) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Exception in configuration ", ioe);
-            } else {
-                LOG.warn("Could not update configuration in Config log");
-            }
-        }
     }
 }
