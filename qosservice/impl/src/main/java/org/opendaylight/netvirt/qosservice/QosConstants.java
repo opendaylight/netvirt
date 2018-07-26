d062809f144907585349480225290fe568f7abac
Michael Vorburger <vorburger@redhat.com>
2018-07-26 12:59:31 +0200

    rm runtime logging change in QosAlertGenerator
    JIRA: CONTROLLER-1845
    Change-Id: I4b92254b0b47a718f9920166b5dbfc177cbf8dc7
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosConstants.java
+++ qosservice/impl/src/main/java/org/opendaylight/netvirt/qosservice/QosConstants.java
@@ -12,10 +12,7 @@ public interface QosConstants {
     int QOS_DEFAULT_FLOW_PRIORITY = 10;
     String ALERT_MSG_FORMAT = "Packet drop threshold hit for qos policy {} with qos-id {} for port port-{}"
         + " on network network-{} rx_received {} rx_dropped {}";
-    String QOS_ALERT_PROPERTIES_PID = "org.opendaylight.netvirt.qosservice";
-    String ORG_OPS4J_PAX_LOGGING = "org.ops4j.pax.logging";
-    String FELIX_FILEINSTALL_FILENAME = "felix.fileinstall.filename";
-    String SERVICE_PID = "service.pid";
+
     String QOS_ALERT_OWNER_ENTITY_TYPE = "netvirt-qos-owner-entity";
 
     short IPV4_ADDR_MASK_BIT = 0;
