e8007e8c27ceefe09e5328cc2838d98733716ee2
Faseela K <faseela.k@ericsson.com>
2018-08-07 20:55:12 +0530

    COE external ip ping failure
    JIRA: NETVIRT-1396
    
    Change-Id: Ib2d1ce72f2382978b132aaeaab90e4156e9de98b
    Signed-off-by: Faseela K <faseela.k@ericsson.com>

--- coe/impl/src/main/java/org/opendaylight/netvirt/coe/utils/CoeUtils.java
+++ coe/impl/src/main/java/org/opendaylight/netvirt/coe/utils/CoeUtils.java
@@ -16,8 +16,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-
-import org.apache.commons.lang3.tuple.Pair;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.ReadTransaction;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
@@ -27,6 +25,8 @@ import org.opendaylight.genius.infra.Datastore;
 import org.opendaylight.genius.infra.TypedReadWriteTransaction;
 import org.opendaylight.genius.infra.TypedWriteTransaction;
 import org.opendaylight.genius.mdsalutil.NwConstants;
+import org.opendaylight.netvirt.coe.api.SouthboundInterfaceInfo;
+import org.opendaylight.netvirt.coe.api.SouthboundInterfaceInfoBuilder;
 import org.opendaylight.netvirt.neutronvpn.api.enums.IpVersionChoice;
 import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.VpnInstances;
 import org.opendaylight.yang.gen.v1.urn.huawei.params.xml.ns.yang.l3vpn.rev140815.VpnInterfaces;
@@ -275,31 +275,35 @@ public final class CoeUtils {
         return elanInstance;
     }
 
-    public static Pair<String, String> getAttachedInterfaceAndMac(OvsdbTerminationPointAugmentation ovsdbTp) {
-        String interfaceName = null;
-        String macAddress = null;
+    public static SouthboundInterfaceInfo getSouthboundInterfaceDetails(OvsdbTerminationPointAugmentation ovsdbTp) {
+        SouthboundInterfaceInfoBuilder southboundInterfaceInfoBuilder = new SouthboundInterfaceInfoBuilder();
         if (ovsdbTp != null) {
             List<InterfaceExternalIds> ifaceExtIds = ovsdbTp.getInterfaceExternalIds();
             if (ifaceExtIds != null) {
                 Iterator var2 = ifaceExtIds.iterator();
                 while (var2.hasNext()) {
-                    if (interfaceName != null && macAddress != null) {
-                        break;
-                    }
                     InterfaceExternalIds entry = (InterfaceExternalIds)var2.next();
                     if (entry.getExternalIdKey().equals("iface-id")) {
-                        interfaceName = entry.getExternalIdValue();
+                        southboundInterfaceInfoBuilder.setInterfaceName(entry.getExternalIdValue());
                         continue;
                     }
                     if (entry.getExternalIdKey().equals("attached-mac")) {
-                        macAddress = entry.getExternalIdValue();
+                        southboundInterfaceInfoBuilder.setMacAddress(entry.getExternalIdValue());
+                        continue;
+                    }
+                    if (entry.getExternalIdKey().equals("ip-address")) {
+                        southboundInterfaceInfoBuilder.setNodeIp(entry.getExternalIdValue());
+                        continue;
+                    }
+                    if (entry.getExternalIdKey().equals("is-service-gateway")) {
+                        southboundInterfaceInfoBuilder.setIsServiceGateway(true);
                         continue;
                     }
                 }
             }
         }
 
-        return Pair.of(interfaceName, macAddress);
+        return southboundInterfaceInfoBuilder.build();
     }
 
     public static InstanceIdentifier<PodIdentifier> getPodMetaInstanceId(String externalInterfaceId) {
