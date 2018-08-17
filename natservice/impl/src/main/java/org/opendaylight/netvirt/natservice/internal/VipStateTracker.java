85a0391d26963d4abfacb00ef3f285efaf6f2029
Josh <jhershbe@redhat.com>
2018-08-17 15:20:29 -0700

    FIP support for Octavia VIPs
    This patch resolves the issue documented in the Jira
    issue referenced below.
    
    The basic solution is to use the OF packet-in event to
    learn the dpn where the NAT flows need to be programmed.
    
    Overview of code changes:
    1. Refactor NatInterfaceStateChangeListener into two classes:
     - NatInterfaceStateChangeListener which just receives events
     - NatSouthboundEventHandlers which contains the logic invoked
       for when an interface state changes (or a garp is received)
    2. Implementation of NatArpNotificationHandler which receives
       the garps and invokes the correct methods in
       NatSouthboundEventHandlers
    3. neutron-vip-state yang model which is used together with
       VipStateTracker (DataObjectCache) to manage state of the
       discovered VIPs. This is required in cases where the
       associated Ocatavia Amphora VM changes to a different
       compute node. In this case the existing flows must be
       removed from the odl compute node.
    4. Tweak VIP learning code to accept neutron ports that are
       owned by "Octavia", previously the code assumed no neutron
       port ever needed to be learned.
    
    Jira: NETVIRT-1402
    
    Change-Id: I7867124f3cbbe88d1ce6d075e51e2b11f941aec2
    Signed-off-by: Josh <jhershbe@redhat.com>

---
+++ natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/VipStateTracker.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.natservice.internal;
+
+import static org.opendaylight.genius.infra.Datastore.OPERATIONAL;
+
+import com.google.common.util.concurrent.FluentFuture;
+import java.math.BigInteger;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunnerImpl;
+import org.opendaylight.genius.mdsalutil.cache.DataObjectCache;
+import org.opendaylight.infrautils.caches.CacheProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.NeutronVipStates;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.neutron.vip.states.VipState;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.neutron.vip.states.VipStateBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.neutron.vip.states.VipStateKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+@Singleton
+public class VipStateTracker extends DataObjectCache<String, VipState> {
+
+    private ManagedNewTransactionRunner txRunner = null;
+
+    @Inject
+    public VipStateTracker(DataBroker dataBroker, CacheProvider cacheProvider) {
+        super(VipState.class,
+            dataBroker,
+            LogicalDatastoreType.OPERATIONAL,
+            InstanceIdentifier.builder(NeutronVipStates.class).child(VipState.class).build(),
+            cacheProvider,
+            (iid, vipState) -> vipState.key().getIp(),
+            ip -> InstanceIdentifier.builder(NeutronVipStates.class) .child(VipState.class, new VipStateKey(ip)).build()
+            );
+        this.txRunner = new ManagedNewTransactionRunnerImpl(dataBroker);
+    }
+
+    public VipState buildVipState(String ip, String mac, BigInteger dpnId, String ifcName) {
+        return new VipStateBuilder().setIp(ip).setDpnId(dpnId).setIfcName(ifcName).build();
+    }
+
+    public FluentFuture<Void> writeVipState(VipState vipState) {
+        return txRunner.callWithNewWriteOnlyTransactionAndSubmit(OPERATIONAL, tx -> {
+            tx.put(InstanceIdentifier.builder(NeutronVipStates.class)
+                            .child(VipState.class, vipState.key()).build(),
+                    vipState, true);
+        });
+    }
+}
