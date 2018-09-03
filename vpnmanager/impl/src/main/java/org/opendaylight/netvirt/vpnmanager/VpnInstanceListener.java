3bd60a6acd8d58818f9c93d7beb098d887579e76
Karthikeyan Krishnan <karthikeyangceb007@gmail.com>
2018-09-03 19:06:11 +0530

    BGP-VPN Instance update with additional RD Support
    Issue:
    ======
    For Extra route use cases, user can update the existing
    BGP-VPN instance with additional RDs which are required
    for extra/static routes.
    Currently RD list update is not getting updated in
    VPN Instance DS. As a result newly added RD is not
    getting advertised to BGP.
    
    Solution:
    ==========
    This fix is addressing the existing BGP-VPN instance
    is getting update with new RDs.Also if extra routes
    are used this newly added RD will advertise this VRF
    into external BGP Update.
    
    Note: Removing the existing RD is not supported by NeutronVPN
    
    Issue: NETVIRT-1416
    
    Change-Id: Iedcdba88084934670c7ba7e8fcf66e0bd91f875b
    Signed-off-by: Karthikeyan Krishnan <karthikeyangceb007@gmail.com>

--- vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnInstanceListener.java
+++ vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnInstanceListener.java
@@ -115,7 +115,7 @@ public class VpnInstanceListener extends AsyncDataTreeChangeListenerBase<VpnInst
         LOG.trace("{} remove: VPN event key: {}, value: {}", LOGGING_PREFIX_DELETE, identifier, del);
         final String vpnName = del.getVpnInstanceName();
         Optional<VpnInstanceOpDataEntry> vpnOpValue;
-        String primaryRd = VpnUtil.getPrimaryRd(del);
+        String primaryRd = VpnUtil.getVpnRd(dataBroker, vpnName);
 
         //TODO(vpnteam): Entire code would need refactoring to listen only on the parent object - VPNInstance
         try {
@@ -151,6 +151,14 @@ public class VpnInstanceListener extends AsyncDataTreeChangeListenerBase<VpnInst
         VpnInstance original, VpnInstance update) {
         LOG.trace("VPN-UPDATE: update: VPN event key: {}, value: {}. Ignoring", identifier, update);
         String vpnName = update.getVpnInstanceName();
+        if ((original.getIpv4Family() != null && update.getIpv4Family() != null)
+                && original.getIpv4Family().getRouteDistinguisher().size()
+                !=  update.getIpv4Family().getRouteDistinguisher().size()) {
+            LOG.debug("VPN-UPDATE: VpnInstance:{} updated with new RDs: {} from old RDs: {}", vpnName,
+                    update.getIpv4Family().getRouteDistinguisher(),  original.getIpv4Family().getRouteDistinguisher());
+            VpnUtil.updateVpnInstanceWithRdList(jobCoordinator, dataBroker, vpnName,
+                    update.getIpv4Family().getRouteDistinguisher());
+        }
         vpnInterfaceManager.updateVpnInterfacesForUnProcessAdjancencies(vpnName);
     }
 
