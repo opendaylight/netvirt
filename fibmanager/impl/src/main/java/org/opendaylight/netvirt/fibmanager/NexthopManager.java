ebfd15751e4e0f96be5792cf16200bbab3d501a9
Swati Udhavrao Niture <swati.udhavrao.niture@ericsson.com>
2018-09-14 15:25:20 +0530

    Changes to handle ReadFailedException properly
    Minor change to fetch tunnel state and
    handle ReadFailedException for ECMP UCs
    
    Change-Id: Ide4ae0f749b5e05fb0c0660a1bf243b1c32228b2
    Signed-off-by: Swati Udhavrao Niture <swati.udhavrao.niture@ericsson.com>

--- fibmanager/impl/src/main/java/org/opendaylight/netvirt/fibmanager/NexthopManager.java
+++ fibmanager/impl/src/main/java/org/opendaylight/netvirt/fibmanager/NexthopManager.java
@@ -983,7 +983,9 @@ public class NexthopManager implements AutoCloseable {
                     return;
                 }
             } catch (ReadFailedException e) {
-                LOG.error("error in fetching tunnel state for interface {}", egressInterface, e);
+                LOG.error("getBucketsForRemoteNexthop: error in fetching tunnel state for interface {}",
+                        egressInterface, e);
+                return;
             }
             if (!TunnelTypeVxlan.class.equals(tunnelType)) {
                 return;
