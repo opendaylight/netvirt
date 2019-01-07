1719d4027f87f4b5717e3fcedea734dd38512a44
Sam Hague <shague@redhat.com>
2019-01-07 08:45:51 -0500

    add NetworkPolicyListener
    Depends-on: Id84841e106352bd4e7937b5493cb3b4cd0df2e8f
    Change-Id: I5f801ec88cb75f3590c34a50402935ed74a25794
    Signed-off-by: Sam Hague <shague@redhat.com>

---
+++ coe/impl/src/main/java/org/opendaylight/netvirt/coe/listeners/NetworkPolicyListener.java
@@ -0,0 +1,248 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.coe.listeners;
+
+import static org.opendaylight.mdsal.binding.util.Datastore.CONFIGURATION;
+import static org.opendaylight.netvirt.coe.utils.AclUtils.DIRECTION_MAP;
+import static org.opendaylight.netvirt.coe.utils.AclUtils.buildName2;
+import static org.opendaylight.netvirt.coe.utils.AclUtils.buildName3;
+import static org.opendaylight.netvirt.coe.utils.AclUtils.getAclIid;
+import static org.opendaylight.netvirt.coe.utils.NetworkPolicyUtils.PROTOCOL_MAP;
+
+import java.util.ArrayList;
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.opendaylight.infrautils.utils.concurrent.LoggingFutures;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.util.RetryingManagedNewTransactionRunner;
+import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
+import org.opendaylight.serviceutils.tools.listener.AbstractSyncDataTreeChangeListener;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.Ipv4Acl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.Acl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.AclBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.AclKey;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.AccessListEntriesBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.Ace;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.AceBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.AceKey;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.ActionsBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.MatchesBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.actions.packet.handling.PermitBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.AceIpBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.ace.matches.ace.type.ace.ip.ace.ip.version.AceIpv4Builder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Prefix;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.packet.fields.rev160218.acl.transport.header.fields.DestinationPortRangeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.NetworkPolicies;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.egress.rule.NetworkPolicyEgressRule;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.egress.rule.network.policy.egress.rule.EgressPorts;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.egress.rule.network.policy.egress.rule.To;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.ingress.rule.NetworkPolicyIngressRule;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.ingress.rule.network.policy.ingress.rule.From;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.ingress.rule.network.policy.ingress.rule.IngressPorts;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.network.policies.NetworkPolicy;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.peer.NetworkPolicyPeer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.port.NetworkPolicyPort;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.spec.NetworkPolicySpec;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.spec.network.policy.spec.Egress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.network.policy.rev181205.network.policy.spec.network.policy.spec.Ingress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.k8s.rev181205.K8s;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.DirectionBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.DirectionEgress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.DirectionIngress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.SecurityRuleAttr;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.SecurityRuleAttrBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class NetworkPolicyListener extends AbstractSyncDataTreeChangeListener<NetworkPolicy> {
+    private static final Logger LOG = LoggerFactory.getLogger(NetworkPolicyListener.class);
+    private final RetryingManagedNewTransactionRunner txRunner;
+
+    @Inject
+    public NetworkPolicyListener(DataBroker dataBroker) {
+        super(dataBroker, LogicalDatastoreType.CONFIGURATION,
+            InstanceIdentifier.create(K8s.class).child(NetworkPolicies.class).child(NetworkPolicy.class));
+        this.txRunner = new RetryingManagedNewTransactionRunner(dataBroker);
+    }
+
+    @Override
+    public void add(@Nonnull InstanceIdentifier<NetworkPolicy> instanceIdentifier, @Nonnull NetworkPolicy policy) {
+        LOG.info("add: id: {}\npolicy: {}", instanceIdentifier, policy);
+        LoggingFutures.addErrorLogging(txRunner.callWithNewWriteOnlyTransactionAndSubmit(CONFIGURATION, tx -> {
+            String aclName = getAclNameFromPolicy(policy);
+            Acl acl = buildAcl(policy);
+            InstanceIdentifier<Acl> aclIid = getAclIid(aclName);
+            // write the ace, and the api will create the acl parent if needed
+            tx.mergeParentStructurePut(aclIid, acl);
+        }), LOG, "Failed to add acl from policy: {}", policy);
+    }
+
+    @Override
+    public void remove(@Nonnull InstanceIdentifier<NetworkPolicy> instanceIdentifier, @Nonnull NetworkPolicy policy) {
+        LOG.info("remove: id: {}\npolicy: {}", instanceIdentifier, policy);
+    }
+
+    @Override
+    public void update(@Nonnull InstanceIdentifier<NetworkPolicy> instanceIdentifier,
+                       @Nonnull NetworkPolicy oldPolicy, @Nonnull NetworkPolicy policy) {
+        LOG.info("update: id: {}\nold policy: {}\nnew policy: {}", instanceIdentifier, oldPolicy, policy);
+    }
+
+    private String getAclNameFromPolicy(NetworkPolicy policy) {
+        String aclName = "";
+        if (policy.getUuid() != null) {
+            aclName = policy.getUuid().getValue();
+        }
+        return aclName;
+    }
+
+    // TODO map empty rules:
+    // ingress:empty - no incoming allowed
+    // egress:empty - no outgoing allowed
+
+    private Acl buildAcl(NetworkPolicy policy) {
+        String aclName = getAclNameFromPolicy(policy);
+        ArrayList<Ace> aceList = new ArrayList<>();
+
+        if (policy.getNetworkPolicySpec() != null) {
+            NetworkPolicySpec spec = policy.getNetworkPolicySpec();
+            if (spec.getIngress() != null) {
+                for (Ingress ingress : spec.getIngress()) {
+                    NetworkPolicyIngressRule rule = ingress.getNetworkPolicyIngressRule();
+                    if (rule.getIngressPorts() != null) {
+                        for (IngressPorts port : rule.getIngressPorts()) {
+                            if (port.getNetworkPolicyPort() != null) {
+                                Ace ace = buildPortAce(false, aclName,
+                                    DirectionIngress.class, port.getNetworkPolicyPort());
+                                aceList.add(ace);
+                            }
+                        }
+                    }
+                    if (rule.getFrom() != null) {
+                        for (From from: rule.getFrom()) {
+                            if (from.getNetworkPolicyPeer() != null) {
+                                Ace ace = buildPolicyAce(false, aclName,
+                                    DirectionIngress.class, from.getNetworkPolicyPeer());
+                                aceList.add(ace);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (spec.getEgress() != null) {
+                for (Egress egress : spec.getEgress()) {
+                    NetworkPolicyEgressRule rule = egress.getNetworkPolicyEgressRule();
+                    if (rule.getEgressPorts() != null) {
+                        for (EgressPorts port : rule.getEgressPorts()) {
+                            if (port.getNetworkPolicyPort() != null) {
+                                Ace ace = buildPortAce(false, aclName,
+                                    DirectionEgress.class, port.getNetworkPolicyPort());
+                                aceList.add(ace);
+                            }
+                        }
+                    }
+                    if (rule.getTo() != null) {
+                        for (To to: rule.getTo()) {
+                            if (to.getNetworkPolicyPeer() != null) {
+                                Ace ace = buildPolicyAce(false, aclName,
+                                    DirectionEgress.class, to.getNetworkPolicyPeer());
+                                aceList.add(ace);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        AccessListEntriesBuilder accessListEntriesBuilder = new AccessListEntriesBuilder();
+        accessListEntriesBuilder.setAce(aceList);
+
+        AclBuilder aclBuilder = new AclBuilder();
+        aclBuilder.setAclName(aclName);
+        aclBuilder.setAclType(Ipv4Acl.class);
+        aclBuilder.setAccessListEntries(accessListEntriesBuilder.build());
+        aclBuilder.withKey(new AclKey(aclBuilder.getAclName(), aclBuilder.getAclType()));
+        return aclBuilder.build();
+    }
+
+    private AceBuilder getAceBuilder(boolean isDeleted, String ruleName,
+                                     @Nonnull Class<? extends DirectionBase> direction,
+                                     AceIpBuilder aceIpBuilder) {
+        MatchesBuilder matchesBuilder = new MatchesBuilder();
+        matchesBuilder.setAceType(aceIpBuilder.build());
+        ActionsBuilder actionsBuilder = new ActionsBuilder();
+        actionsBuilder.setPacketHandling(new PermitBuilder().setPermit(true).build());
+
+        AceBuilder aceBuilder = new AceBuilder();
+        aceBuilder.setRuleName(ruleName);
+        aceBuilder.withKey(new AceKey(aceBuilder.getRuleName()));
+        aceBuilder.setMatches(matchesBuilder.build());
+        aceBuilder.setActions(actionsBuilder.build());
+
+        SecurityRuleAttrBuilder securityRuleAttrBuilder = new SecurityRuleAttrBuilder();
+        securityRuleAttrBuilder.setDeleted(isDeleted);
+        securityRuleAttrBuilder.setDirection(direction);
+        aceBuilder.addAugmentation(SecurityRuleAttr.class, securityRuleAttrBuilder.build());
+        return aceBuilder;
+    }
+
+    private Ace buildPortAce(boolean isDeleted, @Nonnull String aclName,
+                             @Nonnull Class<? extends DirectionBase> direction,
+                             @Nonnull NetworkPolicyPort port) {
+        AceIpBuilder aceIpBuilder = new AceIpBuilder();
+        String ruleName = buildName3(aclName, DIRECTION_MAP.get(direction), "port");
+        if (port.getProtocol() != null) {
+            aceIpBuilder.setProtocol(PROTOCOL_MAP.get(port.getProtocol()));
+            ruleName = buildName2(ruleName, port.getProtocol().toString());
+        }
+
+        // TODO: map a named port
+        if (port.getPort() != null) {
+            DestinationPortRangeBuilder portRangeBuilder = new DestinationPortRangeBuilder();
+            PortNumber portNumber = new PortNumber(Integer.parseInt(port.getPort()));
+            portRangeBuilder.setLowerPort(portNumber);
+            portRangeBuilder.setUpperPort(portNumber);
+            aceIpBuilder.setDestinationPortRange(portRangeBuilder.build());
+            ruleName = buildName2(ruleName, portNumber.getValue().toString());
+        }
+
+        AceBuilder aceBuilder = getAceBuilder(isDeleted, ruleName, direction, aceIpBuilder);
+
+        return aceBuilder.build();
+    }
+
+    private Ace buildPolicyAce(boolean isDeleted, @Nonnull String aclName,
+                               @Nonnull Class<? extends DirectionBase> direction,
+                               @Nonnull NetworkPolicyPeer peer) {
+        AceIpBuilder aceIpBuilder = new AceIpBuilder();
+        String ruleName = buildName3(aclName, DIRECTION_MAP.get(direction), "peer");
+
+        if (peer.getIpBlock() != null) {
+            // TODO handle except
+            String  cidr = peer.getIpBlock().getCidr();
+            ruleName = buildName3(ruleName, "cidr", cidr);
+            AceIpv4Builder aceIpv4Builder = new AceIpv4Builder();
+            if (direction == DirectionIngress.class) {
+                aceIpv4Builder.setSourceIpv4Network(new Ipv4Prefix(cidr));
+            } else {
+                aceIpv4Builder.setDestinationIpv4Network(new Ipv4Prefix(cidr));
+            }
+            aceIpBuilder.setAceIpVersion(aceIpv4Builder.build());
+        }
+        // TODO handle pod-selector and namespace-selector
+
+        AceBuilder aceBuilder = getAceBuilder(isDeleted, ruleName, direction, aceIpBuilder);
+
+        return aceBuilder.build();
+    }
+}
---
+++ coe/impl/src/main/java/org/opendaylight/netvirt/coe/utils/AclUtils.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.coe.utils;
+
+import com.google.common.collect.ImmutableBiMap;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.AccessLists;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.Ipv4Acl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.Acl;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.AclKey;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.AccessListEntries;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.Ace;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev160218.access.lists.acl.access.list.entries.AceKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.DirectionBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.DirectionEgress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.aclservice.rev160608.DirectionIngress;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public final class AclUtils {
+    public static final String INGRESS = "ingress";
+    public static final String EGRESS = "egress";
+    public static final ImmutableBiMap<Class<? extends DirectionBase>, String> DIRECTION_MAP = ImmutableBiMap.of(
+        DirectionIngress.class, INGRESS,
+        DirectionEgress.class, EGRESS
+    );
+
+    private AclUtils() {}
+
+    @Nonnull
+    public static InstanceIdentifier<Acl> getAclIid(@Nonnull String aclName) {
+        return InstanceIdentifier
+            .builder(AccessLists.class)
+            .child(Acl.class, new AclKey(aclName, Ipv4Acl.class))
+            .build();
+    }
+
+    @Nonnull
+    public static InstanceIdentifier<Ace> getAceIid(@Nonnull String aclName, @Nonnull String ruleName) {
+        return getAclIid(aclName).builder()
+            .child(AccessListEntries.class)
+            .child(Ace.class, new AceKey(ruleName))
+            .build();
+    }
+
+    @Nonnull
+    public static String buildName2(String string1, String string2) {
+        return String.join("_", string1, string2);
+    }
+
+    @Nonnull
+    public static String buildName3(String string1, String string2, String string3) {
+        return String.join("_", string1, string2, string3);
+    }
+}
