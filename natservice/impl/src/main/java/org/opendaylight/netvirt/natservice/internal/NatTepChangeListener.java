4b9b5cf1cc8a44c347af60fd24082b9f16d5d1d5
Chetan Arakere Gowdru <chetan.arakere@altencalsoftlabs.com>
2019-07-11 18:45:48 +0530

    DS READ Optimization in NAT
    Description:
    Following Changes done.
    1) Passing certain attributes(external-network,ext-routers, external-vpn)
    as parameter to subsequent methods to avoid DS read operation
    to retrieve the same parameters again.
    
    JIRA: NETVIRT-1612
    Change-Id: I72c60584eff89c4ac258f0d5176c20892dbbc195
    Signed-off-by: Chetan Arakere Gowdru <chetan.arakere@altencalsoftlabs.com>
    Signed-off-by: xcheara <chetan.arakere@altencalsoftlabs.com>

--- natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatTepChangeListener.java
+++ natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatTepChangeListener.java
@@ -447,9 +447,10 @@ public class NatTepChangeListener extends
                             + "associated to the router {}", routerVpnId, routerName);
                 }
                 //Re-elect the other available switch as the NAPT switch and program the NAT flows.
-                NatUtil
-                    .removeSNATFromDPN(dataBroker, mdsalManager, idManager, naptSwitchHA, dpnId,
-                        routerName, routerId, routerVpnId, networkId, extNwProvType, confTx);
+                String externalVpnName = NatUtil.getAssociatedVPN(dataBroker,
+                        routerData.get().getNetworkId());
+                NatUtil.removeSNATFromDPN(dataBroker, mdsalManager, idManager, naptSwitchHA, dpnId,
+                        routerData.get(), routerId, routerVpnId, externalVpnName, extNwProvType, confTx);
             } else {
                 LOG.info(
                     "hndlTepDelForSnatInEachRtr : SNAT is not enabled for router {} to handle addDPN event {}",
