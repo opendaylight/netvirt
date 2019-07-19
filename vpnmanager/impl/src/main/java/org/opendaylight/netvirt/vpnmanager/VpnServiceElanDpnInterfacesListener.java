10d9a6a2ab19149ab55f9eeb4dbf86ca5fc61c8f
Surendar Raju <surendar.raju@ericsson.com>
2019-07-19 15:23:40 +0530

    Normalizing VPN instances with dpnid of ipaddress and vpn interface
    Submitted changes with yang change
    
    vpninteface and ipadresses has been taken off
    from vpn-instance-op-data yang and moved to new
    vpn-instance-to-dpn-op-data yang with rd as primary
    and dpnid as secondary indetifier. This will help
    in idtenfying the vpninterface and ipadressed
    quickly instead of iterating entire vpn-instance-op-data
    
    Change-Id: Ife2d78a7cad45cb446b18301d029c6485e922118
    Signed-off-by: Surendar Raju <surendar.raju@ericsson.com>

--- vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnServiceElanDpnInterfacesListener.java
+++ vpnmanager/impl/src/main/java/org/opendaylight/netvirt/vpnmanager/VpnServiceElanDpnInterfacesListener.java
@@ -16,20 +16,18 @@ import java.util.Optional;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
 import org.opendaylight.genius.interfacemanager.interfaces.IInterfaceManager;
 import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
 import org.opendaylight.infrautils.utils.concurrent.Executors;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.netvirt.fibmanager.api.IFibManager;
-import org.opendaylight.netvirt.vpnmanager.api.VpnHelper;
 import org.opendaylight.serviceutils.tools.listener.AbstractAsyncDataTreeChangeListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.ElanDpnInterfaces;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.dpn.interfaces.ElanDpnInterfacesList;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.dpn.interfaces.elan.dpn.interfaces.list.DpnInterfaces;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.instances.ElanInstance;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.op.data.vpn.instance.op.data.entry.VpnToDpnList;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.dpn.op.elements.vpns.Dpns;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.Uint32;
 import org.opendaylight.yangtools.yang.common.Uint64;
@@ -88,18 +86,19 @@ public class VpnServiceElanDpnInterfacesListener extends AbstractAsyncDataTreeCh
         if (vpnName == null) {
             return;
         }
-        String primaryRd = vpnUtil.getPrimaryRd(vpnName);
+        final String primaryRd = vpnUtil.getPrimaryRd(vpnName);
         if (elanInstance != null && !elanInstance.isExternal() && VpnUtil.isVlan(elanInstance)) {
             jobCoordinator.enqueueJob(elanInstance.getElanInstanceName(), () -> {
                 List<String> addedInterfaces = getUpdatedInterfaceList(update.getInterfaces(),
                     original.getInterfaces());
                 for (String addedInterface : addedInterfaces) {
                     if (interfaceManager.isExternalInterface(addedInterface)) {
-                        InstanceIdentifier<VpnToDpnList> id = VpnHelper.getVpnToDpnListIdentifier(primaryRd, dpnId);
-                        Optional<VpnToDpnList> dpnInVpn = SingleTransactionDataBroker.syncReadOptional(dataBroker,
-                            LogicalDatastoreType.OPERATIONAL, id);
-                        if (!dpnInVpn.isPresent() || (dpnInVpn.get().getVpnInterfaces() != null
-                            && dpnInVpn.get().getVpnInterfaces().size() != 1)) {
+                        InstanceIdentifier<Dpns> dpnElementId = VpnUtil
+                                .getDpnListFromDpnOpElementsIdentifier(primaryRd, dpnId);
+                        Optional<Dpns> dpnOpElement = VpnUtil.read(dataBroker,
+                                LogicalDatastoreType.OPERATIONAL, dpnElementId);
+                        if (!dpnOpElement.isPresent() || (dpnOpElement.get().getVpnInterfaces() != null
+                                && dpnOpElement.get().getVpnInterfaces().size() != 1)) {
                             return Collections.emptyList();
                         }
                         if (!vpnUtil.shouldPopulateFibForVlan(vpnName, elanInstanceName, dpnId)) {
