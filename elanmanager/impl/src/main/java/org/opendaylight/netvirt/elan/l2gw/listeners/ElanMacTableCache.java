0549a40771c84cf4a24ce2de8ad057b7ccc1368c
manojna v <manojna.vijayakrishna@ericsson.com>
2019-07-31 15:56:00 +0530

    Adding l2gw related cache changes
    Performance improvement to reduce DS reads by making
    use of caches efficiently.
    
    Change-Id: I4cdcfad6019c078fc406e41d94010e299dd234d9
    Signed-off-by: manojna v <manojna.vijayakrishna@ericsson.com>

---
+++ elanmanager/impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/listeners/ElanMacTableCache.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2019 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+
+package org.opendaylight.netvirt.elan.l2gw.listeners;
+
+import java.util.concurrent.ConcurrentHashMap;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.ElanForwardingTables;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.forwarding.tables.MacTable;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class ElanMacTableCache extends AsyncClusteredDataTreeChangeListenerBase<MacTable, ElanMacTableCache> {
+    private static final Logger LOG = LoggerFactory.getLogger(ElanMacTableCache.class);
+    private final DataBroker dataBroker;
+    private final ConcurrentHashMap<String, MacTable> macsByElan = new ConcurrentHashMap<>();
+
+    @Inject
+    public ElanMacTableCache(final DataBroker dataBroker) {
+        super(MacTable.class, ElanMacTableCache.class);
+        this.dataBroker = dataBroker;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.registerListener(LogicalDatastoreType.OPERATIONAL, dataBroker);
+    }
+
+    @Override
+    protected InstanceIdentifier<MacTable> getWildCardPath() {
+        return InstanceIdentifier.builder(ElanForwardingTables.class).child(MacTable.class).build();
+    }
+
+    @Override
+    protected ElanMacTableCache getDataTreeChangeListener() {
+        return ElanMacTableCache.this;
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<MacTable> key, MacTable mac) {
+        macsByElan.remove(mac.getElanInstanceName());
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<MacTable> key, MacTable old, MacTable mac) {
+        macsByElan.put(mac.getElanInstanceName(), mac);
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<MacTable> key, MacTable mac) {
+        macsByElan.put(mac.getElanInstanceName(), mac);
+    }
+
+    public MacTable getByElanName(String name) {
+        return macsByElan.get(name);
+    }
+}
---
+++ elanmanager/impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/listeners/HwvtepConfigNodeCache.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2019 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.netvirt.elan.l2gw.listeners;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.datastoreutils.AsyncClusteredDataTreeChangeListenerBase;
+import org.opendaylight.genius.utils.hwvtep.HwvtepSouthboundConstants;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+@Singleton
+public class HwvtepConfigNodeCache extends AsyncClusteredDataTreeChangeListenerBase<Node, HwvtepConfigNodeCache> {
+    private final DataBroker dataBroker;
+    private final Map<InstanceIdentifier<Node>, Node> cache = new ConcurrentHashMap<>();
+    private final Map<InstanceIdentifier<Node>, List<Runnable>> waitList = new ConcurrentHashMap<>();
+
+    @Inject
+    public HwvtepConfigNodeCache(final DataBroker dataBroker) {
+        super(Node.class, HwvtepConfigNodeCache.class);
+        this.dataBroker = dataBroker;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.registerListener(LogicalDatastoreType.CONFIGURATION, dataBroker);
+    }
+
+    @Override
+    protected InstanceIdentifier<Node> getWildCardPath() {
+        return InstanceIdentifier.create(NetworkTopology.class)
+                .child(Topology.class, new TopologyKey(HwvtepSouthboundConstants.HWVTEP_TOPOLOGY_ID))
+                .child(Node.class);
+    }
+
+    @Override
+    protected HwvtepConfigNodeCache getDataTreeChangeListener() {
+        return HwvtepConfigNodeCache.this;
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<Node> key, Node deleted) {
+        cache.remove(key);
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<Node> key, Node old, Node added) {
+        cache.put(key, added);
+    }
+
+    @Override
+    protected synchronized void add(InstanceIdentifier<Node> key, Node added) {
+        cache.put(key, added);
+        if (waitList.containsKey(key)) {
+            waitList.remove(key).stream().forEach(runnable -> runnable.run());
+        }
+    }
+
+    public Node getConfigNode(InstanceIdentifier<Node> key) {
+        return cache.get(key);
+    }
+
+    public synchronized void runAfterNodeAvailable(InstanceIdentifier<Node> key, Runnable runnable) {
+        if (cache.containsKey(key)) {
+            runnable.run();
+        } else {
+            waitList.putIfAbsent(key, new ArrayList<>());
+            waitList.get(key).add(runnable);
+        }
+    }
+}
