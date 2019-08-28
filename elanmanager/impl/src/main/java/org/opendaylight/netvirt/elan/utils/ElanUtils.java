3a27c0df0bbdf80cc093416f5b738e35bcb03078
manojna v <manojna.vijayakrishna@ericsson.com>
2019-08-28 16:37:59 +0530

    Adding BC group update job. Instead of all cics writing bc group information into ds, only Entity Owner cic will write this into the datastore.
    Change-Id: Idb0b81446b2f9d545098a8034cf80b14f1a967fe
    Signed-off-by: manojna v <manojna.vijayakrishna@ericsson.com>

--- elanmanager/impl/src/main/java/org/opendaylight/netvirt/elan/utils/ElanUtils.java
+++ elanmanager/impl/src/main/java/org/opendaylight/netvirt/elan/utils/ElanUtils.java
@@ -1646,4 +1646,8 @@ public class ElanUtils {
         return InstanceIdentifier.builder(Nodes.class).child(Node.class, new NodeKey(new NodeId("openflow:" + dpnId)))
                 .augmentation(FlowCapableNode.class).child(Group.class, new GroupKey(new GroupId(groupId))).build();
     }
+
+    public static String getBcGroupUpdateKey(String elanName) {
+        return "bc.group.update." + elanName;
+    }
 }
