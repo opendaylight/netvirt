ba57627307ac36ea90e9e26e39a8976ab283b95c
Chetan Arakere Gowdru <chetan.arakere@altencalsoftlabs.com>
2019-09-06 18:10:47 +0530

    Scale-in/Scale-out for NAT.
    Description
    -----------
    
    1) NatScaleIn added to handle remove event which is
    registered as a callback on if the node not unmarked as tomstone.
    Whenever Scale-in of an particular DPN fails and TOMSTONE=true entry been
    removed or marked as false, if that DPN have some
    Router Presence(with SNAT enabled) and if the corresponding NAPT switch is
    missing, this DPN will be re-elected as NAPT switch.
    
    2) New Listener - NatTepChangeLister is added to handle remove event. The
    Existing logic of NatTunnelInterfaceStateListener.remove() been moved into
    this Listener. Whenever the TEP on a given DPNID is removed, this API take
    care of withdrawing the FIB entries for those Floating-IP existing on this
    DPN and perform re-election of NAPT Switch for a VRF to which the current
    DPN is elected as NAPT Switch.
    
    3) Changes done to validate the candidate list of DPNs set with
    TOMSTONE=true and exclude them during NAPT Switch election.
    
    Change-Id: I70704c985c3d92078fee80a19b4311de8258f834
    Signed-off-by: Chetan Arakere Gowdru <chetan.arakere@altencalsoftlabs.com>

---
+++ natservice/impl/src/main/java/org/opendaylight/netvirt/natservice/internal/NatScalein.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2019 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.natservice.internal;
+
+import static org.opendaylight.genius.infra.Datastore.CONFIGURATION;
+
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.cloudscaler.api.TombstonedNodeManager;
+import org.opendaylight.genius.datastoreutils.SingleTransactionDataBroker;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
+import org.opendaylight.genius.infra.ManagedNewTransactionRunnerImpl;
+import org.opendaylight.genius.mdsalutil.interfaces.IMdsalApiManager;
+import org.opendaylight.infrautils.jobcoordinator.JobCoordinator;
+import org.opendaylight.netvirt.elanmanager.api.IElanService;
+import org.opendaylight.netvirt.neutronvpn.interfaces.INeutronVpnManager;
+import org.opendaylight.serviceutils.upgrade.UpgradeState;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.genius.idmanager.rev160406.IdManagerService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.dpn.routers.DpnRoutersList;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.dpn.routers.dpn.routers.list.RoutersList;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ProviderTypes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.natservice.rev160111.ext.routers.Routers;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class NatScalein {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NatScalein.class);
+    private final DataBroker dataBroker;
+    private final IMdsalApiManager mdsalManager;
+    private final IdManagerService idManager;
+    private final JobCoordinator jobCoordinator;
+    private NaptSwitchHA naptSwitchHA;
+    private final IElanService elanManager;
+    private final INeutronVpnManager nvpnManager;
+    private final TombstonedNodeManager tombstonedNodeManager;
+    private final ManagedNewTransactionRunner txRunner;
+    private final UpgradeState upgradeState;
+
+    @Inject
+    public NatScalein(final DataBroker dataBroker, final IMdsalApiManager mdsalManager,
+        final IdManagerService idManager, final JobCoordinator jobCoordinator,
+        final NaptSwitchHA naptSwitchHA,
+        final IElanService elanManager,
+        final INeutronVpnManager nvpnManager,
+        final TombstonedNodeManager tombstonedNodeManager,
+        final UpgradeState upgradeState) {
+        this.dataBroker = dataBroker;
+        this.txRunner = new ManagedNewTransactionRunnerImpl(dataBroker);
+        this.mdsalManager = mdsalManager;
+        this.idManager = idManager;
+        this.naptSwitchHA = naptSwitchHA;
+        this.jobCoordinator = jobCoordinator;
+        this.tombstonedNodeManager = tombstonedNodeManager;
+        this.elanManager = elanManager;
+        this.nvpnManager = nvpnManager;
+        this.upgradeState = upgradeState;
+        tombstonedNodeManager.addOnRecoveryCallback((dpnId) -> {
+            jobCoordinator.enqueueJob(dpnId.toString(), () -> {
+                remove(dpnId);
+                return null;
+            });
+            return null;
+        });
+    }
+
+    protected void remove(BigInteger srcDpnId) {
+        LOG.debug("Called Remove on addOnRecoveryCallback");
+        InstanceIdentifier<DpnRoutersList> dpnRoutersListIdentifier = NatUtil
+            .getDpnRoutersId(srcDpnId);
+        Optional<DpnRoutersList> optionalDpnRoutersList =
+            SingleTransactionDataBroker
+                .syncReadOptionalAndTreatReadFailedExceptionAsAbsentOptional(dataBroker,
+                    LogicalDatastoreType.OPERATIONAL, dpnRoutersListIdentifier);
+        if (optionalDpnRoutersList.isPresent()) {
+            List<RoutersList> routersListFromDs = optionalDpnRoutersList.get()
+                .getRoutersList();
+            routersListFromDs.forEach(router -> {
+                String routerName = router.getRouter();
+                Routers extRouters = NatUtil.getRoutersFromConfigDS(dataBroker, routerName);
+                if (extRouters != null && extRouters.isEnableSnat()) {
+                    jobCoordinator.enqueueJob((NatConstants.NAT_DJC_PREFIX + routerName), () -> {
+                        List<ListenableFuture<Void>> futures = new ArrayList<>();
+                        BigInteger naptId = NatUtil
+                            .getPrimaryNaptfromRouterName(dataBroker, routerName);
+                        if (naptId == null || naptId.equals(BigInteger.ZERO)) {
+                            long routerId = NatUtil.getVpnId(dataBroker, routerName);
+                            long routerVpnId = getBgpVpnIdForRouter(routerId, routerName);
+
+                            if (routerVpnId != -1) {
+                                ProviderTypes extNwProvType = NatEvpnUtil
+                                    .getExtNwProvTypeFromRouterName(dataBroker,
+                                        routerName, extRouters.getNetworkId());
+                                txRunner.callWithNewReadWriteTransactionAndSubmit(CONFIGURATION,
+                                    confTx -> {
+                                        NatUtil
+                                            .handleSNATForDPN(dataBroker, mdsalManager, idManager,
+                                                naptSwitchHA,
+                                                srcDpnId, extRouters, routerId, routerVpnId, confTx,
+                                                extNwProvType, upgradeState);
+                                    });
+                            }
+                        }
+                        return futures;
+                    }, NatConstants.NAT_DJC_MAX_RETRIES);
+                }
+            });
+        }
+    }
+
+    private long getBgpVpnIdForRouter(long routerId, String routerName) {
+
+        if (routerId == NatConstants.INVALID_ID) {
+            LOG.error(
+                "NAT Service : SNAT -> Invalid ROUTER-ID {} returned for routerName {}",
+                routerId, routerName);
+            return -1;
+        }
+        Uuid bgpVpnUuid = NatUtil.getVpnForRouter(dataBroker, routerName);
+        long bgpVpnId;
+        if (bgpVpnUuid == null) {
+            LOG.debug(
+                "NAT Service : SNAT -> Internal VPN-ID {} associated to router {}",
+                routerId, routerName);
+            bgpVpnId = routerId;
+        } else {
+            bgpVpnId = NatUtil.getVpnId(dataBroker, bgpVpnUuid.getValue());
+            if (bgpVpnId == NatConstants.INVALID_ID) {
+                LOG.error(
+                    "NAT Service : SNAT -> Invalid Private BGP VPN ID returned for routerName {}",
+                    routerName);
+                return -1;
+            }
+        }
+        return bgpVpnId;
+    }
+}
