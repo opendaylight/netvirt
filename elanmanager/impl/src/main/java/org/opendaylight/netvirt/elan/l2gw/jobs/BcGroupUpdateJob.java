9739679e6facd0b312cc868205843ddac155e533
manojna v <manojna.vijayakrishna@ericsson.com>
2019-09-17 14:49:45 +0530

    Bc Group population during cluster reboot
    these changes make sure that during cluster reboot scenario,
    bc groups remain intact as they were before cluster reboot.
    
    as the elan interfaces gets replayed elan broadcast group
    gets reconstructed from scratch and as the final elan interface is added
    elan broadcast group will become identical to what it was before cluster
    reboot.
    
    During this period where bc group starts with 1 bucket and reaches to
    the correct no of buckets the broadcast traffic will be affected.
    
    while doing bc group update, instead of replacing the group with new set
    of buckets , first read existing buckets and add the new buckets to
    existing buckets.
    
    Change-Id: I111a9efa6c354724681ea4e7fc76cb6f37ba7f1e
    Signed-off-by: manojna v <manojna.vijayakrishna@ericsson.com>

--- elanmanager/impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/jobs/BcGroupUpdateJob.java
+++ elanmanager/impl/src/main/java/org/opendaylight/netvirt/elan/l2gw/jobs/BcGroupUpdateJob.java
@@ -35,16 +35,18 @@ public class BcGroupUpdateJob implements Callable<List<ListenableFuture<Void>>> 
     private final ElanRefUtil elanRefUtil;
     private final ManagedNewTransactionRunner txRunner;
     protected String jobKey;
+    private final boolean createCase;
 
     public BcGroupUpdateJob(String elanName,
                             ElanRefUtil elanRefUtil,
                             ElanL2GatewayMulticastUtils elanL2GatewayMulticastUtils,
-                            DataBroker dataBroker) {
+                            DataBroker dataBroker, boolean createCase) {
         this.jobKey = ElanUtils.getBcGroupUpdateKey(elanName);
         this.txRunner = new ManagedNewTransactionRunnerImpl(dataBroker);
         this.elanName = elanName;
         this.elanRefUtil = elanRefUtil;
         this.elanL2GatewayMulticastUtils = elanL2GatewayMulticastUtils;
+        this.createCase = createCase;
     }
 
     public void submit() {
@@ -57,7 +59,7 @@ public class BcGroupUpdateJob implements Callable<List<ListenableFuture<Void>>> 
         if (elanInstanceOptional.isPresent()) {
             return Lists.newArrayList(txRunner.callWithNewWriteOnlyTransactionAndSubmit(CONFIGURATION,
                 confTx -> elanL2GatewayMulticastUtils.updateRemoteBroadcastGroupForAllElanDpns(
-                        elanInstanceOptional.get(), confTx)));
+                        elanInstanceOptional.get(), createCase, confTx)));
         }
         return null;
     }
@@ -65,7 +67,7 @@ public class BcGroupUpdateJob implements Callable<List<ListenableFuture<Void>>> 
     public static void updateAllBcGroups(String elanName,
                                          ElanRefUtil elanRefUtil,
                                          ElanL2GatewayMulticastUtils elanL2GatewayMulticastUtils,
-                                         DataBroker dataBroker) {
-        new BcGroupUpdateJob(elanName, elanRefUtil, elanL2GatewayMulticastUtils, dataBroker).submit();
+                                         DataBroker dataBroker, boolean createCase) {
+        new BcGroupUpdateJob(elanName, elanRefUtil, elanL2GatewayMulticastUtils, dataBroker, createCase).submit();
     }
 }
