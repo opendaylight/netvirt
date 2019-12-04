3fe50e561dff139f914927eb257feb19a96f66b0
Amitesh Soni <amitesh.soni@ericsson.com>
2019-12-04 15:47:42 +0530

    ELAN Reboot fixes and Flow ID change
    1) Table 51 MAC issue after CSC reboot
    Description: This is to avoid stale entries being created.
    The solution is to construct a flow key with ELAN tag and
    MAC address, which would update the same flow after MAC movement.
    
    2) VM Migration Table 51 stale entry
    Description: Issue is observed when ElanLearnVpnVip MIP MAC
    and Interface MAC are same. During migration, ElanLearntVpnVipToPortListener
    remove deletes MAC entry from ElanInterfaceForwardingEntries,
    which is referred by ElanInterfaceStateChangeListener remove workflow,
    which is started after the ElanInterfaceForwardingEntries thread completes
    its course of action. Both threads have an action to delete DMAC flow entries
    but the first thread refers InterfaceManager for InterfaceInfo
    which returns null, hence skips the delete DMAC workflow, but deletes the
    MAC entry. Other thread refers MAC entry, which has been deleted
    by first thread, hence this too skips delete workflow.
    The solution is to delete MAC entry in first thread only if InterfaceInfo
    is not null so that second thread can delete DMAC entry as well as delete
    MAC entry during its course of action.
    
    Signed-off-by: Amitesh Soni <amitesh.soni@ericsson.com>
    Change-Id: I9a2b72e0d70a2edf9072c77fc35314e1b7a2c46d

--- elanmanager/impl/src/test/java/org/opendaylight/netvirt/elanmanager/tests/ElanServiceTest.java
+++ elanmanager/impl/src/test/java/org/opendaylight/netvirt/elanmanager/tests/ElanServiceTest.java
@@ -233,10 +233,7 @@ public class ElanServiceTest extends  ElanServiceTestBase {
 
         // Read and Compare SMAC flow
         String flowId = new StringBuilder()
-                .append(NwConstants.ELAN_SMAC_TABLE)
                 .append(actualElanInstances.getElanTag())
-                .append(DPN1_ID)
-                .append(interfaceInfo.getInterfaceTag())
                 .append(interfaceInfo.getMacAddress())
                 .toString();
         InstanceIdentifier<Flow> flowInstanceIidSrc = getFlowIid(NwConstants.ELAN_SMAC_TABLE,
@@ -266,10 +263,7 @@ public class ElanServiceTest extends  ElanServiceTestBase {
 
         // Read DMAC Flow in DPN1
         String flowId = new StringBuilder()
-                .append(NwConstants.ELAN_DMAC_TABLE)
                 .append(actualElanInstances.getElanTag())
-                .append(DPN1_ID)
-                .append(interfaceInfo.getInterfaceTag())
                 .append(interfaceInfo.getMacAddress())
                 .toString();
         InstanceIdentifier<Flow> flowInstanceIidDst = getFlowIid(NwConstants.ELAN_DMAC_TABLE,
@@ -300,11 +294,8 @@ public class ElanServiceTest extends  ElanServiceTestBase {
         addElanInterface(ExpectedObjects.ELAN1, interfaceInfo, DPN2IP1);
 
         // Read and Compare DMAC flow in DPN1 for MAC1 of DPN2
-        String flowId = ElanUtils.getKnownDynamicmacFlowRef((short)51,
-                DPN1_ID,
-                DPN2_ID,
-                interfaceInfo.getMacAddress().toString(),
-                actualElanInstances.getElanTag());
+        String flowId = ElanUtils.getKnownDynamicmacFlowRef(actualElanInstances.getElanTag(),
+                interfaceInfo.getMacAddress());
 
         InstanceIdentifier<Flow> flowInstanceIidDst = getFlowIid(NwConstants.ELAN_DMAC_TABLE,
                 new FlowId(flowId), DPN1_ID);
