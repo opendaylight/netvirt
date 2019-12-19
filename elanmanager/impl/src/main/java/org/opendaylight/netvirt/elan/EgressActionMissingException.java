7cb31d57cce4440059cb24a30a5d4768bd36d6ed
Amitesh Soni <amitesh.soni@ericsson.com>
2019-12-19 12:52:37 +0530

    NETVIRT-1631 Refrain programming Table 51 flow if egress action missing
    Issue:
    ======
    Earlier table 51 was programmed even if tunnel egress actions
    were not present, which was affecting dataplane traffic.
    
    Fix:
    ====
    Here fix is, not to program table 51 flow when tunnel egrees actions
    are either null or empty.
    
    Signed-off-by: Amitesh Soni <amitesh.soni@ericsson.com>
    Change-Id: Ibe1d03a598912421ff42c483dcd4ce90b6c5eb8d

---
+++ elanmanager/impl/src/main/java/org/opendaylight/netvirt/elan/EgressActionMissingException.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.elan;
+
+public class EgressActionMissingException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public EgressActionMissingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EgressActionMissingException(String message) {
+        super(message);
+    }
+
+}
