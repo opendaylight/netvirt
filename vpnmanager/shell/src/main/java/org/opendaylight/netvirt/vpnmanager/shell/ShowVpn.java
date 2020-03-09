fe2b12078536337e73c8a1895c0960900ce503dd
Nishchya Gupta <nishchyag@altencalsoftlabs.com>
2020-03-09 17:13:51 +0530

    Modify vpn-show command
    This adds operational subnet route count and operational nexthop
    count to the output of vpn-show command.
    
    Signed-off-by: Nishchya Gupta <nishchyag@altencalsoftlabs.com>
    Change-Id: If0b1b3a2cc0d3fca28660814c78ecc003bb2db88

--- vpnmanager/shell/src/main/java/org/opendaylight/netvirt/vpnmanager/shell/ShowVpn.java
+++ vpnmanager/shell/src/main/java/org/opendaylight/netvirt/vpnmanager/shell/ShowVpn.java
@@ -24,8 +24,16 @@ import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.netvirt.vpnmanager.api.VpnHelper;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3nexthop.rev150409.L3nexthop;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3nexthop.rev150409.l3nexthop.VpnNexthops;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3nexthop.rev150409.l3nexthop.VpnNexthopsKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3nexthop.rev150409.l3nexthop.vpnnexthops.VpnNexthop;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.SubnetOpData;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.VpnInstanceToVpnId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.VpnInterfaceOpData;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.subnet.op.data.SubnetOpDataEntry;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn._interface.op.data.VpnInterfaceOpDataEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn.instance.to.vpn.id.VpnInstanceKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.l3vpn.rev200204.VpnInstances;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.l3vpn.rev200204.VpnInterfaces;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.l3vpn.rev200204.vpn.instances.VpnInstance;
@@ -50,11 +58,16 @@ public class ShowVpn extends OsgiCommandSupport {
     private int operCount = 0;
     private int totalOperCount = 0;
     private int totalConfigCount = 0;
+    int nexthopCount = 0;
+    int totalNexthopCount = 0;
+    int subnetRouteCount = 0;
+    int totalSubnetRouteCount = 0;
     private Integer ifPresent;
     private List<org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.l3vpn.rev200204.vpn.instances
             .VpnInstance> vpnInstanceList = new ArrayList<>();
     private List<VpnInterface> vpnInterfaceConfigList = new ArrayList<>();
     private List<VpnInterfaceOpDataEntry> vpnInterfaceOpList = new ArrayList<>();
+    List<SubnetOpDataEntry> subnetOpDataEntryList = new ArrayList<>();
 
     public void setDataBroker(DataBroker broker) {
         this.dataBroker = broker;
@@ -65,6 +78,7 @@ public class ShowVpn extends OsgiCommandSupport {
     protected Object doExecute() {
         Map<String, Integer> vpnNameToConfigInterfaceMap = new HashMap<>();
         Map<String, Integer> vpnNameToOperInterfaceMap = new HashMap<>();
+        Map<String, Integer> instanceNameToSubnetRouteCountMap = new HashMap<>();
         if (detail == null) {
             showVpn();
             Set<String> vpnInstances = new HashSet<>();
@@ -96,15 +110,31 @@ public class ShowVpn extends OsgiCommandSupport {
                         vpnNameToOperInterfaceMap.get(vpnInterfaceOp.getVpnInstanceName()) + 1);
                 }
             }
-            session.getConsole().println("-----------------------------------------------------------------------");
-            session.getConsole().println(
-                    String.format("         %s   %14s  %5s  %5s", "VpnInstanceName", "RD", "Config Count",
-                            "Oper Count"));
-            session.getConsole().println(
-                    "\n-----------------------------------------------------------------------");
+            for (SubnetOpDataEntry subnetOpDataEntry : subnetOpDataEntryList) {
+                ifPresent = instanceNameToSubnetRouteCountMap.get(subnetOpDataEntry.getVpnName());
+                if (ifPresent == null) {
+                    instanceNameToSubnetRouteCountMap.put(subnetOpDataEntry.getVpnName(), 1);
+                } else {
+                    instanceNameToSubnetRouteCountMap.put(subnetOpDataEntry.getVpnName(),
+                        instanceNameToSubnetRouteCountMap.get(subnetOpDataEntry.getVpnName()) + 1);
+                }
+            }
+            session.getConsole().println("+--------------------------------------+------------+------------------"
+                    + "--------+-------------------------------+---------------------------+");
+            session.getConsole().println(String.format("|                                      |            |%23s   "
+                    + "| %29s | %25s |", "Vpn-Interfaces-Count", "",""));
+            session.getConsole().println("|                                      |            +------------+-------"
+                    + "------+                               |                           |");
+            session.getConsole().println(String.format("|             %s          |%7s     | %10s | %10s | %18s "
+                            + "| %5s |", "VpnInstanceName", "RD","Configured", "Operational",
+                    "Operational subnetRoute Count","Operational NextHop Count"));
+            session.getConsole().println("+--------------------------------------+------------+------------+---------"
+                    + "----+-------------------------------+---------------------------+");
             for (VpnInstance vpnInstance : vpnInstanceList) {
                 configCount = 0;
                 operCount = 0;
+                subnetRouteCount = 0;
+                nexthopCount = 0;
                 Integer count = vpnNameToConfigInterfaceMap.get(vpnInstance.getVpnInstanceName());
                 if (count != null) {
                     configCount = vpnNameToConfigInterfaceMap.get(vpnInstance.getVpnInstanceName());
@@ -115,13 +145,40 @@ public class ShowVpn extends OsgiCommandSupport {
                     operCount = vpnNameToOperInterfaceMap.get(vpnInstance.getVpnInstanceName());
                     totalOperCount = totalOperCount + operCount;
                 }
-                session.getConsole().println(
-                        String.format("%-32s  %-10s  %-10s  %-10s", vpnInstance.getVpnInstanceName(),
-                                vpnInstance.getRouteDistinguisher(), configCount, operCount));
+                count = instanceNameToSubnetRouteCountMap.get(vpnInstance.getVpnInstanceName());
+                if (count != null) {
+                    subnetRouteCount = instanceNameToSubnetRouteCountMap.get(vpnInstance.getVpnInstanceName());
+                    totalSubnetRouteCount = totalSubnetRouteCount + subnetRouteCount;
+                }
+                InstanceIdentifier<org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.l3vpn.rev130911.vpn
+                        .instance.to.vpn.id.VpnInstance> vpnInstanceIdIdentifier = InstanceIdentifier
+                        .builder(VpnInstanceToVpnId.class).child(org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt
+                                .l3vpn.rev130911.vpn.instance.to.vpn.id.VpnInstance.class,
+                                new VpnInstanceKey(vpnInstance.getVpnInstanceName())).build();
+                Optional<org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt
+                        .l3vpn.rev130911.vpn.instance.to.vpn.id.VpnInstance> vpnIdInstance =
+                        read(LogicalDatastoreType.CONFIGURATION, vpnInstanceIdIdentifier);
+                if (vpnIdInstance.isPresent()) {
+                    InstanceIdentifier<VpnNexthops> vpnNexthopidentifier = InstanceIdentifier.builder(L3nexthop.class)
+                            .child(VpnNexthops.class, new VpnNexthopsKey(vpnIdInstance.get().getVpnId())).build();
+                    Optional<VpnNexthops> vpnNexthops = read(LogicalDatastoreType.OPERATIONAL, vpnNexthopidentifier);
+                    if (vpnNexthops.isPresent()) {
+                        List<VpnNexthop> nexthops = vpnNexthops.get().getVpnNexthop();
+                        nexthopCount = nexthops.size();
+                        totalNexthopCount = totalNexthopCount + nexthopCount;
+                    }
+                }
+                session.getConsole().println(String.format("|%-37s | %-10s |     %-6s |     %-7s |               "
+                                + "%-15s |           %-16s|", vpnInstance.getVpnInstanceName(),
+                        vpnInstance.getRouteDistinguisher(), configCount, operCount, subnetRouteCount, nexthopCount));
             }
-            session.getConsole().println("-----------------------------------------------------------------------");
-            session.getConsole().println(
-                    String.format("Total Count:                    %19s    %8s", totalConfigCount, totalOperCount));
+            session.getConsole().println("+--------------------------------------+------------+------------+---------"
+                    + "----+-------------------------------+---------------------------+");
+            session.getConsole().println(String.format("|             %-24s | %-10s |     %-6s |     %-7s |           "
+                            + "    %-15s |           %-16s|", "Total Count", "", totalConfigCount,
+                    totalOperCount, totalSubnetRouteCount, totalNexthopCount));
+            session.getConsole().println("+--------------------------------------+------------+------------+----------"
+                    + "---+-------------------------------+---------------------------+");
             session.getConsole().println(getshowVpnCLIHelp());
         } else {
             showVpn();
@@ -152,10 +209,11 @@ public class ShowVpn extends OsgiCommandSupport {
 
     private void showVpn() {
         InstanceIdentifier<VpnInstances> vpnsIdentifier = InstanceIdentifier.builder(VpnInstances.class).build();
-        InstanceIdentifier<VpnInterfaces> vpnInterfacesIdentifier = InstanceIdentifier.builder(VpnInterfaces
-                .class).build();
+        InstanceIdentifier<VpnInterfaces> vpnInterfacesIdentifier =
+                InstanceIdentifier.builder(VpnInterfaces.class).build();
+        final InstanceIdentifier<SubnetOpData> subOpIdentifier
+                = InstanceIdentifier.builder(SubnetOpData.class).build();
         Optional<VpnInstances> optionalVpnInstances = read(LogicalDatastoreType.CONFIGURATION, vpnsIdentifier);
-
         if (!optionalVpnInstances.isPresent()) {
             LOG.trace("No VPNInstances configured.");
             session.getConsole().println("No VPNInstances configured.");
@@ -182,6 +240,13 @@ public class ShowVpn extends OsgiCommandSupport {
             session.getConsole().println("No Oper VpnInterface is present");
         } else {
             vpnInterfaceOpList = optionalVpnInterfacesOper.get().getVpnInterfaceOpDataEntry();
+        }
+        Optional<SubnetOpData> optionalSubnetRouteOper = read(LogicalDatastoreType.OPERATIONAL, subOpIdentifier);
+        if (!optionalSubnetRouteOper.isPresent()) {
+            LOG.info("showVpn: No Oper Subnet Route  is present");
+            session.getConsole().println("No Oper Subnet Route is present");
+        } else {
+            subnetOpDataEntryList = optionalSubnetRouteOper.get().getSubnetOpDataEntry();
         }
     }
 
