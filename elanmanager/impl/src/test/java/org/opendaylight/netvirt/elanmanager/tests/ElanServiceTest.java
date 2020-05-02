ee97bbe2f56fa93b9058af564c85e23a2cee2997
Karthikeyan Krishnan <karthikeyangceb007@gmail.com>
2020-05-02 21:30:45 +0530

    Fix for ELAN JUNIT TCs failure
    Signed-off-by: Karthikeyan Krishnan <karthikeyangceb007@gmail.com>
    Change-Id: I37f97428b7c6e54674f901e1a4c66e16c66573be

--- elanmanager/impl/src/test/java/org/opendaylight/netvirt/elanmanager/tests/ElanServiceTest.java
+++ elanmanager/impl/src/test/java/org/opendaylight/netvirt/elanmanager/tests/ElanServiceTest.java
@@ -326,7 +326,7 @@ public class ElanServiceTest extends  ElanServiceTestBase {
                 readBgpNetworkFromDS(DPN1IP1));
     }
 
-/*    @Test public void checkEvpnAdvRT2NewInterface() throws Exception {
+    @Test public void checkEvpnAdvRT2NewInterface() throws Exception {
         createElanInstanceAndInterfaceAndAttachEvpn();
 
         // Add Elan interface
@@ -335,9 +335,9 @@ public class ElanServiceTest extends  ElanServiceTestBase {
         AssertDataObjects.assertEqualBeans(
                 ExpectedObjects.checkEvpnAdvertiseRoute(ELAN1_SEGMENT_ID, DPN1MAC2, DPN1_TEPIP, DPN1IP2, RD),
                 readBgpNetworkFromDS(DPN1IP2));
-    }*/
+    }
 
-/*    @Test public void checkEvpnWithdrawRT2DelIntf() throws Exception {
+    @Test public void checkEvpnWithdrawRT2DelIntf() throws Exception {
         createElanInstanceAndInterfaceAndAttachEvpn();
 
         InstanceIdentifier<Networks> iid = evpnTestHelper.buildBgpNetworkIid(DPN1IP1);
@@ -347,7 +347,7 @@ public class ElanServiceTest extends  ElanServiceTestBase {
 
         deleteElanInterface(ELAN_INTERFACES.get(ELAN1 + ":" + DPN1MAC1).getLeft());
         awaitForDataDelete(LogicalDatastoreType.CONFIGURATION, iid);
-    }*/
+    }
 
 /*    @Test public void checkEvpnWithdrawRouteDetachEvpn() throws Exception {
         createElanInstanceAndInterfaceAndAttachEvpn();
