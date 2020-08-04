096da84cc1c2c11dd672400a9a2917d3e73fac98
Robert Varga <robert.varga@pantheon.tech>
2020-08-04 17:27:06 +0200

    Convert statemanager to use mdsal-binding-util
    This is a trivial conversion, as statemanager does not have a related
    API dependency. Perform a straight migration away from Genius.
    
    JIRA: NETVIRT-1696
    Change-Id: I44814f14c1969b04251b649f5a5c1146c00ebfa3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- statemanager/impl/src/main/java/org/opendaylight/netvirt/statemanager/StateManager.java
+++ statemanager/impl/src/main/java/org/opendaylight/netvirt/statemanager/StateManager.java
@@ -5,7 +5,6 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.netvirt.statemanager;
 
 import static org.opendaylight.mdsal.binding.util.Datastore.OPERATIONAL;
