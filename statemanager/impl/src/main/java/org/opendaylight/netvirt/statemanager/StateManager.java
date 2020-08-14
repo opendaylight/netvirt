9e21d278cf6c31ad14d2c913da709c7177af7ff2
Karthikeyan Krishnan <karthikeyangceb007@gmail.com>
2020-08-14 13:42:28 +0530

    Remove GENIUS UTIL references in Statemanager Module
    Remove references to
    org.opendaylight.genius.infra.ManagedNewTransactionRunner in
    statemanager-impl
    
    JIRA: NETVIRT-1708
    
    Signed-off-by: Karthikeyan Krishnan <karthikeyangceb007@gmail.com>
    Change-Id: Ibee64347fdd07dcd093001c9fbadf968f4c7efa7

--- statemanager/impl/src/main/java/org/opendaylight/netvirt/statemanager/StateManager.java
+++ statemanager/impl/src/main/java/org/opendaylight/netvirt/statemanager/StateManager.java
@@ -8,16 +8,16 @@
 
 package org.opendaylight.netvirt.statemanager;
 
-import static org.opendaylight.genius.infra.Datastore.OPERATIONAL;
+import static org.opendaylight.mdsal.binding.util.Datastore.OPERATIONAL;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.concurrent.ExecutionException;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import org.opendaylight.genius.infra.ManagedNewTransactionRunner;
-import org.opendaylight.genius.infra.ManagedNewTransactionRunnerImpl;
 import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.util.ManagedNewTransactionRunner;
+import org.opendaylight.mdsal.binding.util.ManagedNewTransactionRunnerImpl;
 import org.opendaylight.netvirt.bgpmanager.api.IBgpManager;
 import org.opendaylight.netvirt.elanmanager.api.IElanService;
 import org.opendaylight.netvirt.fibmanager.api.IFibManager;
